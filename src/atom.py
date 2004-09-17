3612ae52b8b3185b44c490dae72fb3211bb216b8
Bruce Smith <bruce@nanorex.com>
2004-09-17 00:06:32 +0000

    Execute ~/.atom-debug-rc on startup, if it exists; other debug code. -- bruce

--- src/atom.py
+++ src/atom.py
@@ -10,6 +10,18 @@
 __author__ = "Josh"
 
 import sys, os
+
+# user-specific debug code to be run before any other imports [bruce 040903]
+if __name__=='__main__':
+    try:
+        rc = "~/.atom-debug-rc"
+        rc = os.path.expanduser(rc)
+        if os.path.exists(rc):
+            execfile(rc)
+    except:
+        print """exception in execfile(%r); traceback printed to stderr or console; exiting""" % (rc,)
+        raise
+
 from qt import QApplication, SIGNAL
 
 from constants import *
@@ -36,7 +48,21 @@ if __name__=='__main__':
 
     foo = MWsemantics()
     app.connect(foo.fileExitAction, SIGNAL("activated()"), app.quit)
+
+    try:
+        meth = atom_debug_pre_main_show # do this, if user asked us to by defining it in .atom-debug-rc
+    except:
+        pass
+    else:
+        meth()
     
     foo.show()
+
+    try:
+        meth = atom_debug_post_main_show # do this, if user asked us to by defining it in .atom-debug-rc
+    except:
+        pass
+    else:
+        meth()
 
     app.exec_loop()
