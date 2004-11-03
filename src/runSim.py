cb90657fb68aba1d0aba4ee67c937fbcedfd7070
Mark Sims <mark@nanorex.com>
2004-11-03 17:37:46 +0000

    Changed getouput() to os.popen(), and tightened up gopressed() - Mark

--- src/runSim.py
+++ src/runSim.py
@@ -3,6 +3,7 @@
 from SimSetupDialog import *
 from fileIO import writemmp
 from commands import *
+import os
 
 class runSim(SimSetupDialog):
     def __init__(self, assy):
@@ -22,6 +23,7 @@ class runSim(SimSetupDialog):
         self.filename = ''
 
     def GoPressed(self):
+        QDialog.accept(self)
         if not self.assy.filename: self.assy.filename="simulate.mmp"
         cmd = ("simulator -f" + str(self.nframes)
                + " -t" + str(self.temp)
@@ -29,13 +31,24 @@ class runSim(SimSetupDialog):
 #               + " -s" + str(self.timestep)
                + " " + self.assy.filename)
         print cmd
-        self.assy.w.msgbarLabel.setText("Calculating...")
         QApplication.setOverrideCursor( QCursor(Qt.WaitCursor) )
-        if self.assy.modified: writemmp(self.assy, self.assy.filename)
-        s = getoutput(cmd)
-        QApplication.restoreOverrideCursor()
-        self.assy.w.msgbarLabel.setText("Movie written to "+self.assy.filename[:-3]+'dpb')
-
+        try:
+            self.assy.w.msgbarLabel.setText("Calculating...")
+            if self.assy.modified: writemmp(self.assy, self.assy.filename)
+            pipe = os.popen(cmd)
+            r = pipe.close() # false (0) means success, true means failure
+        except:
+            print_compact_traceback("exception in minimize; continuing: ")
+            s = "internal error (traceback printed elsewhere)"
+            r = -1 # simulate failure
+        QApplication.restoreOverrideCursor() # Restore the cursor
+        if not r:
+            self.assy.w.msgbarLabel.setText("Movie written to "+self.assy.filename[:-3]+'dpb')
+        else:
+            if not s: s = "exit code %r" % r
+            self.assy.w.msgbarLabel.setText("Simulation Failed!") ##e include s?
+            QMessageBox.warning(self.assy.w, "Simulation Failed:", s)
+        return
 
     def StepsChanged(self,a0):
         self.stepsper = 10
