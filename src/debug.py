7b330ed4a060d47a22a67ca32578d31e4df3126f
Bruce Smith <bruce@nanorex.com>
2004-12-06 04:50:07 +0000

    rewrote some functions from scratch; now fully GPL-compatible, Nanorex copyright

--- src/debug.py
+++ src/debug.py
@@ -1,139 +1,98 @@
-"""
-debug.py
+# Copyright (c) 2004 Nanorex, Inc.  All rights reserved.
 
-Some useful debugging functions.
+'''
+debug.py -- debugging functions
 
-Copyright, License, and Warranty info:
+Names and behavior partly modelled after code by Sam Rushing in asyncore.py
+in the Python library, but this implementation is newly written from scratch;
+see PythonDocumentation/ref/types.html for traceback, frame, and code objects,
+and sys module documentation about exc_info() and _getframe().
 
-Some of these functions were originally written by me (Bruce Smith) during 2003,
-as a contractor on a project which was not "work for hire". According to the
-US Copyright Act of 1976, those functions are therefore copyrightable only by me.
-I (Bruce Smith, Sep 23 2004) hereby place those functions (listed below) into the
-public domain.
+#e Desirable new features:
 
-The above applies to the functions compact_traceback_msg and
-print_compact_traceback_and_continue, in their original form.
+- print source lines too;
 
-They have been further modified by me (Bruce Smith, 23 Sep 2004 and later)
-as an employee of Nanorex. Those modifications, and the other functions
-in this file, are, of course, Copyright 2004, Nanorex, Inc.
+- in compact_stack, include info about how many times each frame has been
+previously printed, and/or when it was first seen (by storing something in the
+frame when it's first seen, and perhaps incrementing it each time it's seen).
 
-# BRUCE SMITH DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
-# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
-# NO EVENT SHALL BRUCE SMITH BE LIABLE FOR ANY SPECIAL, INDIRECT OR
-# CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
-# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
-# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
-# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-"""
+$Id$
+'''
 
-from asyncore import compact_traceback
+import sys, os
 
-import sys
+# traceback
 
-def compact_traceback_msg( prefix = 'error: ', newline = ' '):
-    junk, ty, val, tbinfo = compact_traceback()
-    tbinfo = tbinfo.replace( '] [', ']%s[' % newline)
-    return '%s%s:%s %s%s' % ( prefix, ty, val, newline, tbinfo )
+def print_compact_traceback(msg = "exception ignored: "):
+    print >> sys.__stderr__, msg + compact_traceback()
 
-def print_compact_traceback_and_continue(prefix = 'ignoring this exception: ', newline = '\n', self_debug = 0):
-    "#doc; above all else, cause no harm (ie continue even if unable to print any error message), unless self_debug = true"
+def compact_traceback():
+    type, value, traceback = sys.exc_info()
+    if (type, value) == (None, None):
+        del traceback # even though it should be None
+        return "<incorrect call of compact_traceback(): no exception is being handled>"
     try:
-        msg = compact_traceback_msg( prefix = prefix, newline = newline )
+        printlines = []
+        while traceback:
+            # cf. PythonDocumentation/ref/types.html;
+            # starting from current stack level (of exception handler),
+            # going deeper (towards innermost frame, where exception occurred):
+            filename = traceback.tb_frame.f_code.co_filename
+            lineno = traceback.tb_lineno
+            printlines.append("[%s:%r]" % ( os.path.basename(filename), lineno ))
+            traceback = traceback.tb_next
+        del traceback
+        ctb = '\n'.join(printlines)
+        return "%s: %s\n%s" % (type, value, ctb)
     except:
-        if self_debug:
-            raise
-        msg = "... uh oh, exception in print_compact_traceback_and_continue, not printed, sorry"
-    try:
-        print >>sys.__stderr__, msg
-    except:
-        if self_debug:
-            raise
+        del traceback
+        return "<bug in compact_traceback(); exception from that not shown>"
+    pass
+
+# stack
+
+def print_compact_stack( msg = "current stack:\n", skip_innermost_n = 2 ):
+    print >> sys.__stderr__, msg + \
+          compact_stack( skip_innermost_n = skip_innermost_n )
+
+def compact_stack( skip_innermost_n = 1 ):
+    printlines = []
+    frame = sys._getframe( skip_innermost_n)
+    while frame: # innermost first
+        filename = frame.f_code.co_filename
+        lineno = frame.f_lineno
+        printlines.append("[%s:%r]" % ( os.path.basename(filename), lineno ))
+        frame = frame.f_back
+    printlines.reverse() # make it outermost first, like compact_traceback
+    return '\n'.join(printlines)
+
+# test code for those -- but more non-test code follows, below this!
+
+if __name__ == '__main__':
+    print "see sys.__stderr__ (perhaps a separate console) for test output"
+    def f0():
+        return f1()
+    def f1():
+        return f2()
+    def f2():
+        print_compact_stack("in f2(); this is the stack:\n")
         try:
-            msg = "failed to print this to sys.__stderr__:\n" + msg
-            print msg
+            f3()
         except:
-            pass
-    return
-
-print_compact_traceback = print_compact_traceback_and_continue
-
-# THE FOLLOWING MIGHT NOT EVEN BE GPL_COMPATIBLE, UNTIL REWRITTEN [040928]
-
-def compact_stack_orig( newline = '\n', outer_first = 0, inner_del = 0 ): # not used, just for comparison
-    """modified from compact_traceback( )
-    """
-    import inspect as _inspect
-    tbinfo = [] #k array of (file, function, line) tuples
-    for frame_rec in _inspect.stack(0): # arg is number of lines of context in list_of_lines
-        (frame_object, filename, lineno, funcname, list_of_lines, index_of_current_line) = frame_rec
-        del frame_object    
-        tbinfo.append ((
-            str(filename),
-            str(funcname),
-            str(lineno) # this str() is needed, the others are just for safety
-            ))
-
-    tbinfo = tbinfo[inner_del:]
-    if outer_first:
-        tbinfo.reverse()
-    # after this it's almost the same as in compact_traceback
-    info = '[' + (']' + newline + '[').join(map((lambda x: '|'.join(x)), tbinfo)) + ']'
-    return info
-del compact_stack_orig
-
-# BUT WHATEVER IS NEW IN THIS VERSION OF IT IS OK
-
-try:
-    _next_frame_id
-except:
-    _next_frame_id = 1
-
-def compact_stack( newline = '\n', outer_first = 0, inner_del = 0, add_frame_ids = 1 ): ####e add_frame_ids = 0 if turns out not safe
-    """modified from compact_traceback( )
-    add_frame_ids added 040928
-    """
-    import inspect as _inspect
-    tbinfo = [] #k array of (file, function, line) tuples
-    for frame_rec in _inspect.stack(0): # arg is number of lines of context in list_of_lines
-        (frame_object, filename, lineno, funcname, list_of_lines, index_of_current_line) = frame_rec
-        if add_frame_ids: #040928
-            # record in each frame a unique id and a count of how many times we've returned it so far, for comparing successive stacktraces
-            try:
-                fid = frame_object.f_locals['__bruce_frame_id__']
-            except KeyError:
-                global _next_frame_id
-                fid = _next_frame_id
-                _next_frame_id += 1
-                frame_object.f_locals['__bruce_frame_id__'] = fid
-                usage = 1
-                frame_object.f_locals['__bruce_frame_usage__'] = usage
-            else:
-                usage = frame_object.f_locals['__bruce_frame_usage__'] + 1
-                frame_object.f_locals['__bruce_frame_usage__'] = usage
-        else:
-            fid = usage = -1 ###fix
-        del frame_object  
-        tbinfo.append ((
-            str(filename),
-            str(funcname),
-            str(lineno), # this str() is needed, the others are just for safety
-            str(fid),
-            str(usage)
-            ))
-
-    tbinfo = tbinfo[inner_del:]
-    if outer_first:
-        tbinfo.reverse()
-    # after this it's almost the same as in compact_traceback
-    info = '[' + (']' + newline + '[').join(map((lambda x: '|'.join(x)), tbinfo)) + ']'
-    return info
-
-# end of stuff with nonstandard copyright status -- below this line it's normal
+            print_compact_traceback("exception in f3(): ")
+        print >> sys.__stderr__, "done with f2()"
+    def f3():
+        f4()
+    def f4():
+        assert 0, "assert 0"
+    f0()
+    print >> sys.__stderr__, "returned from f0()"
+    print "test done"
+    pass
 
 # ===
 
-# import some things to make debugging commands more convenient ###e also qt, etc
+# import some things to make debugging commands more convenient
 import sys, os, time
 
 # run python commands from various sorts of integrated debugging UIs (for users who are developers); used in GLPane.py.
@@ -184,22 +143,5 @@ def debug_runpycode_from_a_dialog( source = "some debug menu??"):
         print "run py code: cancelled"
     return
 
-def print_compact_stack(msg = "", **kws): # newly written, 040928
-    try:
-        info = compact_stack(**kws)
-    except:
-        raise#####
-        info = "(exception in compact_stack)"
-    print >>sys.__stderr__, "stack: %s%s" % (msg, info)
-    return
-
-if __name__ == '__main__':
-    def ff():
-        print_compact_stack("test 1 in ff:\n")
-        print_compact_stack("test 2 in ff:\n")
-    ff()
-    print_compact_stack("test 3:\n")
-    print >>sys.__stderr__, "done"
-    print "done, see __stderr__"
     
 # end
