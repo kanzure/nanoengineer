6313730a471044e30a339d724b0c474a7d655c3c
Mark Sims <mark@nanorex.com>
2005-01-05 09:48:09 +0000

    Implemented "Save File" and tweaked simulator dialog.

--- src/SimSetupDialog.py
+++ src/SimSetupDialog.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/huaicai/atom/cad/src/SimSetupDialog.ui'
+# Form implementation generated from reading ui file 'C:\atom\cad\src\SimSetupDialog.ui'
 #
-# Created: Thu Dec 9 14:44:31 2004
+# Created: Wed Jan 5 03:51:49 2005
 #      by: The PyQt User Interface Compiler (pyuic) 3.12
 #
 # WARNING! All changes made in this file will be lost!
@@ -10,178 +10,158 @@
 
 from qt import *
 
-image0_data = \
-    "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a\x00\x00\x00\x0d" \
-    "\x49\x48\x44\x52\x00\x00\x00\x16\x00\x00\x00\x16" \
-    "\x08\x06\x00\x00\x00\xc4\xb4\x6c\x3b\x00\x00\x01" \
-    "\x64\x49\x44\x41\x54\x78\x9c\xcd\x95\xb1\x4a\xc3" \
-    "\x50\x14\x86\xbf\x5b\xc4\x76\xd1\x25\xa5\xda\x2e" \
-    "\x8e\x75\xe9\xd3\x18\x30\x93\x4b\xa1\x19\xc4\xc5" \
-    "\x07\x70\xa8\xb8\x85\x6c\x15\x52\x70\xd0\x41\xa2" \
-    "\x44\xf0\x0d\xc4\xb7\xd0\xd1\xe5\x06\xc5\x16\xd4" \
-    "\xa5\x38\x78\x1c\x42\xd2\xa4\x69\x4b\x5b\x5b\xf0" \
-    "\x87\x43\xee\x3d\x37\xf9\xf8\x39\xf7\xdc\x1b\xa5" \
-    "\x43\xcd\x2a\x54\x58\x09\xf5\xdf\x82\xab\xdb\x55" \
-    "\x99\xb8\xa8\x43\x8d\x0e\x35\xae\xd3\x16\x60\xe1" \
-    "\x70\x9d\xb6\xc4\x2c\x1d\x6a\x54\xbc\x79\xb5\x6a" \
-    "\x4d\x02\xdf\x5b\xd8\xbd\x69\xd9\xe8\x50\xab\x78" \
-    "\xbe\x36\xfa\xc2\xde\x7e\x6b\x6e\xe8\xdd\x4d\x37" \
-    "\x97\x9b\x5a\xe3\x2b\x2e\x28\x53\xa2\x4c\x89\xbe" \
-    "\x34\xe8\x4b\x03\xa5\x14\x74\xbb\xf4\xde\x9e\x01" \
-    "\x92\xe7\xcc\xe0\xf3\xaf\x33\x8e\x39\xc4\xa5\x83" \
-    "\x4b\x07\x80\x87\xdb\x23\x00\x5e\xae\x2f\x31\xee" \
-    "\x1f\x01\x30\x2a\xf5\xb1\xdf\xe7\x4a\x11\x3b\x3d" \
-    "\xd9\x38\xc5\xa5\xc3\x01\xcd\x28\xa9\x9a\x98\x96" \
-    "\x22\xf0\x3d\x76\x3e\x00\xdb\x8e\xf2\xad\x09\xa5" \
-    "\x8b\x77\x11\x90\xc0\xf7\x44\x44\xc4\x90\xa2\x18" \
-    "\x52\x94\xb4\x02\xdf\x13\x60\x98\x80\x28\x52\x6b" \
-    "\xe9\xae\x98\xb9\x8f\x4d\xcb\x66\x9e\xae\xc9\x81" \
-    "\x95\x82\x9e\x1a\xd0\x53\x03\x94\x22\x09\x10\x4c" \
-    "\xab\x35\xcc\x21\x51\xa8\x3c\x74\x2c\x78\x59\xca" \
-    "\x6d\x5e\xe0\xe7\x7b\xf2\x4f\x60\xd7\x69\x63\x5a" \
-    "\xf6\xc2\x20\xd7\x69\x67\xe6\xc9\x91\x5e\x2f\x7c" \
-    "\x4e\xbc\x50\xca\x5b\xbb\xc9\xf8\xfd\xf5\x29\x19" \
-    "\x1b\x95\x7a\xe6\x80\x7c\xff\x6c\x26\x15\x5f\x59" \
-    "\x8d\xa7\x3a\x4e\x3b\x1d\x55\xda\x39\x64\xdd\x66" \
-    "\xc0\xcb\xd6\xff\xfc\x83\x4c\xd3\x2f\x40\xa4\xbb" \
-    "\x2c\x12\x83\xbf\x94\x00\x00\x00\x00\x49\x45\x4e" \
-    "\x44\xae\x42\x60\x82"
+image0_data = [
+"22 22 14 1",
+". c None",
+"a c #000000",
+"l c #0000ff",
+"e c #13ff07",
+"# c #8e8c82",
+"i c #95f284",
+"d c #a1ff9c",
+"b c #aca899",
+"j c #e03f3a",
+"h c #ece9d8",
+"f c #edfe2c",
+"k c #ff0000",
+"g c #ff9999",
+"c c #ffffff",
+"......................",
+"......................",
+"......................",
+"......................",
+"..#aaaaaaaaaaaaaaa#...",
+"..abbbbbbbbbbbbbbba...",
+"..abcccccccccccccba...",
+"..abcccdeeffaghchba...",
+"..abccieddfbajgccba...",
+"..abcdedcccabgkgcba...",
+"..abceecccbacckkcba...",
+"..abceecccabcckkcba...",
+"..ablllllllllllllba...",
+"..ablllllllllllllba...",
+"..abbbbbbbbbbbbbbba...",
+"..#aaaaaaaaaaaaaaa#...",
+".........aaa..........",
+".........aaa..........",
+".......aaaaaaa........",
+"......................",
+"......................",
+"......................"
+]
 
 class SimSetupDialog(QDialog):
     def __init__(self,parent = None,name = None,modal = 0,fl = 0):
         QDialog.__init__(self,parent,name,modal,fl)
 
-        self.image0 = QPixmap()
-        self.image0.loadFromData(image0_data,"PNG")
+        self.image0 = QPixmap(image0_data)
+
         if not name:
             self.setName("SimSetupDialog")
 
         self.setIcon(self.image0)
         self.setModal(1)
 
-        SimSetupDialogLayout = QVBoxLayout(self,11,6,"SimSetupDialogLayout")
 
-        self.textLabel6 = QLabel(self,"textLabel6")
-        self.textLabel6.setAlignment(QLabel.WordBreak | QLabel.AlignCenter)
-        SimSetupDialogLayout.addWidget(self.textLabel6)
-
-        layout62 = QGridLayout(None,1,1,0,6,"layout62")
-
-        layout61 = QVBoxLayout(None,0,6,"layout61")
-
-        self.textLabel2 = QLabel(self,"textLabel2")
-        layout61.addWidget(self.textLabel2)
-
-        self.StepsPerFrameWidget = QSpinBox(self,"StepsPerFrameWidget")
-        self.StepsPerFrameWidget.setMinValue(1)
-        self.StepsPerFrameWidget.setValue(10)
-        layout61.addWidget(self.StepsPerFrameWidget)
-
-        layout62.addLayout(layout61,1,0)
+        LayoutWidget = QWidget(self,"layout10")
+        LayoutWidget.setGeometry(QRect(12,10,310,138))
+        layout10 = QGridLayout(LayoutWidget,1,1,11,6,"layout10")
 
         layout58 = QVBoxLayout(None,0,6,"layout58")
 
-        self.textLabel1 = QLabel(self,"textLabel1")
+        self.textLabel1 = QLabel(LayoutWidget,"textLabel1")
         layout58.addWidget(self.textLabel1)
 
-        self.TimeStepWidget = QSpinBox(self,"TimeStepWidget")
+        self.TimeStepWidget = QSpinBox(LayoutWidget,"TimeStepWidget")
         self.TimeStepWidget.setMaxValue(999)
         self.TimeStepWidget.setMinValue(1)
         self.TimeStepWidget.setValue(10)
         layout58.addWidget(self.TimeStepWidget)
 
-        layout62.addLayout(layout58,0,0)
+        layout10.addLayout(layout58,0,0)
 
-        layout59 = QVBoxLayout(None,0,6,"layout59")
+        layout61 = QVBoxLayout(None,0,6,"layout61")
 
-        self.textLabel3 = QLabel(self,"textLabel3")
-        layout59.addWidget(self.textLabel3)
+        self.textLabel2 = QLabel(LayoutWidget,"textLabel2")
+        layout61.addWidget(self.textLabel2)
 
-        self.TemperatureWidget = QSpinBox(self,"TemperatureWidget")
-        self.TemperatureWidget.setMaxValue(999)
-        self.TemperatureWidget.setValue(300)
-        layout59.addWidget(self.TemperatureWidget)
+        self.StepsPerFrameWidget = QSpinBox(LayoutWidget,"StepsPerFrameWidget")
+        self.StepsPerFrameWidget.setMinValue(1)
+        self.StepsPerFrameWidget.setValue(10)
+        layout61.addWidget(self.StepsPerFrameWidget)
 
-        layout62.addLayout(layout59,0,1)
+        layout10.addLayout(layout61,1,0)
 
         layout60 = QVBoxLayout(None,0,6,"layout60")
 
-        self.textLabel5 = QLabel(self,"textLabel5")
+        self.textLabel5 = QLabel(LayoutWidget,"textLabel5")
         layout60.addWidget(self.textLabel5)
 
-        self.NumFramesWidget = QSpinBox(self,"NumFramesWidget")
+        self.NumFramesWidget = QSpinBox(LayoutWidget,"NumFramesWidget")
         self.NumFramesWidget.setMaxValue(90000)
         self.NumFramesWidget.setMinValue(30)
         self.NumFramesWidget.setLineStep(15)
         self.NumFramesWidget.setValue(900)
         layout60.addWidget(self.NumFramesWidget)
 
-        layout62.addLayout(layout60,1,1)
-        SimSetupDialogLayout.addLayout(layout62)
+        layout10.addLayout(layout60,1,1)
 
-        self.FileFormatWidget = QButtonGroup(self,"FileFormatWidget")
-        self.FileFormatWidget.setSizePolicy(QSizePolicy(5,5,0,1,self.FileFormatWidget.sizePolicy().hasHeightForWidth()))
-        self.FileFormatWidget.setAlignment(QButtonGroup.AlignVCenter)
+        layout9 = QVBoxLayout(None,0,6,"layout9")
 
-        self.BinFileWidget = QRadioButton(self.FileFormatWidget,"BinFileWidget")
-        self.BinFileWidget.setGeometry(QRect(20,60,221,22))
-        self.BinFileWidget.setChecked(1)
-        self.FileFormatWidget.insert( self.BinFileWidget,0)
+        self.textLabel3 = QLabel(LayoutWidget,"textLabel3")
+        layout9.addWidget(self.textLabel3)
 
-        self.TextFile = QRadioButton(self.FileFormatWidget,"TextFile")
-        self.TextFile.setGeometry(QRect(20,120,221,22))
-        self.FileFormatWidget.insert( self.TextFile,1)
-        SimSetupDialogLayout.addWidget(self.FileFormatWidget)
+        self.TemperatureWidget = QSpinBox(LayoutWidget,"TemperatureWidget")
+        self.TemperatureWidget.setMaxValue(999)
+        self.TemperatureWidget.setValue(300)
+        layout9.addWidget(self.TemperatureWidget)
 
-        layout63 = QHBoxLayout(None,0,26,"layout63")
+        layout10.addLayout(layout9,0,1)
 
-        self.NameFileButton = QPushButton(self,"NameFileButton")
-        layout63.addWidget(self.NameFileButton)
+        self.SaveButton = QPushButton(self,"SaveButton")
+        self.SaveButton.setGeometry(QRect(13,161,99,29))
 
-        self.GoButton = QPushButton(self,"GoButton")
-        self.GoButton.setDefault(1)
-        layout63.addWidget(self.GoButton)
-        SimSetupDialogLayout.addLayout(layout63)
+        self.CancelButton = QPushButton(self,"CancelButton")
+        self.CancelButton.setGeometry(QRect(222,161,99,29))
+        self.CancelButton.setDefault(1)
+
+        self.MovieButton = QPushButton(self,"MovieButton")
+        self.MovieButton.setGeometry(QRect(118,161,98,29))
+        self.MovieButton.setDefault(1)
 
         self.languageChange()
 
-        self.resize(QSize(408,487).expandedTo(self.minimumSizeHint()))
+        self.resize(QSize(337,216).expandedTo(self.minimumSizeHint()))
         self.clearWState(Qt.WState_Polished)
 
         self.connect(self.NumFramesWidget,SIGNAL("valueChanged(int)"),self.NumFramesValueChanged)
-        self.connect(self.FileFormatWidget,SIGNAL("clicked(int)"),self.FileFormat)
-        self.connect(self.NameFileButton,SIGNAL("clicked()"),self.NameFilePressed)
-        self.connect(self.GoButton,SIGNAL("clicked()"),self.GoPressed)
+        self.connect(self.SaveButton,SIGNAL("clicked()"),self.saveFilePressed)
+        self.connect(self.MovieButton,SIGNAL("clicked()"),self.createMoviePressed)
         self.connect(self.StepsPerFrameWidget,SIGNAL("valueChanged(int)"),self.StepsChanged)
         self.connect(self.TemperatureWidget,SIGNAL("valueChanged(int)"),self.TemperatureChanged)
         self.connect(self.TimeStepWidget,SIGNAL("valueChanged(int)"),self.TimeStepChanged)
+        self.connect(self.CancelButton,SIGNAL("clicked()"),self,SLOT("close()"))
 
 
     def languageChange(self):
-        self.setCaption(self.__tr("Simulator"))
-        self.textLabel6.setText(self.__tr("<b><h1>Simulation Setup</h1></b>"))
-        self.textLabel2.setText(self.__tr("Steps per Frame"))
+        self.setCaption(self.__tr("Simulator Setup"))
         self.textLabel1.setText(self.__tr("Timestep\n"
 "(hundredths of\n"
 "femtosecond)"))
+        self.textLabel2.setText(self.__tr("Steps per Frame"))
+        self.textLabel5.setText(self.__tr("Total frames"))
         self.textLabel3.setText(self.__tr("Temperature\n"
 "(Kelvins)"))
-        self.textLabel5.setText(self.__tr("Total frames"))
-        self.FileFormatWidget.setTitle(self.__tr("File Format"))
-        self.BinFileWidget.setText(self.__tr("Binary trajectory file (*.dpb)"))
-        self.TextFile.setText(self.__tr("Text trajectory file (*.xyz)"))
-        self.NameFileButton.setText(self.__tr("Name File"))
-        self.GoButton.setText(self.__tr("Go"))
+        self.SaveButton.setText(self.__tr("Save File"))
+        self.CancelButton.setText(self.__tr("Cancel"))
+        self.MovieButton.setText(self.__tr("Create Movie"))
 
 
     def NumFramesValueChanged(self,a0):
         print "SimSetupDialog.NumFramesValueChanged(int): Not implemented yet"
 
-    def NameFilePressed(self):
-        print "SimSetupDialog.NameFilePressed(): Not implemented yet"
-
-    def GoPressed(self):
-        print "SimSetupDialog.GoPressed(): Not implemented yet"
+    def createMoviePressed(self):
+        print "SimSetupDialog.createMoviePressed(): Not implemented yet"
 
     def StepsChanged(self,a0):
         print "SimSetupDialog.StepsChanged(int): Not implemented yet"
@@ -192,8 +172,8 @@ class SimSetupDialog(QDialog):
     def TimeStepChanged(self,a0):
         print "SimSetupDialog.TimeStepChanged(int): Not implemented yet"
 
-    def FileFormat(self,a0):
-        print "SimSetupDialog.FileFormat(int): Not implemented yet"
+    def saveFilePressed(self):
+        print "SimSetupDialog.saveFilePressed(): Not implemented yet"
 
     def __tr(self,s,c = None):
         return qApp.translate("SimSetupDialog",s,c)
--- src/runSim.py
+++ src/runSim.py
@@ -4,8 +4,16 @@ from SimSetupDialog import *
 from fileIO import writemmp
 from commands import *
 from debug import *
-import os
+import os, sys, re, signal
 
+def fileparse(name):
+    """breaks name into directory, main name, and extension in a tuple.
+    fileparse('~/foo/bar/gorp.xam') ==> ('~/foo/bar/', 'gorp', '.xam')
+    """
+    m=re.match("(.*\/)*([^\.]+)(\..*)?",name)
+    return ((m.group(1) or "./"), m.group(2), (m.group(3) or ""))
+    
+    
 class runSim(SimSetupDialog):
     def __init__(self, assy):
         SimSetupDialog.__init__(self)
@@ -14,53 +22,108 @@ class runSim(SimSetupDialog):
         self.temp = 300
         self.stepsper = 10
         self.timestep = 10
-        self.filename = ''
         self.fileform = ''
-        self.mext = '.dpb'
+        self.mext = '.xyz'
 
     def NumFramesValueChanged(self,a0):
         self.nframes = a0
 
-    def NameFilePressed(self):
-        self.filename = ''
+    def saveFilePressed(self):
+        if self.assy:
+            if self.assy.filename:
+                dir, fil, ext = fileparse(self.assy.filename)
+                sdir = self.assy.filename
+            else: 
+                dir, fil = "./", self.assy.name
+                sdir = globalParms['WorkingDirectory']
 
-    def GoPressed(self):
+        if self.mext == ".xyz": sfilter = QString("XYZ Format (*.xyz)")
+        else: sfilter = QString("Data Part Binary (*.dpb)")
+        
+        fn = QFileDialog.getSaveFileName(sdir,
+                    "XYZ Format (*.xyz);;Data Part Binary (*.dpb)",
+                    self, "IDONTKNOWWHATTHISIS",
+                    "Save As",
+                    sfilter)
+        
+        if fn:
+            fn = str(fn)
+            dir, fil, ext2 = fileparse(fn)
+            ext =str(sfilter[-5:-1]) # Get "ext" from the sfilter. It *can* be different from "ext2"!!! - Mark
+            safile = dir + fil + ext # full path of "Save As" filename
+            
+            if self.assy.filename != safile: # If the current part name and "Save As" filename are not the same...
+                if os.path.exists(safile): # ...and if the "Save As" file exists...
+
+                    # ... confirm overwrite of the existing file.
+                    ret = QMessageBox.warning( self, self.name(),
+                        "The file \"" + fil + ext + "\" already exists.\n"\
+                        "Do you want to overwrite the existing file or cancel?",
+                        "&Overwrite", "&Cancel", None,
+                        0,      # Enter == button 0
+                        1 )     # Escape == button 1
+
+                    if ret==1: # The user cancelled
+                        self.assy.w.statusBar.message( "Cancelled.  File not saved." )
+                        return # Cancel clicked or Alt+C pressed or Escape pressed
+            
+            self.mext = ext
+            natoms = len(self.assy.alist)
+
+            if ext == '.dpb': # DPB file format
+                ftype = 'DPB'
+                self.fileform = ''
+                filesize = (self.nframes * natoms * 3) + 4
+            else: # XYZ file format
+                ftype = 'XYZ'
+                self.fileform = '-x'
+                filesize = (self.nframes * natoms * 3) + 4 # XYZ SIZE: THIS IS WRONG
+
+            self.hide()
+            
+            r = self.saveMovie(safile, filesize)
+            
+            if not r: # Movie file saved successfully.
+                self.assy.w.statusBar.message( ftype + " file saved: " + safile)
+
+
+    def createMoviePressed(self):
         QDialog.accept(self)
-        import os, sys
+        moviefile = self.assy.filename[:-4] + '.dpb'
+        natoms = len(self.assy.alist)
+        dpbsize = (self.nframes * natoms * 3) + 4
         
-        #If a new model, save first
-        if not self.assy.alist:
-            QMessageBox.critical(self, "Error", "You must save the current model before running a simulation.")
-            return
+        r = self.saveMovie(moviefile, dpbsize)
         
-        #If the current file directory is not writable or file name has space in it, warn user
-        fPath, fName = os.path.split(self.assy.filename)
-        if not os.access(fPath, os.W_OK):
-            QMessageBox.critical(self, "Error", "You need to make sure you have write permission in the opened file directory.")
-            return
+        if not r: # Movie file saved successfully.
+            msg = "Total time to create movie file: %d seconds" % self.assy.w.progressbar.duration
+            self.assy.w.statusBar.message(msg) 
+            msg = "Movie written to [" + moviefile + "]."\
+                        "To play movie, click on the <b>Movie Player</b> <img source=\"movieicon\"> icon."
+            # This makes a copy of the movie tool icon to put in the HistoryMegawidget.
+            QMimeSourceFactory.defaultFactory().setPixmap( "movieicon", 
+                        self.assy.w.toolsMovieAction.iconSet().pixmap() )
+            self.assy.w.statusBar.message(msg)
+
+        return
+
+    def saveMovie(self, moviefile, filesize):
         
-        import re    
-        m = re.search(' +',  self.assy.filename)
-        if  m:
-            QMessageBox.critical(self, "Errors", "You need to make sure you don't have file name/path with space in it.")
-            return
-                
         filePath = os.path.dirname(os.path.abspath(sys.argv[0]))
-       
-        args = [filePath + '/../bin/simulator', '-f' + str(self.nframes), '-t' + str(self.temp), '-i' + str(self.stepsper),  str(self.fileform),  self.assy.filename]
+        program = filePath +  '/../bin/simulator'      
+        args = [program, '-f' + str(self.nframes), '-t' + str(self.temp), '-i' + str(self.stepsper), str(self.fileform),  '-o' + moviefile, self.assy.filename]
+        basename = os.path.basename(moviefile)
         
         QApplication.setOverrideCursor( QCursor(Qt.WaitCursor) )
         self.assy.w.statusBar.message("<span style=\"color:#006600\">Simulator: Calculating...</span>")
         
         try:
             if self.assy.modified: writemmp(self.assy, self.assy.filename, False)
-            
-            outfile = self.assy.filename[:-4] + self.mext
-            if os.path.exists(outfile): os.remove (outfile) # Delete before spawning.
+            if os.path.exists(moviefile): os.remove (moviefile) # Delete before spawning.
+            print  "program = ",program
+            print  "Spawnv args are %r" % (args,) # this %r remains (see above)
             kid = os.spawnv(os.P_NOWAIT, filePath + '/../bin/simulator', args)
-            natoms = len(self.assy.alist)
-            dpbsize = (self.nframes * natoms * 3) + 4
-            r = self.assy.w.progressbar.launch(dpbsize, outfile, "Simulate", "Calculating...")
+            r = self.assy.w.progressbar.launch(filesize, moviefile, "Simulate", "Writing movie file " + basename + "...", 1)
             s = None
         except:
             print_compact_traceback("exception in simulation; continuing: ")
@@ -69,36 +132,25 @@ class runSim(SimSetupDialog):
         
         QApplication.restoreOverrideCursor() # Restore the cursor
         
-        if not r: # Simulator competed.
-            msg = "Movie written to [" + outfile + "]."\
-                        "To play movie, click on the <b>Movie Player</b> <img source=\"movieicon\"> icon."
-            QMimeSourceFactory.defaultFactory().setPixmap( "movieicon", 
-                        self.assy.w.toolsMovieAction.iconSet().pixmap() )
-            self.assy.w.statusBar.message(msg)
-
-        elif r == 1: # User pressed abort on progress dialog...
-            self.assy.w.statusBar.message("<span style=\"color:#ff0000\">Simulate: Aborted.</span>")         
+        if not r: return r
+        
+        if r == 1: # User pressed abort on progress dialog...
+            self.assy.w.statusBar.message("<span style=\"color:#ff0000\">Simulator: Aborted.</span>")         
             # We should kill the kid, but not sure how on Windows
+            if sys.platform not in ['win32']: os.kill(kid, signal.SIGKILL) # Not tested - Mark 050105
             
         else: # Something failed...
             if not s: s = "exit code %r" % r
             self.assy.w.statusBar.message("Simulation Failed!") ##e include s?
             QMessageBox.warning(self.assy.w, "Simulation Failed:", s)
-        return
+
+        return r
 
     def StepsChanged(self,a0):
-        self.stepsper = 10
+        self.stepsper = a0
 
     def TemperatureChanged(self,a0):
-        self.temp = 300
+        self.temp = a0
 
     def TimeStepChanged(self,a0):
-        self.timestep = 10
-
-    def FileFormat(self,a0):
-        if a0 == 0: # DPB file format
-            self.fileform = ''
-            self.mext = '.dpb'
-        else: # XYZ file format
-            self.fileform = '-x'
-            self.mext = '.xyz'
+        self.timestep = a0
