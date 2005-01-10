1f0ed3053cd4c5591499320748608a851976a7d4
Mark Sims <mark@nanorex.com>
2005-01-10 18:18:08 +0000

    New Movie class and Movie Mode files

---
+++ src/movie.py
@@ -0,0 +1,224 @@
+# Copyright (c) 2005 Nanorex, Inc.  All rights reserved.
+"""
+movie.py -- the Movie class.
+
+This file mainly defines class Movie
+
+$Id$
+"""
+
+__author__ = "Mark"
+
+import os, sys
+from struct import unpack
+from runSim import runSim
+from qt import qApp
+
+FWD = 1
+REV = -1
+
+class Movie:
+    """
+    Movie object.
+    """
+    
+    name = "" # for use before __init__ runs (used in __str__ of subclasses)
+    
+    def __init__(self, assembly, name=None):
+        self.assy = assembly
+        # for future use: name of the movie that appears in the modelTree. 
+        self.name = name or "" # assumed to be a string by some code
+        # the name of the movie file
+        self.filename = ""
+#        pid = os.getpid()
+#        self.filename = os.path.join(self.assy.w.tmpFilePath, "sim-%d.dpb" % pid)
+        # movie "file object"
+        self.fileobj = None
+        # the total number of frames in this movie
+        self.totalFrames = 0
+        # the most recent frame number of this movie that was played
+        self.currentFrame = 0
+        # a flag that indicates whether this moviefile is open or closed 
+        self.isOpen = False
+#        print "movie.__init__(). self.isOpen =", self.isOpen
+        # a flag that indicates whether this movie is running via the timer
+        self.playDirection = FWD
+        # a flag that indicates if the movie and the part are synchronized
+        self.isValid = False
+        # the number of atoms in each frame of the movie.
+#        self.natoms = len(self.assy.alist)
+        self.natoms = 0
+        # show each frame when in _playFrame
+        self.showEachFrame = False
+        # a flag that indicated the movie is paused
+        self.isPaused = True
+        # simulator parameters that were used when creating this movie
+        # these should be stored in the dpb file header so they
+        # can be retrieved later.  These are the default values used by
+        # the simsetup dialog.
+        self.temp = 300
+        self.stepsper = 10
+        self.timestep = 10
+        
+# movie methods ##########################
+
+    def _setup(self):
+        """Setup this movie for playing
+        """
+        if not os.path.exists(self.filename): 
+            self.assy.w.history.message("Cannot play movie file [" + self.filename + "]. It does not exist.")
+            return
+        self.assy.movsetup()
+        self.fileobj=open(self.filename,'rb')
+        self.isOpen = True
+#        print "movie._setup(). self.isOpen =", self.isOpen
+        
+        # Read header (4 bytes) from file containing the number of frames in the moviefile.
+        self.totalFrames = unpack('i',self.fileobj.read(4))[0]
+
+        # Compute natoms
+        filesize = os.path.getsize(self.filename)
+        self.natoms = (filesize - 4) / (self.totalFrames * 3)
+        
+        # Set file position at currentFrame.
+        filepos = (self.currentFrame * self.natoms * 3) + 4
+        self.fileobj.seek( filepos )
+
+        self.assy.w.movieProgressBar.setTotalSteps(self.totalFrames) # Progress bar
+        self.assy.w.movieProgressBar.setProgress(self.currentFrame)
+
+        self.assy.w.frameNumberSB.setValue(self.currentFrame) # Spinbox
+        self.assy.w.frameNumberSB.setMaxValue(self.totalFrames)
+
+#        self.assy.w.frameNumberLCD.display(self.currentFrame) # LCD        
+#        self.assy.w.frameNumberSL.setValue(self.currentFrame) # SL = Slider
+#        self.assy.w.frameNumberSL.setMaxValue(self.totalFrames)
+
+        # Debugging Code
+#        msg = "Movie Ready: Number of Frames: " + str(self.totalFrames) + \
+#                ", Current Frame:" +  str(self.currentFrame) +\
+#                ", Number of Atoms:" + str(self.natoms)
+#        self.assy.w.history.message(msg)
+        
+        # Debugging Code
+#        filepos = self.fileobj.tell() # Current file position
+#        msg = "Frame:" + str(self.currentFrame) + ", filepos =" + str(filepos)
+#        self.assy.w.history.message(msg)
+
+    def _close(self):
+        """Close movie file and adjust atom positions.
+        """
+        self.fileobj.close()
+        self.assy.movend()
+        
+    def _play(self, direction = FWD):
+        """Start playing movie from the current frame.
+        """
+        self.assy.w.history.message("Playing movie file [" + self.assy.m.filename + "]  Total Frames: " + str(self.totalFrames))
+        self.playDirection = direction
+        self._continue(0)
+        
+    def _continue(self, hflag = True):
+        """Continue playing movie
+        hflag - if True, print history message
+        """
+        
+        # In case the movie is playing
+        # This is temporary.  I intend to 
+        self._pause(0) 
+        
+        if hflag: self.assy.w.history.message("Movie continued.")
+        
+        self.showEachFrame = True
+        
+        # Continue playing movie.
+        if self.playDirection == FWD: self._playFrame(self.totalFrames)
+        else: self._playFrame(0)
+
+    def _pause(self, hflag = True):
+        """Pause movie.
+        hflag - if True, print history message
+        """
+        self.isPaused = True
+        if hflag: self.assy.w.history.message("Movie paused.")
+
+    def _playFrame(self, fnum):
+        """Show a specific frame of the movie. If "isPlaying" is true, 
+        it will show each frame between "fnum" and "currentFrame".
+        fnum - frame number to display in the movie.
+        """
+        
+        self.isPaused = False
+        
+        # Don't let movie run out of bounds.
+        if fnum < 0 or fnum > self.totalFrames or fnum == self.currentFrame:
+            self.isPaused = True # May not be needed.  Doing it anyway.
+            return
+
+        # "inc" is the frame increment (FWD = 1, REV = -1) .
+        if fnum > self.currentFrame: inc = FWD
+        else: inc = REV
+        
+#        print "BEGIN: fnum = ", fnum, ", currentFrame =", self.currentFrame, ", inc =",inc
+        
+        # This is the main loop to compute atom positions from the current frame to "fnum"
+        # After this loop completes, we paint the model.
+        while self.currentFrame != fnum:
+
+            self.currentFrame += inc
+            self.assy.w.movieProgressBar.setProgress(self.currentFrame) # Progress bar
+
+#            print "IN LOOP1: fnum = ", fnum, ", currentFrame =", self.currentFrame, ", inc =",inc
+
+            # Forward one frame
+            if inc == FWD:
+                
+                # Debugging code - Mark 050109
+#                filepos = self.fileobj.tell()
+#                msg = "FWD Frame:" + str(self.currentFrame) + ", filepos =" + str(filepos) + ", inc =" + str(inc)
+#                msg += ", natoms =" + str(self.natoms)
+#                self.assy.w.history.message(msg)
+#                print msg
+
+                self.assy.movatoms(self.fileobj)
+             
+            # Backward one frame   
+            else: 
+                filepos = (self.currentFrame * self.natoms * 3) + 4
+                self.fileobj.seek( filepos )
+                
+                # Debugging code - Mark 050109
+#                filepos = self.fileobj.tell()
+#                msg = "REV Frame:" + str(self.currentFrame) + ", filepos =" + str(filepos) + ", inc =" + str(inc)
+#                msg += ", natoms =" + str(self.natoms)
+#                self.assy.w.history.message(msg)
+#                print msg
+
+                self.assy.movatoms(self.fileobj, 0)
+            
+            # update the GLPane and frame number each frame
+            if self.showEachFrame:
+                qApp.processEvents() # Process queued events (i.e. clicking Abort button).
+                self.assy.w.frameNumberSB.setValue(self.currentFrame) # Spinbox
+                self.assy.o.paintGL()
+            
+            # Pause was pressed while playing movie.    
+            if self.isPaused:
+                self.isPaused = False
+                self.showEachFrame = False
+                break
+                
+        # End of loop
+        
+        # if we just played in reverse, reset the file position in case we play forward next time.
+        if inc == REV: self.fileobj.seek( filepos )
+        
+        # Update dashboard and show frame.        
+        self.assy.w.frameNumberSB.setValue(self.currentFrame) # Spinbox
+        self.assy.o.paintGL()
+
+        # This is a temporary workaround for an unusual bug I can't figure out.
+        # When the REV play button is pressed during FWD play, the movie
+        # will play forward again after REV play gets to frame 0.  Very strange.
+        # Mark 050110
+        if fnum == self.currentFrame: self._pause(0) # Force pause.
---
+++ src/movieMode.py
@@ -0,0 +1,108 @@
+# Copyright (c) 2005 Nanorex, Inc.  All rights reserved.
+"""
+movieMode.py -- movie player mode.
+
+$Id$
+"""
+
+__author__ = "Mark"
+
+from modes import *
+
+class movieMode(basicMode):
+    """ This class is used to play movie files.
+       Users know it as "Movie mode".
+    """
+
+    # class constants
+    backgroundColor = 189/255.0, 228/255.0, 238/255.0
+    modename = 'MOVIE'
+    default_mode_status_text = "Mode: Movie Player"
+    
+    # no __init__ method needed
+    
+    # methods related to entering this mode
+    
+    def Enter(self): # bruce 040922 split setMode into Enter and init_gui (fyi)
+        basicMode.Enter(self)
+        self.o.assy.unpickatoms()
+        self.o.assy.unpickparts()
+        self.o.assy.selwhat = 0
+
+    # init_gui handles all the GUI display when entering this mode [mark 041004
+    def init_gui(self):
+        
+        # We have a moviefile ready to go.  It's showtime!!!
+        self.w.toolsMoviePlayerAction.setOn(1) # toggle on the Movie Player icon
+#        self.w.moviePauseAction.setVisible(0)
+#        self.w.moviePlayAction.setVisible(1)
+#        self.w.movieProgressBar.reset()
+#       self.w.frameNumberLCD.display(self.o.assy.currentFrame)
+        self.w.frameNumberSB.setValue(self.o.assy.m.currentFrame) # SB = Spinbox
+#        self.w.frameNumberSL.setMaxValue(self.o.assy.m.totalFrames)
+#        self.w.frameNumberSL.setValue(self.o.assy.m.currentFrame) # SL = Slider
+        self.o.assy.m._setup()
+        self.w.moviePlayerDashboard.show()
+        self.w.modifyMinimizeAction.setEnabled(0) # Disable "Minimize"
+        self.w.toolsSimulatorAction.setEnabled(0) # Disable "Simulator"
+        self.w.fileSaveAction.setEnabled(0) # Disable "File Save"
+        self.w.fileSaveAsAction.setEnabled(0) # Disable "File Save"
+        self.w.fileOpenAction.setEnabled(0) # Disable "File Open"
+
+    # methods related to exiting this mode [bruce 040922 made these from
+    # old Done method, and added new code; there was no Flush method]
+
+    def haveNontrivialState(self):
+#        print "movieMode.haveNontrivialState() called"
+        self.o.assy.m._close()
+        return False
+        #return self.modified # bruce 040923 new code
+
+    def StateDone(self):
+#        print "movieMode.StateDone() called"
+        self.o.assy.m._close()
+        return None
+    # we never have undone state, but we have to implement this method,
+    # since our haveNontrivialState can return True
+    
+    # restore_gui handles all the GUI display when leavinging this mode [mark 041004]
+    def restore_gui(self):
+#        print "movieMode.restore_gui() called"
+        self.w.moviePlayerDashboard.hide()
+        self.w.modifyMinimizeAction.setEnabled(1) # Enable "Minimize"
+        self.w.toolsSimulatorAction.setEnabled(1) # Enable "Simulator"
+        self.w.fileSaveAction.setEnabled(1) # Enable "File Save"
+        self.w.fileSaveAsAction.setEnabled(1) # Enable "File Save"
+        self.w.fileOpenAction.setEnabled(1) # Enable "File Open"
+
+
+    def makeMenus(self):
+        self.Menu_spec = [
+            None,
+         ]
+                
+    def Draw(self):
+        # bruce comment 040922: code is almost identical with modifyMode.Draw;
+        # the difference (no check for self.o.assy existing) might be a bug in this version, or might have no effect.
+        basicMode.Draw(self)   
+        #self.griddraw()
+        if self.sellist: self.pickdraw()
+        self.o.assy.draw(self.o)
+        
+    # other dashboard methods
+    
+
+        
+    # mouse and key events
+    
+    def keyPress(self,key):
+        if key == Qt.Key_Delete:
+            print "delete key"
+            pass
+        if key == Qt.Key_End:
+            pass
+        if key == Qt.Key_Left or key == Qt.Key_Down:
+            self.o.assy.m._playFrame(self.o.assy.m.currentFrame - 1)
+        if key == Qt.Key_Right or key == Qt.Key_Up:
+            self.o.assy.m._playFrame(self.o.assy.m.currentFrame +1)
+        return
