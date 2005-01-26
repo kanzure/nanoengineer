d974d5af5048225d0cbd5dcfc03e92e6e9e80d3c
Huaicai Mo <hmo@nanorex.com>
2005-01-26 20:27:02 +0000

    Fixed the view moving problem.

--- src/modes.py
+++ src/modes.py
@@ -791,16 +791,26 @@ class basicMode(anyMode):
         """Move point of view so that objects appear to follow
         the mouse on the screen.
         """
-        w=self.o.width+0.0 
         h=self.o.height+0.0
         deltaMouse = V(event.pos().x() - self.o.MousePos[0],
                        self.o.MousePos[1] - event.pos().y(), 0.0)
-        move = self.o.quat.unrot(self.o.scale * deltaMouse/(h*0.5))
+        #move = self.o.quat.unrot(self.o.scale * deltaMouse/(h*0.5))
+        
         # bruce comment 040908, about josh code: 'move' is mouse
         # motion in model coords. We want center of view, -self.pov,
         # to move in opposite direction as mouse, so that after
         # recentering view on that point, objects have moved with
         # mouse.
+        
+        ### Huaicai 1/26/05: delta Xe, delta Ye  depend on Ze, here
+        ### Ze is just an estimate, so Xe and Ye are estimates too, but
+        ### they seems more accurate than before. To accurately 
+        ### calculate it, we need to find a depth value for a point on 
+        ### the model.
+        Ze = 2.0*self.o.near*self.o.far*self.o.scale/(self.o.near+self.o.far)
+        tY = (self.o.zoomFactor*Ze)*2.0/h
+        
+        move = self.o.quat.unrot(deltaMouse*tY)
         self.o.pov += move
         self.o.paintGL()
         self.o.SaveMouse(event)
