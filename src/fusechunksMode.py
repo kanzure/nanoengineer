0abb4ef8c26dc738f8eb93ad491026cdaa71858a
Mark Sims <mark@nanorex.com>
2005-03-26 04:16:41 +0000

    Fixed items in catchall bug 486: #1, #3, #4, #5, #7, #8 and #9.

--- src/fusechunksMode.py
+++ src/fusechunksMode.py
@@ -9,8 +9,9 @@ __author__ = "Mark"
 
 #from modes import *
 from modifyMode import *
-from extrudeMode import mergeable_singlets_Q_and_offset
+from extrudeMode import mergeable_singlets_Q_and_offset, lambda_tol_nbonds
 from chunk import bond_at_singlets
+from HistoryWidget import redmsg
 
 class fusechunksMode(modifyMode):
     "Allows user to move one chunk and fuse it to other chunks in the part"
@@ -32,11 +33,17 @@ class fusechunksMode(modifyMode):
         self.o.setCursor(self.w.MoveSelectCursor) # load default cursor for MODIFY mode
         self.w.toolsFuseChunksAction.setOn(1) # toggle on the Fuse Chunks icon
         self.w.fuseChunksDashboard.show() # show the Fuse Chunks dashboard
-        self.w.toleranceLCD.display((self.w.toleranceSL.value() + 50) * .01)
         self.w.connect(self.w.makeBondsPB,SIGNAL("clicked()"),self.make_bonds)
         self.w.connect(self.w.toleranceSL,SIGNAL("valueChanged(int)"),self.tolerance_changed)
-        self.o.assy.unpickparts()
-        self.selected_chunk = None
+        
+        # If only one chunk is selected when coming in, make it the selected_chunk.
+        # If more than one chunk is selected, unselect them all.
+        if len(self.o.assy.selmols) == 1: 
+            self.selected_chunk = self.o.assy.selmols[0]
+            self.find_bondable_pairs() # Find bondable pairs of singlets
+        else:
+            self.o.assy.unpickparts()
+            self.selected_chunk = None
     
     def restore_gui(self):
         self.w.fuseChunksDashboard.hide()
@@ -44,11 +51,11 @@ class fusechunksMode(modifyMode):
         self.w.disconnect(self.w.toleranceSL,SIGNAL("valueChanged(int)"),self.tolerance_changed)
 
     def tolerance_changed(self, val):
-        self.tol = (val + 50) * .01
-        self.w.toleranceLCD.display(self.tol)
+        self.tol = val * .01
+        if not self.selected_chunk: return
         self.find_bondable_pairs() # Find bondable pairs of singlets
         self.o.gl_update()
-        
+
     def leftDrag(self, event):
         """Move the selected chunk in the plane of the screen following
         the mouse.
@@ -120,6 +127,10 @@ class fusechunksMode(modifyMode):
         self.find_bondable_pairs() # Find bondable pairs of singlets
         
         self.o.gl_update()
+        
+    def leftDouble(self, event):
+        # This keeps us from ending Fuse Chunks, as is the case with Move Chunks.
+        pass
                 
     def EndPick(self, event, selSense):
         """Pick if click.  Only one chunk can be selected .
@@ -127,16 +138,21 @@ class fusechunksMode(modifyMode):
         if not self.picking: return
         self.picking = False
 
-        if self.dragdist<7:
-            # didn't move much, call it a click
+        if self.dragdist<7: # didn't move much, call it a click
+            
             # Pick a chunk and only one chunk
             if selSense == 0:  # Control key pressed, so unpick
                 self.o.assy.unpick_at_event(event)
                 self.selected_chunk = None
-            else:  # Pick this chunk
-                self.o.assy.onlypick_at_event(event)
-                if self.o.assy.selmols: self.selected_chunk = self.o.assy.selmols[0]
+                self.bondable_pairs = [] 
+                self.ways_of_bonding = {}
 
+            else:  # Pick a new chunk
+                self.o.assy.onlypick_at_event(event)
+                if self.o.assy.selmols: 
+                    self.selected_chunk = self.o.assy.selmols[0]
+                    self.find_bondable_pairs() # Find bondable pairs of singlets
+             
             self.w.win_update()
                     
     def Draw(self):
@@ -193,25 +209,67 @@ class fusechunksMode(modifyMode):
                                         self.ways_of_bonding[s2.key] += 1
                                     else:
                                         self.ways_of_bonding[s2.key] = 1
+        
+        # Update tolerance label and status bar msgs.
+        nbonds = len(self.bondable_pairs)
+        tol_str = lambda_tol_nbonds(self.tol, nbonds)
+        self.w.toleranceLB.setText(tol_str)
+        
+        if nbonds:
+            self.w.history.transient_msg("")
+        else:
+            self.w.history.transient_msg("Drag chunk with open bonds near another chunk with open bonds to create new bonds.")
 
     def make_bonds(self):
         "Make bonds between all bondable pairs of singlets"
         
         self.bondable_pairs_atoms = []
+        singlets_with_two_bonds = []
+        not_bonded = 0
         
+#        print self.bondable_pairs
+        
+        # This first section of code bonds each bondable pair of singlets.
         for s1, s2 in self.bondable_pairs:
+            # Make sure each singlet of the pair has only one way of bonding.
+            # If either singlet has more than one ways to bond, we aren't going to bond them.
             if self.ways_of_bonding[s1.key] == 1 and self.ways_of_bonding[s2.key] == 1:
-                # record the real atoms in case you want to undo the bond later (before general Undo exists)
+                # Record the real atoms in case I want to undo the bond later (before general Undo exists)
+                # Current, this undo feature is not implemented here. Mark 050325
                 a1 = s1.singlet_neighbor()
                 a2 = s2.singlet_neighbor()
                 self.bondable_pairs_atoms.append( (a1,a2) ) # Add this pair to the list
-                # Bond the singlets.
-                bond_at_singlets(s1, s2, move = False)
-        
-        # This must be done before gl_update, or it will attempt to draw the singlets again.
-        self.bondable_pairs = [] 
-        self.ways_of_bonding = {} 
+                bond_at_singlets(s1, s2, move = False) # Bond the singlets.
+                self.o.assy.changed() # The assy has changed.
+            else:
+                not_bonded += 1
+                # One of the two singlets had more than one way to bond.
+                # We keep track of it so we can print a msg to the user
+                # that we aren't bonding this pair (and other previous pairs).
+        if not_bonded:
+            if not_bonded == 1:
+                msg = "%d open bond had multiple bonds. It was not bonded." % (not_bonded,)
+            else:
+                msg = "%d open bonds had multiple bonds. They were not bonded." % (not_bonded,)
+            self.w.history.message(redmsg(msg))
+                        
+        # Merge the chunks if the "merge chunks" checkbox is checked
+        if self.w.mergeCB.isChecked() and self.bondable_pairs_atoms:
+            merged_chunks = []
+            for a1, a2 in self.bondable_pairs_atoms:
+                # Ignore a1, they are atoms from selected_chunk
+                # It is possible that a2 is an atom from selected_chunk, so check it
+                if a2.molecule != self.selected_chunk:
+                    if a2.molecule not in merged_chunks:
+                        merged_chunks.append(a2.molecule)
+                        self.selected_chunk.merge(a2.molecule)
+
+        # This must be done before gl_update, or it will try to draw the 
+        # bondable singlets again, which generates errors.
+        if self.bondable_pairs_atoms:
+            self.bondable_pairs = []
+            self.ways_of_bonding = {}
                 
         self.o.gl_update()
-        
+
 # end of class fusechunksMode
