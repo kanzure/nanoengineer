6e1c14795243d66eb59048b7f96f992d0197cf29
Bruce Smith <bruce@nanorex.com>
2005-04-15 19:18:37 +0000

    bugfix hhmmss_str, revise docstrings/option-names, prep for superclass renaming

--- src/ProgressBar.py
+++ src/ProgressBar.py
@@ -8,34 +8,61 @@ __author__ = "Mark"
 
 import os, time
 from qt import *
-from ProgressBarDialog import *
 
-class ProgressBar( progressBarDialog ):
+
+#bruce 050415 I want to rename progressBarDialog to ProgressBarDialog
+# but plan to ask someone else to do it in the .ui file.
+# So I'll temporarily kluge this to work either way:
+
+progressBarDialog = ProgressBarDialog = None
+
+from ProgressBarDialog import * # should define one or the other of those two classnames but not both
+
+assert not (progressBarDialog and ProgressBarDialog), "p and P are %r and %r" % (progressBarDialog , ProgressBarDialog)
+
+ProgressBarDialog = progressBarDialog or ProgressBarDialog
+del progressBarDialog
+
+#end bruce 050415 temporary kluge
+
+
+class ProgressBar( ProgressBarDialog ):
     def __init__(self):
         ### Huaicai 1/10/05: make the dialog as a modal dialog, 
         ### otherwise, if the user close the main window, it will get 
         ### "RuntimeError: underlying C/C++ object has been deleted   
-        progressBarDialog.__init__(self, None, None, True)
+        ProgressBarDialog.__init__(self, None, None, True)
         
         self.duration = 0 # Seconds that the progress bar takes to complete
+            # [bruce 050415 comment: this is a public atttribute, used by external code
+            #  after the progress bar is hidden and the launch method has returned.]
 
     # Start the progressbar
-    def launch( self , nsteps, filename = '', caption = "Progress", message = "Calculating...", dflag = 0):
-        """Launch a progress bar dialog and update while a file is being written.
+    def launch( self , nsteps, filename = '', caption = "Progress", message = "Calculating...", show_duration = 0):
+        """Launch a progress bar dialog, and update it while a file is being written
+        by some other process, to show the size of that file as progress, while waiting
+        for that size to reach a given value; when it does, hide the dialog and return 0.
+        If the user first presses the Abort button on the dialog, hide it and return 1.
+        Parameters:
             nsteps - total steps for the progress bar (final size in bytes of filename)
             filename - name of file we are waiting for.
             caption - dialog caption
             message - text that appears above the progress bar
-            dflag - if True, display duration (in seconds) below progressbar
+            show_duration - if True, display duration (in seconds) below progressbar
+        Return value: 0 if file reached desired size, 1 if user hit abort button.
         """
-        
+        #bruce 050415 revised docstring, renamed dflag -> show_duration
         self.abort = False
         filesize = 0
         sinc = .1
         self.stime = time.time()
         
         # This is a temporary fix for bug #490.  Mark 050325
-        if caption == "Minimize": 
+        # [bruce 050415 comment: Mark's comment#1 in bug 490 says that its effect
+        #  is to remove the progressbar from the dialog during Minimize.
+        #  Note that this is hiding or showing self.progressbar, not self as in
+        #  the code below. ###e should change this to use a new optional flag]
+        if caption.startswith("Minimize"): #bruce 050415 changed == to startswith
             self.progress.hide()
         else:
             self.progress.show()
@@ -57,7 +84,7 @@ class ProgressBar( progressBarDialog ):
             self.progress.setProgress( filesize )
             qApp.processEvents() # Process queued events (i.e. clicking Abort button).
             
-            if dflag: # Display duration.
+            if show_duration: # Display duration.
                 elapsedtime = self.duration
                 self.duration = time.time() - self.stime
                 if elapsedtime == self.duration: continue
@@ -73,9 +100,10 @@ class ProgressBar( progressBarDialog ):
         # End of Main loop
         self.progress.setProgress(nsteps) # 100% done
         self.duration = time.time() - self.stime
-        time.sleep(0.1)  # Give the progress bar a moment show 100%
+        time.sleep(0.1)  # Give the progress bar a moment to show 100%
         self.hide()
         self.progress.reset() # Reset the progress bar.
+            # [bruce 050415 comment: we should probably reset it at start of launch method, too. ###e]
         return 0
         
     def abort(self):
@@ -84,11 +112,20 @@ class ProgressBar( progressBarDialog ):
         
     def hhmmss_str(self, secs):
         """Given the number of seconds, return the elapsed time as a string in hh:mm:ss format"""
-        if secs < 3600: hours = 0
-        else: hours = int(secs/3600.0)
-        minutes = int(secs/60.0 - hours*60.0)
-        seconds = int(secs - minutes*60.0)
+        # [bruce 050415 comment: this is sometimes called from external code
+        #  after the progressbar is hidden and our launch method has returned.]
+        # bruce 050415 revising this to use pure int computations (so bugs from
+        #  numeric roundoff can't occur) and to fix a bug when hours > 0 (commented below).
+        secs = int(secs)
+        hours = int(secs/3600) # use int divisor, not float
+            # (btw, the int() wrapper has no effect until python int '/' operator changes to produce nonints)
+        minutes = int(secs/60 - hours*60)
+        seconds = int(secs - minutes*60 - hours*3600) #bruce 050415 bugfix: also subtract hours
         if hours:
             return '%02d:%02d:%02d' % (hours, minutes, seconds)
         else:
             return '%02d:%02d' % (minutes, seconds)
+        pass
+    pass # end of class ProgressBar
+
+# end
