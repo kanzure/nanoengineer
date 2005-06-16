2176af18a6367090129cc4d3745296fbf70bf7c3
Huaicai Mo <hmo@nanorex.com>
2005-06-16 18:32:39 +0000

    Some changes to support pickle/unpickle

--- src/SimServer.py
+++ src/SimServer.py
@@ -6,10 +6,23 @@ $Id$
 '''
 __author__ = "Mark"
 
+import sys
+
 class SimServer:
     '''a SimServer has all the attributes needed to connect to and run a SimJob'''
     
-    def __init__(self, parms):
+    server_parms = {
+        'hostname':'localhost',
+        'ipaddress':'127.0.0.1',
+        'method':'Local access',
+        'engine':'PC GAMESS',
+        'program':'C:\\PCGAMESS\\gamess.exe',
+        'tmpdir':'C:\\PCGAMESS\\',
+        'platform':'Windows',
+        'username':'nanorex',
+        'password':''}
+        
+    def __init__(self):
         
         # The parameters (parms) for the SimServer object are provided in a dictionary in key:value pairs
         # For the Gamess Jig, the parms are defined in the jig_Gamess.py.
@@ -23,16 +36,33 @@ class SimServer:
         # username: User name
         # password: Password
         
-        self.parms = parms.keys()
-        self.parms.sort() # Sort parms.
-        self.edit_cntl = None
+        self.parms = SimServer.server_parms
+        if sys.platform == 'linux2':
+            self.parms['platform'] = 'Linux'
+            self.parms['program'] = '/usr/bin/rumgms'
+        elif sys.platform == 'darwin':
+            self.parms['program'] = '/usr/bin/rumgms'
+            self.parms['platform'] = 'Mac Os'
         
+        self.parms.keys().sort() # Sort parms.
+            
         # WARNING: Bugs will be caused if any of SimJob's own methods or 
         # instance variables had the same name as any of the parameter ('k') values.
-
-        for k in parms:
-            self.__dict__[k] = parms[k]        
-            
+        for k in self.parms:
+             self.__dict__[k] = self.parms[k]        
+                
+        self.edit_cntl = None
+    
+    def __getstate__(self):
+        """Called by pickle """
+        return self.parms, self.edit_cntl
+        
+    
+    def __setstate__(self, state):
+        """Called by unPickle"""
+        self.parms, self.edit_cntl = state
+        self.set_parms(self.parms)     
+    
     def write_parms(self, f):
         'Write server parms to file f'
         
@@ -43,9 +73,15 @@ class SimServer:
             phrase = rem + k + ': ' + str(self.__dict__[k])
             f.write (phrase + '\n')
         f.write (rem+'\n')
-        
+    
+    def set_parms(self, parms):
+        self.parms = parms
+        for k in parms:
+             self.__dict__[k] = parms[k]        
+   
     def get_comment_character(self):
-        if self.platform == 'win32':
+        if sys.platform == 'win32':
             return 'REM ' # Batch file comment for Windows
         else:
             return '# ' # Script file comment for Linux and Mac
+            
