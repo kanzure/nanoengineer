86e48bd0c65338d16ef1896295cb3d19337f887f
Bruce Smith <bruce@nanorex.com>
2005-06-29 22:42:23 +0000

    code cleanup

--- src/ops_connected.py
+++ src/ops_connected.py
@@ -9,11 +9,12 @@ History:
 bruce 050507 made this by collecting appropriate methods (by Josh) from class Part.
 
 bruce 050520 added new code (mostly in a separate new file) for Select Doubly.
+
+bruce 050629 code cleanup.
 """
 __author__ = "Josh"
 
 from HistoryWidget import greenmsg, redmsg
-## from qt import QApplication, QCursor, Qt
 
 class ops_connected_Mixin:
     "Mixin for providing Select Connected and Select Doubly methods to class Part"
@@ -47,30 +48,23 @@ class ops_connected_Mixin:
             self.w.history.message(redmsg("Select Doubly: No atom(s) selected."))
             return
             
-##        QApplication.setOverrideCursor( QCursor(Qt.WaitCursor) )
         self.w.history.message(greenmsg("Select Doubly:"))
         
-##        self.w.history.message("Working.  Please wait...")
         alreadySelected = len(self.selatoms.values())
-        ### new code, experimental, but seems to work -- bruce 050520
-        from op_select_doubly import select_doubly
+        from op_select_doubly import select_doubly # new code, bruce 050520
         #e could also reload it now to speed devel!
         select_doubly(self.selatoms.values()) #e optim
-        ### old code:
-        ## self.markdouble()
         totalSelected = len(self.selatoms.values())
         self.w.history.message("%d new doubly connected atom(s) selected (besides the %d initially selected)." % \
                                (totalSelected - alreadySelected, alreadySelected) ) ###e improve msg; note (s) is not in fix_plurals
-        
-##        QApplication.restoreOverrideCursor() # Restore the cursor
-        
+                
         if totalSelected > alreadySelected:
             ## otherwise, means nothing new selected. Am I right? ---Huaicai, not analyze the markdouble() algorithm yet 
             #self.w.win_update()
             self.o.gl_update()
         return
 
-    # == helpers for SelectConnected and [not anymore] SelectDoubly
+    # == helpers for SelectConnected (for SelectDoubly, see separate file imported above)
     
     # select all atoms connected by a sequence of bonds to
     # an already selected one
@@ -86,53 +80,6 @@ class ops_connected_Mixin:
             atom.pick()
             for a in atom.neighbors():
                  self.conncomp(a)
-
-##    # select all atoms connected by two disjoint sequences of bonds to
-##    # an already selected one. This picks stiff components but doesn't
-##    # cross single-bond or single-atom bearings or hinges
-##    # does select e.g. hydrogens connected to the component and nothing else
-##    # [bruce 050519 comment: this produces incorrect output, and has done so
-##    #  since at least Alpha1. And it's very slow for big parts.]
-##    def markdouble(self):
-##        self.father= {}
-##        self.stack = []
-##        self.out = []
-##        self.dfi={}
-##        self.p={}
-##        self.i=0
-##        for a in self.selatoms.values():
-##            if a not in self.dfi:
-##                self.father[a]=None
-##                self.blocks(a)
-##        for (a1,a2) in self.out[-1]:
-##            a1.pick()
-##            a2.pick()
-##        for mol in self.molecules:
-##            for a in mol.atoms.values():
-##                if len(a.bonds) == 1 and a.neighbors()[0].picked:
-##                    a.pick()
-##
-##    # compared to that, the doubly-connected components algo is hairy.
-##    # cf Gibbons: Algorithmic Graph Theory, Cambridge 1985
-##    def blocks(self, atom):
-##        self.dfi[atom]=self.i
-##        self.p[atom] = self.i
-##        self.i += 1
-##        for a2 in atom.neighbors():
-##            if atom.key < a2.key: pair = (atom, a2)
-##            else: pair = (a2, atom)
-##            if not pair in self.stack: self.stack += [pair]
-##            if a2 in self.dfi:
-##                if a2 != self.father[atom]:
-##                    self.p[atom] = min(self.p[atom], self.dfi[a2])
-##            else:
-##                self.father[a2] = atom
-##                self.blocks(a2)
-##                if self.p[a2] >= self.dfi[atom]:
-##                    pop = self.stack.index(pair)
-##                    self.out += [self.stack[pop:]]
-##                    self.stack = self.stack[:pop]
-##                self.p[atom] = min(self.p[atom], self.p[a2])
 
     pass # end of class ops_connected_Mixin
 
