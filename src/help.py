fc8ae930f025c7ae64c0b5a00dc32ad4e10f8721
Mark Sims <mark@nanorex.com>
2005-08-17 19:32:06 +0000

    Added Mac specific help file for Mouse Controls.  Also renamed HTML files keyboardaccelerators to keyboardshortcuts.

--- src/help.py
+++ src/help.py
@@ -46,45 +46,23 @@ class Help(HelpDialog):
     def _setup_mouse_controls_page(self):
         ''' Setup the Mouse Controls help page.
         '''
-        text = "<b>Mouse Controls</b>"\
-        "<p>"\
-        "Left Button does something according to mode, see below"\
-        "<p>"\
-        "Middle Button : Rotate View"\
-        "<p>"\
-        "Middle Button+Shift : Pan View"\
-        "<p>"\
-        "Middle Button+Cntl/Cmd : Zoom View (vertical mouse motion) / Turn (horizontal mouse motion)"\
-        "<p>"\
-        "Mouse Wheel : Zoom, half-speed with Shift, double-speed with Cntl/Cmd"\
-        "<p>"\
-        "Right Button : Context Menu"\
-        "<p>"\
-        "<b>Build Mode:</b><br>"\
-        "Left-Click deposits an atom or chunk from the clipboard in empty space or bonds it to an open bond.<br>"\
-        "Left-Drag on an atom moves the molecule.<br>"\
-        "Shift+Left-Drag moves atoms or open bonds.<br>"\
-        "Cntl+Left-Click deletes a highlighted atom.<br>"
-
-        #self.mouse_controls_textbrowser.setText(text)
-
         filePath = os.path.dirname(os.path.abspath(sys.argv[0]))
-        htmlDoc = os.path.normpath(filePath + '/../doc/mousecontrols.htm')
+        if sys.platform == 'darwin':
+            htmlDoc = os.path.normpath(filePath + '/../doc/mousecontrols-mac.htm')
+        else:
+            htmlDoc = os.path.normpath(filePath + '/../doc/mousecontrols.htm')
         self.mouse_controls_textbrowser.setSource(htmlDoc)
 
         
     def _setup_keyboard_shortcuts_page(self):
         ''' Setup the Keyboard Shortcuts help page.
         '''
-        import sys
-        #text = "Keyboard Shortcuts"
-        #self.keyboard_shortcuts_textbrowser.setText(text)
         filePath = os.path.dirname(os.path.abspath(sys.argv[0]))
         
         if sys.platform == 'darwin':
-            htmlDoc = os.path.normpath(filePath + '/../doc/keyboardaccelerators-mac.htm')
+            htmlDoc = os.path.normpath(filePath + '/../doc/keyboardshortcuts-mac.htm')
         else:
-            htmlDoc = os.path.normpath(filePath + '/../doc/keyboardaccelerators.htm')
+            htmlDoc = os.path.normpath(filePath + '/../doc/keyboardshortcuts.htm')
         self.keyboard_shortcuts_textbrowser.setSource(htmlDoc)
 
         
