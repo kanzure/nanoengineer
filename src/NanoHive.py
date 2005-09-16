742dc994eaa5251996344fcf5d0e3981578ec0c9
Mark Sims <mark@nanorex.com>
2005-09-16 05:06:52 +0000

    Added support for Nano-Hive

---
+++ src/NanoHive.py
@@ -0,0 +1,324 @@
+# Copyright (c) 2005 Nanorex, Inc.  All rights reserved.
+'''
+NanoHive.py
+
+$Id$
+
+History:
+
+Created by Mark.
+'''
+__author__ = "Mark"
+
+from qt import *
+from NanoHiveDialog import NanoHiveDialog
+from HistoryWidget import redmsg, greenmsg, orangemsg
+import env, os
+
+debug_sim = 0 # DO NOT COMMIT with 1
+
+cmd = greenmsg("Nano-Hive: ")
+
+
+# get_partname() should be a global function.  Mark 050915.
+def get_partname(assy):
+    'Returns the base name of the part.  If the filename is None, returns'
+    if assy.filename:
+        from movieMode import filesplit
+        junk, basename, ext = filesplit(assy.filename)
+        return basename
+    else:
+        return "Untitled"
+            
+
+class NanoHive(NanoHiveDialog):
+    '''The Nano-Hive dialog
+    '''
+       
+    def __init__(self, assy):
+        NanoHiveDialog.__init__(self)
+        self.assy = assy
+        self.part=assy.part
+        #print "NanoHive.__init__(): self.assy.filename=", self.assy.filename
+        
+        # This is where What's This descriptions should go for the Nano-Hive dialog.
+        # Mark 050831.
+        from whatsthis import create_whats_this_descriptions_for_NanoHive_dialog
+        create_whats_this_descriptions_for_NanoHive_dialog(self)
+        
+        return
+
+    def showDialog(self, assy):
+        '''Display the dialog '''
+        self.assy=assy
+        self.part=assy.part
+        
+        # Make sure there is something to simulate.
+        if not self.part.molecules: # Nothing in the part to simulate.
+            msg = redmsg("Nothing to simulate.")
+            env.history.message(cmd + msg)
+            return
+        
+        env.history.message(cmd + "Enter simulation parameters and select <b>Run Simulation.</b>")
+        
+        # Fill in the name_linedit widget.
+        self.name_linedit.setText(get_partname(self.assy))
+        
+        self.exec_loop()
+        return
+                
+    def accept(self):
+        '''The slot method for the 'OK' button.'''
+        QDialog.accept(self)
+        self.run_nanohive()
+        
+    def reject(self):
+        '''The slot method for the "Cancel" button.'''
+        QDialog.reject(self)
+
+    def run_nanohive(self):
+        
+        # Validate Nano-Hive Program
+        #r = self._validate_nanohive_program()
+        
+        #if r: # Nano-Hive program was not found/valid.
+         #   return 1 # Job Cancelled.
+        
+        mmp_filename, simspec_filename, simflow_filename, outdir, partname = self.get_nanohive_filenames()
+        
+        
+        
+        # 1. Write the MMP file that Nano-Hive will use for the sim run.
+        self.assy.writemmpfile(mmp_filename)
+            
+        # 2. Write the sim-spec file using the parameters from the Nano-Hive dialog/widgets
+        self.write_simspec_file(simspec_filename, simflow_filename, mmp_filename, outdir, partname)
+        
+        # 3. Write the Sim Flow file
+        self.write_simflow_file(simflow_filename)
+        
+        # 3. Execute Nano-Hive, to be written by Brian H.
+        
+        # Informative messages (temporary).
+        print "\n-------------------------------------"
+        print "Here are the locations of the files that Nano-Hive needs:"
+        print "MMP File: ", mmp_filename
+        print "SimSpec File: ", simspec_filename
+        print "SimFlow File: ", simflow_filename
+        print "Brian H. will write the code for generating the SimFlow file."
+        print "-------------------------------------\n"
+        
+        infotext = "-------------------------------------<br>"\
+                        "Here are the locations of the files that Nano-Hive needs:<br>"\
+                        "MMP File: " + mmp_filename + "<br>"\
+                        "SimSpec File: " + simspec_filename + "<br>"\
+                        "SimFlow File: " + simflow_filename + "<br>"\
+                        "Brian H. will write the code for generating the SimFlow file." + "<br>"\
+                        "-------------------------------------<br>"
+        
+        env.history.message(infotext)
+        
+
+    def get_nanohive_filenames(self):
+        '''Returns the file names for Nano-Hive in this order:
+            1. MMP filename (full path)
+            2. Simspec filename (full path)
+            3. Simflow filename (full path)
+            4. Nano-Hive directory where all the files are placed
+            5. Partname, which is the base name of the current part (assy).
+        '''
+        from platform import find_or_make_Nanorex_subdir
+        nhdir = find_or_make_Nanorex_subdir("Nano-Hive")
+        n = QString(self.name_linedit.text())
+        fn = n.stripWhiteSpace() # make sure suffix is not just whitespaces
+
+        if fn:
+            name = str(fn)
+            # Brian H. says that "name" cannot have any whitespace characters. 
+            # He suggests that we replace whitespace chars with underscores.
+            # It is late and I don't have time to look up the Python function for this.
+            # DON'T FORGET.  Mark 050915 at 11:45PM.  ZZZ.
+        else:
+            name = "Untitled"
+            
+        mmp_fn = os.path.join(nhdir,str(name)+".mmp")
+        simspec_fn = os.path.join(nhdir,str(name)+"_simspec.xml")
+        simflow_fn = os.path.join(nhdir,str(name)+"_simflow.tcl")
+        
+        return mmp_fn, simspec_fn, simflow_fn, nhdir, name
+        
+    def write_simspec_file(self, filename, simflow_filename, mmp_filename, outdir, partname):
+        '''Writes the Nano-Hive Sim Spec file, which is an XML file that describes the
+        simulation environment, plugin selection and plugin parameters.
+        '''
+        
+        # The Nano-Hive Sim Spec file has 4 different sections that must be written in the following order:
+        # 1. Header Section
+        # 2. Physical Interaction Plugin(s) Section
+        # 3. Results Plugin(s) Section
+        # 4. Footer Section.
+        # Mark 050915.
+        
+        iterations = self.nframes_spinbox.value() # Iterations = Frames
+        spf = self.stepsper_spinbox.value() * 1e-17 # Steps per Frame
+        temp = self.temp_spinbox.value() # Temp in Kelvin
+        
+        # This should be an env pref, set in the Prefs UI (like GAMESS).
+        nh_program = "C:/Program Files/Nano-Hive/bin/win32-x86/NanoHive.exe"
+        
+        # I get the Nano-Hive home directory by stripping off the last two directories.
+        head, tail = os.path.split(nh_program)
+        nh_home, tail = os.path.split(head)
+        
+        # print "NanoHive.write_simspec_file(): Nano-Hive Home:", nh_home
+        
+        f = open(filename,'w') # Open Nano-Hive Sim Spec file.
+        
+        # Write SimSpec header ########################################
+        
+        f.write ('<simulation>\n')
+        #f.write('    <description>%s</description>\n' % self.description_textedit.text())
+        f.write('    <description>nanoENGINEER-1</description>\n') # Temp description.
+        f.write('    <parameter name="timeQuantumLength" value="%e" />\n' % spf)
+        f.write('    <parameter name="environmentTemperature" value="%d" />\n' % temp)
+        f.write('    <parameter name="startIteration" value="0" />\n')
+        f.write('    <parameter name="iterations" value="%d" />\n' % iterations)
+        
+        f.write('        <simulationFlow file="%s">\n' % simflow_filename)
+        f.write('          <input type="nanorexMMP" file="%s" />\n' % mmp_filename)
+        
+        # This traverser is for the local machine.
+        f.write('          <traverser name="traverser" plugin="RC_Traverser" />\n')
+        
+        # The bond calculator
+        f.write('          <calculator name="bondCalculator" plugin="BondCalculator" />\n')
+        
+        # Physical Interaction Plugins ########################################
+        
+        if self.MPQC_ESP_checkbox.isChecked():
+            f.write('          <calculator name="qmCalculator" plugin="MPQC_SClib">\n')
+            f.write('          <parameter name="logDirectory" value="%s/log" />\n' % nh_home)
+            f.write('          <parameter name="dataDirectory" value="%s/data/MQPC_SClib" />\n' % nh_home)
+            f.write('          <parameter name="basis" value="STO-3G" />\n')
+            f.write('          <parameter name="method" value="HF" />\n')
+            f.write('          <parameter name="gradientDynamics" value="no" />\n')
+            f.write('          <parameter name="outputType" value="ESPplane" />\n')
+            f.write('          <parameter name="resolution" value="20" />\n')
+            f.write('          <parameter name="centerPoint" value="0e-10,0.3e-10, 0e-10" />\n')
+            f.write('          <parameter name="normalPoint" value="0e-10 1.3e-10 0e-10" />\n')
+            f.write('          <parameter name="outputLength" value="10.0e-10" />\n')
+            f.write('          <parameter name="cutoffHeight" value="1.0e-10" />\n')
+            f.write('          <parameter name="cutoffWidth" value="0.5e-10" />\n')
+            f.write('          </calculator>\n')
+            
+        if self.MPQC_GD_checkbox.isChecked():
+            f.write('          <calculator name="qmDynamicsInteraction" plugin="MPQC_SClib">\n')
+            f.write('          <parameter name="logDirectory" value="%s/log" />\n' % nh_home)
+            f.write('          <parameter name="dataDirectory" value="%s/data/MQPC_SClib" />\n' % nh_home)
+            f.write('          <parameter name="basis" value="STO-3G" />\n')
+            f.write('          <parameter name="method" value="HF" />\n')
+            f.write('          <parameter name="gradientDynamics" value="yes" />\n')
+            f.write('          <parameter name="deltaTbyTau" value="1.0" />\n')
+            f.write('          </calculator>\n')
+                
+        if self.AIREBO_checkbox.isChecked():
+            f.write('          <calculator name="physicalInteraction" plugin="REBO_MBM">\n')
+            f.write('              <parameter name="dataDirectory" value="%s/data/REBO_MBM" />\n' % nh_home)
+            f.write('          </calculator>\n')
+
+        # Results Plugins ########################################
+        
+        if self.Measurements_to_File_checkbox.isChecked():
+            f.write('          <result name="MeasurementSetToFile" plugin="MeasurementSetToFile">\n')
+            f.write('            <parameter name="outputInterval" value="1" />\n')
+            f.write('            <parameter name="outputFile"\n')
+            f.write('                       value="%s/data.txt" />\n' % outdir)
+            f.write('            <parameter name="datumSeparator" value="\t" />\n')
+            f.write('          </result>\n')
+
+        if self.POVRayVideo_checkbox.isChecked():
+            # Need subdirectory for all the POV-Ray pov files.
+            # Also need to add lighting, scene setup, background color, etc.
+            
+            f.write('          <result name="POVRayVideo" plugin="POVRayVideo">\n')
+            f.write('          <parameter name="outputInterval" value="1" />\n')
+            f.write('          <parameter name="lengthMultiplier" value="1e10" />\n')
+            f.write('          <parameter name="outputDirectory"\n')
+            f.write('             value="%s/povray" />\n' % outdir)
+            f.write('          <parameter name="outputIdentifier" value="%s" />\n' % partname)
+            # POV-Ray template.  Mark needs to write the include file for lighting and camera angle(s)
+            # and put that file in the appropriate place (outdir/povray).
+            f.write('          <parameter name="povTemplateFilename"\n')
+            f.write('            value="%s/data/pov.tmplt" />')
+            f.write('          <parameter name="mpegTemplateFilename"\n')
+            f.write('            value="%s/data/mpeg_encode.param.tmplt" />\n' % nh_home)
+            f.write('          </result>\n')
+
+        # NetCDF Plugin
+        
+        f.write('          <result name="simResult" plugin="NetCDF_DataSet">\n')
+        f.write('              <parameter name="outputInterval" value="1" />\n')
+        f.write('              <parameter name="outputDirectory"\n')
+        f.write('                       value="%s" />\n' % outdir)
+        f.write('              <parameter name="maxDataSets" value="-1" />\n')
+        f.write('          </result>\n')
+        
+        # Footer
+        
+        f.write('      </simulationFlow>\n')
+        f.write('</simulation>\n')
+        
+        f.close()
+        
+    def write_simflow_file(self, filename):
+        '''Writes the Nano-Hive Sim Flow file, which is a TCL script used by Nano-Hive to
+        run the simulation.  It describes the workflow of the simulation.
+        '''
+        # Brian H. to code this method.
+        pass
+        
+
+# This is not implemented yet.         
+    def _validate_nanohive_program(self):
+        '''Private method:
+        Checks that the Nano-Hive program path exists in the user pref database
+        and that the file it points to exists.  If the Nano-Hive path does not exist, the
+        user will be prompted with the file chooser to select the Nano-Hive executable.
+        This function does not check whether the Nano-Hive path is actually Nano-Hive.
+        Returns:  0 = Valid
+                        1 = Invalid
+        '''
+        # Get Nano-Hive executable path from the user preferences
+        prefs = preferences.prefs_context()
+        self.server.program = prefs.get(nanohive_prefs_key)
+        
+        if not self.server.program:
+            msg = "The Nano-Hive executable path is not set.\n"
+        elif os.path.exists(self.server.program):
+            return 0
+        else:
+            msg = self.server.program + " does not exist.\n"
+            
+        # Nano-Hive Dialog is the parent for messagebox and file chooser.
+        parent = self.edit_cntl # THIS IS WRONG.
+            
+        ret = QMessageBox.warning( parent, "Nano-Hive Executable Path",
+            msg + "Please select OK to set the location of Nano-Hive for this computer.",
+            "&OK", "Cancel", None,
+            0, 1 )
+                
+        if ret==0: # OK
+            #from UserPrefs import get_gamess_path
+            #self.server.program = get_gamess_path(parent)
+            from UserPrefs import get_filename_and_save_in_prefs
+            self.server.program = \
+                get_filename_and_save_in_prefs(parent, gmspath_prefs_key, 'Choose Nano-Hive Executable')
+            if not self.server.program:
+                return 1 # Cancelled from file chooser.
+            
+        elif ret==1: # Cancel
+            return 1
+
+        return 0
+
+# end
---
+++ src/NanoHiveDialog.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\atom\cad\src\NanoHiveDialog.ui'
+#
+# Created: Wed Sep 14 14:58:17 2005
+#      by: The PyQt User Interface Compiler (pyuic) 3.12
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from qt import *
+
+image0_data = \
+    "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a\x00\x00\x00\x0d" \
+    "\x49\x48\x44\x52\x00\x00\x00\x14\x00\x00\x00\x14" \
+    "\x08\x06\x00\x00\x00\x8d\x89\x1d\x0d\x00\x00\x02" \
+    "\xd2\x49\x44\x41\x54\x78\x9c\x85\x94\xcd\x6b\x13" \
+    "\x51\x14\xc5\xcf\x7b\x93\x0f\x11\xb4\x0a\x7e\xd4" \
+    "\xd6\xb4\xb5\x8d\x28\xd3\xf8\x81\xff\x80\xe0\x3f" \
+    "\x60\xd1\x85\x1b\x51\xaa\x1b\x57\x0a\x2e\xfc\xd8" \
+    "\x48\x0b\x52\x50\x70\xe7\xa2\xb6\x9a\x60\x5d\x29" \
+    "\x98\xb4\x5d\xab\x20\x8a\x3b\x5d\x69\xd3\x2e\x9c" \
+    "\x24\x1a\x5b\xdb\x0a\x8d\xd5\x2a\x4e\xf3\xee\x71" \
+    "\x51\x7d\xc9\x74\x1a\x7a\x61\xe0\xcd\x70\xee\x7b" \
+    "\xf7\x77\xe6\xcc\x00\x75\x55\x90\x29\x1e\xaa\xc6" \
+    "\x79\xd0\xc4\xf8\x42\xc6\x89\x06\xf5\x9c\xa3\x74" \
+    "\x09\x1e\x30\x11\x16\x64\x2a\xa0\xd3\xff\x17\x5f" \
+    "\xa4\xc8\xd9\xe5\x69\xf8\x9f\x88\x5f\x9f\x7c\xcc" \
+    "\xff\xfe\x8a\x32\x0b\xf4\xe9\xdb\x06\x9f\x3e\xcb" \
+    "\x2c\xf0\xdb\xaf\x39\xf8\x45\xa0\x5a\xd2\x98\x5d" \
+    "\x9e\x46\x99\x85\xf0\xe1\x29\x71\xd8\x95\x07\x81" \
+    "\x95\x2b\x31\x0a\xba\x04\x3d\xe6\xad\xd8\x63\x9e" \
+    "\x2e\xc1\x44\x4e\x59\x5d\x57\x7e\x45\x17\x9a\x50" \
+    "\x94\x59\x13\xef\x3d\xdf\x62\x5e\x66\x56\x1a\xa8" \
+    "\xa0\xa8\xe1\xd0\x69\xe4\x46\xad\xb2\xcc\x70\xb0" \
+    "\x32\xc0\x96\x0c\xd8\x92\x01\xaf\x94\x7a\xe9\x12" \
+    "\x4c\x89\xc3\xac\xa4\x09\x00\x3f\xa4\xc2\x31\x33" \
+    "\xc2\x9b\xc5\xcb\x56\x37\x58\x19\x60\x96\x99\x30" \
+    "\xb2\x4b\x04\x90\x5b\x87\x1d\xbb\x6e\x1f\x8e\x33" \
+    "\x65\x22\x7c\xca\xfb\x2b\xc8\xa3\x58\x1f\xd9\x91" \
+    "\x48\xc3\xe9\x97\xd5\x1f\x88\xaa\x36\xc6\xab\x9b" \
+    "\xcf\x6e\xa8\x19\x41\x54\x36\x34\xd6\xab\xc6\xfb" \
+    "\x45\x18\xb3\x6b\x2b\x1b\x33\x23\x9c\x5b\x9c\x45" \
+    "\x5f\xee\x2a\xa8\x04\xa7\x8f\x9e\x43\x16\x69\xf4" \
+    "\x2c\xf6\xe2\x49\xe9\x21\xf4\x26\xe2\xc6\x91\x3b" \
+    "\xd8\xd2\xb4\x15\x13\xa5\xf7\x18\x7e\x7e\x17\x0a" \
+    "\x1a\xd7\x4f\xf4\x61\x67\x53\x33\x7a\xf4\xd9\xe0" \
+    "\x91\x29\xe3\x70\xdf\x44\xb4\x16\x9b\x9c\xa2\x4b" \
+    "\xb0\xe3\x75\xcd\xaf\x8e\x57\xa0\x6b\x54\x28\x36" \
+    "\xdd\xa2\x2d\xb4\x35\x4e\xb4\x41\xb5\x41\x74\x82" \
+    "\xfc\x0a\x8a\xd6\x08\x44\x18\x85\x82\x84\x91\x73" \
+    "\x92\xe1\xdc\xe2\x2c\xfa\x73\xd7\x00\x00\x17\x8e" \
+    "\x5d\x42\xaa\xed\x30\x2a\xdf\x17\xd0\xff\xee\x32" \
+    "\xe4\x27\x70\xaa\xed\x1c\x46\x37\xa7\x71\x12\xe7" \
+    "\xf1\xe8\xe5\x03\x00\x40\x5f\xcf\x6d\xec\x6c\x6a" \
+    "\xc6\x71\x7d\x26\x88\xdc\x2d\x9a\xfb\x27\x36\x58" \
+    "\x94\x3d\xd9\x18\x5d\x82\x59\xc9\x30\x65\x1c\xee" \
+    "\xba\xbf\x76\xa4\xba\xf2\xe0\x41\x13\x0b\xc7\x86" \
+    "\x10\x54\xf1\xc7\x1e\x50\x55\xbe\x7d\x4e\x25\x81" \
+    "\x97\xcc\x55\xd6\xb0\x0e\xd9\x6e\xa8\xa8\x11\x63" \
+    "\x2d\x36\x51\xc6\xff\x79\xa2\x11\x91\xb8\xbd\xaf" \
+    "\xb9\x57\x2b\xa3\xd7\xc8\x68\x96\x19\xde\x5b\xb8" \
+    "\xc5\xf6\x74\x9c\x2d\x19\xb0\xbf\x74\x91\x63\x66" \
+    "\x84\x3f\xa4\x42\x00\xc8\x4a\x9a\x29\xe3\xb0\xdb" \
+    "\x68\x5e\x2d\x9e\x67\xeb\x03\x67\x9d\x4f\x4f\xc0" \
+    "\xe4\x07\x6d\xbd\x69\xcb\x69\x76\x8b\xa6\x27\x93" \
+    "\x04\x80\x79\x99\xe1\xb8\x3c\x0a\xfd\x6d\x92\x1f" \
+    "\x34\x5d\x41\x38\x36\xff\xb8\xeb\xb0\x08\xda\x3e" \
+    "\x60\xbb\xde\xa5\x3c\x99\xa4\x23\x51\x00\xd5\x40" \
+    "\x8f\xa6\x03\x60\x55\xe4\x3e\xd3\xe3\x1b\xff\x19" \
+    "\xf7\x7e\x54\x4c\x7a\xe0\xe3\xa5\x21\x7e\xa6\x17" \
+    "\xfc\xc1\x8a\xcf\xb2\x14\xf8\x78\x69\x88\x49\x0f" \
+    "\x4c\x7a\xe0\x1b\xff\x19\xcb\x52\x08\x4f\x98\x50" \
+    "\x9d\xca\x93\x49\xc6\x3b\x34\x44\x19\x6c\x53\x3b" \
+    "\x90\x50\x9d\x81\x6c\xc5\x74\x4c\x01\xc0\x0b\x19" \
+    "\xe7\xc6\xf6\x18\x04\x06\xcd\xba\x15\xbb\xd5\x1e" \
+    "\xab\xfb\x0b\xbe\x4b\xc9\x96\x7b\xd3\x32\x16\x00" \
+    "\x00\x00\x00\x49\x45\x4e\x44\xae\x42\x60\x82"
+
+class NanoHiveDialog(QDialog):
+    def __init__(self,parent = None,name = None,modal = 0,fl = 0):
+        QDialog.__init__(self,parent,name,modal,fl)
+
+        self.image0 = QPixmap()
+        self.image0.loadFromData(image0_data,"PNG")
+        if not name:
+            self.setName("NanoHiveDialog")
+
+        self.setIcon(self.image0)
+        self.setModal(1)
+
+        NanoHiveDialogLayout = QGridLayout(self,1,1,11,21,"NanoHiveDialogLayout")
+
+        layout21 = QHBoxLayout(None,0,6,"layout21")
+
+        self.namelbl = QLabel(self,"namelbl")
+        self.namelbl.setAlignment(QLabel.AlignVCenter | QLabel.AlignRight)
+        layout21.addWidget(self.namelbl)
+
+        self.name_linedit = QLineEdit(self,"name_linedit")
+        self.name_linedit.setFrameShape(QLineEdit.LineEditPanel)
+        self.name_linedit.setFrameShadow(QLineEdit.Sunken)
+        layout21.addWidget(self.name_linedit)
+
+        NanoHiveDialogLayout.addLayout(layout21,0,0)
+
+        self.parms_grpbox = QGroupBox(self,"parms_grpbox")
+        self.parms_grpbox.setColumnLayout(0,Qt.Vertical)
+        self.parms_grpbox.layout().setSpacing(6)
+        self.parms_grpbox.layout().setMargin(11)
+        parms_grpboxLayout = QGridLayout(self.parms_grpbox.layout())
+        parms_grpboxLayout.setAlignment(Qt.AlignTop)
+
+        layout27 = QHBoxLayout(None,0,6,"layout27")
+
+        layout24 = QVBoxLayout(None,0,6,"layout24")
+
+        self.textLabel5 = QLabel(self.parms_grpbox,"textLabel5")
+        self.textLabel5.setAlignment(QLabel.AlignVCenter | QLabel.AlignRight)
+        layout24.addWidget(self.textLabel5)
+
+        self.textLabel2 = QLabel(self.parms_grpbox,"textLabel2")
+        self.textLabel2.setAlignment(QLabel.AlignVCenter | QLabel.AlignRight)
+        layout24.addWidget(self.textLabel2)
+
+        self.textLabel3 = QLabel(self.parms_grpbox,"textLabel3")
+        self.textLabel3.setAlignment(QLabel.AlignVCenter | QLabel.AlignRight)
+        layout24.addWidget(self.textLabel3)
+        layout27.addLayout(layout24)
+
+        layout25 = QVBoxLayout(None,0,6,"layout25")
+
+        self.nframes_spinbox = QSpinBox(self.parms_grpbox,"nframes_spinbox")
+        self.nframes_spinbox.setMaxValue(90000)
+        self.nframes_spinbox.setMinValue(1)
+        self.nframes_spinbox.setLineStep(15)
+        self.nframes_spinbox.setValue(900)
+        layout25.addWidget(self.nframes_spinbox)
+
+        self.stepsper_spinbox = QSpinBox(self.parms_grpbox,"stepsper_spinbox")
+        self.stepsper_spinbox.setMaxValue(99999)
+        self.stepsper_spinbox.setMinValue(1)
+        self.stepsper_spinbox.setValue(10)
+        layout25.addWidget(self.stepsper_spinbox)
+
+        self.temp_spinbox = QSpinBox(self.parms_grpbox,"temp_spinbox")
+        self.temp_spinbox.setMaxValue(99999)
+        self.temp_spinbox.setValue(300)
+        layout25.addWidget(self.temp_spinbox)
+        layout27.addLayout(layout25)
+
+        layout26 = QVBoxLayout(None,0,6,"layout26")
+        spacer4 = QSpacerItem(255,20,QSizePolicy.Expanding,QSizePolicy.Minimum)
+        layout26.addItem(spacer4)
+
+        self.textLabel2_2 = QLabel(self.parms_grpbox,"textLabel2_2")
+        layout26.addWidget(self.textLabel2_2)
+
+        self.textLabel3_2 = QLabel(self.parms_grpbox,"textLabel3_2")
+        layout26.addWidget(self.textLabel3_2)
+        layout27.addLayout(layout26)
+
+        parms_grpboxLayout.addLayout(layout27,0,0)
+
+        NanoHiveDialogLayout.addWidget(self.parms_grpbox,1,0)
+
+        self.buttonGroup1 = QButtonGroup(self,"buttonGroup1")
+        self.buttonGroup1.setColumnLayout(0,Qt.Vertical)
+        self.buttonGroup1.layout().setSpacing(6)
+        self.buttonGroup1.layout().setMargin(11)
+        buttonGroup1Layout = QVBoxLayout(self.buttonGroup1.layout())
+        buttonGroup1Layout.setAlignment(Qt.AlignTop)
+
+        self.MPQC_ESP_checkbox = QCheckBox(self.buttonGroup1,"MPQC_ESP_checkbox")
+        buttonGroup1Layout.addWidget(self.MPQC_ESP_checkbox)
+
+        self.MPQC_GD_checkbox = QCheckBox(self.buttonGroup1,"MPQC_GD_checkbox")
+        buttonGroup1Layout.addWidget(self.MPQC_GD_checkbox)
+
+        self.AIREBO_checkbox = QCheckBox(self.buttonGroup1,"AIREBO_checkbox")
+        buttonGroup1Layout.addWidget(self.AIREBO_checkbox)
+
+        NanoHiveDialogLayout.addWidget(self.buttonGroup1,2,0)
+
+        self.buttonGroup1_2 = QButtonGroup(self,"buttonGroup1_2")
+        self.buttonGroup1_2.setColumnLayout(0,Qt.Vertical)
+        self.buttonGroup1_2.layout().setSpacing(6)
+        self.buttonGroup1_2.layout().setMargin(11)
+        buttonGroup1_2Layout = QVBoxLayout(self.buttonGroup1_2.layout())
+        buttonGroup1_2Layout.setAlignment(Qt.AlignTop)
+
+        self.Measurements_to_File_checkbox = QCheckBox(self.buttonGroup1_2,"Measurements_to_File_checkbox")
+        buttonGroup1_2Layout.addWidget(self.Measurements_to_File_checkbox)
+
+        self.POVRayVideo_checkbox = QCheckBox(self.buttonGroup1_2,"POVRayVideo_checkbox")
+        buttonGroup1_2Layout.addWidget(self.POVRayVideo_checkbox)
+
+        NanoHiveDialogLayout.addWidget(self.buttonGroup1_2,3,0)
+
+        self.server_grpbox = QGroupBox(self,"server_grpbox")
+        self.server_grpbox.setColumnLayout(0,Qt.Vertical)
+        self.server_grpbox.layout().setSpacing(6)
+        self.server_grpbox.layout().setMargin(11)
+        server_grpboxLayout = QGridLayout(self.server_grpbox.layout())
+        server_grpboxLayout.setAlignment(Qt.AlignTop)
+
+        self.server_combox = QComboBox(0,self.server_grpbox,"server_combox")
+
+        server_grpboxLayout.addWidget(self.server_combox,0,0)
+        spacer5 = QSpacerItem(40,20,QSizePolicy.Expanding,QSizePolicy.Minimum)
+        server_grpboxLayout.addItem(spacer5,0,1)
+
+        NanoHiveDialogLayout.addWidget(self.server_grpbox,4,0)
+        spacer6 = QSpacerItem(20,16,QSizePolicy.Minimum,QSizePolicy.Expanding)
+        NanoHiveDialogLayout.addItem(spacer6,5,0)
+
+        layout28 = QHBoxLayout(None,0,6,"layout28")
+
+        self.run_sim_btn = QPushButton(self,"run_sim_btn")
+        self.run_sim_btn.setDefault(1)
+        layout28.addWidget(self.run_sim_btn)
+
+        self.cancel_btn = QPushButton(self,"cancel_btn")
+        self.cancel_btn.setDefault(0)
+        layout28.addWidget(self.cancel_btn)
+
+        NanoHiveDialogLayout.addLayout(layout28,6,0)
+
+        self.languageChange()
+
+        self.resize(QSize(333,561).expandedTo(self.minimumSizeHint()))
+        self.clearWState(Qt.WState_Polished)
+
+        self.connect(self.run_sim_btn,SIGNAL("clicked()"),self,SLOT("accept()"))
+        self.connect(self.cancel_btn,SIGNAL("clicked()"),self,SLOT("reject()"))
+
+
+    def languageChange(self):
+        self.setCaption(self.__tr("Nano-Hive Setup"))
+        self.namelbl.setText(self.__tr("Name :"))
+        self.name_linedit.setText(QString.null)
+        self.parms_grpbox.setTitle(self.__tr("Parameters"))
+        self.textLabel5.setText(self.__tr("Total Frames:"))
+        self.textLabel2.setText(self.__tr("Steps per Frame :"))
+        self.textLabel3.setText(self.__tr("Temperature :"))
+        self.textLabel2_2.setText(self.__tr("0.1 femtosecond"))
+        self.textLabel3_2.setText(self.__tr("Kelvin"))
+        self.buttonGroup1.setTitle(self.__tr("Physical Interaction Plugins"))
+        self.MPQC_ESP_checkbox.setText(self.__tr("MPQC - Electrostatic Potential Plane"))
+        self.MPQC_GD_checkbox.setText(self.__tr("MPQC - Gradient Dynamics"))
+        self.AIREBO_checkbox.setText(self.__tr("AIREBO"))
+        self.buttonGroup1_2.setTitle(self.__tr("Results Plugins"))
+        self.Measurements_to_File_checkbox.setText(self.__tr("Measurement Set to File"))
+        self.POVRayVideo_checkbox.setText(self.__tr("POV-Ray Video"))
+        self.server_grpbox.setTitle(self.__tr("Server"))
+        self.server_combox.clear()
+        self.server_combox.insertItem(self.__tr("My Computer"))
+        self.run_sim_btn.setText(self.__tr("Run Simulation"))
+        self.cancel_btn.setText(self.__tr("Cancel"))
+
+
+    def __tr(self,s,c = None):
+        return qApp.translate("NanoHiveDialog",s,c)
