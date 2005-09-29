520655b0839e9d192e34605d00ae60ec9bf13e2b
Mark Sims <mark@nanorex.com>
2005-09-29 14:24:13 +0000

    Cleaned up code and fixed bug 1014.

--- src/ESPWindowProp.py
+++ src/ESPWindowProp.py
@@ -8,83 +8,94 @@ $Id$
 from qt import *
 from ESPWindowPropDialog import *
 from widgets import RGBf_to_QColor, QColor_to_RGBf
-from constants import SELWHAT_ATOMS
+import copy
+#from constants import SELWHAT_ATOMS
+
 
 class ESPWindowProp(ESPWindowPropDialog):
     def __init__(self, esp_window, glpane):
 
         ESPWindowPropDialog.__init__(self)
-        self.esp_window = esp_window
+        self.jig = esp_window
         self.glpane = glpane
         self.setup()
         
         ##Store the original selection state:
         #self.sel_what = glpane.assy.selwhat
         #glpane.assy.selwhat = SELWHAT_ATOMS #
+        # Note to Huaicai.  I think you put this code above in.  Please sign your name to it and
+        # be sure to remove it (and the import of SELWHAT_ATOMS) if you decide this is not needed.
+        # Mark 050929.
     
     
     def setup(self):
-        self.oldNormColor = self.esp_window.normcolor # Border color
-        self.oldFillColor = self.esp_window.fill_color
         
-        self.name_linedit.setText(self.esp_window.name)
+        # Need to ask Bruce about the pros/cons of copying the jig here.  
+        # It sure would be nice to use the copy of the jig in self.reject() like this:
+        #     self.jig = self.original_jig
+        # so I didn't have to copy each attr back when the user hit the Cancel button.  Mark 050929.
+        self.original_jig = copy.copy(self.jig) 
         
-        self.fill_color = RGBf_to_QColor(self.esp_window.fill_color)
-        self.border_color = RGBf_to_QColor(self.esp_window.normcolor)
+        # Jig color
+        # self.original_normcolor = self.jig.normcolor
+        self.fill_QColor = RGBf_to_QColor(self.jig.fill_color) # Used as default color by Color Chooser
+        self.border_QColor = RGBf_to_QColor(self.jig.normcolor) # Used as default color by Color Chooser
+        self.fill_color_pixmap.setPaletteBackgroundColor(self.fill_QColor)
+        self.border_color_pixmap.setPaletteBackgroundColor(self.border_QColor)
         
-        self.width_spinbox.setValue(self.esp_window.width)
-        self.window_offset_spinbox.setValue(self.esp_window.window_offset)
-        self.edge_offset_spinbox.setValue(self.esp_window.edge_offset)
-        self.resolution_spinbox.setValue(self.esp_window.resolution)
+        self.name_linedit.setText(self.jig.name)
+        self.width_spinbox.setValue(self.jig.width)
+        self.window_offset_spinbox.setValue(self.jig.window_offset)
+        self.edge_offset_spinbox.setValue(self.jig.edge_offset)
+        self.resolution_spinbox.setValue(self.jig.resolution)
         
-        self.fill_color_pixmap.setPaletteBackgroundColor(self.fill_color)
-        self.border_color_pixmap.setPaletteBackgroundColor(self.border_color)
-        
-        self.show_esp_bbox_checkbox.setChecked(self.esp_window.show_esp_bbox)
-        self.opacity_spinbox.setValue(int(self.esp_window.opacity*255))
+        self.show_esp_bbox_checkbox.setChecked(self.jig.show_esp_bbox)
+        self.opacity_spinbox.setValue(int(self.jig.opacity*255))
         
         
     def change_fill_color(self):
         '''Slot method to change fill color.'''
-        color = QColorDialog.getColor(self.fill_color, self, "ColorDialog")
+        color = QColorDialog.getColor(self.fill_QColor, self, "ColorDialog")
 
         if color.isValid():
             self.fill_color_pixmap.setPaletteBackgroundColor(color)
-            self.esp_window.fill_color = QColor_to_RGBf(color)
+            self.fill_QColor = color
+            self.jig.fill_color = QColor_to_RGBf(color)
             self.glpane.gl_update()
             
     def change_border_color(self):
         '''Slot method change border color.'''
-        color = QColorDialog.getColor(self.border_color, self, "ColorDialog")
+        color = QColorDialog.getColor(self.border_QColor, self, "ColorDialog")
 
         if color.isValid():
             self.border_color_pixmap.setPaletteBackgroundColor(color)
-            self.esp_window.color = self.esp_window.normcolor = QColor_to_RGBf(color)
+            self.border_QColor = color
+            self.jig.color = self.jig.normcolor = QColor_to_RGBf(color)
             self.glpane.gl_update()
             
     def change_width(self, val):
         "Slot for Width spinbox"
-        self.esp_window.width = float(val)
+        self.jig.width = float(val)
         self.glpane.gl_update()
         
     def change_window_offset(self, val):
         "Slot for Width spinbox"
-        self.esp_window.window_offset = float(val)
+        self.jig.window_offset = float(val)
         self.glpane.gl_update()
         
     def change_edge_offset(self, val):
         "Slot for Width spinbox"
-        self.esp_window.edge_offset = float(val)
+        self.jig.edge_offset = float(val)
         self.glpane.gl_update()
         
     def show_esp_bbox(self, val):
         "Slot for Show ESP Window Volume checkbox"
-        self.esp_window.show_esp_bbox = val
+        self.jig.show_esp_bbox = val
         self.glpane.gl_update()
     
     def opacityChanged(self, val):
         '''Slot for opacity spin box '''
-        self.esp_window.opacity = val/255.0
+        self.jig.opacity = val/255.0
         self.glpane.gl_update()
         
     def highlight_atoms_in_bbox(self, val):
@@ -94,34 +105,43 @@ class ESPWindowProp(ESPWindowPropDialog):
         
     def select_atoms_inside_esp_bbox(self):
         "Slot for Select Atoms Inside Volume button, which selects all the atoms inside the bbox"
-        self.esp_window.selectAtoms()
+        self.jig.selectAtoms()
         self.glpane.gl_update()
         
         
     def accept(self):
         '''Slot for the 'OK' button '''
-        self.esp_window.cancelled = False   
+        self.jig.cancelled = False   
         
         text =  QString(self.name_linedit.text())        
         text = text.stripWhiteSpace() # make sure name is not just whitespaces
-        if text: self.esp_window.name = str(text) 
+        if text: self.jig.name = str(text) 
         
-        self.esp_window.width = float(self.width_spinbox.value())
-        self.esp_window.resolution = float(self.resolution_spinbox.value())
+        self.jig.width = float(self.width_spinbox.value())
+        self.jig.resolution = float(self.resolution_spinbox.value())
         
         #self.glpane.assy.selwhat = self.sel_what
         
-        self.esp_window.assy.w.win_update() # Update model tree
-        self.esp_window.assy.changed()
+        self.jig.assy.w.win_update() # Update model tree
+        self.jig.assy.changed()
         
         QDialog.accept(self)
         
 
     def reject(self):
         '''Slot for the 'Cancel' button '''
-        self.esp_window.fill_color = self.oldFillColor 
-        self.esp_window.color = self.esp_window.normcolor = self.oldNormColor   # Border color
- 
+        
+        # Restore the original attribute values from the copied jig.
+        self.jig.color = self.jig.normcolor = self.original_jig.normcolor # Border color
+        self.jig.fill_color = self.original_jig.fill_color
+        self.jig.name = self.original_jig.name
+        self.jig.width = self.original_jig.width
+        self.jig.window_offset = self.original_jig.window_offset
+        self.jig.edge_offset = self.original_jig.edge_offset
+        self.jig.resolution = self.original_jig.resolution
+        self.jig.show_esp_bbox = self.original_jig.show_esp_bbox
+        self.jig.opacity = self.original_jig.opacity
+
         #self.glpane.assy.selwhat = self.sel_what
         self.glpane.gl_update()
         
