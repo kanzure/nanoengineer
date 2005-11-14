1c996ff206ac6543fe7a69a752fc7a5032e52361
Will Ware <wware@alum.mit.edu>
2005-11-14 12:15:05 +0000

    Clean up and simplify.

--- src/version.py
+++ src/version.py
@@ -4,33 +4,37 @@ and other stuff like author list, program name, release info, etc.
 Example usage:
 from version import Version
 v = Version()
-print v
-print v.product
-print v.authors
+print v, v.product, v.authors
 """
 
-# Use Alex Martelli's singleton recipe
-# http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/66531
+# Alphabetical by last name
+__author__ = """Josh Hall
+Eric Messick
+Huaicai Mo
+Ninad Sathaye
+Mark Sims
+Bruce Smith
+Will Ware"""
 
 class Version:
     # Every instance of Version will share the same state
-    __shared_state = {}
-    def __init__(self, major=None, minor=None,
-                 tiny=None, teensy=None):
+    __shared_state = {
+        "major": 0,
+        "minor": 0,
+        "tiny": 6,
+        "teensy": 7,   # the teensy attribute is optional
+        "releaseType": "Alpha",
+        "releaseDate": "January 1, 2025",
+        "product": "nanoENGINEER-1",
+        "copyright": "Copyright (C) 2004-2005, Nanorex, Inc.",
+        "authors": __author__
+        }
+    def __init__(self):
+        # Use Alex Martelli's singleton recipe
+        # http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/66531
         self.__dict__ = self.__shared_state
-        if major != None:
-            self.__shared_state["major"] = major
-            self.__shared_state["minor"] = minor
-            self.__shared_state["tiny"] = tiny
-            if teensy != None:
-                self.__shared_state["teensy"] = teensy
-    def __setattr__(self, attr, value):
-        # attributes can be write-protected
-        if self.__shared_state.has_key("write_protect"):
-            raise AttributeError, attr
-        self.__shared_state[attr] = value
-    def writeProtect(self):
-        self.__shared_state["write_protect"] = True
+    def __setattr__(self, attr, value):  # attributes write-protected
+        raise AttributeError, attr
     def __repr__(self):
         major = self.__shared_state["major"]
         minor = self.__shared_state["minor"]
@@ -41,30 +45,11 @@ class Version:
             str += ".%d" % teensy
         return str
 
-v = Version(0, 0, 6, 7)
-v.releaseType = "Alpha"
-v.releaseDate = "January 1, 2025"
-
-v.product = "nanoENGINEER-1"
-v.copyright = "Copyright (C) 2004-2005, Nanorex, Inc."
-
-# Last names in alphabetical order
-v.authors = """Josh Hall
-Eric Messick
-Huaicai Mo
-Ninad Sathaye
-Mark Sims
-Bruce Smith
-Will Ware"""
-
-#v.writeProtect()
-v.write_protect = True
-del v   # leave the namespace uncluttered
-
 ###############################
 
 if __name__ == "__main__":
     v = Version()
+    print v
     for x in dir(v):
         print x + ":", getattr(v, x)
         print
