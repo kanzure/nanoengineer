8903f519a1e1dbe6e38a7d0bd401910973f5be99
Will Ware <wware@alum.mit.edu>
2005-11-21 18:23:48 +0000

    More documentation, and if no XYZ file is given, use the position info from the MMP file.

--- src/tests/scripts/findterms.py
+++ src/tests/scripts/findterms.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python
 
-"""This script examines the structure in an MMP file to enumerate
-all the bond length terms and bond angle terms for the structure.
+"""This script examines the structure in an MMP file to enumerate all
+the bond length terms and bond angle terms for the structure. If an
+XYZ file is given, use the position information from the XYZ file,
+otherwise use position information from the MMP file. Bond lengths are
+in angstroms, bond angles are in degrees.
 
 Usage:
+    findterms.py input.mmp > output.txt
     findterms.py input1.mmp input2.xyz > output.txt
+
+Example output:
+LENGTH 0 1 1.53077692692
+LENGTH 0 3 1.09477166569
+....
+ANGLE 0 1 2 112.961986081
+ANGLE 0 1 6 109.491950915
+....
 
 $Id$
 """
@@ -70,12 +82,23 @@ def measureAngle(xyz, first, second, third):
 
 def main(argv):
     mmpInputFile = argv[0]
-    xyzInputFile = argv[1]
-
-    xyz = XyzFile()
-    xyz.read(xyzInputFile)
     mmp = MmpFile()
     mmp.read(mmpInputFile)
+
+    xyz = XyzFile()
+    if len(argv) > 1:
+        xyzInputFile = argv[1]
+        xyz.read(xyzInputFile)
+    else:
+        # copy xyz file from mmp file
+        import Atom
+        for i in range(len(mmp)):
+            ma = mmp.atoms[i]
+            element = Atom._PeriodicTable[ma.elem]
+            x, y, z = ma.x, ma.y, ma.z
+            a = Atom.Atom()
+            a.fromXyz(element, x, y, z)
+            xyz.atoms.append(a)
 
     assert len(xyz) != 0
     assert len(xyz) == len(mmp)
