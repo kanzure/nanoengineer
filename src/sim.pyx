c863b7529e81190b8b005670a39477c3d587a5fb
Will Ware <wware@alum.mit.edu>
2005-12-03 03:40:24 +0000

    Don't show all those warnings from the Pyrex-generated code. Permit access to simulator global variables to normal non-Pyrex Python scripts.

--- src/sim.pyx
+++ src/sim.pyx
@@ -1,3 +1,13 @@
+"""Example usage script
+
+import sim
+s = sim.Simulator()
+s.ToMinimize = 1
+s.DumpAsText = 1
+s.read("tests/rigid_organics/test_C6H10.mmp")
+s.everythingElse()
+"""
+
 cdef extern from "simhelp.c":
     ctypedef struct SimArgs:
         char *printPotential
@@ -34,41 +44,134 @@ cdef extern from "simhelp.c":
     void everythingElse()
     cdef char *structCompareHelp()
 
-def initsim(filename,
-            printPotentialInitial=1, # pm
-            printPotentialIncrement=1, # pm
-            printPotentialLimit=200, # pm
-            printPotentialEnergy=0,
-            printPotential="",
-            ofilename="",
-            tfilename=""):
+class Simulator:
+    """Pyrex permits access to doc strings"""
 
-    if not filename:
-        raise Exception("Need a filename (probably MMP)")
+    def __getattr__(self, key):
+        """Access to simulator's command line options and globals.c
+        goodies
+        """
+        if key == "debug_flags":
+            return debug_flags
+        elif key == "Iteration":
+            return Iteration
+        elif key == "ToMinimize":
+            return ToMinimize
+        elif key == "IterPerFrame":
+            return IterPerFrame
+        elif key == "NumFrames":
+            return NumFrames
+        elif key == "DumpAsText":
+            return DumpAsText
+        elif key == "DumpIntermediateText":
+            return DumpIntermediateText
+        elif key == "PrintFrameNums":
+            return PrintFrameNums
+        elif key == "OutputFormat":
+            return OutputFormat
+        elif key == "KeyRecordInterval":
+            return KeyRecordInterval
+        elif key == "DirectEvaluate":
+            return DirectEvaluate
+        elif key == "IDKey":
+            return IDKey
+        elif key == "baseFilename":
+            return baseFilename
+        elif key == "Dt":
+            return Dt
+        elif key == "Dx":
+            return Dx
+        elif key == "Dmass":
+            return Dmass
+        elif key == "Temperature":
+            return Temperature
 
-    myArgs.printPotential = printPotential
-    myArgs.printPotentialInitial = printPotentialInitial
-    myArgs.printPotentialIncrement = printPotentialIncrement
-    myArgs.printPotentialLimit = printPotentialLimit
-    myArgs.printPotentialEnergy = printPotentialEnergy
-    myArgs.printPotential = printPotential
-    myArgs.ofilename = ofilename
-    myArgs.tfilename = tfilename
-    myArgs.filename = filename
+    def __setattr__(self, key, value):
+        """Access to simulator's command line options and globals.c
+        goodies
+        """
+        if key == "debug_flags":
+            global debug_flags
+            debug_flags = value
+        elif key == "Iteration":
+            global Iteration
+            Iteration = value
+        elif key == "ToMinimize":
+            global ToMinimize
+            ToMinimize = value
+        elif key == "IterPerFrame":
+            global IterPerFrame
+            IterPerFrame = value
+        elif key == "NumFrames":
+            global NumFrames
+            NumFrames = value
+        elif key == "DumpAsText":
+            global DumpAsText
+            DumpAsText = value
+        elif key == "DumpIntermediateText":
+            global DumpIntermediateText
+            DumpIntermediateText = value
+        elif key == "PrintFrameNums":
+            global PrintFrameNums
+            PrintFrameNums = value
+        elif key == "OutputFormat":
+            global OutputFormat
+            OutputFormat = value
+        elif key == "KeyRecordInterval":
+            global KeyRecordInterval
+            KeyRecordInterval = value
+        elif key == "DirectEvaluate":
+            global DirectEvaluate
+            DirectEvaluate = value
+        elif key == "IDKey":
+            global IDKey
+            IDKey = value
+        elif key == "baseFilename":
+            global baseFilename
+            baseFilename = value
+        elif key == "Dt":
+            global Dt
+            Dt = value
+        elif key == "Dx":
+            global Dx
+            Dx = value
+        elif key == "Dmass":
+            global Dmass
+            Dmass = value
+        elif key == "Temperature":
+            global Temperature
+            Temperature = value
 
-    if initsimhelp():
-        raise Exception, "please only run initsim() once!"
+    def everythingElse(self):
+        everythingElse()
 
-def structCompare():
-    r = structCompareHelp()
-    if r:
-        raise Exception, r
+    def read(self, filename,
+             printPotentialInitial=1, # pm
+             printPotentialIncrement=1, # pm
+             printPotentialLimit=200, # pm
+             printPotentialEnergy=0,
+             printPotential="",
+             ofilename="",
+             tfilename=""):
 
-def test():
-    global ToMinimize, DumpAsText  # don't forget this
-    ToMinimize=1
-    DumpAsText=1
-    initsim(filename="tests/rigid_organics/test_C6H10.mmp")
-    readPart()
-    #dumpPart()
-    everythingElse()
+        if not filename:
+            raise Exception("Need a filename (probably MMP)")
+
+        myArgs.printPotential = printPotential
+        myArgs.printPotentialInitial = printPotentialInitial
+        myArgs.printPotentialIncrement = printPotentialIncrement
+        myArgs.printPotentialLimit = printPotentialLimit
+        myArgs.printPotentialEnergy = printPotentialEnergy
+        myArgs.printPotential = printPotential
+        myArgs.ofilename = ofilename
+        myArgs.tfilename = tfilename
+        myArgs.filename = filename
+
+        if initsimhelp():
+            raise Exception, "please only run initsim() once!"
+        readPart()
+
+    def structCompare(self):
+        r = structCompareHelp()
+        if r:
+            raise Exception, r
