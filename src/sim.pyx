884dc7db09793c33a7a28895c01b32ef6565dabb
Will Ware <wware@alum.mit.edu>
2005-12-03 02:51:03 +0000

    Correct a misunderstanding about the Pyrex interface to C globals. Makes the code much simpler.

--- src/sim.pyx
+++ src/sim.pyx
@@ -1,22 +1,5 @@
 cdef extern from "simhelp.c":
     ctypedef struct SimArgs:
-        int debug_flags
-        int Iteration
-        int ToMinimize
-        int IterPerFrame
-        int NumFrames
-        int DumpAsText
-        int DumpIntermediateText
-        int PrintFrameNums
-        int OutputFormat
-        int KeyRecordInterval
-        int DirectEvaluate
-        char *IDKey
-        char *baseFilename
-        double Dt
-        double Dx
-        double Dmass
-        double Temperature
         char *printPotential
         double printPotentialInitial
         double printPotentialIncrement
@@ -25,63 +8,44 @@ cdef extern from "simhelp.c":
         char *ofilename
         char *tfilename
         char *filename
-    cdef SimArgs myArgs
+    # stuff from globals.c
+    int debug_flags
+    int Iteration
+    int ToMinimize
+    int IterPerFrame
+    int NumFrames
+    int DumpAsText
+    int DumpIntermediateText
+    int PrintFrameNums
+    int OutputFormat
+    int KeyRecordInterval
+    int DirectEvaluate
+    char *IDKey
+    char *baseFilename
+    double Dt
+    double Dx
+    double Dmass
+    double Temperature
+    # end of globals.c stuff
+    SimArgs myArgs
     int initsimhelp()
     void readPart()
     void dumpPart()
     void everythingElse()
     cdef char *structCompareHelp()
 
-def initsim(debug_flags=0,
-            Iteration=0,
-            ToMinimize=0,
-            IterPerFrame=10,
-            NumFrames=100,
-            DumpAsText=0,
-            DumpIntermediateText=0,
-            PrintFrameNums=1,
-            OutputFormat=1,
-            KeyRecordInterval=32,
-            DirectEvaluate=1, # XXX should default to 0 eventually
-            IDKey="",
-            baseFilename="",
-            Dt=1e-16,              # seconds
-            Dx=1e-12,              # meters
-            Dmass=1e-27,           # units of mass vs. kg
-            Temperature=300.0,     # Kelvins
+def initsim(filename,
             printPotentialInitial=1, # pm
             printPotentialIncrement=1, # pm
             printPotentialLimit=200, # pm
             printPotentialEnergy=0,
             printPotential="",
             ofilename="",
-            tfilename="",
-            filename=""):
-    """There has GOT to be a better way to do this than what I'm doing
-    here. But I don't think Pyrex can write to a C global variable. So
-    pack everything into one big horrible struct on the Python side,
-    then unpack it on the C side. Ugly.
-    """
+            tfilename=""):
+
     if not filename:
         raise Exception("Need a filename (probably MMP)")
 
-    myArgs.debug_flags = debug_flags
-    myArgs.Iteration = Iteration
-    myArgs.ToMinimize = ToMinimize
-    myArgs.IterPerFrame = IterPerFrame
-    myArgs.NumFrames = NumFrames
-    myArgs.DumpAsText = DumpAsText
-    myArgs.DumpIntermediateText = DumpIntermediateText
-    myArgs.PrintFrameNums = PrintFrameNums
-    myArgs.OutputFormat = OutputFormat
-    myArgs.KeyRecordInterval = KeyRecordInterval
-    myArgs.DirectEvaluate = DirectEvaluate
-    myArgs.IDKey = IDKey
-    myArgs.baseFilename = baseFilename
-    myArgs.Dt = Dt
-    myArgs.Dx = Dx
-    myArgs.Dmass = Dmass
-    myArgs.Temperature = Temperature
     myArgs.printPotential = printPotential
     myArgs.printPotentialInitial = printPotentialInitial
     myArgs.printPotentialIncrement = printPotentialIncrement
@@ -101,8 +65,10 @@ def structCompare():
         raise Exception, r
 
 def test():
-    initsim(ToMinimize=1, DumpAsText=1,
-            filename="tests/rigid_organics/test_C6H10.mmp")
+    global ToMinimize, DumpAsText  # don't forget this
+    ToMinimize=1
+    DumpAsText=1
+    initsim(filename="tests/rigid_organics/test_C6H10.mmp")
     readPart()
     #dumpPart()
     everythingElse()
--- src/simhelp.c
+++ src/simhelp.c
@@ -5,23 +5,6 @@
 #include "simulator.h"
 
 typedef struct {
-    int debug_flags;
-    int Iteration;
-    int ToMinimize;
-    int IterPerFrame;
-    int NumFrames;
-    int DumpAsText;
-    int DumpIntermediateText;
-    int PrintFrameNums;
-    int OutputFormat;
-    int KeyRecordInterval;
-    int DirectEvaluate; // XXX should default to 0 eventually
-    char *IDKey;
-    char *baseFilename;
-    double Dt;              // seconds
-    double Dx;              // meters
-    double Dmass;           // units of mass vs. kg
-    double Temperature;     /* Kelvins */
     char *printPotential;
     double printPotentialInitial; // pm
     double printPotentialIncrement; // pm
@@ -31,8 +14,45 @@ typedef struct {
     char *tfilename;
     char *filename;
 } SimArgs;
-
 SimArgs myArgs;
+
+/* ----- globals.c stuff --------- */
+int debug_flags = 0;
+int Interrupted = 0; /* set to 1 when a SIGTERM is received */
+struct xyz Center, Bbox[2];
+int Iteration=0;
+// definitions for command line args
+int ToMinimize=0;
+int IterPerFrame=10;
+int NumFrames=100;
+int DumpAsText=0;
+int DumpIntermediateText=0;
+int PrintFrameNums=1;
+int OutputFormat=1;
+int KeyRecordInterval=32;
+int DirectEvaluate=1; // XXX should default to 0 eventually
+char *IDKey="";
+char OutFileName[1024];
+char TraceFileName[1024];
+char *baseFilename;
+// for writing the differential position and trace files
+FILE *outf, *tracef;
+int Count = 0;
+/** constants: timestep (.1 femtosecond), scale of distance (picometers) */
+double Dt = 1e-16;              // seconds
+double Dx = 1e-12;              // meters
+double Dmass = 1e-27;           // units of mass vs. kg
+double Temperature = 300.0;	/* Kelvins */
+double Boltz = 1.38e-23;	/* k, in J/K */
+double Pi = 3.1415926;
+double totClipped=0.0;  // internal thermostat for numerical stability
+double Gamma = 0.01; // for Langevin thermostats
+//double Gamma = 0.1; // for Langevin thermostats
+// double G1=(1.01-0.27*Gamma)*1.4*sqrt(Gamma);
+double G1=(1.01-0.27*0.01)*1.4*0.1;
+//double G1=(1.01-0.27*0.1)*1.4*0.31623;
+
+/* -------------------------- */
 
 int initsimhelp(void);
 void readPart(void);
@@ -42,7 +62,7 @@ char * structCompareHelp(void);
 
 static char retval[100];
 static struct part *part;
-static char buf[1024], *filename, *ofilename, *tfilename;
+static char buf[1024], *filename;
 static int initializedAlready = 0;
 
 int initsimhelp(void)
@@ -60,23 +80,6 @@ int initsimhelp(void)
 	return 1;
     initializedAlready = 1;
 
-    debug_flags = myArgs.debug_flags;
-    Iteration = myArgs.Iteration;
-    ToMinimize = myArgs.ToMinimize;
-    IterPerFrame = myArgs.IterPerFrame;
-    NumFrames = myArgs.NumFrames;
-    DumpAsText = myArgs.DumpAsText;
-    DumpIntermediateText = myArgs.DumpIntermediateText;
-    PrintFrameNums = myArgs.PrintFrameNums;
-    OutputFormat = myArgs.OutputFormat;
-    KeyRecordInterval = myArgs.KeyRecordInterval;
-    DirectEvaluate = myArgs.DirectEvaluate;
-    IDKey = myArgs.IDKey;
-    baseFilename = myArgs.baseFilename;
-    Dt = myArgs.Dt;
-    Dx = myArgs.Dx;
-    Dmass = myArgs.Dmass;
-    Temperature = myArgs.Temperature;
     printPotential = myArgs.printPotential;
     printPotentialInitial = myArgs.printPotentialInitial;
     printPotentialIncrement = myArgs.printPotentialIncrement;
@@ -92,12 +95,12 @@ int initsimhelp(void)
     }
     if (strchr(filename, '.')) {
         sprintf(buf, "%s", filename);
-    } else if (strlen(baseFilename) > 0) {
+    } else if (baseFilename != NULL && strlen(baseFilename) > 0) {
         sprintf(buf, "%s.xyz", filename);
     } else {
         sprintf(buf, "%s.mmp", filename);
     }
-    if (strlen(ofilename) == 0) {
+    if (ofilename == NULL || strlen(ofilename) == 0) {
 	strcpy(OutFileName,buf);
 	p = strchr(OutFileName, '.');
 	if (p) {
@@ -113,7 +116,7 @@ int initsimhelp(void)
             strcat(OutFileName,".dpb");
         }
     }
-    if (strlen(tfilename) == 0) {
+    if (tfilename == NULL || strlen(tfilename) == 0) {
 	strcpy(TraceFileName,buf);
 	p = strchr(TraceFileName, '.');
 	if (p) {
@@ -184,6 +187,10 @@ void everythingElse(void)
         dynamicsMovie(part);
     }
 
+    /* I'd like to remove the "return exitvalue" from doneExit() and
+     * do it separately, pending Eric's approval.
+     */
+
     //doneExit(0, tracef, "");
 }
 
@@ -199,313 +206,46 @@ void printPotential(void)
 }
 #endif
 
+/**
+ * If we return a non-empty string, it's an error message.
+ */
 char * structCompareHelp(void) {
-    if (strlen(baseFilename) > 0) {
-        int i1;
-        int i2;
-        struct xyz *basePositions;
-        struct xyz *initialPositions;
+    int i1;
+    int i2;
+    struct xyz *basePositions;
+    struct xyz *initialPositions;
         
-        basePositions = readXYZ(baseFilename, &i1);
-        if (basePositions == NULL) {
-	    sprintf(retval, "could not read base positions file from \"%s\"", baseFilename);
-            return retval;
-        }
-        initialPositions = readXYZ(myArgs.filename, &i2);
-        if (initialPositions == NULL) {
-	    sprintf(retval, "could not read comparison positions file \"%s\"", myArgs.filename);
-            return retval;
-        }
-        if (i1 != i2) {
-	    sprintf(retval, "structures to compare must have same number of atoms");
-            return retval;
-        }
-        if (doStructureCompare(i1, basePositions, initialPositions,
-			       NumFrames, 1e-8, 1e-4, 1.0+1e-4)) {
-	    sprintf(retval, "structure comparison failed");
-	    return retval;
-	}
+    if (baseFilename == NULL || strlen(baseFilename) == 0) {
+	sprintf(retval, "No baseFilename");
+	return retval;
+    }
+    basePositions = readXYZ(baseFilename, &i1);
+    if (basePositions == NULL) {
+	sprintf(retval,
+		"could not read base positions file from \"%s\"",
+		baseFilename);
+	return retval;
+    }
+    initialPositions = readXYZ(myArgs.filename, &i2);
+    if (initialPositions == NULL) {
+	sprintf(retval,
+		"could not read comparison positions file \"%s\"",
+		myArgs.filename);
+	return retval;
+    }
+    if (i1 != i2) {
+	sprintf(retval,
+		"structures to compare must have same number of atoms");
+	return retval;
+    }
+    if (doStructureCompare(i1, basePositions, initialPositions,
+			   NumFrames, 1e-8, 1e-4, 1.0+1e-4)) {
+	sprintf(retval, "structure comparison failed");
+	return retval;
     }
     retval[0] = '\0';
     return retval;
 }
-
-#if 0
-int
-main(int argc,char **argv)
-{
-    struct part *part;
-    int opt, n;
-    int printPotentialEnergy = 0;
-    double potentialEnergy;
-    int dump_part = 0;
-    char *printPotential = NULL;
-    double printPotentialInitial = 1; // pm
-    double printPotentialIncrement = 1; // pm
-    double printPotentialLimit = 200; // pm
-    char buf[1024], *filename, *ofilename, *tfilename, *c;
-	
-    if (signal(SIGTERM, &SIGTERMhandler) == SIG_ERR) {
-        perror("signal(SIGTERM)");
-        exit(1);
-    }
-
-    //setupPositionsArrays();
-	
-    //vsetc(Cog,0.0);
-    //vsetc(P,0.0);
-    //vsetc(Omega,0.0);
-
-    //debug_flags = D_BEND_ONLY;
-    
-    filename = (char *)0;
-    ofilename = (char *)0;
-    tfilename = (char *)0;
-
-    while ((opt = getopt_long(argc, argv,
-			    "hnmEi:f:s:t:xXONI:K:rD:o:q:B:",
-			    option_vec, NULL)) != -1) {
-	switch(opt) {
-	case 'h':
-	    usage();
-	case OPT_DUMP_PART:
-	    dump_part = 1;
-	    break;
-        case OPT_PRINT_POTENTIAL:
-            printPotential = optarg;
-	    break;
-        case OPT_INITIAL:
-	    printPotentialInitial = atof(optarg);
-	    break;
-        case OPT_INCREMENT:
-	    printPotentialIncrement = atof(optarg);
-	    break;
-        case OPT_LIMIT:
-	    printPotentialLimit = atof(optarg);
-	    break;
-        case OPT_DIRECT_EVALUATE:
-            DirectEvaluate = !DirectEvaluate;
-	    break;
-	case 'n':
-	    // ignored
-	    break;
-	case 'm':
-	    ToMinimize=1;
-	    break;
-	case 'E':
-	    printPotentialEnergy=1;
-	    break;
-	case 'i':
-	    IterPerFrame = atoi(optarg);
-	    break;
-	case 'f':
-	    NumFrames = atoi(optarg);
-	    break;
-	case 's':
-	    Dt = atof(optarg);
-	    break;
-	case 't':
-	    Temperature = atof(optarg);
-	    break;
-	case 'x':
-	    DumpAsText = 1;
-	    break;
-	case 'X':
-	    DumpIntermediateText = 1;
-	    break;
-	case 'O':
-	    OutputFormat = 1;
-	    break;
-	case 'N':
-	    OutputFormat = 2;
-	    break;
-	case 'I':
-	    IDKey = optarg;
-	    break;
-	case 'K':
-	    KeyRecordInterval = atoi(optarg);
-	    break;
-	case 'r':
-	    PrintFrameNums = 0;
-	    break;
-	case 'D':
-	    n = atoi(optarg);
-	    if (n < 32 && n >= 0) {
-		debug_flags |= 1 << n;
-	    }
-	    break;
-	case 'o':
-	    ofilename = optarg;
-	    break;
-	case 'q':
-	    tfilename = optarg;
-	    break;
-	case 'B':
-	    baseFilename = optarg;
-	    break;
-        case ':':
-        case '?':
-	default:
-	    usage();
-	    exit(1);
-	}
-    }
-    if (optind + 1 == argc)    // (optind < argc) if not paranoid
-	filename = argv[optind];
-
-    if (DumpAsText) {
-        OutputFormat = 0;
-    }
-
-    if (!filename) {
-        usage();
-    }
-
-    if (baseFilename != NULL) {
-        int i1;
-        int i2;
-        struct xyz *basePositions;
-        struct xyz *initialPositions;
-        
-        basePositions = readXYZ(baseFilename, &i1);
-        if (basePositions == NULL) {
-            fprintf(stderr, "could not read base positions file from -B<filename>\n");
-            exit(1);
-        }
-        initialPositions = readXYZ(filename, &i2);
-        if (initialPositions == NULL) {
-            fprintf(stderr, "could not read comparison positions file\n");
-            exit(1);
-        }
-        if (i1 != i2) {
-            fprintf(stderr, "structures to compare must have same number of atoms\n");
-            exit(1);
-        }
-        exit(doStructureCompare(i1, basePositions, initialPositions,
-                                NumFrames, 1e-8, 1e-4, 1.0+1e-4));
-    }
-
-    if (strchr(filename, '.')) {
-        sprintf(buf, "%s", filename);
-    } else if (baseFilename != NULL) {
-        sprintf(buf, "%s.xyz", filename);
-    } else {
-        sprintf(buf, "%s.mmp", filename);
-    }
-
-    if (! ofilename) {
-	strcpy(OutFileName,buf);
-	c=strchr(OutFileName, '.');
-	if (c) {
-            *c='\0';
-        }
-    } else {
-        strcpy(OutFileName,ofilename);
-    }
-    
-    if (! strchr(OutFileName, '.')) {
-	if (DumpAsText || baseFilename != NULL) {
-            strcat(OutFileName,".xyz");
-        } else {
-            strcat(OutFileName,".dpb");
-        }
-    }
-
-    if (! tfilename) {
-	strcpy(TraceFileName,buf);
-	c=strchr(TraceFileName, '.');
-	if (c) {
-            *c='\0';
-        }
-    } else {
-        strcpy(TraceFileName,tfilename);
-    }
-    
-    if (! strchr(TraceFileName, '.')) {
-        strcat(TraceFileName,".trc");
-    }
-    if (!printPotentialEnergy) {
-        tracef = fopen(TraceFileName, "w");
-        if (!tracef) {
-            perror(TraceFileName);
-            exit(1);
-        }
-    }
-
-    if (IterPerFrame <= 0) IterPerFrame = 1;
-
-    initializeBondTable();
-
-    if (printPotential) {
-        printPotentialAndGradientFunctions(printPotential,
-                                           printPotentialInitial,
-                                           printPotentialIncrement,
-                                           printPotentialLimit);
-        exit(0);
-    }
-    
-    part = readMMP(buf);
-    updateVanDerWaals(part, NULL, part->positions);
-    generateStretches(part);
-    generateBends(part);
-
-    if (printPotentialEnergy) {
-        struct xyz *force = (struct xyz *)allocate(sizeof(struct xyz) * part->num_atoms);
-        potentialEnergy = calculatePotential(part, part->positions);
-        calculateGradient(part, part->positions, force);
-        printf("%e %e %e %e (Potential energy in aJ, gradient of atom 1)\n", potentialEnergy, force[1].x, force[1].y, force[1].z);
-        exit(0);
-    }
-
-    if (dump_part) {
-        printPart(stdout, part);
-        exit(0);
-    }
-
-    /*
-    fprintf(stderr, " center of mass velocity: %f\n", vlen(vdif(CoM(Positions),CoM(OldPositions))));
-    fprintf(stderr, " center of mass: %f -- %f\n", vlen(CoM(Positions)), vlen(Cog));
-    fprintf(stderr, " total momentum: %f\n",P);
-    */
-    traceHeader(tracef, filename, OutFileName, TraceFileName, 
-                part, NumFrames, IterPerFrame, Temperature);
-
-    if  (ToMinimize) {
-	NumFrames = max(NumFrames,(int)sqrt((double)part->num_atoms));
-	Temperature = 0.0;
-    } else {
-        traceJigHeader(tracef, part);
-    }
-
-    printf("iters per frame = %d\n",IterPerFrame);
-    printf("number of frames = %d\n",NumFrames);
-    printf("timestep = %e\n",Dt);
-    printf("temp = %f\n",Temperature);
-    if (DumpAsText) printf("dump as text\n");
-
-    printf("< %s  > %s\n", buf, OutFileName);
-
-    // XXX put me back
-    //printf("\nTotal Ke = %e\n",TotalKE);
-
-    outf = fopen(OutFileName, DumpAsText ? "w" : "wb");
-    if (outf == NULL) {
-        perror(OutFileName);
-        exit(1);
-    }
-    writeOutputHeader(outf, part);
-
-    if  (ToMinimize) {
-	minimizeStructure(part);
-    }
-    else {
-        dynamicsMovie(part);
-    }
-
-    doneExit(0, tracef, "");
-    return 1; // not reached
-}
-#endif
 
 /*
  * Local Variables:
