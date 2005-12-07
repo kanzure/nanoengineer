1a9630b03f48afc6614c5deb1fc8e59252439783
Will Ware <wware@alum.mit.edu>
2005-12-07 16:06:13 +0000

    The cron job isn't finishing. Use cron to send a reminder, and do the fetch by hand.

--- downloads/getTarballs.py
+++ downloads/getTarballs.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
-import os, smtplib
+"""This is a script to make tarballs of the Nanorex CVS repositories
+maintained at cvsdude.org.
+"""
+
+import os, sys, smtplib
 
 tarballURLs = {
     "cad.tar.gz": "http://cvsdude.org/download.pl?id=xAWFjEtta9XaU",
@@ -19,18 +23,30 @@ LISTENERS = [
     # whoever else might want to know
     ]
 
-HAPPY_MESSAGE = {
-    "Subject": "Your tarballs are ready   :)",
-    "Text": "cdrecord -v -speed=4 dev=ATAPI:0,0,0 %s\n   or\n" % ISOFILE
-    }
+class Message:
+    def __init__(self, subject, text=None):
+        self.subject = subject
+        self.text = text
+    def send(self, debug=False):
+        smtpserver = "smtp.rcn.com"
+        username = "wware1"
+        password = "Grendel9"
+        fromaddr = MYSELF
+        toaddrs = LISTENERS
+        msg = "From: %s\r\n" % fromaddr
+        msg += "To: %s\r\n" % (", ".join(toaddrs))
+        msg += "Subject: %s\r\n" % self.subject
+        msg += "\r\n"
+        msg += "%s\r\n" % self.text
+        server = smtplib.SMTP(smtpserver)
+        server.login(username, password)
+        if debug: server.set_debuglevel(1)
+        server.sendmail(fromaddr, toaddrs, msg)
+        server.quit()
 
-for filename in fileList:
-    HAPPY_MESSAGE["Text"] += "wget http://willware.net:8080/nanorex-tarballs/" + filename + "\n"
 
-SAD_MESSAGE = {
-    "Subject": "Your tarballs are NOT ready  :(",
-    "Text": "Please run getTarballs.py by hand and investigate"
-    }
+RESULT = Message(":(  Your tarballs are NOT ready, please investigate",
+                 "Nothing happened yet")
 
 class BadDownload(Exception):
     pass
@@ -40,37 +56,81 @@ def system(cmd):
         raise BadDownload, cmd
 
 def getTarballs():
-    system("rm -rf " + TMP_DIR)
-    system("mkdir " + TMP_DIR)
-    for tarball in tarballURLs.keys():
-        url = tarballURLs[tarball]
-        tarballPath = TMP_DIR + "/" + tarball
-        cmd = "wget -O %s %s" % (tarballPath, url)
-        system(cmd)
-    system("mkisofs -J -R -o %s %s" % (ISOFILE, TMP_DIR))
-    for file in fileList:
-        system("scp %s/%s %s" % (TMP_DIR, file, WEBSERVER_DIR))
+    """Attempt to fetch CVS tarballs from cvsdude server. If unsuccessful,
+        place an error message in SAD_MESSAGE.
+    """
+    global RESULT
+    try:
+        happy = Message(":)  Your tarballs are ready",
+                        "cdrecord -v -speed=4 dev=ATAPI:0,0,0 %s\n   or\n" % ISOFILE)
+        for filename in fileList:
+            happy.text += "wget http://willware.net:8080/nanorex-tarballs/" + filename + "\n"
+        system("rm -rf " + TMP_DIR)
+        system("mkdir " + TMP_DIR)
+        for tarball in tarballURLs.keys():
+            url = tarballURLs[tarball]
+            tarballPath = TMP_DIR + "/" + tarball
+            cmd = "wget -O %s %s" % (tarballPath, url)
+            system(cmd)
+        system("mkisofs -J -R -o %s %s" % (ISOFILE, TMP_DIR))
+        for file in fileList:
+            system("scp %s/%s %s" % (TMP_DIR, file, WEBSERVER_DIR))
+        RESULT = happy
+    except BadDownload, e:
+        RESULT.text = "Command failed in getTarballs.py: " + e.args[0]
 
-def sendEmail(message, debug=False):
-    smtpserver = "smtp.rcn.com"
-    username = "wware1"
-    password = "Grendel9"
-    fromaddr = MYSELF
-    toaddrs = LISTENERS
-    msg = "From: %s\r\n" % fromaddr
-    msg += "To: %s\r\n" % (", ".join(toaddrs))
-    msg += "Subject: %s\r\n" % message["Subject"]
-    msg += "\r\n"
-    msg += "%s\r\n" % message["Text"]
-    server = smtplib.SMTP(smtpserver)
-    server.login(username, password)
-    if debug: server.set_debuglevel(1)
-    server.sendmail(fromaddr, toaddrs, msg)
-    server.quit()
+##############################################
+# Process command line
 
-try:
-    getTarballs()
-    sendEmail(HAPPY_MESSAGE)
-except BadDownload, e:
-    SAD_MESSAGE["Text"] = "Command failed in getTarballs.py: " + e.args[0]
-    sendEmail(SAD_MESSAGE)
+if __name__ == "__main__":
+
+    def sendReminder():
+        """Send a reminder to the user to do getTarballs by hand. I find that on
+        my own machine, I can't run the whole process from a cron job. I don't know
+        why, but my network doesn't seem to like it.
+        """
+        reminder = Message("Reminder to fetch CVS tarballs",
+                           "Run the Distribution/downloads/getTarballs.py script")
+        reminder.send()
+
+    def sendResult():
+        """If an error message has been placed in SAD_MESSAGE, then
+        send it to let the user know that he needs to investigate why
+        the download failed.
+        """
+        RESULT.send()
+
+    def help():
+        """print help information
+        """
+        print __doc__
+        global options
+        for opt in options:
+            print opt.__name__ + "\n        " + opt.__doc__
+        sys.exit(0)
+
+    options = (sendReminder,
+               getTarballs,
+               sendResult,
+               help)
+
+    # Default behavior is to just do the very slow tests
+    defaultArgs = ("getTarballs", "sendResult")
+
+    args = sys.argv[1:]
+    if len(args) < 1:
+        args = defaultArgs
+
+    class Found(Exception): pass
+
+    for arg in args:
+        try:
+            for opt in options:
+                if opt.__name__ == arg:
+                    opt()
+                    raise Found
+            print "Don't understand command line arg:", arg
+            print "Type:  %s help" % sys.argv[0]
+            sys.exit(1)
+        except Found:
+            pass
