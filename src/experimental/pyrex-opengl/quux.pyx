baf3a85040dc7f43de08a5ad604b04ff25b856df
Will Ware <wware@alum.mit.edu>
2006-01-23 05:55:52 +0000

    progress toward getting the API implemented

--- src/experimental/pyrex-opengl/quux.pyx
+++ src/experimental/pyrex-opengl/quux.pyx
@@ -1,5 +1,5 @@
-cdef extern from "quux_help.c":
-    __glColor3f(float,float,float)
+import Numeric
+import unittest
 
 cdef extern from "Numeric/arrayobject.h":
     struct PyArray_Descr:
@@ -13,11 +13,24 @@ cdef extern from "Numeric/arrayobject.h":
         cdef PyArray_Descr *descr
         cdef int flags
 
-def glColor3f(r, g, b):
-    __glColor3f(r, g, b)
+cdef extern from "quux_help.c":
+    __glColor3f(float,float,float)
+    _shapeRendererInit()
+    _shapeRendererSetFrustum(float frustum[6])
+    _shapeRendererSetViewport(int viewport[4])
+    _shapeRendererSetModelView(float modelview[6])
+    _shapeRendererUpdateLODEval()
+    _shapeRendererSetLODScale(float s)
+    _shapeRendererDrawSpheres(int count, float center[][3],
+                              float radius[], float color[][4])
+    _shapeRendererDrawCylinders(int count, float pos1[][3],
+                                float pos2[][3], float radius[],
+                                float color[][4])
+    _checkArray(ArrayType a)
 
-def hackNumericArray(ArrayType a):
-    print "Type:", chr(a.descr.type)
+#####################################
+
+def __hackNumericArray(ArrayType a):
     if chr(a.descr.type) != "f":
         raise TypeError("Float array required")
     if a.nd != 2:
@@ -27,22 +40,84 @@ def hackNumericArray(ArrayType a):
     nrows = a.dimensions[0]
     ncols = a.dimensions[1]
     elems = <float *>a.data
-    hyphen = "-"
-    divider = ("+" + 10 * hyphen) * ncols + "+"
-    print divider
+    result = [ ]
     for row in range(nrows):
+        thisRow = [ ]
         for col in range(ncols):
-            x = elems[row * ncols + col]
-            print "| %8f" % x,
-        print "|"
-        print divider
+            thisRow.append(elems[row * ncols + col])
+        result.append(thisRow)
+    return result
+
+def hackNumericArray(ArrayType a):
+    return _checkArray(a)
+
+####################################
+
+def glColor3f(r, g, b):
+    __glColor3f(r, g, b)
+
+
+def shapeRendererInit():
+    _shapeRendererInit()
+
+def shapeRendererSetFrustum(ArrayType frustum):
+    if chr(frustum.descr.type) != "f":
+        raise TypeError("Float array required")
+    if frustum.nd != 1:
+        raise ValueError("1 dimensional array required")
+    cdef int nrows, ncols
+    cdef float *elems, x
+    nelems = frustum.dimensions[0]
+    assert nelems == 6
+    _shapeRendererSetFrustum(<float *>frustum.data)
+
+def shapeRendererSetViewport(viewport):
+    pass
+
+def shapeRendererSetModelView(modelview):
+    pass
+
+def shapeRendererUpdateLODEval():
+    pass
+
+def shapeRendererSetLODScale(s):
+    pass
+
+def shapeRendererDrawSpheres(count, center,
+                             radius, color):
+    pass
+
+def shapeRendererDrawCylinders(count, pos1,
+                               pos2, radius,
+                               color):
+    pass
+
+####################################
+
+class Tests(unittest.TestCase):
+
+    def testColor(self):
+        glColor3f(1,2,3)
+
+    def testHackNumericArray(self):
+        a = Numeric.array((Numeric.array((1, 2), 'f'),
+                           Numeric.array((3, 4), 'f')), 'f')
+        assert hackNumericArray(a) == (1.0, 2.0, 3.0, 4.0)
+
+    def testSetFrustum(self):
+        frustum = Numeric.array((1, 2, 3, 4, 5, 6), 'f')
+        shapeRendererSetFrustum(frustum)
+
+    def testSetFrustumWrongNumberOfFloats(self):
+        try:
+            frustum = Numeric.array((1, 2, 3, 4, 5), 'f')
+            shapeRendererSetFrustum(frustum)
+        except AssertionError:
+            pass
 
 def test():
-    import Numeric
-    print "Testing the OpenGL interface"
-    glColor3f(1,2,3)
-    a = (Numeric.array((1, 2), 'f'),
-         Numeric.array((3, 4), 'f'))
-    a = Numeric.array(a, 'f')
-    hackNumericArray(a)
-    print "Done testing"
+    suite = unittest.makeSuite(Tests, 'test')
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+
--- src/experimental/pyrex-opengl/quux_help.c
+++ src/experimental/pyrex-opengl/quux_help.c
@@ -8,8 +8,292 @@ PyObject *
 __glColor3f(float r, float g, float b)
 {
     /* Don't call glGetError() in this function! */
-    printf("Hello from inside Will's code\n");
+    //printf("Hello from inside Will's code\n");
     glColor3f(r, g, b);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *
+_checkArray(PyArrayObject *a)
+{
+    PyObject *retval;
+    float *data = (float *) a->data;
+    if (a->nd != 2) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of dimensions");
+	return NULL;
+    }
+    if (a->dimensions[0] != 2 || a->dimensions[0] != 2) {
+	PyErr_SetString(PyExc_ValueError, "wrong dimensions");
+	return NULL;
+    }
+    retval = PyTuple_New(4);
+    PyTuple_SetItem(retval, 0, PyFloat_FromDouble((double) data[0]));
+    PyTuple_SetItem(retval, 1, PyFloat_FromDouble((double) data[1]));
+    PyTuple_SetItem(retval, 2, PyFloat_FromDouble((double) data[2]));
+    PyTuple_SetItem(retval, 3, PyFloat_FromDouble((double) data[3]));
+    return retval;
+}
+
+
+
+
+/*
+ * Brad's stuff
+ */
+
+extern void shapeRendererInit();
+
+extern void shapeRendererSetFrustum(float frustum[6]);
+extern void shapeRendererSetViewport(int viewport[4]);
+extern void shapeRendererSetModelView(float modelview[6]);
+extern void shapeRendererUpdateLODEval();
+extern void shapeRendererSetLODScale(float s);
+
+extern void shapeRendererDrawSpheres(int count, float center[][3],
+				     float radius[], float color[][4]);
+extern void shapeRendererDrawCylinders(int count, float pos1[][3],
+				       float pos2[][3], float radius[],
+				       float color[][4]);
+
+// #define ZZ()  printf("%s\n", __FUNCTION__)
+#define ZZ()
+
+void shapeRendererInit()
+{
+    ZZ();
+}
+
+void shapeRendererSetFrustum(float frustum[6])
+{
+    ZZ();
+}
+
+void shapeRendererSetViewport(int viewport[4])
+{
+    ZZ();
+}
+
+void shapeRendererSetModelView(float modelview[6])
+{
+    ZZ();
+}
+
+void shapeRendererUpdateLODEval()
+{
+    ZZ();
+}
+
+void shapeRendererSetLODScale(float s)
+{
+    ZZ();
+}
+
+void shapeRendererDrawSpheres(int count, float center[][3],
+				     float radius[], float color[][4])
+{
+    ZZ();
+}
+
+void shapeRendererDrawCylinders(int count, float pos1[][3],
+				       float pos2[][3], float radius[],
+				       float color[][4])
+{
+    ZZ();
+}
+
+
+
+/*
+ * Wrappers for Brad's stuff
+ */
+
+extern PyObject *_shapeRendererInit();
+
+extern PyObject *_shapeRendererSetFrustum(float frustum[6]);
+extern PyObject *_shapeRendererSetViewport(int viewport[4]);
+extern PyObject *_shapeRendererSetModelView(float modelview[6]);
+extern PyObject *_shapeRendererUpdateLODEval(void);
+extern PyObject *_shapeRendererSetLODScale(float s);
+
+extern PyObject *_shapeRendererDrawSpheres(int count,
+					   PyArrayObject *center,
+					   PyArrayObject *radius,
+					   PyArrayObject *color);
+extern PyObject *_shapeRendererDrawCylinders(int count,
+					     PyArrayObject *pos1,
+					     PyArrayObject *pos2,
+					     PyArrayObject *radius,
+					     PyArrayObject *color);
+
+
+
+PyObject *_shapeRendererInit()
+{
+    shapeRendererInit();
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *_shapeRendererSetFrustum(float frustum[6])
+{
+    shapeRendererSetFrustum(frustum);
+#if 0
+    printf("%g %g %g %g %g %g\n",
+	   frustum[0], frustum[1], frustum[2],
+	   frustum[3], frustum[4], frustum[5]);
+#endif
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *_shapeRendererSetViewport(int viewport[4])
+{
+    shapeRendererSetViewport(viewport);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *_shapeRendererSetModelView(float modelview[6])
+{
+    shapeRendererSetModelView(modelview);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *_shapeRendererUpdateLODEval(void)
+{
+    shapeRendererUpdateLODEval();
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *_shapeRendererSetLODScale(float s)
+{
+    shapeRendererSetLODScale(s);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *_shapeRendererDrawSpheres(int count,
+				    PyArrayObject *center,
+				    PyArrayObject *radius,
+				    PyArrayObject *color)
+{
+    float center_data[count][3];
+    float radius_data[count];
+    float color_data[count][4];
+
+    if (center->nd != 2) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of dimensions: center");
+	return NULL;
+    }
+    if (center->dimensions[0] != count) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of atoms: center");
+	return NULL;
+    }
+    if (center->dimensions[1] != 3) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of columns: center");
+	return NULL;
+    }
+    memmove(center_data, center->data, 3 * count * sizeof(float));
+
+    if (radius->nd != 1) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of dimensions: radius");
+	return NULL;
+    }
+    if (radius->dimensions[0] != count) {
+	PyErr_SetString(PyExc_ValueError, "wrong dimension: radius");
+	return NULL;
+    }
+    memmove(radius_data, radius->data, count * sizeof(float));
+
+    if (color->nd != 2) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of dimensions");
+	return NULL;
+    }
+    if (color->dimensions[0] != count) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of atoms: color");
+	return NULL;
+    }
+    if (color->dimensions[1] != 4) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of columns: color");
+	return NULL;
+    }
+    memmove(color_data, color->data, 4 * count * sizeof(float));
+
+    shapeRendererDrawSpheres(count,
+			     center_data,
+			     radius_data,
+			     color_data);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *_shapeRendererDrawCylinders(int count,
+				      PyArrayObject *pos1,
+				      PyArrayObject *pos2,
+				      PyArrayObject *radius,
+				      PyArrayObject *color)
+{
+    float pos1_data[count][3];
+    float pos2_data[count][3];
+    float radius_data[count];
+    float color_data[count][4];
+
+    if (pos1->nd != 2) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of dimensions: pos1");
+	return NULL;
+    }
+    if (pos1->dimensions[0] != count) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of atoms: pos1");
+	return NULL;
+    }
+    if (pos1->dimensions[1] != 3) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of columns: pos1");
+	return NULL;
+    }
+    memmove(pos1_data, pos1->data, 3 * count * sizeof(float));
+
+    if (pos2->nd != 2) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of dimensions: pos2");
+	return NULL;
+    }
+    if (pos2->dimensions[0] != count) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of atoms: pos2");
+	return NULL;
+    }
+    if (pos2->dimensions[1] != 3) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of columns: pos2");
+	return NULL;
+    }
+    memmove(pos2_data, pos2->data, 3 * count * sizeof(float));
+
+    if (radius->nd != 1) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of dimensions: radius");
+	return NULL;
+    }
+    if (radius->dimensions[0] != count) {
+	PyErr_SetString(PyExc_ValueError, "wrong dimension: radius");
+	return NULL;
+    }
+    memmove(radius_data, radius->data, count * sizeof(float));
+
+    if (color->nd != 2) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of dimensions");
+	return NULL;
+    }
+    if (color->dimensions[0] != count) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of atoms: color");
+	return NULL;
+    }
+    if (color->dimensions[1] != 4) {
+	PyErr_SetString(PyExc_ValueError, "wrong number of columns: color");
+	return NULL;
+    }
+    memmove(color_data, color->data, 4 * count * sizeof(float));
+
+    shapeRendererDrawCylinders(count, pos1_data, pos2_data, radius_data, color_data);
     Py_INCREF(Py_None);
     return Py_None;
 }
