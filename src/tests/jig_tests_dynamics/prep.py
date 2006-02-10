7286dc9147745a25845cfa408c6e1ebdd26af9f0
Will Ware <wware@alum.mit.edu>
2006-02-10 21:00:30 +0000

    Here's what the unit tests will look like.

--- src/tests/jig_tests_dynamics/prep.py
+++ src/tests/jig_tests_dynamics/prep.py
@@ -6,7 +6,16 @@ def readlines(filename):
     return map(lambda x: x.rstrip(),
                open(filename).readlines())
 
+# These guys have nan's in them
+troublesome = [
+    "motor_tests/011_rotarymotor-_0_torque_and_0_speed.mmp",
+    "motor_tests/016_rotarymotor-negative_torque_and_0_speed.mmp",
+    "motor_tests/018_rotarymotor-positive_torque_and_0_speed.mmp",
+    "motor_tests/021_rotarymotor-dyno_jig_test_to_same_chunk.mmp"
+]
+
 files = readlines("mmpfiles")
+files = filter(lambda x: x not in troublesome, files)
 
 def simplifiedMMP(mmp_in, mmp_out):
     # first we translate from a complete MMP file to one the
@@ -55,12 +64,52 @@ def simplifiedMMP(mmp_in, mmp_out):
         outf.write(L + "\n")
     outf.close()
 
-for f in files:
-    f1 = string.replace(f, "(", "\(")
-    f1 = string.replace(f1, ")", "\)")
-    srcfile = "/tmp/foo.mmp"
-    xyzfile = f1[:-4] + ".xyz"
-    tracefile = f1[:-4] + ".trace"
-    simplifiedMMP(f, srcfile)
-    cmd = "../../simulator -i100 -f1 -x -o%s -q%s %s" % (xyzfile, tracefile, srcfile)
-    assert os.system(cmd) == 0
+def setupResults():
+    for f in files:
+        f1 = string.replace(f, "(", "\(")
+        f1 = string.replace(f1, ")", "\)")
+        srcfile = "/tmp/foo.mmp"
+        xyzfile = f1[:-4] + ".xyz"
+        tracefile = f1[:-4] + ".trace"
+        simplifiedMMP(f, srcfile)
+        cmd = ("../../simulator -i100 -f1 -x -o%s -q%s %s"
+               % (xyzfile, tracefile, srcfile))
+        assert os.system(cmd) == 0
+
+def compareTraces(goodTrace, iffyTrace):
+    def lastLineOfReadings(f):
+        lines = readlines(f)
+        n = None
+        for i in range(len(lines)):
+            if lines[i].startswith("# Done:"):
+                n = i - 1
+                break
+        if n == None: return None
+        return map(string.atof, lines[n].split())
+    good = lastLineOfReadings(goodTrace)
+    iffy = lastLineOfReadings(iffyTrace)
+    assert len(iffy) == len(good)
+    for x, y in map(None, good, iffy):
+        if y == 0:
+            assert x == 0
+        elif y > 0:
+            assert 0.99 * y <= x <= 1.01 * y
+        else:
+            assert 0.99 * y >= x >= 1.01 * y
+
+def testResults():
+    # here's where we try to reproduce the results
+    for f in files:
+        srcfile = "/tmp/foo.mmp"
+        questionableXyzfile = "/tmp/foo.xyz"
+        questionableTracefile = "/tmp/foo.trace"
+        goodXyzfile = f[:-4] + ".xyz"
+        goodTracefile = f[:-4] + ".trace"
+        simplifiedMMP(f, srcfile)
+        cmd = ("../../simulator -i100 -f1 -x -o%s -q%s %s"
+               % (questionableXyzfile, questionableTracefile, srcfile))
+        assert os.system(cmd) == 0
+        compareTraces(goodTracefile, questionableTracefile)
+
+if __name__ == "__main__":
+    testResults()
