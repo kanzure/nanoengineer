6b0d9ae823703ac1091dfafbb41f7d94fa618f46
Will Ware <wware@alum.mit.edu>
2006-05-22 15:26:43 +0000

    Fix bug 1931, by doing nothing if neither strand A nor strand B are selected. Prepare the design of both the UI and the generator code for when I get around to putting in bases for A-DNA and B-DNA.

--- src/DnaGenerator.py
+++ src/DnaGenerator.py
@@ -30,6 +30,9 @@ def rotateTranslate(v, theta, z):
 
 atompat = re.compile("atom (\d+) \((\d+)\) \((-?\d+), (-?\d+), (-?\d+)\)")
 
+def odd(n):
+    return (n & 1) != 0
+
 class Dna:
     """This base class may want to provide some provision for the
     fact that we have a two-base periodicity in Z DNA. Or should that
@@ -59,86 +62,91 @@ class Dna:
             theta = 0.0
             z = 0.5 * self.BASE_SPACING * (len(sequence) - 1)
             for i in range(len(sequence)):
-                basename = {
-                    'C': 'cytosine',
-                    'G': 'guanine',
-                    'A': 'adenine',
-                    'T': 'thymine',
-                    }[sequence[i]]
-                if (i & 1) != 0:
-                    suffix = 'outer'
-                    zoffset = 2.045
-                else:
-                    suffix = 'inner'
-                    zoffset = 0.0
-                basefile = 'experimental/zdna-bases/%s-%s.mmp' % (basename, suffix)
-                def tfm(v, theta=theta, z1=z+zoffset):
+                basefile, zoffset, thetaOffset = \
+                          self.strandAinfo(sequence, i)
+                def tfm(v, theta=theta+thetaOffset, z1=z+zoffset):
                     return rotateTranslate(v, theta, z1)
                 self.insertmmp(mol, basefile, tfm)
                 theta -= self.TWIST_PER_BASE
                 z -= self.BASE_SPACING
 
         if strandB:
-            theta = self.STRAND_B_ANGLE + (pi / 6) * (len(sequence) - 1)
-            z = -2.1 - 0.5 * self.BASE_SPACING * (len(sequence) - 1)
+            theta = 0.0
+            z = 0.5 * self.BASE_SPACING * (len(sequence) - 1)
             for i in range(len(sequence)):
                 # The 3'-to-5' direction is reversed for strand B.
-                j = len(sequence) - 1 - i
-                basename = {
-                    'G': 'cytosine',
-                    'C': 'guanine',
-                    'T': 'adenine',
-                    'A': 'thymine',
-                    }[sequence[j]]
-                if (j & 1) != 0:
-                    suffix = 'inner'
-                    zoffset = 2.045
-                    thetaCenter = 0.0
-                else:
-                    suffix = 'outer'
-                    zoffset = 0.0
-                    thetaCenter = 0.0
-                basefile = 'experimental/zdna-bases/%s-%s.mmp' % (basename, suffix)
-                def flip_3_5(v, thetaCenter=thetaCenter):
+                basefile, zoffset, thetaOffset = \
+                          self.strandBinfo(sequence, i)
+                def tfm(v, theta=theta+thetaOffset, z1=z+zoffset):
                     # flip theta, flip z
-                    x, y, z = tuple(v)
-                    r, theta = (x**2 + y**2) ** 0.5, atan2(y, x)
-                    theta = 2 * thetaCenter - theta
-                    x, y = r * cos(theta), r * sin(theta)
-                    return V(x, y, -z)
-                def tfm(v, theta=theta, z1=z+zoffset, flip_3_5=flip_3_5):
-                    return rotateTranslate(flip_3_5(v), theta, z1)
+                    # We can flip theta just by reversing the sign of y
+                    return rotateTranslate(V(v[0], -v[1], -v[2]), theta, z1)
                 self.insertmmp(mol, basefile, tfm)
-                theta += self.TWIST_PER_BASE
-                z += self.BASE_SPACING
+                theta -= self.TWIST_PER_BASE
+                z -= self.BASE_SPACING
 
 
 # There are three kinds of DNA helix geometries, A, B, and Z. I got my hands
 # on a Z file first, so that's what I'm going with.
 
-class Unimplemented(Dna):
-    def make(self, mol, sequence, strandA, strandB):
-        raise Exception("This flavor of DNA is not yet implemented");
+class A_Dna(Dna):
+    TWIST_PER_BASE = 0  # WRONG
+    BASE_SPACING = 0    # WRONG
+    def strandAinfo(self, sequence, i):
+        raise Exception("Not yet implemented -- please try Z DNA for now");
+    def strandBinfo(self, sequence, i):
+        raise Exception("Not yet implemented -- please try Z DNA for now");
 
-class A_Dna(Unimplemented):
-    pass
-
-class B_Dna(Unimplemented):
-    pass
-
+class B_Dna(Dna):
+    TWIST_PER_BASE = 0  # WRONG
+    BASE_SPACING = 0    # WRONG
+    def strandAinfo(self, sequence, i):
+        raise Exception("Not yet implemented -- please try Z DNA for now");
+    def strandBinfo(self, sequence, i):
+        raise Exception("Not yet implemented -- please try Z DNA for now");
 
 class Z_Dna(Dna):
+    TWIST_PER_BASE = pi / 6     # in radians
+    BASE_SPACING = 3.715        # in angstroms
 
     # Z-DNA has a periodicity of two bases. The odd-numbered basis
     # have one orientation, the even-numbered bases have another.
-
-    TWIST_PER_BASE = pi / 6     # in radians
-    BASE_SPACING = 3.715        # in angstroms
-    # This value for the strand B angle really surprises me. I don't understand it.
-    STRAND_B_ANGLE = 0.5 * pi
-
     # Take care of the directory name, and the alternation of inner and
     # outer bases, which won't occur in A and B DNA.
+    def strandAinfo(self, sequence, i):
+        basename = {
+            'C': 'cytosine',
+            'G': 'guanine',
+            'A': 'adenine',
+            'T': 'thymine',
+            }[sequence[i]]
+        if odd(i):
+            suffix = 'outer'
+            zoffset = 2.045
+        else:
+            suffix = 'inner'
+            zoffset = 0.0
+        thetaOffset = 0.0
+        basefile = 'experimental/zdna-bases/%s-%s.mmp' % (basename, suffix)
+        return (basefile, zoffset, thetaOffset)
+
+    def strandBinfo(self, sequence, i):
+        basename = {
+            'G': 'cytosine',
+            'C': 'guanine',
+            'T': 'adenine',
+            'A': 'thymine',
+            }[sequence[i]]
+        if odd(i):
+            suffix = 'inner'
+            zoffset = -0.055
+        else:
+            suffix = 'outer'
+            zoffset = -2.1
+        thetaOffset = 0.5 * pi
+        basefile = 'experimental/zdna-bases/%s-%s.mmp' % (basename, suffix)
+        return (basefile, zoffset, thetaOffset)
+
 
 #################################
 
@@ -150,37 +158,49 @@ class DnaGenerator(DnaGeneratorDialog):
     def __init__(self, win):
         DnaGeneratorDialog.__init__(self, win) # win is parent.  Fixes bug 1089.  Mark 051119.
         self.win = win
+        self.dnaType = 'B'
         
         # Default DNA parameters
+        #self.dnaTypeButtonGroup.setCheckable(True)
+        self.bDnaButton.setChecked(True)
         self.strandAchkbox.setChecked(True)
         self.strandBchkbox.setChecked(True)
         self.seq_linedit.setText('GATTACA')
 
+    def dnaTypeClicked(self, a0):
+        if a0 == 2: self.dnaType = 'A'
+        elif a0 == 1: self.dnaType = 'B'
+        elif a0 == 0: self.dnaType = 'Z'
+
     def accept(self):
         'Slot for the OK button'
 
-        env.history.message(cmd + "Creating DNA. This may take a moment...")
-        QApplication.setOverrideCursor( QCursor(Qt.WaitCursor) )
-        try:
-            self.buildChunk()
-            env.history.message(cmd + "Done.")
-        except Exception, e:
-            env.history.message(cmd + redmsg(" - ".join(map(str, e.args))))
-        QApplication.restoreOverrideCursor() # Restore the cursor
+        strandA = self.strandAchkbox.isChecked()
+        strandB = self.strandBchkbox.isChecked()
+        if strandA or strandB:
+            env.history.message(cmd + "Creating DNA. This may take a moment...")
+            QApplication.setOverrideCursor( QCursor(Qt.WaitCursor) )
+            try:
+                mol = molecule(self.win.assy, chem.gensym("DNA-"))
+                if self.dnaType == 'A':
+                    dna = A_Dna()
+                elif self.dnaType == 'B':
+                    dna = B_Dna()
+                elif self.dnaType == 'Z':
+                    dna = Z_Dna()
+                dna.make(mol, self.seq_linedit.text(), strandA, strandB)
+                inferBonds(mol)
+                part = self.win.assy.part
+                part.ensure_toplevel_group()
+                part.topnode.addchild(mol)
+                self.win.mt.mt_update()
+                env.history.message(cmd + "Done.")
+            except Exception, e:
+                env.history.message(cmd + redmsg(" - ".join(map(str, e.args))))
+            QApplication.restoreOverrideCursor() # Restore the cursor
         QDialog.accept(self)
 
     def reject(self):
-        env.history.message(cmd + "DNA not created, there was a problem.")
-
-    def buildChunk(self):
-        mol = molecule(self.win.assy, chem.gensym("DNA-"))
-        # dna = A_Dna()
-        # dna = B_Dna()
-        dna = Z_Dna()
-        dna.make(mol, self.seq_linedit.text(),
-                 self.strandAchkbox.isChecked(), self.strandBchkbox.isChecked())
-        inferBonds(mol)
-        part = self.win.assy.part
-        part.ensure_toplevel_group()
-        part.topnode.addchild(mol)
-        self.win.mt.mt_update()
+        # The user hit the cancel button. End the dialog without
+        # saying or doing anything.
+        QDialog.accept(self)
--- src/DnaGeneratorDialog.py
+++ src/DnaGeneratorDialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'DnaGeneratorDialog.ui'
 #
-# Created: Fri May 19 13:26:17 2006
+# Created: Mon May 22 11:19:00 2006
 #      by: The PyQt User Interface Compiler (pyuic) 3.14.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -23,19 +23,11 @@ class DnaGeneratorDialog(QDialog):
         self.textLabel1 = QLabel(self,"textLabel1")
         self.textLabel1.setGeometry(QRect(10,20,90,30))
 
-        self.strandAchkbox = QCheckBox(self,"strandAchkbox")
-        self.strandAchkbox.setGeometry(QRect(10,50,80,22))
-        self.strandAchkbox.setChecked(1)
-
         self.seq_linedit = QLineEdit(self,"seq_linedit")
-        self.seq_linedit.setGeometry(QRect(100,20,240,24))
-
-        self.strandBchkbox = QCheckBox(self,"strandBchkbox")
-        self.strandBchkbox.setGeometry(QRect(260,50,80,22))
-        self.strandBchkbox.setChecked(1)
+        self.seq_linedit.setGeometry(QRect(100,20,150,24))
 
         LayoutWidget = QWidget(self,"layout30")
-        LayoutWidget.setGeometry(QRect(10,80,240,50))
+        LayoutWidget.setGeometry(QRect(10,170,220,40))
         layout30 = QHBoxLayout(LayoutWidget,11,6,"layout30")
         spacer1 = QSpacerItem(92,20,QSizePolicy.Expanding,QSizePolicy.Minimum)
         layout30.addItem(spacer1)
@@ -49,13 +41,35 @@ class DnaGeneratorDialog(QDialog):
         self.cancel_btn.setMinimumSize(QSize(0,30))
         layout30.addWidget(self.cancel_btn)
 
+        self.strandBchkbox = QCheckBox(self,"strandBchkbox")
+        self.strandBchkbox.setGeometry(QRect(150,100,80,30))
+        self.strandBchkbox.setChecked(1)
+
+        self.strandAchkbox = QCheckBox(self,"strandAchkbox")
+        self.strandAchkbox.setGeometry(QRect(150,60,80,30))
+        self.strandAchkbox.setChecked(1)
+
+        self.dnaTypeButtonGroup = QButtonGroup(self,"dnaTypeButtonGroup")
+        self.dnaTypeButtonGroup.setGeometry(QRect(10,50,90,101))
+
+        self.zDnaButton = QRadioButton(self.dnaTypeButtonGroup,"zDnaButton")
+        self.zDnaButton.setGeometry(QRect(11,71,70,20))
+
+        self.bDnaButton = QRadioButton(self.dnaTypeButtonGroup,"bDnaButton")
+        self.bDnaButton.setGeometry(QRect(10,40,70,31))
+        self.bDnaButton.setChecked(1)
+
+        self.aDnaButton = QRadioButton(self.dnaTypeButtonGroup,"aDnaButton")
+        self.aDnaButton.setGeometry(QRect(10,20,70,20))
+
         self.languageChange()
 
-        self.resize(QSize(358,161).expandedTo(self.minimumSizeHint()))
+        self.resize(QSize(279,232).expandedTo(self.minimumSizeHint()))
         self.clearWState(Qt.WState_Polished)
 
         self.connect(self.ok_btn,SIGNAL("clicked()"),self.accept)
         self.connect(self.cancel_btn,SIGNAL("clicked()"),self.reject)
+        self.connect(self.dnaTypeButtonGroup,SIGNAL("clicked(int)"),self.dnaTypeClicked)
 
         self.setTabOrder(self.ok_btn,self.cancel_btn)
 
@@ -63,14 +77,21 @@ class DnaGeneratorDialog(QDialog):
     def languageChange(self):
         self.setCaption(self.__tr("Dna Generator"))
         self.textLabel1.setText(self.__tr("Sequence :"))
-        self.strandAchkbox.setText(self.__tr("Strand A"))
         self.seq_linedit.setText(self.__tr("GATTACA"))
-        self.strandBchkbox.setText(self.__tr("Strand B"))
         self.ok_btn.setText(self.__tr("&OK"))
         self.ok_btn.setAccel(self.__tr("Alt+O"))
         self.cancel_btn.setText(self.__tr("&Cancel"))
         self.cancel_btn.setAccel(self.__tr("Alt+C"))
+        self.strandBchkbox.setText(self.__tr("Strand B"))
+        self.strandAchkbox.setText(self.__tr("Strand A"))
+        self.dnaTypeButtonGroup.setTitle(self.__tr("DNA type"))
+        self.zDnaButton.setText(self.__tr("Z DNA"))
+        self.bDnaButton.setText(self.__tr("B DNA"))
+        self.aDnaButton.setText(self.__tr("A DNA"))
 
+
+    def dnaTypeClicked(self,a0):
+        print "DnaGeneratorDialog.dnaTypeClicked(int): Not implemented yet"
 
     def __tr(self,s,c = None):
         return qApp.translate("DnaGeneratorDialog",s,c)
