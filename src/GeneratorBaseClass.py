3380e16a87f34e621e9899a45c60aebc5ddacc64
Will Ware <wware@alum.mit.edu>
2006-06-01 19:06:56 +0000

    More logic cleanup, and some pydoc stuff.

--- src/GeneratorBaseClass.py
+++ src/GeneratorBaseClass.py
@@ -3,13 +3,9 @@
 GeneratorBaseClass.py
 
 $Id$
-
-There is some logic associated with Preview/OK/Abort that's complicated enough
-to put it in one place, so that individual generators can focus on what they
-need to do. As much as possible, the individual generator should not need to
-worry about the GUI.
 """
 
+import platform
 import env
 from qt import Qt, QApplication, QCursor, QDialog, QImage, QPixmap
 from Sponsors import findSponsor
@@ -17,36 +13,41 @@ from HistoryWidget import redmsg, orangemsg, greenmsg
 
 __author__ = "Will"
 
-DEBUG = False
+class AbstractMethod(Exception):
+    def __init__(self):
+        Exception.__init__(self, 'Abstract method - must be overloaded')
 
 class GeneratorBaseClass:
+    """There is some logic associated with Preview/OK/Abort that's
+    complicated enough to put it in one place, so that individual
+    generators can focus on what they need to do. As much as possible,
+    the individual generator should not need to worry about the GUI.
+    """
 
     sponsor_keyword = None
 
     # pass window arg to constructor rather than use a global, wware 051103
     def __init__(self, win):
         self.win = win
-        self.group = None
+        self.struct = None
         self.previousParams = None
         assert self.sponsor_keyword != None
         self.sponsor = sponsor = findSponsor(self.sponsor_keyword)
         sponsor.configureSponsorButton(self.sponsor_btn)
 
     def build_struct(self):
-        '''Build the structure in question. This is an abstract
-        method and must be overloaded in the specific generator.
-        The return value should
+        '''Build the structure in question. This is an abstract method
+        and must be overloaded in the specific generator. The return
+        value should be the structure, i.e. some flavor of a Node.
         '''
-        raise Exception("Not implemented in the base class")
+        raise AbstractMethod()
 
     def remove_struct(self):
-        if self.group != None:
-            part = self.win.assy.part
-            part.ensure_toplevel_group()
-            part.topnode.delmember(self.group)
+        if self.struct != None:
+            self.struct.kill()
+            self.struct = None
             self.win.win_update()
             self.win.mt.mt_update()
-            self.group = None
 
     def preview_btn_clicked(self):
         QApplication.setOverrideCursor( QCursor(Qt.WaitCursor) )
@@ -55,27 +56,33 @@ class GeneratorBaseClass:
         except Exception, e:
             env.history.message(self.cmd + redmsg(" - ".join(map(str, e.args))))
             self.remove_struct()
-            if DEBUG: raise
+            if platform.atom_debug: raise
         QApplication.restoreOverrideCursor() # Restore the cursor
         self.win.win_update()
         self.win.mt.mt_update()
 
     def gather_parameters(self):
-        '''Return a tuple of the current parameters'''
-        raise Exception("Not implemented in the base class")
+        '''Return a tuple of the current parameters. This is an
+        abstract method and must be overloaded in the specific
+        generator.'''
+        raise AbstractMethod()
 
     def done_msg(self):
-        raise Exception("Not implemented in the base class")
+        '''Tell what message to print when the structure has been
+        built. This is an abstract method and must be overloaded in
+        the specific generator.
+        '''
+        raise AbstractMethod()
 
     def _build_struct(self):
         params = self.gather_parameters()
         if params != self.previousParams:
             self.remove_struct()
             self.previousParams = params
-        self.group = grp = self.build_struct(params)
+        self.struct = struct = self.build_struct(params)
         part = self.win.assy.part
         part.ensure_toplevel_group()
-        part.topnode.addchild(grp)
+        part.topnode.addchild(struct)
         self.win.win_update()
         self.win.mt.mt_update()
 
@@ -85,28 +92,30 @@ class GeneratorBaseClass:
         try:
             self._build_struct()
             env.history.message(self.cmd + self.done_msg())
-            self.group = None
+            self.struct = None
         except Exception, e:
             env.history.message(self.cmd + redmsg(" - ".join(map(str, e.args))))
             self.remove_struct()
-            if DEBUG: raise
+            if platform.atom_debug: raise
         QApplication.restoreOverrideCursor() # Restore the cursor
         QDialog.accept(self)
 
     def done_btn_clicked(self):
+        'Slot for the Done button'
         self.ok_btn_clicked()
 
     def abort_btn_clicked(self):
+        'Slot for the Abort button'
         self.cancel_btn_clicked()
 
     def cancel_btn_clicked(self):
-        'Slot for the OK button'
+        'Slot for the Cancel button'
         self.remove_struct()
         QDialog.accept(self)
 
     def close(self, e=None):
-        """When the user closes dialog by clicking the 'X' button on
-        the dialog title bar, this method is called.
+        """When the user closes the dialog by clicking the 'X' button
+        on the dialog title bar, do whatever the cancel button does.
         """
         try:
             self.cancel_btn_clicked()
