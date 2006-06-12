3981b7311a8bd24cb6f018e5f1c3b9b4c7de632e
Will Ware <wware@alum.mit.edu>
2006-06-12 20:41:23 +0000

    Move the pyrexc-finding code into the sim repository.

---
+++ src/findpyrex.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2006 Nanorex, Inc.  All rights reserved.
+
+__author__ = "Will"
+
+import sys, os, Pyrex
+
+def find_pyrexc():
+    if sys.platform == 'darwin':
+        x = os.path.dirname(Pyrex.__file__).split('/')
+        return '/'.join(x[:-4] + ['bin', 'pyrexc'])
+    elif sys.platform == 'linux2':
+        if os.path.exists('/usr/bin/pyrexc'):
+            return '/usr/bin/pyrexc'
+        if os.path.exists('/usr/local/bin/pyrexc'):
+            return '/usr/local/bin/pyrexc'
+        raise Exception('cannot find pyrexc')
+    else:
+        # windows
+        return 'python c:/Python' + sys.version[:3] + '/Scripts/pyrexc.py'
