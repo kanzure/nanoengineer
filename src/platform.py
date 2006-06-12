1e63043f943be29ec6dd9a928901abbba7c78531
Will Ware <wware@alum.mit.edu>
2006-06-12 15:31:24 +0000

    Put in a function for locating the pyrex compiler.

--- src/platform.py
+++ src/platform.py
@@ -685,6 +685,21 @@ def get_gms_name():
     
     return gms_name
 
+def find_pyrexc():
+    import Pyrex
+    if sys.platform == 'darwin':
+        x = os.path.dirname(Pyrex.__file__).split('/')
+        return '/'.join(x[:-4] + ['bin', 'pyrexc'])
+    elif sys.platform == 'linux2':
+        if os.path.exists('/usr/bin/pyrexc'):
+            return '/usr/bin/pyrexc'
+        if os.path.exists('/usr/local/bin/pyrexc'):
+            return '/usr/local/bin/pyrexc'
+        raise Exception('cannot find pyrexc')
+    else:
+        # windows
+        return 'python c:/Python' + sys.version[:3] + '/Scripts/pyrexc.py'
+
 # ==
 
 #bruce 051201 moved wiki help code from here to new file wiki_help.py
