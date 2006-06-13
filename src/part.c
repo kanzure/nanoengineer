29ca1959cd80b8ef64a9917b20cb5a8ab526b566
Eric Messick <ericm@nanorex.com>
2006-06-13 00:14:04 +0000

    tweak out-of-plane stiffness

--- src/part.c
+++ src/part.c
@@ -447,7 +447,7 @@ makeOutOfPlane(struct part *p, int index, struct atom *a)
     o->a3 = b->a1 == a ? b->a2 : b->a1;
 
     // A is in aJ/pm^2
-    o->A = 0.005; // XXX need to get actual value from real parameters
+    o->A = 0.0005; // XXX need to get actual value from real parameters
 }
 
 // Creates an outOfPlane for each sp2 atom
