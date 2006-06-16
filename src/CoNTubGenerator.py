720360b07eb949c677385bf431f23b912be36e6d
Bruce Smith <bruce@nanorex.com>
2006-06-16 05:23:31 +0000

    not done

--- src/CoNTubGenerator.py
+++ src/CoNTubGenerator.py
@@ -12,10 +12,12 @@ as much as possible be isolated into small parts of this, with most of it
 knowing nothing about CoNTub's specific functionality or parameters.
 """
 
+###@@@ where i am -- see all the IMPLEMS far below [060615 eve]
+
 __author__ = "bruce"
 
 #k not all imports needed?
-## from GeneratorDialogs import ParameterizedDialog ###IMPLEM file and class
+## from GeneratorDialogs import ParameterDialog ###IMPLEM file and class
 import env
 from HistoryWidget import redmsg, orangemsg, greenmsg
 ##from widgets import double_fixup
@@ -23,7 +25,9 @@ from HistoryWidget import redmsg, orangemsg, greenmsg
 from GeneratorBaseClass import GeneratorBaseClass
 from debug import print_compact_traceback
 import os, sys
-from platform import find_or_make_Nanorex_subdir, find_or_make_any_directory
+from platform import find_or_make_Nanorex_subdir, find_or_make_any_directory, tempfiles_dir
+
+### current bug: menu icon is nondeterministic. guess: need to keep a reference to the iconset that we make for it. #####@@@@@
 
 # ==
 
@@ -88,6 +92,8 @@ class PluginlikeGenerator:
     ok_to_install_in_UI = False # changed to True in instances which become ok to install into the UI; see also errorcode
     # default values of subclass-specific class constants
     what_we_generate = "Something"
+
+    # init methods
     
     def register(subclass): # staticmethod
         win = env.mainwindow()
@@ -103,33 +109,164 @@ class PluginlikeGenerator:
         return
     register = staticmethod(register)
 
-    errorcode = 0 # set to something true if an error occurs which should prevent us from continuing to set up this plugin
+    errorcode = 0
+        # this gets set to something true (by self.fatal) if an error occurs which should
+        # permanently disable this plugin (during setup or use)
+    errortext = ""
+        # this gets set to errortext for the first error that permanently disabled this plugin
+
     def fatal(self, errortext, errorcode = 1):
-        "Init submethods call this if they detect a fatal setup error; it prints the message appropriately and sets self.errorcode."
-        assert errorcode
+        """Our submethods call this to report a fatal setup/use error; it prints errortext appropriately
+        and sets self.errorcode and self.errortext.
+        """
+        if not errorcode:
+            print "bug: fatal errorcode must be a boolean-true value, not %r" % (errorcode,)
+            errorcode = 1
         if self.errorcode:
-            print "plugin %r bug: self.errorcode was already set before fatal was called
-        print "plugin %r setup error: %s" % (errortext,)
+            print "plugin %r bug: self.errorcode was already set before fatal was called" % (self.plugin_name,)
+        if not self.errorcode or not self.errortext:
+            self.errortext = errortext # permanent record for use by callers
+        self.errorcode = errorcode
+        msg = "plugin %r fatal error: %s" % (self.plugin_name, errortext,)
+        print msg
+        env.history.message(redmsg(msg)) # it might be too early for this to be seen
+        return errorcode
 
     def __init__(self, win):
         self.win = win
 
-        # find plugin dir -- if not, error, and don't install in UI
-        # (as if we don't know the metadata needed to do so)
-        path = self.find_plugin_dir() # might set self.find_plugin_dir_errortext
-        if not path:
-            ###e too soon for history message??
-            self.fatal( self.find_plugin_dir_errortext)
-            return
+        # All these submethods should call self.fatal to report permanent fatal errors.
+        # And after calling the ones that can, we should check self.errorcode before continuing.
+        
+        # Find plugin dir -- if we can't, it has to be a fatal error,
+        # since (once this is using a real plugin API) we won't have the metadata
+        # needed to install the plugin in the UI.
+        
+        path = self.find_plugin_dir()
+        if self.errorcode: return
         self.plugin_dir = path # for error messages, and used by runtime methods
         
-        # make sure the stuff we need is in there (and try to use it to set up the dialogs, commands, etc)
-        if not self.setup_from_plugin_dir():
-            # that printed error msgs if it needed to
-            return
-        
+        # make sure the stuff we need is in the plugin dir (and try to use it to set up the dialogs, commands, etc)
+        self.setup_from_plugin_dir() # this prints error msgs if it needs to
+        if self.errorcode: return
+
+        # don't create a working directory until the plugin is first used
+        # (since we don't want to create one at all, if it's not used in the session,
+        #  since they might be created in a session-specific place)
+        self.working_directory = None
+
+        if env.debug(): print "plugin init is permitting ok_to_install_in_UI = True" ###@@@
         self.ok_to_install_in_UI = True
         return
+
+    def find_plugin_dir(self):
+        ok, path = find_plugin_dir(self.plugin_name)
+        if ok:
+            assert os.path.isdir(path)
+            return path
+        else:
+            errortext = path
+            self.fatal( errortext)
+            return None
+        pass
+    
+    def setup_from_plugin_dir(self):
+        "Using self.plugin_dir, setup dialogs, commands, etc. Report errors to self.fatal as usual."
+        param_desc_path = os.path.join(self.plugin_dir, self.parameter_set_filename)
+        self.param_desc_path = param_desc_path
+        if not os.path.isfile(param_desc_path):
+            return self.fatal("can't find param description file [%s]" % (param_desc_path,))
+        ###e get the param set, create the dialog, check the executable exists, etc
+        # (even run a self test if it defines one? or wait 'til first used?)
+        return
+
+    def install_in_UI(self):
+        """Create a menu command, or whatever other UI elements should invoke the plugin's generator.
+        Report errors to self.fatal as usual.
+        """
+        assert self.ok_to_install_in_UI
+        #e create whatever we want to be persistent which was not already done in setup_from_plugin_dir (nothing yet?)
+        
+        #e install the necessary commands in the UI (eg in insert menu)
+        ### WRONG -- menu text should not contain Insert, but undo cmdname should (so separate option is needed), and needs icon
+        ###e add options for things like tooltip text, whatsthis text, iconset
+        options = [('iconset','junk.png')]
+        self.menu_item_id = add_insert_menu_item( self.win, self.command_for_insert_menu, self.what_we_generate, options)
+        ###e make that a menu item controller, and give it a method to disable the menu item, and do that on error(??) ###@@@
+        pass
+
+    # runtime methods
+    
+    def create_working_directory_if_needed(self):
+        """If it hasn't been done already, create a temporary directory (fixed pathname per plugin per session)
+        for this plugin to use. Report errors to self.fatal as usual.
+        """
+        if self.working_directory:
+            return
+        subdir = os.path.join( tempfiles_dir(), "plugin-" + self.plugin_name )
+        errorcode, path = find_or_make_any_directory(subdir)
+        if errorcode:
+            # should never happen, but make sure caller checks self.errorcode (set by fatal) just in case #####@@@@@
+            errortext = path
+            return self.fatal(errortext)
+        self.working_directory = subdir
+        return
+
+    dialog = None
+    param_desc_path_modtime = None
+    
+    def make_dialog_if_needed(self):
+        "Create self.dialog if necessary."
+        # For developers, remake the dialog from its description file each time that file changes.
+        # (The point of only remaking it then is not speed, but to test the code when it doesn't get remade,
+        #  since that's what always happens for non-developers.)
+        # (Someday, when remaking it, copy its window geometry from the old one. Then put that code into the MMKit too. ###e)        
+        # For others, only make it the first time.
+
+        import __main__
+        if (not __main__._end_user or env.debug()) and self.dialog:
+            # For developers, remake the dialog if its description file changed (by zapping the old dialog here).
+            zapit = False
+            modtime = os.stat(self.param_desc_path).st_mtime
+            if modtime != self.param_desc_path_modtime:
+                zapit = True
+                self.param_desc_path_modtime = modtime
+            if zapit:
+                #e save geometry?
+                self.dialog.hide()
+                self.dialog.destroy() ###k
+                self.dialog = None
+            pass
+        if not self.dialog:
+            if env.debug(): print "making dialog from", self.parameter_set_filename
+            self.dialog = ParameterDialog( self.param_desc_path )### IMPLEM
+            #e set its geometry if that was saved (from above code or maybe in prefs db)
+        return
+    
+    def command_for_insert_menu(self):
+        """Run an Insert Whatever menu command to let the user generate things using this plugin.
+        """
+        if self.errorcode:
+            env.history.message(redmsg("Plugin %r is permanently disabled due to this error, reported previously: %s" % \
+                               (self.plugin_name, self.errortext)))
+            return
+        self.create_working_directory_if_needed()
+        assert not self.errorcode
+        print 'ought to insert a', self.what_we_generate ###@@@
+        self.make_dialog_if_needed()
+        dialog = self.dialog
+        ###e Not yet properly handled: retaining default values from last time it was used. (Should pass dict of them to the maker.)
+        dialog.set_defaults({}) ### IMPLEM 
+        controller = PluginlikeGenerator_DialogController(self)### IMPLEM
+        dialog.set_controller( controller )### IMPLEM; this should refdecr the prior controller and that should destroy it...
+        dialog.show()
+        # now it's able to take commands and run its callbacks; that does not happen inside this method, though, does it?
+        # hmm, good question... if it's modal, this makes things easier (re preview and bug protection)...
+        # and it means the undo wrapping was ok... but what do we do here to make it modal?
+        # 1. find out by test if other generators are modal.
+        # 2. find out from code, how.
+        
+        pass###e
         
 ##    def commandline_args(self, params):
 ##        "Return the arguments for the HJ commandline, not including the output filename..." ###
@@ -141,52 +278,7 @@ class PluginlikeGenerator:
 ##        # run it
 ##        # look at exitcode
 ##        # etc
-    def find_plugin_dir(self):
-        ok, path = find_plugin_dir(self.plugin_name)
-        if ok:
-            assert os.path.isdir(path)
-            return path
-        else:
-            self.find_plugin_dir_errortext = path
-            return None
-        pass
-    
-    def setup_from_plugin_dir(self):
-        "Using self.plugin_dir, setup dialogs, commands, etc. Report errors to self.fatal as usual."
-        param_desc_path = os.path.join(self.plugin_dir, self.parameter_set_filename)
-        self.param_desc_path = param_desc_path
-        if not os.path.isfile(param_desc_path):
-            return self.fatal("can't find param description file [%s]" % (param_desc_path,)
-        ###E more -- the dialog, param set, executable exists, etc (even a self test if it defines one?)
-        return
-    
-    def create_working_directory(self): # we should not call this until the plugin is first used. #####@@@@@
-        "Create a temporary directory (fixed pathname per plugin) for this plugin to use. Report errors to self.fatal as usual."
-        subdir = os.path.join( tempfiles_dir(), "plugin-" + self.plugin_name )
-        errorcode, path = find_or_make_any_directory(subdir)
-        if errorcode:
-            # should never happen; not sure we can rely on this fatal system since this is done only when needed ###@@@
-            errortext = path
-            return self.fatal(errortext)
-        self.working_path = working_path
-        return
-        
-    def install_in_UI(self):
-        assert self.ok_to_install_in_UI
-        #e create whatever we want to be persistent which was not already done in setup_from_plugin_dir (e.g. a job dir for it)
-        
-        0
-        #e install the necessary commands in the UI (eg in insert menu)
-        ### WRONG -- menu text should not contain Insert, but undo cmdname should (so separate option is needed), and needs icon
-        ###e add options for things like tooltip text, whatsthis text, iconset
-        options = [('iconset','junk.png')]
-        add_insert_menu_item( self.win, self.insert_menu_command, self.what_we_generate, options)
-        pass
-    def insert_menu_command(self):
-        """Run an Insert Whatever menu command to let the user generate things using this plugin.
-        """
-        print 'ought to insert a', self.what_we_generate
-        pass###e
+
     pass # end of class PluginlikeGenerator
 
 class HeterojunctionGenerator(PluginlikeGenerator):
@@ -204,7 +296,7 @@ class HeterojunctionGenerator(PluginlikeGenerator):
     menu_item_icon = "blablabla"
     plugin_name = "CoNTub"
         # used as directory name, looked for in ~/Nanorex/Plugins someday, and in cad/plugins now and someday...
-    parameter_set_filename = "HJ_param_desc.txt" #e or .desc?
+    parameter_set_filename = "HJ_param.desc"
     executable = "HJ" # no .exe, we'll add that if necessary on Windows ## this might not be required of every class
     outputfiles_pattern = "$out1.mmp"
     executable_args_pattern = "$p1 $p2 $out1.mmp"
