b972c661bd23b87175b4e1b8dc022ddb9f239b41
Will Ware <wware@alum.mit.edu>
2006-06-23 14:42:31 +0000

    Generate the ".o: .cpp" dependencies separately from the header file dependencies. That way, an object file with no header files still gets built.

--- plugins/CoNTub/procdeps.py
+++ plugins/CoNTub/procdeps.py
@@ -4,7 +4,9 @@
 import sys, re
 
 srcdir = sys.argv[1]
-substitution = sys.argv[2]
+objdir = sys.argv[2]
+substitution = sys.argv[3]
+objs = sys.argv[4:]
 
 # This is a little kludgey. The idea is to allow either C files (with
 # ".o:.c") or C++ files (with ".o:.cpp") and ideally as many other
@@ -21,18 +23,26 @@ def subAll(pattern, repl, str):
             return str
         str = s
 
+print '# Automatically generated dependencies from procdeps.py'
+
+for obj in objs:
+    # Generate the source file for this object file, and put it in SRCDIR.
+    src = re.sub(obj_ending + '$', src_ending, obj)
+    src = re.sub(objdir + '/', '$(SRCDIR)/', src)
+    # Put this object file in OBJDIR.
+    obj = re.sub(objdir + '/', '$(OBJDIR)/', obj)
+    # Print a dependency line.
+    print obj + ': ' + src
+
 for L in sys.stdin.readlines():
     L = L.rstrip()
     # Remove all absolute header files, like /usr/include stuff.
     L = subAll(' /[^ ]*\.h', '', L)
     if L and not L.startswith('#') and not L.rstrip().endswith(':'):
         obj, hfiles = L.split(':')
-        # Generate the source file for this object file, and put it in SRCDIR.
-        src = re.sub(obj_ending + '$', src_ending, obj)
-        src = re.sub(srcdir + '/', '$(SRCDIR)/', src)
         # Put this object file in OBJDIR.
         obj = re.sub(srcdir + '/', '$(OBJDIR)/', obj)
         # Put the header files in SRCDIR.
         hfiles = subAll(srcdir + '/', '$(SRCDIR)/', hfiles)
         # Print the modified dependency line.
-        print obj + ': ' + src + hfiles
+        print obj + ':' + hfiles
