364b7816c5cc781b89417a9ec4531c26765e3a28
Will Ware <wware@alum.mit.edu>
2006-07-04 22:05:21 +0000

    oops, wrong signal

--- src/NanotubeGeneratorDialog.py
+++ src/NanotubeGeneratorDialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'NanotubeGeneratorDialog.ui'
 #
-# Created: Tue Jul 4 17:54:51 2006
+# Created: Tue Jul 4 18:05:15 2006
 #      by: The PyQt User Interface Compiler (pyuic) 3.14.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -765,8 +765,8 @@ class nanotube_dialog(QDialog):
         self.connect(self.restore_defaults_btn,SIGNAL("clicked()"),self.defaults_btn_clicked)
         self.connect(self.chirality_n_spinbox,SIGNAL("valueChanged(int)"),self.parameter_fixup)
         self.connect(self.chirality_m_spinbox,SIGNAL("valueChanged(int)"),self.parameter_fixup)
-        self.connect(self.members_combox,SIGNAL("textChanged(const QString&)"),self.parameter_fixup)
         self.connect(self.endings_combox,SIGNAL("textChanged(const QString&)"),self.parameter_fixup)
+        self.connect(self.members_combox,SIGNAL("activated(const QString&)"),self.parameter_fixup)
 
 
     def languageChange(self):
