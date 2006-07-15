f164f09ab71678cd7f2c7e1b2328362443cb40f9
Al Oshevchenko <oshevchenko@nanorex.com>
2006-07-15 18:59:55 +0000

    added new functions and c++ files

--- src/experimental/oleksandr/csurface.h
+++ src/experimental/oleksandr/csurface.h
@@ -1,17 +1,62 @@
+/*
+  Name: csurface.h
+  Copyright: 2006 Nanorex, Inc.  All rights reserved.
+  Author: Oleksandr Shevchenko
+  Description: C helper for psurface.pyx 
+*/
+
+#if !defined(CSURFACE_INCLUDED)
+#define CSURFACE_INCLUDED
+
+#include "cppsurface.h"
+
 void cAdd(double x, double y, double z, double r)
 {
+	cppAdd(x,y,z,r);
 }
 void cCreateSurface()
 {
+	cppCreateSurface();
 }
 void cClear()
 {
+	cppClear();
 }
 int cNp()
 {
-	return 1; 
+	return cppNp();
 }
 int cNt()
 {
-	return 2; 
+	return cppNt();
 }
+double cPx(int i)
+{
+	return cppPx(i);
+}
+double cPy(int i)
+{
+	return cppPy(i);
+}
+double cPz(int i)
+{
+	return cppPz(i);
+}
+double cNx(int i)
+{
+	return cppNx(i);
+}
+double cNy(int i)
+{
+	return cppNy(i);
+}
+double cNz(int i)
+{
+	return cppNz(i);
+}
+int cI(int i)
+{
+	return cppI(i);
+}
+
+#endif  								// CSURFACE_INCLUDED
--- src/experimental/oleksandr/psurface.pyx
+++ src/experimental/oleksandr/psurface.pyx
@@ -4,6 +4,13 @@ cdef extern from "csurface.h":
     void cClear()
     int cNp()
     int cNt()
+    double cPx(int i)
+    double cPy(int i)
+    double cPz(int i)
+    double cNx(int i)
+    double cNy(int i)
+    double cNz(int i)
+    int cI(int i)
 def Add(double x, double y, double z, double r):    
     cAdd(x, y, z, r)    
 def CreateSurface():    
@@ -14,7 +21,20 @@ def Np():
     return cNp()    
 def Nt():    
     return cNt()    
-   
+def Px(int i):    
+    return cPx(i)    
+def Py(int i):    
+    return cPy(i)    
+def Pz(int i):    
+    return cPz(i)    
+def Nx(int i):    
+    return cNx(i)    
+def Ny(int i):    
+    return cNy(i)    
+def Nz(int i):    
+    return cNz(i)    
+def I(int i):    
+    return cI(i)    
 
 
 
