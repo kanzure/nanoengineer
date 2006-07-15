665ce304e6565151f6bb2b2c1bb42fe0c87263d2
Al Oshevchenko <oshevchenko@nanorex.com>
2006-07-15 18:58:34 +0000

    added new c++ file to call surface stuff from pyrex

---
+++ src/experimental/oleksandr/cppsurface.cpp
@@ -0,0 +1,55 @@
+/*
+  Name: cppsurface.cpp
+  Copyright: 2006 Nanorex, Inc.  All rights reserved.
+  Author: Oleksandr Shevchenko
+  Description: CPP functions to call from C 
+*/
+
+#include "cppsurface.h"
+
+void cppAdd(double x, double y, double z, double r)
+{
+}
+void cppCreateSurface()
+{
+}
+void cppClear()
+{
+}
+int cppNp()
+{
+    return 0;
+}
+int cppNt()
+{
+    return 1;
+}
+double cppPx(int i)
+{
+    return i;
+}
+double cppPy(int i)
+{
+    return i;
+}
+double cppPz(int i)
+{
+    return i;
+}
+double cppNx(int i)
+{
+    return i;
+}
+double cppNy(int i)
+{
+    return i;
+}
+double cppNz(int i)
+{
+    return i;
+}
+int cppI(int i)
+{
+    return i;
+}
+
---
+++ src/experimental/oleksandr/cppsurface.h
@@ -0,0 +1,32 @@
+/*
+  Name: cppsurface.h
+  Copyright: 2006 Nanorex, Inc.  All rights reserved.
+  Author: Oleksandr Shevchenko
+  Description: CPP functions to call from C 
+*/
+
+#if !defined(CPPSURFACE_INCLUDED)
+#define CPPSURFACE_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cppAdd(double x, double y, double z, double r);
+void cppCreateSurface();
+void cppClear();
+int cppNp();
+int cppNt();
+double cppPx(int i);
+double cppPy(int i);
+double cppPz(int i);
+double cppNx(int i);
+double cppNy(int i);
+double cppNz(int i);
+int cppI(int i);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  								// CPPSURFACE_INCLUDED
