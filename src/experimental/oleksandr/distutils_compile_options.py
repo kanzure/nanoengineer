7a013ca954af51db8b9d8b67cffbd20d408ded28
Will Ware <wware@alum.mit.edu>
2006-07-18 18:02:35 +0000

    The psurface code should now build on all three platforms. Not yet tested on the Mac.

---
+++ src/experimental/oleksandr/distutils_compile_options.py
@@ -0,0 +1,22 @@
+import sys
+import distutils.sysconfig
+import distutils.ccompiler
+
+class FakeCompiler:
+    def __init__(self):
+        self.compiler_type = distutils.ccompiler.get_default_compiler()
+    def set_executables(self, **kw):
+        for k in kw: setattr(self, k, kw[k])
+
+mcc = FakeCompiler()
+distutils.sysconfig.customize_compiler(mcc)
+
+if len(sys.argv) < 2:
+    import pprint
+    pprint.pprint(mcc.__dict__)
+else:
+    for arg in sys.argv[1:]:
+        cmd = getattr(mcc, arg)
+        if cmd.startswith("gcc ") or cmd.startswith("g++ "):
+            cmd = cmd[4:]
+        print cmd
