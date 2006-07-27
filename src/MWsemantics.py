7ad67ddefef28e621bd87760579eb4f81ee2e6c2
Will Ware <wware@alum.mit.edu>
2006-07-27 20:42:27 +0000

    incorrect capitalization, endsWith, should be endswith

--- src/MWsemantics.py
+++ src/MWsemantics.py
@@ -1078,7 +1078,7 @@ class MWsemantics( fileSlotsMixin, viewSlotsMixin, movieDashboardSlotsMixin, Mai
         versionString = repr(v) + (" (%s)" % v.releaseType)
         date = "Release Date: " + v.releaseDate
         filePath = os.path.dirname(os.path.abspath(sys.argv[0]))
-	if filePath.endsWith('/Contents/Resources'):
+	if filePath.endswith('/Contents/Resources'):
             filePath = filePath[:-19]
         installdir = "Running from: " + filePath
         techsupport = "For technical support, send email to support@nanorex.com"
