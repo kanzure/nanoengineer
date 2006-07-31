a840b88140c4775032e7bb8461efeb794e7f575e
Will Ware <wware@alum.mit.edu>
2006-07-31 20:39:11 +0000

    This is a bunch of stuff sitting on my hard disk that I thought I checked in long ago. Of potential educational value for future developers.

---
+++ src/experimental/pyrex-example/futz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+
+// Position of the eye in spherical coordinates
+#define EYE_THETA  0.0
+#define EYE_PHI  0.0
+#define EYE_R  100.0
+
+// Position of the eye in cartesian coordinates
+#define EYE_X  (EYE_R * cos(EYE_THETA) * cos(EYE_PHI))
+#define EYE_Y  (EYE_R * sin(EYE_THETA) * cos(EYE_PHI))
+#define EYE_Z  (EYE_R * sin(EYE_PHI))
+
+
+void
+glSetup(int windowWidth, int windowHeight)
+{
+
+	exit(1);
+
+}
+
+
+void
+line(float x1, float y1, float z1,
+     float x2, float y2, float z2,
+     float r, float g, float b)
+{
+
+}
+
+
+void
+sphere(double x, double y, double z, double radius, double r, double g, double b)
+{
+}
+
---
+++ src/experimental/pyrex-example/putz.pyx
@@ -0,0 +1,50 @@
+import math
+import qt
+import qtgl
+import sys
+import random
+
+cdef extern from "futz.c":
+    # Does OpenGL compel the use of floats, or are doubles OK?
+    void line(float x1, float y1, float z1,
+              float x2, float y2, float z2,
+              float r, float g, float b)
+    void sphere(double x, double y, double z,
+                double radius,
+                double r, double g, double b)
+    void glSetup(int windowWidth, int windowHeight)
+
+
+class GLPane(qtgl.QGLWidget):
+    def __init__(self, master=None, name=None, win=None):
+        qtgl.QGLWidget.__init__(self, master, name)
+        self.win = win
+        self.redrawGL = True
+        self.setFocusPolicy(qt.QWidget.StrongFocus)
+        glSetup(600, 400)
+        print "GLPane constructor ran"
+        self.update()
+    def update(self):
+        line(100 * random.random(), 100 * random.random(), 100 * random.random(),
+             100 * random.random(), 100 * random.random(), 100 * random.random(),
+             random.random(), random.random(), random.random())
+        qtgl.QGLWidget.update(self)
+    # use self.update to repaint
+
+class MainWindow(qt.QMainWindow):
+    def __init__(self):
+        qt.QMainWindow.__init__(self)
+        self.setEnabled(1)
+        self.setOpaqueMoving(0)
+        self.glpane = GLPane(self, "glpane", self)
+    def show(self):
+        self.glpane.show()
+        qt.QMainWindow.show(self)
+
+
+def main():
+    qt.QApplication.setColorSpec(qt.QApplication.CustomColor)
+    app = qt.QApplication(sys.argv)
+    app.connect(app, qt.SIGNAL("lastWindowClosed()"), app.quit)
+    win = MainWindow()
+    win.show()
