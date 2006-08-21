a662e7937a6579aac75d758f8621b70cfd73dbde
Ninad Sathaye <ninad@nanorex.com>
2006-08-21 23:23:03 +0000

    Dynamic tool tip that shows angle too. Several changesTested but might have some bugs. See code comments. Need to discuss with Bruce/Mark about a kludge I have put in there.  (that removes highlighted objject from select atom's list temporarily and appends it back later. ) It doesn't cause any bugs in this feature but need to know if it is safe in general. If not, then what is a better way to do it.

--- src/DynamicTip.py
+++ src/DynamicTip.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2004-2006 Nanorex, Inc.  All rights reserved.
 """
-DynamicTip.py -- 
+DynamicTip.py 
 
 For the support of dynamic, informative tooltips of a highligthed object in the GLPane. 
 
 History: 
 060817 Mark created dynamicTip class
-060818 Ninad moved DynamiceTip class into this file DynamicTip.py
+060818 Ninad moved DynamiceTip class into this file DynamicTip.py and added more
 
 
 $Id$
@@ -18,6 +18,7 @@ import env
 import preferences
 from prefs_constants import dynamicToolTipWakeUpDelay_prefs_key
 from selectMode import *
+from math import *
 
 
 
@@ -31,6 +32,7 @@ class DynamicTip(QToolTip): # Mark and Ninad 060817.
         # <toolTipShown> is a flag set to True when a tooltip is currently displayed for the 
         # highlighted object under the cursor.
         self.toolTipShown = False
+        
      
     def maybeTip(self, cursorPos):
         """Determines if this tooltip should be displayed. The tooltip will be displayed at
@@ -107,6 +109,8 @@ class DynamicTip(QToolTip): # Mark and Ninad 060817.
         from ops_select import ops_select_Mixin 
         
         glpane = self.glpane
+        
+        objStr = self.getHighlightedObjectName()
                        
         selectedAtomList = glpane.assy.getOnlyAtomsSelectedByUser()
         selectedJigList = glpane.assy.getSelectedJigs()
@@ -114,8 +118,9 @@ class DynamicTip(QToolTip): # Mark and Ninad 060817.
         ppa2 = glpane.assy.ppa2 # previously picked atom
         ppa2Exists = self.lastPickedInSelAtomList(ppa2, selectedAtomList)
         
-        objStr = self.getHighlightedObjectName()
-        
+        ppa3 = glpane.assy.ppa3 #atom picked before ppa2
+        ppa3Exists = self.lastTwoPickedInSelAtomList(ppa2, ppa3, selectedAtomList) #checks if *both* ppa2 and ppa3 exist
+                
         if len(selectedAtomList) == 0: 
             return objStr
             
@@ -125,28 +130,33 @@ class DynamicTip(QToolTip): # Mark and Ninad 060817.
         #Known bug: If many atoms selected, if ppa2 and ppa2 exists and if ppa2 is deleted, it doesn't display the distance between 
         #highlighted and ppa3. (as of 060818 it doesn't even display the atom info ..but thats not a bug just NIY that I need to 
         #handle somewhere else. 
-        elif len(selectedAtomList) == 1 or ppa2Exists:
+        elif isinstance(glpane.selobj, atom) and (len(selectedAtomList) == 1 ):
             if self.getDistHighlightedAtomAndSelectedAtom(selectedAtomList, ppa2):
                 distStr = self.getDistHighlightedAtomAndSelectedAtom(selectedAtomList, ppa2)
-                print "in getDistLoop"
-                #ninad060818 this decides what tooltip to display
-                if isinstance(glpane.selobj, atom):
+                return objStr + "\n" + distStr
+            else:
+                return objStr
+                
+        #ninad060821 Give the angle info if only 2 atoms are selected (and the selection doesn't include highlighted atom)
+        #if ppa2 and ppa3 both exist (and still selected) then it returns angle between them 
+        #If the highlighted atom is also selected/last picked/lasttolastpicked , only give atom and distance info don't give angle info
+        #If distance info is not available for some reasons (e.g. no ppa2 or more than 2 atoms region selected  etc, return Distance info only)
+        
+        elif  isinstance(glpane.selobj, atom) and ( len(selectedAtomList) == 2 or len(selectedAtomList) == 3):
+            if self.getAngleHighlightedAtomAndSelAtoms(ppa2, ppa3,selectedAtomList):
+                angleStr = self.getAngleHighlightedAtomAndSelAtoms(ppa2, ppa3,selectedAtomList)
+                return objStr + "\n" + angleStr
+            else:
+                if self.getDistHighlightedAtomAndSelectedAtom(selectedAtomList, ppa2):
+                    distStr = self.getDistHighlightedAtomAndSelectedAtom(selectedAtomList, ppa2)
                     return objStr + "\n" + distStr
                 else:
                     return objStr
-            else:
-                return objStr
         else:
             return objStr #@@@ ninad060818 ...if we begin to support other objects (other than jig/chunk/bonds/atoms)
                                 #then we need to retirn glpane.selobj
-            
-        '''elif "two atoms are selected":
-            if self.getAngleHighlightedAtomAndSelAtoms():
-                angleStr = self.getAngleHighlightedAtomAndSelAtoms()
-                distStr = self.getDistHighlightedAtomAndSelectedAtom()
-                return angleStr + "\n" + distStr
                 
-        elif "three atoms are selected":
+        '''elif "three atoms are selected":
             self
             torsionStr = self.getTorsionHighlightedAtomAndSelAtoms()
             angleStr = self.getAngleHighlightedAtomAndSelAtoms()
@@ -184,37 +194,97 @@ class DynamicTip(QToolTip): # Mark and Ninad 060817.
         
         from VQT import vlen
         """
-        Returns the distance between the most recently selected object (must be still selected) and the highlighted object
-        If the highlighed atom is also selected, it excludes it while finding the last selected atoms.
+        Returns the distance between the selected atom  and the highlighted atom. 
         If there is only one atom selected and is same as highlighed atom, then it returns None.  (then the function calling this 
         routine needs to handle that case.) 
         """
        
         glpane = self.glpane
         
-        if ppa2:
-            selectedAtom = ppa2 #ninad060818 This handles a case when there are many atoms selected and there still exists a 'last picked' one
-        else:
+        selectedAtom = None
+        
+        #ninad060821 It is posible that 2 atoms are selected and one is highlighted. This condition allows the function use in the conditional loop that shows angle between the selkected and highlighted atoms
+        if  len(selectedAtomList) ==2 and glpane.selobj in selectedAtomList: #this means the highlighted object is also in this list
+            i = selectedAtomList.index(glpane.selobj)
+            if i == 0: #ninad060821 This is a clumsy way of knowing which atom is which. Okay for now since there are only 2 atoms 
+                selectedAtom = selectedAtomList[1]
+            else:
+                selectedAtom = selectedAtomList[0]
+                
+        if len(selectedAtomList) == 1:
+            #ninad060821 disabled the case where many atoms are selected and there still exists a last picked.  I did this becasue
+            #it is confusing. Example: I picked an atom. Now I region selected another atom (after pick operation) then I highlight an atom. 
+            #it shows the distance between the highlighed and the picked and not highlighted and region selected. This is not good
+            #If we have a way to know when region select operation was performed (before or after) then we can implement it 
+            #again. Probably selectedAtomList maintains this record? Should we check the list to see if ppa2 comes before or after the 
+            # region selection?  It is complecated. Need to discuss with Bruce and Mark. Not implementing it right now. 
+            #This also invalidates the need to pass ppa2 as an arg to this function. Still keeping it until I hear back from Bruce/Mark
+            
+            #if ppa2:
+                #selectedAtom = ppa2 #ninad060818 This handles a case when there are many atoms selected and there still exists a 'last picked' one
+            #else:
+                #selectedAtom = selectedAtomList[0] #buggy if there are more than 2 atoms selected. But its okay because I am handling it correctly elsewhere where (I am calling this function) ninad060821
             selectedAtom = selectedAtomList[0]
             
         xyz = glpane.selobj.posn()
         
         #ninad060818 No need to display disance info if highlighed object and lastpicked/ only selected object are identical
-        if selectedAtom is not glpane.selobj: 
-            distanceStr = ("Distance %s-%s : %.2f A"%(glpane.selobj, selectedAtom,vlen(xyz - selectedAtom.posn())))
-            return distanceStr
+        if selectedAtom:
+            if selectedAtom is not glpane.selobj: 
+                distStr = ("Distance %s-%s : %.2f A"%(glpane.selobj, selectedAtom,vlen(xyz - selectedAtom.posn())))
+                return distStr
+            else:
+                return False
         else:
             return False
     
-    def getAngleHighlightedAtomAndSelAtoms(self): 
+    def getAngleHighlightedAtomAndSelAtoms(self,ppa2, ppa3,selectedAtomList):
+        
         """
         Returns the angle between the last two selected atoms and the current highlighted atom. 
-        If the highlighed atom is also selected, it excludes it while finding the last 2 selected atoms. 
         If the highlighed atom is also one of the selected atoms and there are only 2 selected  atoms other than 
          the highlighted one then it returns None.(then the function calling this routine needs to handle that case.) 
         
         """
-        return False
+        from chem import atom_angle_radians
+        
+        glpane = self.glpane
+        lastSelAtom = None
+        secondLastSelAtom = None
+        ppa3Exists = self.lastTwoPickedInSelAtomList(ppa2, ppa3, selectedAtomList) #checks if *both* ppa2 and ppa3 exist
+        
+        #if len(selectedAtomList) < 2 or len(selectedAtomList) > 3:
+         #   return False
+        
+        if  len(selectedAtomList) ==3 and glpane.selobj in selectedAtomList:
+            if ppa3Exists and not (glpane.selobj is ppa2 or glpane.selobj is ppa3):
+                lastSelAtom = ppa2
+                secondLastSelAtom = ppa3
+            else:
+                selectedAtomList.remove(glpane.selobj) #@@@@ninad060821 kludge to get the other two selected atoms. Bad idea? Neeed help.
+                lastSelAtom = selectedAtomList[0]
+                secondLastSelAtom = selectedAtomList[1] # ninad060821 There is no second last selected atom btw. Here it is just one of the 
+                                                                    #selected atoms.
+                selectedAtomList.append(glpane.selobj) #@@@@add back the highlighted object to the selected atom list ninad060821
+            
+        if len(selectedAtomList) == 2: #here I (ninad) don't care about whether itselected atom is also highlighted. It is handled below. 
+            if ppa3Exists:
+                lastSelAtom = ppa2
+                secondLastSelAtom = ppa3
+            else:
+                 lastSelAtom = selectedAtomList[0]
+                 secondLastSelAtom = selectedAtomList[1]
+            #ninad060821 No need to display angle info if highlighed object and lastpicked or secondlast picked
+            #  object are identical
+            if glpane.selobj in selectedAtomList: 
+                return False
+        
+        if lastSelAtom and secondLastSelAtom:
+            angle = atom_angle_radians( glpane.selobj, lastSelAtom,secondLastSelAtom ) * 180/pi
+            angleStr = ("Angle %s-%s-%s: %.3f degrees"%(glpane.selobj, lastSelAtom,secondLastSelAtom,angle))
+            return angleStr
+        else:
+            return False
     
     def getTorsionHighlightedAtomAndSelAtoms(self):
         """
@@ -236,12 +306,16 @@ class DynamicTip(QToolTip): # Mark and Ninad 060817.
         else:
             return False
     
-    def lastTwoPickedInSelAtomsList(self, ppa2,ppa3):
+    def lastTwoPickedInSelAtomList(self, ppa2,ppa3,selectedAtomList):
         '''Checks whether *both* the last two picked atoms (ppa2 and ppa3) exist in the atom list
            Returns True of False. 
         '''
+        if (ppa2 and ppa3) in selectedAtomList:
+            return True
+        else:
+            return False
     
-    def lastThreePickedInSelAtomsList(self, ppa2, ppa3, ppa4):
+    def lastThreePickedInSelAtomList(self, ppa2, ppa3, ppa4):
         '''Checks whether *all*  the three picked atoms (ppa2 , ppa3 and ppa4) exist in the atom list
            Returns True of False.  Note: there is no ppa4 yet - ninad060818
         '''
