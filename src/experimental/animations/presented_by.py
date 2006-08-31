13d901e5476de8e5efef9aec447c0405d9378b19
Will Ware <wware@alum.mit.edu>
2006-08-31 23:06:10 +0000

    Get some suggestions from Mark and Eric D.

--- src/experimental/animations/presented_by.py
+++ src/experimental/animations/presented_by.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 
 import animate, jobqueue
+from math import *
 
 preamble = "Presented by"
 
 def drawOneFrame(t, filename):
     # t varies from 0.0 to 1.0 over the course of the sequence
+    A = -4
+    m = (exp(A * t) - exp(A * 0.75)) / (1.0 - exp(A * 0.75))
+    m = max(m, 0.0)
     preamble_xpos = 80
-    preamble_ypos = 180 * t
-    finalx = 50
-    nanorex_xpos = 600 - (600 - finalx) * t
+    preamble_ypos = 180 - 200.0 * m * cos(30 * t)
+    nanorex_xpos = 50
     nanorex_ypos = 140
-    cmd = (('composite -geometry +%d+%d ' % (nanorex_xpos, nanorex_ypos)) +
+    cmd = (('composite -dissolve %d -geometry +%d+%d ' % (100 * t, nanorex_xpos, nanorex_ypos)) +
            'Nanorex_logos/nanorex_logo_text_outline_medium.png blank.jpg /tmp/foo.jpg')
     jobqueue.do(cmd)
     cmd = 'convert -fill white'
@@ -21,5 +24,5 @@ def drawOneFrame(t, filename):
     jobqueue.do(cmd)
 
 m = animate.MpegSequence()
-m.simpleSequence(5 * m.SECOND, drawOneFrame, step=1, repeat_final_frame=m.SECOND)
+m.simpleSequence(2 * m.SECOND, drawOneFrame, step=1, repeat_final_frame=m.SECOND)
 m.encode()
