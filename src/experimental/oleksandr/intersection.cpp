0d55c692fd23045c88045ff18a48b708507aa68c
Al Oshevchenko <oshevchenko@nanorex.com>
2006-11-04 23:33:43 +0000

    added new class for check intersection

---
+++ src/experimental/oleksandr/intersection.cpp
@@ -0,0 +1,16 @@
+/*
+  Name: intersection.cpp
+  Copyright: 2006 Nanorex, Inc.  All rights reserved.
+  Author: Oleksandr Shevchenko
+  Description: class for intersection representation   
+*/
+
+#include "intersection.h"
+
+//----------------------------------------------------------------------------
+// Destructor
+
+Intersection::~Intersection()
+{
+}
+
