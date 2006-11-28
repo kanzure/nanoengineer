41e3457bbc8908ad41eb7e90fcdc90030a5215ca
Bruce Smith <bruce@nanorex.com>
2006-11-28 00:02:40 +0000

    towards PixelTester

--- src/exprs/images.py
+++ src/exprs/images.py
@@ -367,4 +367,39 @@ class PixelGrabber(InstanceOrExpr, DelegatingMixin):#e draft, API needs revision
         return
     pass # end of class PixelGrabber
 
+# ==
+
+# PixelTester is untested -- not even parsed by python yet
+
+##e needs save button, per-session filenames, access to on disk log of prior files,
+# or way of reading new image from same file, and including reread-policy
+# (and id if done "by command to instance")
+# along with filename in the texture-dict key
+# (but optim for when multiple reads got same image data, whether by same instance or different ones??
+#  not simple -- eg if single textures have varying data or (worse for displists) names, other issues, and somewhat orthogonal)
+
+class PixelTester(InstanceOrExpr, DelegatingMixin): # ought to be InstanceMacro but trying this alternate style just to see it
+    # args
+    testexpr = Arg(Widget2D) # instantiated right here, hope that's ok
+    testname = Arg(str) # required for now, used to form filename
+    # value
+    filename = format_Expr("/tmp/%s.jpg" % testname)
+    delegate = SimpleColumn(
+        
+        TextRect("saved image from PRIOR session, in blue box"), # kluge: execute this first, so we read file before writing it
+        Boxed(bordercolor = blue)(
+            Image(filename)
+        ),
+        Spacer(0.3),
+        
+        TextRect("live widget, in purple box"),
+        Boxed(bordercolor = purple)(
+            PixelGrabber(testexpr, filename)
+        ),
+
+        ##e and put current session image here, for comparison, or put top on in a tab control for flicker test
+    )
+    pass # end of class PixelTester
+
+        
 # end
