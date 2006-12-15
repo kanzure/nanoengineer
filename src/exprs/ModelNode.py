a539a6bef18fea10426f8bba3af07275e26455b0
Bruce Smith <bruce@nanorex.com>
2006-12-15 19:22:08 +0000

    scratch

--- src/exprs/ModelNode.py
+++ src/exprs/ModelNode.py
@@ -48,28 +48,65 @@ since it is defined to take a snapshot.
 ...
 """
 
-# See DebugNode (where?) and TestNode when making this.
-
-from Utility import Node
-
-class ModelNode(Node):
-    pass
-
-# end except for old code
+from basic import *
+from basic import _self, _my, _this
 
 from Utility import SimpleCopyMixin, Node, imagename_to_pixmap, genViewNum
+from state_utils import copy_val, S_DATA #k copy_val in basic? where is S_DATA really?
 
-class TestNode(Node, SimpleCopyMixin): # see TestNode.py... not yet more than a stub... see also DebugNode
-    """Abstract class for a kind of Node it's easy to experiment with.
-    Just make sure the subclass does the stuff needed by SimpleCopyMixin,
-    and that all the attrs can be saved as info records in a simple way by the code herein --
-    or maybe we'll save them in a Files Directory shelf instead? Not sure yet.
-    """
+from Rect import Sphere #reload
+
+class ModelNode(Node, SimpleCopyMixin, InstanceOrExpr):
+    __metaclass__ = ExprsMeta
+    ###IMPLEM - rewrite the rest of this class
+    def __init__(self, stuff, name = None):
+        assy = env.mainwindow().assy #k wrongheaded??
+        Node.__init__(self, assy, name)
+        self.stuff = stuff # exprs for what to draw (list of objects)
+    _s_attr_stuff = S_DATA
     def draw(self, glpane, dispdef):
-        "Use our subtype to find some rules in a widget expr..."
-        # some WE is sitting there in the mode, knowing how to draw things for it...
-        # so we ask it...
-        # maybe it got passed to this method? ideally as dispdef, but that would break old code...
-        # so simplest way is as a dynamic glpane attr, and this is tolerable until we have a chance to clean up all Node draw methods.
-        pass
+        if self.picked: # only draw when picked! (good?)
+            draw_stuff(self.stuff, glpane)
+    def writemmp(self, mapping):
+        """Write this Node to an mmp file, as controlled by mapping,
+        which should be an instance of files_mmp.writemmp_mapping.
+        """
+        line = "# nim: mmp record for %r" % self.__class__.__name__
+        mapping.write(line + '\n')
+        # no warning; this happens all the time as we make undo checkpoints
+        return
+    def __CM_upgrade_my_code(self): # experiment
+        "replace self with an updated version using the latest code, for self *and* self's data!"
+        name = self.__class__.__name__
+        print "name is",name # "DebugNode"
+        print self.__class__.__module__ 
+        #e rest is nim; like copy_val but treats instances differently, maps them through an upgrader
     pass
+
+class Sphere_ExampleModelNode(ModelNode):
+    "A sphere."
+    pos = StateArg(Position, ORIGIN)
+        #e or can all arg/option formulas be treated as state, if we want them to be? (no, decl needed)
+        #e or can/should the decl "this is changeable, ie state" as a flag option to the Arg macro?
+        #e can it be set to a new *formula*, not just a new constant value? (for constraint purposes)
+        #e - if so, does this require a decl to say it's possible, or is it always possible? (note: it affects mmp writing and copy)
+    
+    if 0:
+        # StateArg might be equivalent to this (except for the name to use in the Arg if it matters, e.g. for StateOption):
+        _orig_pos = Arg(Position, ORIGIN) #e can we pass an index to override '_orig_pos' if it matters?
+        pos = State(Position, _orig_pos) # see similar code in drag_demo (in class Vertex I think)
+        
+    radius = StateArg(Width, 1)
+    color = StateArgOrOption(Color, gray)
+    def draw_unpicked(self):
+        ###KLUGE: this is copied from Sphere.draw in Rect.py.
+        ##e Instead, we should define and cache a real Sphere using self-formulas, and draw that,
+        # and delegate to it for lbox as well.
+        from drawer import drawsphere # drawsphere(color, pos, radius, detailLevel)
+        drawsphere(self.fix_color(self.color), self.center, self.radius, self.detailLevel) # some of this won't work ######IMPLEM
+    # so is using Sphere hard?
+    _value = Sphere(pos, radius, color) # and something to delegate to it... but only for drawing??
+    pass
+
+# end
+
