a28793ef205e006fb5006a3b2ea2ed1572df5f5d
Bruce Smith <bruce@nanorex.com>
2007-01-09 23:05:52 +0000

    precaution: also delegate _needs_repaint

--- src/exprs/GLPane_overrider.py
+++ src/exprs/GLPane_overrider.py
@@ -71,7 +71,8 @@ class GLPane_overrider(Delegator, GLPane_mixin_for_DisplistChunk, object):
     overrides = 'selobj glselect_dict need_setup_lighting wants_gl_update vdist'\
                 'glselect_wanted targetdepth current_glselect wX wY glselect compassPosition'\
                 'near far quat pov zoomFactor scale'\
-                '_glnames _testmode_stuff _testmode_stuff_2 _testmode_stuff_3 _alpha'
+                '_glnames _testmode_stuff _testmode_stuff_2 _testmode_stuff_3 _alpha' \
+                '_needs_repaint' 
     for attr in overrides.split():
         exec( "%s = delegated_state_attr('delegate', '%s')" % (attr, attr) )
     
