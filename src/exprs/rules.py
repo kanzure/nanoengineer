480bbd804abafab3bf64efe578a17b9a4854b836
Bruce Smith <bruce@nanorex.com>
2007-01-12 17:56:51 +0000

    notes and scratch code towards Rule, RuleList, WithViewRule

--- src/exprs/rules.py
+++ src/exprs/rules.py
@@ -2,6 +2,8 @@
 rules.py
 
 $Id$
+
+This is really a scratch/notes file, though it has some code near the end that might become real.
 """
 
 # Coming out of this comment just added to demo_drag.py:
@@ -99,3 +101,167 @@ class rules_in_series(RuleOp):
 # Note someday it'll do compiling and other fancy stuff too... and something else I just forgot I wanted to say... oh yeah, weak index-part handling
 # maybe type coercion too... and easy formation of incrly modified versions...
 # 
+
+
+# ... what about some example rules?
+
+# can an expr that needs one arg of a certain type, count as a rule?
+# calling it means applying it... as expr or instance, maybe doesn't matter(??)...
+# if the types don't match, it might raise an exception -- why not?
+# so that might actually work.
+
+Vertex # a type of model obj
+
+class VertexViewer(...):
+    vertex = Arg(Vertex) # requires arg to be a Vertex object... hmm, do we say it has to be an Instance of one, already??
+        # that means it has a pos, rel to whatever obj the vertex type is rel to... but how do we capture that idea
+        # that Vertex is really a parametrized type? well, put that off for now since it's not one in demo_drag.py (safe??)
+    
+    delegate = Translate( Rect(0.2, color = vertex.color), vertex.pos ) # or so
+    pass
+
+# my worries about the above include:
+# - is the exception for argtype being wrong really the same (or a subclass) of the one for RuleFailedException?
+#   in particular, is it "not an error" (unless it was your last rule in a series)?
+# - is RuleFailedException a dangerous concept, since if it gets out of the inside of a rule (eg while running its body),
+#   it might seem to apply to the rule itself?
+# - how do we capture the idea that Vertex is really a parametrized type?
+#   do we cheat and say "a vertex also has a host object"? (which determines the type of pos it has, including its coord sys)
+#   actually that might be a good idea (ie not cheating) -- since we can select vertices of different hosts at one time!
+#   So letting host (and pos type) just be another attr of each vertex (with optims someday when they're constant) does make sense.
+
+# let's make another example rule where we explicitly list the argtype, avoiding some of the worries above.
+
+Rule(Vertex, VertexViewer) # arg1 is a type, arg2 is a func from arg1 (of that type) to the result (error if it fails)
+
+# worries about *that*:
+# - does it use up the word Rule for a special case of argstyle which might not even be the most common one?
+# - it seems to *prove* that either:
+#   - argtype failure in VertexViewer should not raise RuleFailedException, or,
+#   - if it does, Rule should not pass it on unchanged. (which is not hard, i guess, just slightly non-POLS.)
+
+# Here is more evidence that argtype mismatch is not RuleFailedException -- it can happen arbitrary deep inside something,
+# and percolate up w/o being transformed.
+#
+# One way out is to wrap VertexViewer with something to turn it into a rule, which turns argtype mismatch into RuleFailedException --
+# but this is only ok if it can verify that the arg whose type did not match was an arg of its direct argument VertexViewer.
+# But it could check that (if we defined the data of the argtype exception to include the object whose arg had the wrong type,
+#  and if we ignore the possibility of cheating when constructing one to raise)... so we could define such a wrapper --
+# and that wrapper could even be autoapplied to turn an expr needing one arg into a rule, in argslots needing a rule.
+
+# ===
+
+# Actually I think returning None is better than raising an exception, for a rule about what things should look like.
+# Then you serially combine rules with Or, and exceptions are errors, so catching them is less often needed.
+# But can None be the "no answer result" for *all* rules (regardless of purpose)? Surely not. So we need a special object,
+# which will be boolean false, so in cases where all correct answers are true, we can still use "or" to combine them.
+# But then the general rule utilities can't use "or". So another possibility is that they return None or a true thing,
+# and the true thing contains the answer, but is not only the answer. E.g. they return (ok, res) or (bindings, res) or wrapped(res).
+# On failure do they return (False, None) or just None or a weird object that acts like both? Failure is common -- just None.
+
+class rules_in_series(RuleOp):
+    #e doc; all correct rule results must be true; the convention for encoding arbitrary objects is up to the rule-user;
+    # in the present app we're using them to encode exprs or exprheads or Instances, no all legit values are true
+    def apply(self, thing):
+        r1, r2 = self.args
+        return r1(thing) or r2(thing)
+    pass
+
+# that's simpler!
+
+# (maybe "in parallel" is more accurate -- no obj goes thru more than one of the rules. Just call it a "rule list" for now.)
+
+# (it has some relation to or_Expr, but it can't be the same thing unless our instantiation env can supply the "thing"...
+#  i.e. be different than usual in semantics. Save that idea for later.)
+
+# when the time comes for optim for lots of rules, rules can have methods to add themselves to classifier trees
+# which have questions and answer-keyed dicts as nodes.
+
+# what we need for use_VertexView is just a simple way to say "in this env, if obj.type is X, then view obj using Y".
+# And we want to use things like that to construct mappings for other purposes too.
+#
+# So we want a Rule object API (for rules and rulesets), ops like rule lists, a way to make a Mapping from a Rule,
+# and a standard mapping in the env for model_object_viewers, and a standard way to locally incrementally pre-extend its rulelist.
+
+WithRule(expr, rule) # this is fine if there is only one ruleset we need to extend, and if "in the dynenv" can be implicit.
+
+WithEnv(expr, rule) # no, sounds like it's the whole env.
+
+WithEnvRule(expr, rule) # explicit env, still assumes one ruleset in env. The rule would have to apply to all exprs for all purposes...
+
+WithViewRule(expr, rule) # says the rule is for how to view something... implies it's in dynenv, since all viewing rules are.
+
+LocalEnv(expr, view_rule = rule) # ....
+
+  # I think I don't want to force this mod into a general form for other kinds of mods, now -- the relation to the general form
+  # might change, and the namespace of different kinds doesn't have enough names in it to pick good names for specific ones.
+
+UseViewRule # I guess I like With better than Use -- so far all forms of "with" mean "with local mods, not destructive to parent".
+
+WithViewRule( expr, (Vertex, VertexView)) # 2-tuple (type, func) can be interpreted as Rule if you try... seems good.
+    # But if we adopt that, and also for lists of rules, we better decide whether it has to be done to every value at runtime,
+    # when the arg is a formula. It also makes the code harder to read, perhaps...
+
+WithViewRule( expr, Rule(Vertex, VertexView)) # use this form
+
+    # this form of Rule(type, func) can be general if we say that type can be fancy, as long as it returns False or (True, *args)
+    # with *args passed as extra args to func! Not sure if that way of fancification makes sense... ok for now,
+    # since there'll be few enough of these (for some time from now) that changing them all in name or syntax would be ok.
+    
+    # BTW can a func, or an exprhead needing one arg, be interchangeable here? I hope so and guess so, but don't know for sure,
+    # and I doubt it's already implemented. It might require instantiating such an expr without its arg, getting a func
+    # that wants an arg instance! But then I'd worry about whether erroneous uses of that feature could be detected.
+    # Maybe they could by attr access too early... or maybe the argslot would have to specifically permit this kind of instantiation.
+    
+class Rule(FunctionExpr): # FunctionExpr means it instantiates into a function, and has the __call__ interface to self.apply
+        #e or we might decide that any sort of expr can define apply, with that meaning on its instances
+        # (if so should we rename apply to _e_apply or _i_apply or so? probably _i_apply.)
+    """Rule(type, func) is an Expr for a rule which matches any Instance which has the given type, and wraps it with func.
+    Once instantiated, it can be applied to an Instance (thing) by being called on it (Python __call__),
+    returning None if it doesn't match, or func(thing) if it does. (That result is presumed to be an Instance,
+    and is required to be true, though perhaps neither condition is checked.)
+       Note that it is meant for use on Instances, and is hard to use on arbitrary Python values if any legal values can be false.
+    """
+    type = Arg(Type)
+    func = Arg(Function)
+    def apply(self, thing):
+        "return None if it doesn't match, or func(thing) if it does"
+        type = self.type
+        func = self.func
+        ## if type.matches(thing): ####k??? this assumes canon_type or other coercion to Type tames type and gives it this method
+        if isa(type)(thing): # this (isa(), vs. x.matches()) is easier to support since it can work for python types too
+            res = func(thing)
+            assert res, "%r requires %r(%r) == %r to not be false" % (self, func, thing, res)
+            return res
+        return None
+    pass
+
+class RuleList(...):
+    args = ArgList(Rule) # assumes Rule is also usable as a type -- though above Rule class has no provision for that
+        ###IMPLEM ArgList -- it makes args a list of instances, doing instantiation and type coercion on each one (lazy per elt??)
+        # (note: this semantics is precisely the same as a few other files desiring ArgList, so it must be an ok design!)
+        ###e do we want it to let args also be python lists of rules (or list_Exprs thast make them)? probably yes -- someday.
+        # (just as in Column) (all it needs is a more flexible typecheck and a system like in Column to map the args...)
+    def apply(self, thing):
+        for rule in self.args:
+            res = rule(thing)
+            if res:
+                return res
+            assert res is None, "Rule %r applied to %r was %r -- should be None or something true" % (rule, thing, res)
+            #e maybe weaken that to a warning, so we can treat it as None and continue?
+            continue
+        return None
+    pass
+
+class WithViewRule( ...) # it's not a good sign that i keep deferring the superclass -- it probably means InstanceOrExpr spelling is too long
+    """#doc; example:
+    WithViewRule( expr, Rule(Vertex, VertexView))
+    """
+    #e modify env locally... turn into a special form of WithEnvMods. See other file for stub of that.
+    pass
+
+# ... the above needs ArgList, and might need working type coercion too.
+# let's implement some iterators (like rect with corners) first, and both of those internal features.
+# Just make another macro like Arg, make it get told its starting pos like Arg does,
+# and let the type coercion in those macros *not* work by using type in direct call (like it may do now, not sure).
+# [070112 morn]
