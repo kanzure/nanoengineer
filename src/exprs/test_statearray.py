2e03b53a3dd5c5cc4dde68b9b9b3d6dd7de4bfed
Bruce Smith <bruce@nanorex.com>
2007-03-14 02:12:04 +0000

    scratch

--- src/exprs/test_statearray.py
+++ src/exprs/test_statearray.py
@@ -88,8 +88,8 @@ class _color_toggler(DelegatingInstanceOrExpr):
     pass
 
 class test_StateArrayRefs(DelegatingInstanceOrExpr): ### has some WRONGnesses
-    indices = range(4)
-    colors = StateArrayRefs(Color, red)
+    indices = range(10)
+    colors = StateArrayRefs(Color, pink)
         #e i want an arg for the index set... maybe even the initial set, or total set, so i can iter over it...
         # NOTE: at least one dictlike class has that feature - review the ones in py_utils, see what _CK_ uses
     def _color_toggler_for_index(self, index):#e should we use _CV_ for this?? if not, it must be too hard to use!!!
