9a45a32c74ad13dc2cc607c1cf6d4d4c981beba6
Bruce Smith <bruce@nanorex.com>
2007-04-28 06:52:36 +0000

    print when we run

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ Minimal setup.py example, run with:
 % python setup.py py2app
 """
 
+import sys
+print "running Distribution/setup.py, sys.argv is %r" % (sys.argv,)
+
 from distutils.core import setup
 import py2app
 setup(
