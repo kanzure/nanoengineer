082684feec74bf258583dbec95f5f8064e9e15ec
Mark Sims <mark@nanorex.com>
2007-05-17 20:53:02 +0000

    Added PropMgr layout (margin and spacing) constants.

--- src/PropMgr_Constants.py
+++ src/PropMgr_Constants.py
@@ -17,3 +17,19 @@ pmHidePreviewButton = 8
 pmHideWhatsThisButton = 16
 pmHideAllButtons = pmHideDoneButton | pmHideCancelButton | pmHideRestoreDefaultsButton | \
           pmHidePreviewButton | pmHideWhatsThisButton
+
+# PropMgr Layout constants.
+pmMainVboxLayoutMargin = 0 # PropMgr's master VboxLayout margin
+pmMainVboxLayoutSpacing = 0 # PropMgr's master VboxLayout spacing
+pmHeaderFrameMargin = 2 # margin around icon and title.
+pmHeaderFrameSpacing = 5 # space between icon and title.
+pmSponsorFrameMargin = 0 # margin around sponsor button.
+pmSponsorFrameSpacing = 0 # has no effect.
+pmTopRowBtnsMargin = 2 # margin around buttons.
+pmTopRowBtnsSpacing = 2 # space between buttons.
+pmMsgGrpBoxMargin = 0 # margin around yellow text window.
+pmMsgGrpBoxSpacing = 0 # has no effect.
+pmGrpBoxVboxLayoutMargin = 0 # Groupbox VboxLayout margin
+pmGrpBoxVboxLayoutSpacing = 2 # Groupbox VboxLayout spacing
+pmGridLayoutMargin = 2 # Grid contains all widgets in a grpbox
+pmGridLayoutSpacing = 4 # Grid contains all widgets in a grpbox
