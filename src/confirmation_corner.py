81271dc512441f9271c98017f685978708af66ee
Bruce Smith <bruce@nanorex.com>
2007-06-21 17:01:38 +0000

    scratch

--- src/confirmation_corner.py
+++ src/confirmation_corner.py
@@ -16,8 +16,10 @@ from exprs.images import Image
 from exprs.Overlay import Overlay
 
 
-class MouseEventHandler: #e refile #e put implems in subclass #e some methods may need mode and/or glpane arg...
-    "interface for objects used as glpane.mouse_event_handler [abstract class]"
+class MouseEventHandler_API: #e refile #e put implems in subclass #e some methods may need mode and/or glpane arg...
+    """API (and default method implems) for the MouseEventHandler interface
+    (for objects used as glpane.mouse_event_handler) [abstract class]
+    """
     def mouseMoveEvent(self, event):
         ""
     def mouseDoubleClickEvent(self, event):
@@ -36,17 +38,19 @@ class MouseEventHandler: #e refile #e put implems in subclass #e some methods ma
         ""
     pass
 
-class MouseEventHandlerSubclass(MouseEventHandler): #e rename # an instance can be returned from find_or_make for testing...
+class cc_MouseEventHandler(MouseEventHandler_API): #e rename # an instance can be returned from find_or_make for testing...
     "###doc"
+    def __init__(self, glpane):
+        self.glpane = glpane
     def update_cursor(self, mode):
          ###stub for testing
-        glpane = mode.o
+        assert self.glpane is mode.o
         win = mode.w
-        glpane.setCursor(win.RotateCursor)
+        self.glpane.setCursor(win.RotateCursor)
     def want_event_position(self, wX, wY):
         ###stub for testing
-        dx = self.o.width - wX
-        dy = self.o.height - wY
+        dx = self.glpane.width - wX
+        dy = self.glpane.height - wY
         if dx + dy <= 100:
             return True
         return False
@@ -54,14 +58,18 @@ class MouseEventHandlerSubclass(MouseEventHandler): #e rename # an instance can 
         pass ###stub
     pass
 
-    
-def interpret_cctype(cctype, mode):
+def interpret_cctype(cctype, mode): # not used, unless perhaps via cc_scratch.py
     "return None or an expr, ..."
     return Rect(green) ###STUB
 
 def find_or_make(cctype, mode):
-    "Return a confirmation corner instance for mode, of the given cctype."
-    return None ##### STUB
+    "Return a confirmation corner instance for mode, of the given cctype. [Called from basicMode.draw_overlay]"
+    ##### STUB
+    try:
+        mode._confirmation_corner__cached_meh
+    except AttributeError:
+        mode._confirmation_corner__cached_meh = cc_MouseEventHandler(mode.o)
+    return mode._confirmation_corner__cached_meh
     # see exprs/cc_scratch.py
 
 # end
