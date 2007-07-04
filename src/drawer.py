82cf4799df34bb88c56991a4798bdaee8318d757
Bruce Smith <bruce@nanorex.com>
2007-07-04 05:55:55 +0000

    fix missing glScalef import

--- src/drawer.py
+++ src/drawer.py
@@ -151,7 +151,13 @@ try:
 except:
     # The installed version of OpenGL requires argument-typed glScale calls.
     from OpenGL.GL import glScalef as glScale
-	
+
+from OpenGL.GL import glScalef
+    # Note: this is NOT redundant with the above import of glScale --
+    # without it, displaying an ESP Image gives a NameError traceback
+    # and doesn't work. [Fixed by bruce 070703; bug caught by Eric M using
+    # PyChecker; bug introduced sometime after A9.1 went out.]
+
 try:
     from OpenGL.GL import glFog
 except:
