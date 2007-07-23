9d574a8264560e4feab0a5daf3c6f291bd3a19fc
Ninad Sathaye <ninad@nanorex.com>
2007-07-23 15:55:20 +0000

    changes to make sure that the move and fuse PMs show  up after closing Plane PM while in those modes. (was needed after move mode and fuse chunks mode propMgr object related changes)

--- src/ReferenceGeometry.py
+++ src/ReferenceGeometry.py
@@ -300,10 +300,10 @@ class GeometryGeneratorBaseClass(SponsorableMixin, PropertyManagerMixin):
         # The following reopens the property manager of the mode after 
         #when the PM of the reference geometry is closed. -- Ninad 20070603        
         if self.win.assy.o.mode.modename in \
-           ['MODIFY', 'FUSE', 'MOVIE']:
-            self.modePropertyManager = self.win.assy.o.mode
-        elif self.win.assy.o.mode.modename == 'DEPOSIT':
+           ['MODIFY', 'FUSECHUNKS', 'DEPOSIT']:
             self.modePropertyManager = self.win.assy.o.mode.propMgr
+        elif self.win.assy.o.mode.modename == 'MOVIE':
+            self.modePropertyManager = self.win.assy.o.mode
         else:
             self.modePropertyManager = None
         
@@ -328,10 +328,10 @@ class GeometryGeneratorBaseClass(SponsorableMixin, PropertyManagerMixin):
         self.closePropertyManager()
         
         if self.win.assy.o.mode.modename in \
-           ['MODIFY', 'FUSE', 'MOVIE']:
-            self.modePropertyManager = self.win.assy.o.mode
-        elif self.win.assy.o.mode.modename == 'DEPOSIT':
+           ['MODIFY', 'FUSECHUNKS', 'DEPOSIT']:
             self.modePropertyManager = self.win.assy.o.mode.propMgr
+        elif self.win.assy.o.mode.modename == 'MOVIE':
+            self.modePropertyManager = self.win.assy.o.mode
         else:
             self.modePropertyManager = None
             
