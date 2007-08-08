492ea6a2fed98d92b9d584a73380cd0de713a529
Brian Helfrich <helfrich9000@gmail.com>
2007-08-08 22:20:14 +0000

    Added UML image to documentation.

--- src/PM/__init__.py
+++ src/PM/__init__.py
@@ -6,6 +6,8 @@ The main class is the PM module is PM_Dialog, which is a base class. It can
 contain one or more group boxes (PM_GroupBox) that can contain one or more
 PM widgets.
 
+IMAGE(http://www.nanoengineer-1.net/mediawiki/images/1/1d/PM-UML.png)
+
 @see: U{B{Property Manager dialog}<http://www.nanoengineer-1.net/mediawiki/
     index.php?title=Property_Manager>}
 """
