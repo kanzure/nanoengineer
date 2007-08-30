362a29fb71dbafa15c82f398e3bf1a3b463dae98
Bruce Smith <bruce@nanorex.com>
2007-08-30 23:25:39 +0000

    split out some commands; use initialize function

---
+++ cad/src/example_expr_command.py
@@ -0,0 +1,94 @@
+# Copyright 2007 Nanorex, Inc.  See LICENSE file for details. 
+"""
+example_expr_command.py -- example of how to use an interactive graphics
+expr in a command (unfinished, so partly scratch code)
+ 
+$Id$
+
+History:
+
+070830 bruce split this out of test_commands.py and test_command_PMs.py,
+in which it was called ExampleCommand2E
+"""
+
+# == PM class
+
+from test_command_PMs import ExampleCommand2E_PM #### TODO: pull in here
+
+# == command class
+
+from test_commands import ExampleCommand2
+
+# these imports are not needed in a minimal example like ExampleCommand2:
+
+from OpenGL.GL import GL_LEQUAL
+from drawer import drawline
+from constants import red, green
+from VQT import V
+##from exprs.ExprsConstants import PIXELS
+##from exprs.images import Image
+##from exprs.Overlay import Overlay
+from exprs.instance_helpers import get_glpane_InstanceHolder
+from exprs.Rect import Rect # needed for Image size option and/or for testing
+from exprs.Boxed import Boxed
+from exprs.draggable import DraggablyBoxed
+
+from exprs.instance_helpers import InstanceMacro
+from exprs.attr_decl_macros import State
+from exprs.TextRect import TextRect#k
+class TextState(InstanceMacro):#e rename?
+    text = State(str, "initial text", doc = "text")#k
+    _value = TextRect(text) #k value #e need size?s
+    pass
+
+class ExampleCommand2E(ExampleCommand2, object):
+    """
+    Add things not needed in a minimal example, to try them out.
+    (Uses a PM which is the same as ExampleCommand2 except for title.)
+    """
+    # Note: object superclass is only needed to permit super(ExampleCommand2E, self) to work.
+    # object superclass should not come first, or it overrides __new__
+    # (maybe could fix using def __init__ -- not tried, since object coming last works ok)
+
+    modename = 'ExampleCommand2E-modename'
+    default_mode_status_text = "ExampleCommand2E"
+    PM_class = ExampleCommand2E_PM
+
+    standard_glDepthFunc = GL_LEQUAL # overrides default value of GL_LESS from GLPane
+        # note: this is to prevent this warning:
+        ## fyi (printonce): Overlay in reverse order (need to override standard_glDepthFunc in your new mode??)
+        # but we should probably make it the default for all modes soon.
+
+    def __init__(self, glpane):
+        "create an expr instance, to draw in addition to the model"
+        super(ExampleCommand2E, self).__init__(glpane)
+        ## expr1 = Rect(4,1,green)
+        expr1 = TextState()
+        expr2 = DraggablyBoxed(expr1, resizable = True)
+            ###BUG: resizing is projecting mouseray in the wrong way, when plane is tilted!
+            # I vaguely recall that the Boxed resizable option was only coded for use in 2D widgets,
+            # whereas some other constrained drag code is correct for 3D but not yet directly usable in Boxed.
+            # So this is just an example interactive expr, not the best way to do resizing in 3D. (Though it could be fixed.)
+
+        # note: this code is similar to expr_instance_for_imagename in confirmation_corner.py
+        ih = get_glpane_InstanceHolder(glpane)
+        expr = expr2
+        index = (id(self),) # WARNING: needs to be unique, we're sharing this InstanceHolder with everything else in NE1
+        self._expr_instance = ih.Instance( expr, index, skip_expr_compare = True)
+
+        return
+        
+    def Draw(self):
+        """
+        Do some custom drawing (in the model's abs coordsys) after drawing the model.
+        """
+        #print "start ExampleCommand2E Draw"
+        glpane = self.o
+        super(ExampleCommand2E, self).Draw()
+        drawline(red, V(1,0,1), V(1,1,1), width = 2)
+        self._expr_instance.draw()
+        #print "end ExampleCommand2E Draw"
+    
+    pass # end of class ExampleCommand2E
+
+# end
--- cad/src/test_commands.py
+++ cad/src/test_commands.py
@@ -26,6 +26,8 @@ Cosmetic bugs:
 
 TODO:
 
+Split into several files in a subdirectory.
+
 When cleaning up PropMgrBaseClass etc, note some other things Mark wants to work on soon:
 
 - add message boxes to users of PropertyManagerMixin, in a way easily moved over to PropMgrBaseClass when they use that
@@ -37,13 +39,10 @@ When cleaning up PropMgrBaseClass etc, note some other things Mark wants to work
 
 Fix problems with Example_TemporaryCommand_useParentPM (commented where it's used)
 
-Improve connectWithState, as commented near its test code
-
 """
 
 from test_command_PMs import ExampleCommand1_PM
 from test_command_PMs import ExampleCommand2_PM
-from test_command_PMs import ExampleCommand2E_PM
 
 from PM.PM_WidgetsDemoPropertyManager import PM_WidgetsDemoPropertyManager
 
@@ -151,200 +150,6 @@ class ExampleCommand2( Example_TemporaryCommand_useParentPM): # WRONG: this has 
 
 # ==
 
-# these imports are not needed in a minimal example like ExampleCommand2;
-# to make that clear, we put them down here instead of at the top of the file
-from OpenGL.GL import GL_LEQUAL
-from drawer import drawline
-from constants import red, green
-from VQT import V
-##from exprs.ExprsConstants import PIXELS
-##from exprs.images import Image
-##from exprs.Overlay import Overlay
-from exprs.instance_helpers import get_glpane_InstanceHolder
-from exprs.Rect import Rect # needed for Image size option and/or for testing
-from exprs.Boxed import Boxed
-from exprs.draggable import DraggablyBoxed
-
-from exprs.instance_helpers import InstanceMacro
-from exprs.attr_decl_macros import State
-from exprs.TextRect import TextRect#k
-class TextState(InstanceMacro):#e rename?
-    text = State(str, "initial text", doc = "text")#k
-    _value = TextRect(text) #k value #e need size?s
-    pass
-
-class ExampleCommand2E(ExampleCommand2, object):
-    """
-    Add things not needed in a minimal example, to try them out.
-    (Uses a PM which is the same as ExampleCommand2 except for title.)
-    """
-    # Note: object superclass is only needed to permit super(ExampleCommand2E, self) to work.
-    # object superclass should not come first, or it overrides __new__
-    # (maybe could fix using def __init__ -- not tried, since object coming last works ok)
-
-    modename = 'ExampleCommand2E-modename'
-    default_mode_status_text = "ExampleCommand2E"
-    PM_class = ExampleCommand2E_PM
-
-    standard_glDepthFunc = GL_LEQUAL # overrides default value of GL_LESS from GLPane
-        # note: this is to prevent this warning:
-        ## fyi (printonce): Overlay in reverse order (need to override standard_glDepthFunc in your new mode??)
-        # but we should probably make it the default for all modes soon.
-
-    def __init__(self, glpane):
-        "create an expr instance, to draw in addition to the model"
-        super(ExampleCommand2E, self).__init__(glpane)
-        ## expr1 = Rect(4,1,green)
-        expr1 = TextState()
-        expr2 = DraggablyBoxed(expr1, resizable = True)
-            ###BUG: resizing is projecting mouseray in the wrong way, when plane is tilted!
-            # I vaguely recall that the Boxed resizable option was only coded for use in 2D widgets,
-            # whereas some other constrained drag code is correct for 3D but not yet directly usable in Boxed.
-            # So this is just an example interactive expr, not the best way to do resizing in 3D. (Though it could be fixed.)
-
-        # note: this code is similar to expr_instance_for_imagename in confirmation_corner.py
-        ih = get_glpane_InstanceHolder(glpane)
-        expr = expr2
-        index = (id(self),) # WARNING: needs to be unique, we're sharing this InstanceHolder with everything else in NE1
-        self._expr_instance = ih.Instance( expr, index, skip_expr_compare = True)
-
-        return
-        
-    def Draw(self):
-        """
-        Do some custom drawing (in the model's abs coordsys) after drawing the model.
-        """
-        #print "start ExampleCommand2E Draw"
-        glpane = self.o
-        super(ExampleCommand2E, self).Draw()
-        from drawer import drawline
-        from constants import red
-        from VQT import V
-        drawline(red, V(1,0,1), V(1,1,1), width = 2)
-        self._expr_instance.draw()
-        #print "end ExampleCommand2E Draw"
-    
-    pass # end of class ExampleCommand2E
-
-# ==
-
-# this should be split into several files; then these imports will be at
-# the top of one of the split files
-
-from exprs.ExprsMeta import ExprsMeta
-from exprs.StatePlace import StatePlace
-from exprs.instance_helpers import IorE_guest_mixin
-
-from test_command_PMs import test_connectWithState_PM
-from test_command_PMs import CYLINDER_VERTICAL_DEFAULT_VALUE, CYLINDER_WIDTH_DEFAULT_VALUE
-    ### REVISE: the default value should come from the stateref, when using the State macro,
-    # so it can be defined only in this file and not needed via globals by the PM
-
-# REVISE: for prefs state, what is defined in what file?
-# can we make the PM code not even know whether specific state is defined in prefs or in the mode or in a node?
-# (i don't yet know how, esp for state in a node where the choice of nodes depends on other state,
-#  but it's a good goal -- do we need to get the stateref itself from the command in a standard way? ### REVIEW)
-
-# RELATED ISSUE: are staterefs useful when we don't have a UI to connect widgets to them? guess: yes.
-
-# RELATED: can there be an object with modifiable attrs which refers to prefs values?
-# If there was, the attr names would come from where? (the table in preferences.py i guess)
-# Or, always define them in your own objs as needed using a State-like macro??
-
-from test_command_PMs import CYLINDER_HEIGHT_PREFS_KEY, CYLINDER_HEIGHT_DEFAULT_VALUE
-from test_command_PMs import cylinder_round_caps
-    ### better to define here... ### REVISE
-
-def cylinder_height():
-    import env
-    return env.prefs.get( CYLINDER_HEIGHT_PREFS_KEY, CYLINDER_HEIGHT_DEFAULT_VALUE)
-
-def set_cylinder_height(val):
-    import env
-    env.prefs[CYLINDER_HEIGHT_PREFS_KEY] = val
-
-from constants import pink
-DX = V(1,0,0)
-DY = V(0,1,0)
-ORIGIN = V(0,0,0) # import these from where?
-from drawer import drawcylinder, drawsphere
-
-class test_connectWithState(ExampleCommand, IorE_guest_mixin):
-    # the following are needed for now in order to use the State macro,
-    # along with the IorE_guest_mixin superclass; this will be cleaned up:
-    __metaclass__ = ExprsMeta
-    transient_state = StatePlace('transient')
-    _e_is_instance = True
-
-    # class constants needed by mode API for example commands
-    modename = 'test_connectWithState-modename'
-    default_mode_status_text = "test_connectWithState"
-    PM_class = test_connectWithState_PM
-
-    # this might be needed if we draw any exprs:
-    standard_glDepthFunc = GL_LEQUAL
-
-    # tracked state -- this initializes specially defined instance variables
-    # which will track all their uses and changes so that connectWithState
-    # works for them:
-    cylinderVertical = State(bool, False)
-    cylinderWidth = State(float, 2.0)
-    cylinderColor = State('color-stub', pink) # type should be Color (nim), but type is not yet used
-    
-        # note: you can add _e_debug = True to one or more of these State definitions
-        # to see debug prints about some accesses to this state.
-    
-    # initial values of instance variables
-    propMgr = None
-
-    def __init__(self, glpane):
-        super(test_connectWithState, self).__init__(glpane)
-            # that only calls some mode's init method,
-            # so (for now) call this separately:
-        IorE_guest_mixin.__init__(self, glpane)
-        return
-
-    def Draw(self):
-        color = self.cylinderColor
-        length = cylinder_height()
-        if self.cylinderVertical:
-            direction = DY
-        else:
-            direction = DX
-        end1 = ORIGIN - direction * length/2.0
-        end2 = ORIGIN + direction * length/2.0
-        radius = self.cylinderWidth / 2.0
-        capped = True
-        drawcylinder(color, end1, end2, radius, capped)
-
-        if cylinder_round_caps():
-            detailLevel = 2
-            drawsphere( color, end1, radius, detailLevel)
-            drawsphere( color, end2, radius, detailLevel)
-        
-        return
-
-    def cmd_Bigger(self):
-        self.cylinderWidth += 0.5
-        set_cylinder_height( cylinder_height() + 0.5)
-        # TODO: enforce maxima
-        return
-
-    def cmd_Smaller(self):
-        self.cylinderWidth -= 0.5
-        set_cylinder_height( cylinder_height() - 0.5)
-        # enforce minima (###BUG: not the same ones as declared in the PM)
-        ### REVISE: min & max should be declared in State macro and (optionally) enforced by it
-        if self.cylinderWidth < 0.1:
-            self.cylinderWidth = 0.1
-        if cylinder_height() < 0.1:
-            set_cylinder_height(0.1)
-        return
-    
-    pass
-
-# ==
-
 class PM_WidgetDemo(ExampleCommand):
     """
     Used to demo all the PM widgets.
@@ -357,6 +162,10 @@ class PM_WidgetDemo(ExampleCommand):
     pass
 
 # == generic example or debug/devel code below here
+
+### TODO: split this part (here to end) into a separate file
+
+import EndUser, Initialize
 
 def construct_cmdrun( cmd_class, glpane):
     """Construct and return a new "CommandRun" object, for use in the given glpane.
@@ -397,7 +206,8 @@ def start_cmdrun( cmdrun):
 def enter_example_command(widget, example_command_classname):
     assert isinstance(widget, GLPane)
     glpane = widget
-    if 1 and 'reload before use (this module only)': ###during devel only
+    if 0 and EndUser.enableDeveloperFeatures(): ###during devel only; broken due to file splitting
+        # reload before use (this module only)
         if 0 and 'try reloading preqs too': ### can't work easily, glpane stores all the mode classes (not just their names)...
             glpane._reinit_modes() # just to get out of current mode safely
             import modes
@@ -411,9 +221,13 @@ def enter_example_command(widget, example_command_classname):
                 # wrong: uses old classes from glpane
         import test_command_PMs
         reload(test_command_PMs)
+        Initialize.forgetInitialization(__name__)
         import test_commands
         reload(test_commands)
         from test_commands import enter_example_command_doit
+    else:
+        from test_commands import enter_example_command_doit
+        
     enter_example_command_doit(glpane, example_command_classname)
     return
 
@@ -425,8 +239,23 @@ def enter_example_command_doit(glpane, example_command_classname):
     start_cmdrun(cmdrun)
     return
 
-for classname in ["ExampleCommand1", "ExampleCommand2", "ExampleCommand2E", "PM_WidgetDemo", "test_connectWithState"]:
-    cmdname = classname # for now
-    register_debug_menu_command( cmdname, (lambda widget, classname = classname: enter_example_command(widget, classname)) )
+def initialize():
+    if (Initialize.startInitialization(__name__)):
+        return
     
+    # initialization code
+    global test_connectWithState, ExampleCommand2E # this is needed due to globals() above (kluge)
+    from test_connectWithState import test_connectWithState
+        ### once we split this file, we can avoid import loop & still import this at top
+    from example_expr_command import ExampleCommand2E # ditto
+
+    for classname in ["ExampleCommand1", "ExampleCommand2", "ExampleCommand2E", "PM_WidgetDemo", "test_connectWithState"]:
+        cmdname = classname # for now
+        register_debug_menu_command( cmdname, (lambda widget, classname = classname: enter_example_command(widget, classname)) )
+    
+    Initialize.endInitialization(__name__)
+    return
+
+initialize() ### TODO: call this from another file, and after the reload of this module above
+
 # end
