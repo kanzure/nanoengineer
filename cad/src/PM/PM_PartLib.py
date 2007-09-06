f7b840931bc5d71fe2eb6e08356739c1fcb6eff7
Ninad Sathaye <ninad@nanorex.com>
2007-09-06 20:09:26 +0000

    files to support new Partlib mode (see also rev 10028)

---
+++ cad/src/PM/PM_PartLib.py
@@ -0,0 +1,133 @@
+#Copyright 2004-2007 Nanorex, Inc.  See LICENSE file for details.
+"""
+PM_PartLib.py
+
+The PM_PartLib class provides a groupbox that contains the partlib (any user 
+specified directory). The parts from the partlib can be pasted in the 3D 
+Workspace. The selected item in this  list is shown by its elementViewer 
+(an instance of L{PM_PreviewGroupBox}) The object being previewed can then be 
+deposited into the 3D workspace.
+
+@author: Bruce, Huaicai, Mark, Ninad
+@version: $Id$
+@copyright: 2004-2007 Nanorex, Inc.  See LICENSE file for details.
+
+History:
+The Partlib existed as a tab in the MMKit of Build Atoms Mode. (MMKit has been 
+deprecated since 2007-08-29.) 
+
+ninad 2007-09-06: Created. 
+"""
+import os
+import platform
+
+from constants      import diTUBES
+from ThumbView      import MMKitView
+from assembly       import assembly
+from files_mmp      import readmmp
+
+from PM.PM_GroupBox    import PM_GroupBox
+from PM.PM_TreeView    import PM_TreeView
+
+class PM_PartLib(PM_GroupBox):
+    """
+    The PM_PartLib class provides a groupbox containing a partlib directory
+    The selected part in this list is shown by its elementViewer 
+    (an instance of L{PM_PreviewGroupBox})
+    The part being previewed can then be deposited into the 3D workspace.
+    """
+    def __init__(self, 
+                 parentWidget,
+                 title = 'Partlib',
+                 win   = None,
+                 elementViewer = None
+                 ):            
+        self.w = win
+        self.elementViewer = elementViewer
+        self.elementViewer.setDisplay(diTUBES)
+        self.partLib = None
+        self.newModel = None
+        
+        PM_GroupBox.__init__(self, parentWidget, title)
+                
+        self._loadPartLibGroupBox()
+    
+    def connect_or_disconnect_signals(self, isConnect):         
+        """
+        Connect or disconnect widget signals sent to their slot methods.
+        @param isConnect: If True the widget will send the signals to the slot 
+                          method. 
+        @type  isConnect: boolean
+        """
+        ##if isConnect:
+        ##    change_connect = self.w.connect
+        ##else:
+        ##    change_connect = self.w.disconnect
+        
+        #Following doesn't work for some reasons so this call is disabled. 
+        #Instead , see PM_TreeView.mouseReleaseEvent where self.partChanged is 
+        #called. 
+        ##change_connect(self.partLib, 
+        ##               SIGNAL("selectionChanged(QItemSelection *,\
+        ##               QItemSelection *)"),
+        ##               self.partChanged)
+        pass
+       
+
+    def _loadPartLibGroupBox(self):
+        """
+        """
+        self.partLib = PM_TreeView(self)                    
+        self.gridLayout.addWidget(self.partLib)
+        #Append to the widget list. This is important for expand -collapse 
+        #functions (of the groupbox) to work properly.
+        self._widgetList.append(self.partLib)
+
+    def _updateElementViewer(self, newModel = None):
+        """
+        Update the view of L{self.elementViewer}
+        @param newModel: The model correseponding to the item selected 
+                         in L{self.clipboardListWidget}. 
+        @type  newModel: L{molecule} or L{Group}
+        """
+        if not self.elementViewer:
+            return  
+                
+        assert isinstance(self.elementViewer, MMKitView)  
+        
+        self.elementViewer.resetView()             
+        if newModel:
+            self.elementViewer.updateModel(newModel)
+           
+    def partChanged(self, selectedItem):
+        """
+        Method  called when user changed the partlib brower tree.
+        
+        @param selectedItem: Item currently selected in the L{self.partLib}
+        @type  selectedItem: L{self.partLib.FileItem} 
+        
+        @attention: This is called in the L{PM_TreeView.mouseReleaseEvent}. The 
+        'selectionChanged' signal for self.partLib apparently was not emitted
+        so that code has been removed.                        
+        """    
+        #Copying some old code from deprecated MMKit.py -- ninad 2007-09-06
+        item = selectedItem
+        self.newModel = None
+        if isinstance(item, self.partLib.FileItem):
+            mmpFile = str(item.getFileObj())
+            if os.path.isfile(mmpFile):
+                self.newModel = assembly(self.w, 
+                                         os.path.normpath(mmpFile)) 
+                self.newModel.o = self.elementViewer 
+                readmmp(self.newModel, mmpFile)
+                self.newModel.update_parts() #k not sure if needed after readmmp
+                self.newModel.checkparts()
+                if self.newModel.shelf.members:
+                    for m in self.newModel.shelf.members[:]:
+                        m.kill() #k guess about a correct way to handle them
+                    self.newModel.update_parts() #k probably not needed
+                    self.newModel.checkparts() #k probably not needed
+                    
+        self._updateElementViewer(self.newModel)
+        return
+ 
---
+++ cad/src/PM/PM_TreeView.py
@@ -0,0 +1,169 @@
+#Copyright 2004-2007 Nanorex, Inc.  See LICENSE file for details.
+"""
+PM_TreeView.py
+
+The PM_TreeView class provides a partlib object (as a 'treeview' of any user 
+specified directory) to the client code. 
+The parts from the partlib can be pasted in the 3D  Workspace. 
+
+@author: Bruce, Huaicai, Mark, Ninad
+@version: $Id$
+@copyright: 2004-2007 Nanorex, Inc.  See LICENSE file for details.
+
+ninad 2007-09-06: Created. 
+"""
+
+from PyQt4.Qt import QTreeView
+from PyQt4.Qt import QDir
+from PyQt4.Qt import QDirModel
+from PyQt4.Qt import Qt
+
+import os
+import env
+import sys
+
+from HistoryWidget import redmsg
+
+class PM_TreeView(QTreeView):           
+    def __init__(self, parent):        
+        """
+        """
+        QTreeView.__init__(self, parent)            
+        self.parent = parent
+        self.setEnabled(True)
+        
+        self.model = QDirModel(
+            ['*.mmp', '*.MMP'], # name filters
+            QDir.AllEntries|QDir.AllDirs|QDir.NoDotAndDotDot, # filters
+            QDir.Name # sort order
+            )
+        
+        # explanation of filters (from Qt 4.2 doc for QDirModel):
+        # - QDir.AllEntries = list files, dirs, drives, symlinks.
+        # - QDir.AllDirs = include dirs regardless of other filters 
+        #   [guess: needed to ignore the name filters for dirs]
+        # - QDir.NoDotAndDotDot = don't include '.' and '..' dirs
+        #
+        # about dirnames of "CVS":
+        # The docs don't mention any way to filter the dirnames using a 
+        # callback, or any choices besides "filter them same as filenames" or 
+        # "don't filter them". So there is no documented way to filter out the 
+        # "CVS" subdirectories like we did in Qt3
+        # (short of subclassing this and overriding some methods,
+        #  but the docs don't make it obvious how to do that correctly).
+        # Fortunately, autoBuild.py removes them from the partlib copy in built 
+        # releases.
+        #
+        # Other QDirModel methods we might want to use:
+        # QDirModel.refresh, to update its state from the filesystem 
+        # (but see the docs --
+        #  they imply we might have to pass the model's root pathname to that 
+        #  method,
+        #  even if it hasn't changed, but they're not entirely clear on that).
+        #
+        # [bruce 070502 comments]
+            
+        self.path = None
+        self.setModel(self.model)    
+        self.setWindowTitle(self.tr("Tree View"))
+        
+        self.setItemsExpandable(True)
+        self.setAlternatingRowColors(True)        
+        self.setColumnWidth(0, 150) 
+        self.setSortingEnabled(True)
+        self.sortByColumn(0, Qt.AscendingOrder)
+        self.setMinimumHeight(300)
+        
+        for i in range(2,4):
+            self.setColumnWidth(i, 4)    
+        
+        filePath = os.path.dirname(os.path.abspath(sys.argv[0]))
+        libDir = os.path.normpath(filePath + '/../partlib')
+        
+        self.libPathKey = '/nanorex/nE-1/libraryPath'
+        libDir = env.prefs.get(self.libPathKey, libDir)
+
+        if os.path.isdir(libDir):
+            self.rootDir = libDir
+            self.setRootPath(libDir)
+        else:
+            self.rootDir = None
+            env.history.message(redmsg("The part library directory: %s doesn't"\
+                                       " exist." %libDir))
+            
+        self.show()
+
+    #Ninad 070326 reimplementing mouseReleaseEvent and resizeEvent
+    #for PM_TreeView Class (which is a subclass of QTreeView) 
+    #The old code reimplemented 'event' class which handles *all* events. 
+    #There was a bad bug which didn't send an event when the widget is resized
+    # and then the seletcion is changed. In NE1Qt3 this wasn't a problem because 
+    #it only had one column. Now that we have multiple columns 
+    #(which are needed to show the horizontal scrollbar. 
+    # While using Library page only resize event or mouse release events
+    #by the user should update the thumbview. 
+    #The Qt documentation also suggests reimplementing subevents instead of the 
+    #main event handler method (event()) 
+    def mouseReleaseEvent(self, evt):
+        """
+        Reimplementation of mouseReleaseEvent method of QTreeView
+        """
+        if self.selectedItem() is not None:
+            self.parent.partChanged(self.selectedItem())
+        return QTreeView.mouseReleaseEvent(self, evt)
+        
+    def resizeEvent(self, evt):
+        """
+        Reimplementation of resizeEvent method of QTreeView
+        """
+        if self.selectedItem() is not None:
+            self.parent.partChanged(self.selectedItem())
+        return QTreeView.resizeEvent(self, evt)                              
+
+    def setRootPath(self, path):
+        """
+        Set the root path for the tree view.
+        @param path: The directory path to be set as a root path
+        """
+        self.path = path
+        self.setRootIndex(self.model.index(path))
+    
+    def selectedItem(self):
+        """
+        Returns the Item selected in the QTreeview as a L{self.FileItem}
+        @return: The Item selected in the QTreeview converted to a 
+                 L{self.FileItem} object
+        @rtype: L{self.FileItem}
+        """
+        indexes = self.selectedIndexes()
+        if not indexes:
+            return None
+        index = indexes[0]
+        if not index.isValid():
+            return None
+        
+        return self.FileItem(str(self.model.filePath(index)))
+    
+    class FileItem:
+        """
+        Class FileItem provides the filename object for class PM_TreeView
+        """
+        def __init__(self, path):
+            """
+            Constructor for the class
+            """
+            self.path = path
+            dummy, self.fileName = os.path.split(path)
+        def name(self):
+            """
+            Returns the file name
+            @return: self.fileName
+            """
+            return self.fileName
+        
+        def getFileObj(self):
+            """
+            Returns file path (self.path)
+            @return: L{self.path}
+            """
+            return self.path
