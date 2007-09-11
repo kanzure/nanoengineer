bd01a8e6fe710371d813f6a499a7e6617f837706
Ninad Sathaye <ninad@nanorex.com>
2007-09-11 19:08:06 +0000

    fixed jig property dialog border icon bug (not displayed in release built) , other minor formatting changes

--- cad/src/JigProp.py
+++ cad/src/JigProp.py
@@ -10,71 +10,97 @@ History: Original code from GroundProps.py and cleaned up by Mark.
 
 __author__ = "Mark"
 
-from PyQt4 import QtGui
 from PyQt4.Qt import QDialog
 from PyQt4.Qt import SIGNAL
 from PyQt4.Qt import QColorDialog
+from icon_utilities import geticon
 
 from JigPropDialog import Ui_JigPropDialog
-from widgets import RGBf_to_QColor, QColor_to_RGBf, get_widget_with_color_palette
+
+from widgets import RGBf_to_QColor, QColor_to_RGBf
+from widgets import get_widget_with_color_palette
 
 # This Jig Property dialog and its slot methods can be used for any simple jig
-# that has only a name and a color attribute changable by the user.  It is currently
-# used by both the Anchor and AtomSet jig.  Mark 050928.
+# that has only a name and a color attribute changable by the user.  
+# It is currently used by both the Anchor and AtomSet jig.  Mark 050928.
         
 class JigProp(QDialog, Ui_JigPropDialog):
+    """
+    This Jig Property dialog and its slot methods can be used for any simple jig
+    that has only a name and a color attribute changable by the user.  
+    It is currently used by both the Anchor and AtomSet jig.
+    """
     def __init__(self, jig, glpane):
+        """
+        Constructor for the class JigProp. 
+        @param jig: the jig whose property dialog will be shown
+        @type  jig: L{Jig}
+        @param glpane: GLPane object
+        @type  glpane: L{GLPane}
+        """
 
         QDialog.__init__(self)
-	self.setupUi(self)
-        self.connect(self.cancel_btn,SIGNAL("clicked()"),self.reject)
-        self.connect(self.ok_btn,SIGNAL("clicked()"),self.accept)
-        self.connect(self.choose_color_btn,SIGNAL("clicked()"),self.change_jig_color)
+        self.setupUi(self)
+        self.connect(self.cancel_btn, SIGNAL("clicked()"), self.reject)
+        self.connect(self.ok_btn, SIGNAL("clicked()"), self.accept)
+        self.connect(self.choose_color_btn,
+                     SIGNAL("clicked()"),
+                     self.change_jig_color)
         
         # Set the dialog's border icon and caption based on the jig type.
         jigtype_name = jig.__class__.__name__ 
-        from Utility import imagename_to_pixmap
-	
-	self.setWindowIcon(QtGui.QIcon("ui/border/"+ jigtype_name)) # Fixes bug 1208. mark 060112.
-	
+        
+        # Fixes bug 1208. mark 060112.  
+        self.setWindowIcon(geticon("ui/border/"+ jigtype_name)) 
+        
         self.setWindowTitle(jigtype_name + " Properties")
             
         self.jig = jig
         self.glpane = glpane
 
     def setup(self):
-        '''Initializes the dialog and it's widgets '''
+        """
+        Initializes the dialog and it's widgets 
+        """
         
-        self.jig_attrs = self.jig.copyable_attrs_dict() # Save the jig's attributes in case of Cancel.
+        # Save the jig's attributes in case of Cancel.
+        self.jig_attrs = self.jig.copyable_attrs_dict() 
         
         # Jig color
-        self.jig_QColor = RGBf_to_QColor(self.jig.normcolor) # Used as default color by Color Chooser
-	self.jig_color_pixmap = get_widget_with_color_palette(
+        # Used as default color by Color Chooser
+        self.jig_QColor = RGBf_to_QColor(self.jig.normcolor) 
+        self.jig_color_pixmap = get_widget_with_color_palette(
             self.jig_color_pixmap, self.jig_QColor)
 
         # Jig name
         self.nameLineEdit.setText(self.jig.name)
 
     def change_jig_color(self):
-        '''Slot method to change the jig's color.'''
+        """
+        Slot method to change the jig's color.
+        """
         color = QColorDialog.getColor(self.jig_QColor, self)
 
         if color.isValid():    
             self.jig_QColor = color
-	    self.jig_color_pixmap = get_widget_with_color_palette(
-		self.jig_color_pixmap, self.jig_QColor)
+            self.jig_color_pixmap = get_widget_with_color_palette(
+                self.jig_color_pixmap, self.jig_QColor)
             self.jig.color = self.jig.normcolor = QColor_to_RGBf(color)
             self.glpane.gl_update()
 
     def accept(self):
-        '''Slot for the 'OK' button '''
+        """
+        Slot for the 'OK' button 
+        """
         self.jig.try_rename(self.nameLineEdit.text())
         self.jig.assy.w.win_update() # Update model tree
         self.jig.assy.changed()
         QDialog.accept(self)
         
     def reject(self):
-        '''Slot for the 'Cancel' button '''
+        """
+        Slot for the 'Cancel' button 
+        """
         self.jig.attr_update(self.jig_attrs) # Restore attributes of the jig.
         self.glpane.gl_update()
         QDialog.reject(self)
