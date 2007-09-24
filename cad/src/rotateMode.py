d7a93446bc176c9a33e8292ce514e7fcec092299
Brian Helfrich <helfrich9000@gmail.com>
2007-09-24 21:09:40 +0000

    Fixing previous history-loss actions.

--- cad/src/rotateMode.py
+++ cad/src/rotateMode.py
@@ -1,17 +1,16 @@
 # Copyright 2005-2007 Nanorex, Inc.  See LICENSE file for details. 
 """
-Rotate mode functionality.
+Encapsulates rotate mode functionality.
 
 @author:    Mark Sims
 @copyright: Copyright 2007 Nanorex, Inc.  See LICENSE file for details.
-@version:   $Id: $
+@version:   $Id$
 @license:   GPL
 """
 
-from ArrangementMode import ArrangementMode
+from panMode import panlikeMode
 
-
-class RotateMode(ArrangementMode):
+class rotateMode(panlikeMode):
     """
     Encapsulates rotate mode functionality.
     """
@@ -21,25 +20,21 @@ class RotateMode(ArrangementMode):
     # Changed 'Mode' to 'Tool'. Fixes bug 1298. Mark 3/23/2006
     default_mode_status_text = "Tool: Rotate"
 
-    
     def init_gui(self):
         # Toggle on the Rotate Tool icon
         self.win.rotateToolAction.setChecked(1)
         self.glpane.setCursor(self.win.RotateCursor)
     
-        
     def restore_gui(self):
         # Toggle off the Rotate Tool icon
         self.win.rotateToolAction.setChecked(0)
 
-        
     def leftDown(self, event):
         self.glpane.SaveMouse(event)
         self.glpane.trackball.start(self.glpane.MousePos[0],
                                     self.glpane.MousePos[1])
         self.picking = False
 
-        
     def leftDrag(self, event):
         self.glpane.SaveMouse(event)
         q = self.glpane.trackball.update(self.glpane.MousePos[0],
@@ -48,7 +43,6 @@ class RotateMode(ArrangementMode):
         self.glpane.gl_update()
         self.picking = False
 
-        
     def update_cursor_for_no_MB(self): # Fixes bug 1638. Mark 3/12/2006
         """
         Update the cursor for 'Rotate' mode.
