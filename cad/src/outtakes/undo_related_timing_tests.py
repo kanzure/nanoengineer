f03bdd01075c5ed7e2e0ec62439b1cbb4923d051
Bruce Smith <bruce@nanorex.com>
2007-10-04 17:07:03 +0000

    remove obsolete test code; minor code cleanup

---
+++ cad/src/outtakes/undo_related_timing_tests.py
@@ -0,0 +1,228 @@
+"""
+$Id$
+"""
+
+# debug/test functions (in other submenu of debug menu) which won't be kept around. 
+# [moved out of undo.py by bruce 071004]
+
+# not all of these are needed, perhaps:
+from cPickle import dump, load, HIGHEST_PROTOCOL
+import env
+from debug import register_debug_menu_command, register_debug_menu_command_maker
+    ###@@@ don't put all those commands in there -- use a submenu, use atom-debug,
+    # or let them only show up if a related flag is set, or so...
+from PyQt4.Qt import SIGNAL, QObject, QWidget #k ok to do these imports at toplevel? I hope so, since we need them in several places.
+from constants import genKey, noop
+import platform # for atom_debug [bruce 060128, suggested by Mark;
+    # if this works, we should simplify some defs which worry if it's too early for this]
+
+
+from debug import call_func_with_timing_histmsg
+
+def atpos_list(part):
+    "Return a list of atom-position arrays, one per chunk in part. Warning: might include shared mutable arrays."
+    res = []
+    for m in part.molecules:
+        res.append( m.atpos)
+    return res
+
+def save_atpos_list(part, filename):
+    save_obj( atpos_list(part), filename)
+
+def save_obj( thing, filename):
+    file = open(filename, "wb")
+    dump( thing, file, HIGHEST_PROTOCOL) # protocols: 0 ascii, 1 binary, 2 (new in 2.3) better for new style classes
+    file.close()
+
+def load_obj(filename):
+    file = open(filename, "rb")
+    res = load(file)
+    file.close()
+    return res
+
+def saveposns(part, filename):
+    env.history.message( "save main part atom posns to file: " + filename )
+    def doit():
+        save_atpos_list(part, filename)
+    call_func_with_timing_histmsg( doit)
+
+def saveposns_cmd( target): # arg is the widget that has this debug menu
+    win = target.topLevelWidget()
+    assy = win.assy
+    part = assy.tree.part
+    filename = assy.filename + "X"
+    saveposns(part, filename)
+    return
+
+## register_debug_menu_command("save atom posns", saveposns_cmd) # put this command into debug menu
+
+# ==
+
+def loadposns( part, filename): # doesn't move atoms (just a file-reading speed test, for now)
+    env.history.message( "load atom posns from file (discards them, doesn't move atoms): " + filename )
+    def doit():
+        return load_obj(filename)
+    posns = call_func_with_timing_histmsg( doit)
+    return
+
+def loadposns_cmd( target):
+    win = target.topLevelWidget()
+    assy = win.assy
+    part = assy.tree.part
+    filename = assy.filename + "X"
+    loadposns(part, filename)
+    return
+
+## register_debug_menu_command("load atom posns", loadposns_cmd)
+
+# ==
+
+from Numeric import concatenate, array, UnsignedInt8
+
+def atom_array_of_part(part):
+    "Return an Array of all atoms in the Part. Try to be linear time."
+    res = []
+    for m in part.molecules:
+        res.append( array(m.atlist) )
+    # concatenate might fail for chunks with exactly 1 atom -- fix later ####@@@@
+    return concatenate(res) ###k
+
+def saveelts( part, filename):
+    env.history.message( "save main part element symbols -- no, atomic numbers -- to file: " + filename )
+    def doit():
+        atoms = atom_array_of_part(part)
+        ## elts = [atm.element.symbol for atm in atoms]
+        elts = [atm.element.eltnum for atm in atoms]
+        env.history.message( "%d element nums, first few are %r" % (len(elts), elts[:5] ) )
+        thing = array(elts)
+        save_obj(thing, filename)
+    call_func_with_timing_histmsg( doit)
+    return
+
+def saveelts_cmd( target):
+    win = target.topLevelWidget()
+    assy = win.assy
+    part = assy.tree.part
+    filename = assy.filename + "E" # E, not X
+    saveelts(part, filename)
+    return
+
+## register_debug_menu_command("save atom element numbers", saveelts_cmd)
+
+# ==
+
+def savebtypes( part, filename):
+    env.history.message( "save main part bond type v6 ints to file: " + filename )
+    def doit():
+        atoms = atom_array_of_part(part)
+        res = []
+        for atm in atoms:
+            for b in atm.bonds:
+                # don't worry for now about hitting each bond twice... well, do, and see if this makes it slower, as i guess.
+                if b.atom1 is atm:
+                    res.append(b.v6) # a small int
+        env.history.message( "%d btype ints, first few are %r" % (len(res), res[:5] ) )
+        thing = array(res, UnsignedInt8) # tell it to use a smaller type; see numpy.pdf page 14 on typecodes.
+            # update, bruce 070612: we still use Numeric Python (not numarray or numpy). I am not sure what URL
+            # is referred to by "numpy.pdf" above, but it is probably (and should be) about Numeric Python.
+        save_obj(thing, filename)
+    call_func_with_timing_histmsg( doit)
+    return
+
+def savebtypes_cmd( target):
+    win = target.topLevelWidget()
+    assy = win.assy
+    part = assy.tree.part
+    filename = assy.filename + "B" # B, not E or X
+    savebtypes(part, filename)
+    return
+
+## register_debug_menu_command("save bond type v6 ints", savebtypes_cmd)
+
+# ==
+
+# moved to pyrex_test.py
+
+### this depends on undotest.pyx having been compiled by Pyrex ####@@@@
+##
+##def count_bonds_cmd( target):
+##    win = target.topLevelWidget()
+##    assy = win.assy
+##    part = assy.tree.part
+##    mols = part.molecules
+##    env.history.message( "count bonds (twice each) in %d mols:" % len(mols) )
+##    from undotest import nbonds # count bonds (twice each) in a sequence of molecules
+##    def doit():
+##        return nbonds(mols)
+##    nb = call_func_with_timing_histmsg( doit)
+##    env.history.message("count was %d, half that is %d" % (nb, nb/2) )
+##    return
+##
+##register_debug_menu_command("count bonds", count_bonds_cmd)
+
+# ==
+
+def blerg_try1(mols):
+    dict1 = {}
+    for m in mols:
+        atlist = m.atlist
+        atpos = m.atpos
+        keys = map( lambda a: a.key, atlist )
+        # now efficiently loop over both those lists. I think there's a better way in python
+        # (and surely it should be done in pyrex or Numeric anyway), but try easy way first:
+        for key, pos in zip(keys, atpos):
+            dict1[key] = pos
+    # now dict1 maps key->pos for every atom in the system
+    return len(dict1), 0 # untested retval
+
+def update_keyposdict_keyv6dict_from_mol( dict1, dictb, m):
+    atlist = m.atlist
+    atpos = m.atpos
+    ## keys = map( lambda a: a.key, atlist )
+    # now efficiently loop over both those lists. I think there's a better way in python
+    # (and surely it should be done in pyrex or Numeric anyway), but try easy way first:
+    for atm, pos in zip(atlist, atpos):
+        ## dict1[id(atm)] = pos
+        dict1[atm.key] = pos
+        for b in atm.bonds:
+            # use b.key -- not ideal but good enough test for now 
+            dictb[b.key] = b.v6 # this runs twice per bond
+    return
+
+def diff_keyposdict_from_mol( dict1, dict2, m):
+    """Like update_keyposdict_from_mol, but use dict1 as old state, and store in dict2 just the items that differ.
+    Deleted atoms must be handled separately -- this won't help find them even when run on all mols.
+    [Could change that by having this delete what it found from dict1, at further substantial speed cost -- not worth it.]
+    """
+    atlist = m.atlist
+    atpos = m.atpos
+    for atm, pos in zip(atlist, atpos):
+        key = atm.key
+        if pos != dict1.get(key): # Numeric compare -- might be slow, we'll see
+            dict2[key] = pos
+    return
+    
+def blerg(mols):
+    dict1 = {}
+    dict2 = {}
+    dictb = {}
+    for m in mols:
+        update_keyposdict_keyv6dict_from_mol( dict1, dictb, m)
+        diff_keyposdict_from_mol( dict1, dict2, m)
+    # now dict1 maps key->pos for every atom in the system
+    # and dictb has all v6's of all bonds
+    return len(dict1), len(dictb)
+
+def blerg_cmd( target):
+    win = target.topLevelWidget()
+    assy = win.assy
+    part = assy.tree.part
+    mols = part.molecules
+    env.history.message( "blorg in %d mols:" % len(mols) )
+    def doit():
+        return blerg(mols)
+    na, nb = call_func_with_timing_histmsg( doit)
+    env.history.message("count was %d, %d" % (na,nb,) )
+    return
+
+## register_debug_menu_command("make key->pos dict", blerg_cmd)
--- cad/src/undo.py
+++ cad/src/undo.py
@@ -1,5 +1,5 @@
 # Copyright 2005-2007 Nanorex, Inc.  See LICENSE file for details. 
-'''
+"""
 undo.py
 
 Undo-related code.
@@ -25,245 +25,26 @@ and perhaps HistoryWidget.py.
 A result of the mess of modules might be that too much gets imported
 at app-startup time (even before .atom-debug-rc gets to run).
 This needs to be cleaned up, but it's probably not urgent.
+"""
 
-'''
 __author__ = 'bruce'
 
 debug_print_undo = False # DO NOT COMMIT with True -- causes lots of debug prints regardless of atom_debug
 
 _use_hcmi_hack = True # enable signal->slot call intercepting code, to check for bugs that mess up other things [bruce 050922]
+    # i suspect this now has to be true (undo won't work without it) -- if true, remove this [bruce 071003 comment] 
 
 debug_print_fewer_args_retries = False # debug prints for fewer-args retries; DO NOT COMMIT with True
 
 
 ###@@@ import this module from a better place than we do now!
 
-from cPickle import dump, load, HIGHEST_PROTOCOL
 import env
-from debug import register_debug_menu_command, register_debug_menu_command_maker
-    ###@@@ don't put all those commands in there -- use a submenu, use atom-debug,
-    # or let them only show up if a related flag is set, or so...
-from PyQt4.Qt import SIGNAL, QObject, QWidget #k ok to do these imports at toplevel? I hope so, since we need them in several places.
-from constants import genKey, noop
-import platform # for atom_debug [bruce 060128, suggested by Mark;
-    # if this works, we should simplify some defs which worry if it's too early for this]
+from debug import register_debug_menu_command
+from PyQt4.Qt import QObject ## , QWidget, SIGNAL
+import platform # for atom_debug
 
 # ==
-
-# debug/test functions (in other submenu of debug menu) which won't be kept around.
-
-from debug import call_func_with_timing_histmsg
-
-def atpos_list(part):
-    "Return a list of atom-position arrays, one per chunk in part. Warning: might include shared mutable arrays."
-    res = []
-    for m in part.molecules:
-        res.append( m.atpos)
-    return res
-
-def save_atpos_list(part, filename):
-    save_obj( atpos_list(part), filename)
-
-def save_obj( thing, filename):
-    file = open(filename, "wb")
-    dump( thing, file, HIGHEST_PROTOCOL) # protocols: 0 ascii, 1 binary, 2 (new in 2.3) better for new style classes
-    file.close()
-
-def load_obj(filename):
-    file = open(filename, "rb")
-    res = load(file)
-    file.close()
-    return res
-
-def saveposns(part, filename):
-    env.history.message( "save main part atom posns to file: " + filename )
-    def doit():
-        save_atpos_list(part, filename)
-    call_func_with_timing_histmsg( doit)
-
-def saveposns_cmd( target): # arg is the widget that has this debug menu
-    win = target.topLevelWidget()
-    assy = win.assy
-    part = assy.tree.part
-    filename = assy.filename + "X"
-    saveposns(part, filename)
-    return
-
-## register_debug_menu_command("save atom posns", saveposns_cmd) # put this command into debug menu
-
-# ==
-
-def loadposns( part, filename): # doesn't move atoms (just a file-reading speed test, for now)
-    env.history.message( "load atom posns from file (discards them, doesn't move atoms): " + filename )
-    def doit():
-        return load_obj(filename)
-    posns = call_func_with_timing_histmsg( doit)
-    return
-
-def loadposns_cmd( target):
-    win = target.topLevelWidget()
-    assy = win.assy
-    part = assy.tree.part
-    filename = assy.filename + "X"
-    loadposns(part, filename)
-    return
-
-## register_debug_menu_command("load atom posns", loadposns_cmd)
-
-# ==
-
-from Numeric import concatenate, array, UnsignedInt8
-
-def atom_array_of_part(part):
-    "Return an Array of all atoms in the Part. Try to be linear time."
-    res = []
-    for m in part.molecules:
-        res.append( array(m.atlist) )
-    # concatenate might fail for chunks with exactly 1 atom -- fix later ####@@@@
-    return concatenate(res) ###k
-
-def saveelts( part, filename):
-    env.history.message( "save main part element symbols -- no, atomic numbers -- to file: " + filename )
-    def doit():
-        atoms = atom_array_of_part(part)
-        ## elts = [atm.element.symbol for atm in atoms]
-        elts = [atm.element.eltnum for atm in atoms]
-        env.history.message( "%d element nums, first few are %r" % (len(elts), elts[:5] ) )
-        thing = array(elts)
-        save_obj(thing, filename)
-    call_func_with_timing_histmsg( doit)
-    return
-
-def saveelts_cmd( target):
-    win = target.topLevelWidget()
-    assy = win.assy
-    part = assy.tree.part
-    filename = assy.filename + "E" # E, not X
-    saveelts(part, filename)
-    return
-
-## register_debug_menu_command("save atom element numbers", saveelts_cmd)
-
-# ==
-
-def savebtypes( part, filename):
-    env.history.message( "save main part bond type v6 ints to file: " + filename )
-    def doit():
-        atoms = atom_array_of_part(part)
-        res = []
-        for atm in atoms:
-            for b in atm.bonds:
-                # don't worry for now about hitting each bond twice... well, do, and see if this makes it slower, as i guess.
-                if b.atom1 is atm:
-                    res.append(b.v6) # a small int
-        env.history.message( "%d btype ints, first few are %r" % (len(res), res[:5] ) )
-        thing = array(res, UnsignedInt8) # tell it to use a smaller type; see numpy.pdf page 14 on typecodes.
-            # update, bruce 070612: we still use Numeric Python (not numarray or numpy). I am not sure what URL
-            # is referred to by "numpy.pdf" above, but it is probably (and should be) about Numeric Python.
-        save_obj(thing, filename)
-    call_func_with_timing_histmsg( doit)
-    return
-
-def savebtypes_cmd( target):
-    win = target.topLevelWidget()
-    assy = win.assy
-    part = assy.tree.part
-    filename = assy.filename + "B" # B, not E or X
-    savebtypes(part, filename)
-    return
-
-## register_debug_menu_command("save bond type v6 ints", savebtypes_cmd)
-
-# ==
-
-# moved to pyrex_test.py
-
-### this depends on undotest.pyx having been compiled by Pyrex ####@@@@
-##
-##def count_bonds_cmd( target):
-##    win = target.topLevelWidget()
-##    assy = win.assy
-##    part = assy.tree.part
-##    mols = part.molecules
-##    env.history.message( "count bonds (twice each) in %d mols:" % len(mols) )
-##    from undotest import nbonds # count bonds (twice each) in a sequence of molecules
-##    def doit():
-##        return nbonds(mols)
-##    nb = call_func_with_timing_histmsg( doit)
-##    env.history.message("count was %d, half that is %d" % (nb, nb/2) )
-##    return
-##
-##register_debug_menu_command("count bonds", count_bonds_cmd)
-
-# ==
-
-def blerg_try1(mols):
-    dict1 = {}
-    for m in mols:
-        atlist = m.atlist
-        atpos = m.atpos
-        keys = map( lambda a: a.key, atlist )
-        # now efficiently loop over both those lists. I think there's a better way in python
-        # (and surely it should be done in pyrex or Numeric anyway), but try easy way first:
-        for key, pos in zip(keys, atpos):
-            dict1[key] = pos
-    # now dict1 maps key->pos for every atom in the system
-    return len(dict1), 0 # untested retval
-
-def update_keyposdict_keyv6dict_from_mol( dict1, dictb, m):
-    atlist = m.atlist
-    atpos = m.atpos
-    ## keys = map( lambda a: a.key, atlist )
-    # now efficiently loop over both those lists. I think there's a better way in python
-    # (and surely it should be done in pyrex or Numeric anyway), but try easy way first:
-    for atm, pos in zip(atlist, atpos):
-        ## dict1[id(atm)] = pos
-        dict1[atm.key] = pos
-        for b in atm.bonds:
-            # use b.key -- not ideal but good enough test for now 
-            dictb[b.key] = b.v6 # this runs twice per bond
-    return
-
-def diff_keyposdict_from_mol( dict1, dict2, m):
-    """Like update_keyposdict_from_mol, but use dict1 as old state, and store in dict2 just the items that differ.
-    Deleted atoms must be handled separately -- this won't help find them even when run on all mols.
-    [Could change that by having this delete what it found from dict1, at further substantial speed cost -- not worth it.]
-    """
-    atlist = m.atlist
-    atpos = m.atpos
-    for atm, pos in zip(atlist, atpos):
-        key = atm.key
-        if pos != dict1.get(key): # Numeric compare -- might be slow, we'll see
-            dict2[key] = pos
-    return
-    
-def blerg(mols):
-    dict1 = {}
-    dict2 = {}
-    dictb = {}
-    for m in mols:
-        update_keyposdict_keyv6dict_from_mol( dict1, dictb, m)
-        diff_keyposdict_from_mol( dict1, dict2, m)
-    # now dict1 maps key->pos for every atom in the system
-    # and dictb has all v6's of all bonds
-    return len(dict1), len(dictb)
-
-def blerg_cmd( target):
-    win = target.topLevelWidget()
-    assy = win.assy
-    part = assy.tree.part
-    mols = part.molecules
-    env.history.message( "blorg in %d mols:" % len(mols) )
-    def doit():
-        return blerg(mols)
-    na, nb = call_func_with_timing_histmsg( doit)
-    env.history.message("count was %d, %d" % (na,nb,) )
-    return
-
-## register_debug_menu_command("make key->pos dict", blerg_cmd)
-
-# ===  [real code starts here]
-
 
 def reload_undo(target=None):
     import undo_archive
@@ -554,7 +335,6 @@ class hacked_connect_method_installer: #e could be refactored into hacked-method
         return res
     def debug_print_stats(self, msg = '?'):
         self.stage = msg
-        #e change to debug print only, but how soon can we import platform?
         if debug_print_undo:
             print "hcmi %r: %r" % (self.stage, self.conns)
     def maybe_wrapslot(self, sender, signal, slotboundmethod, keepcache_object = None):
@@ -617,8 +397,6 @@ def hack_qwidget_pre_win_init(): # call this once, or more times if you can't av
 # app startup code must call these at the right times:
 
 def call_asap_after_QWidget_and_platform_imports_are_ok():
-    import PyQt4.Qt
-    import platform # make sure these imports can be done now; platform will be needed later for atom_debug
     if not _use_hcmi_hack: return
     hack_qwidget_pre_win_init()
     _hcmi.debug_print_stats('first call')
@@ -733,12 +511,21 @@ def normalize_signal(signal):
 # ==
 
 def args_info(func1):
-    """Given a function or method object, return (success, maxargs, kws_ok),
-    where success is a boolean saying whether we succeeded in finding out other retvals (if not they are "loose guesses"),
-    maxargs is the max number of positional args it can take, according to introspection, or None if infinite or we can't tell,
-    and kws_ok says whether it can accept any keyword args whatsoever, whether of specific or arb names (or is True if we can't tell).
     """
-    return False, None, True # what to return if we can't tell -- this is a stub to always return it
+    Given a function or method object, try to find out what argument lists it can accept.
+    Return value is a tuple (success, maxargs, kws_ok),
+    where success is a boolean saying whether we succeeded in finding out anything for sure
+    (if not, the other tuple elements are "loose guesses");
+    maxargs is the maximum number of positional args the function can be called with,
+    according to introspection, or None if this is infinite or we can't tell;
+    and kws_ok says whether the function can accept any keyword arguments
+    (or is True if we can't tell).
+    """
+    return False, None, True # stub - return values represent complete uncertainty;
+     # TODO: to do this right, we should use code from some introspection module,
+     # such as inspect.getargspec,
+     # but have a fallback to the above values in case that fails for
+     # certain kinds of callables.
 
 # ==
 
