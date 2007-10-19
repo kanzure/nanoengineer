8421a39d95a7a13542a71f0f7ad77ae45caf9398
Bruce Smith <bruce@nanorex.com>
2007-10-19 05:07:58 +0000

    improve doc; explain how to make atom_debug true from launch

--- cad/src/platform.py
+++ cad/src/platform.py
@@ -1,21 +1,49 @@
 # Copyright 2004-2007 Nanorex, Inc.  See LICENSE file for details. 
 
+"""
+platform.py -- provide the atom_debug developer debugging flag.
+
+TODO: rename this module and the flag. (Hard, since lots of uses.)
+And, change some of the uses into more specific debugging flags.
+
+$Id$
+
+Notes:
+
+atom_debug can be set on startup from an environment variable,
+or by a developer directly editing this file in their local copy.
+(See this module's code for details.)
+
+It can be changed during one session of NE1 using the ATOM_DEBUG
+checkmark item in the GLPane debug menu.
+
+It is not persistent between sessions.
+
+TODO: document code usage notes -- how to use it, what to use it for.
+"""
+
 import os
 
-# atom_debug variable:
+# Developers who want atom_debug to start out True on every launch
+# can uncomment the following line -- but don't commit that change!
 
-# When we start, figure out whether user wants to enable general debugging code
-# which turns on extra internal error checking (perhaps slowing down the code).
-# There is no need to document this, since it is intended for developers familiar
-# with the python code.
-# I put this into platform.py in case the way of initializing it is platform-specific.
-# If we think of a more sensible place to put this, we can move it. [bruce 041103]
+## atom_debug = True
+
+# WARNING: having atom_debug set from startup is currently broken,
+# at least when ALTERNATE_CAD_SRC_PATH is being used. It causes code
+# to run which doesn't otherwise run, and which fails with fatal tracebacks.
+# I don't know why, or for how long this has been true. I used to start
+# with it set all the time, but that was a few months back and before I
+# routinely used ALTERNATE_CAD_SRC_PATH (in which state I've never before
+# started with it set). [bruce 071018]
+
+# ==
 
 try:
     atom_debug # don't disturb it if already set (e.g. by .atom-debug-rc)
 except:
     try:
-        atom_debug = os.environ['ATOM_DEBUG'] # as a string; suggest "1" or "0"
+        atom_debug = os.environ['ATOM_DEBUG'] # as a string; should be "1" or "0"
     except:
         atom_debug = 0
     try:
@@ -26,3 +54,5 @@ except:
 
 if atom_debug:
     print "fyi: user has requested ATOM_DEBUG feature; extra debugging code enabled; might be slower"
+
+# end
