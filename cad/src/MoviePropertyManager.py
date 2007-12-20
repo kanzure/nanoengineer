dd4ffeccf2d4bbddfe58fb96ea9c8f005154f1ae
Mark Sims <mark@nanorex.com>
2007-12-20 23:15:26 +0000

    Code cleanup in preparation of more (minor) work on this file: - Converted tabs to spaces - Misc. whitespace cleanup

--- cad/src/MoviePropertyManager.py
+++ cad/src/MoviePropertyManager.py
@@ -8,7 +8,6 @@ MoviePropertyManager.py
 History:
 ninad20070507 : Converted movie dashboard into movie Property manager
 
-
 """
 __author__  = "Ninad"
 
@@ -26,14 +25,14 @@ class MoviePropertyManager(Ui_MoviePropertyManager):
     def __init__(self, parentMode):
         """
         Constructor for the B{Movie} property manager.
-        
+
         @param parentMode: The parent mode where this Property Manager is used
         @type  parentMode: L{movieMode} 
         """
         Ui_MoviePropertyManager.__init__(self, parentMode)
         self._addGroupBoxes() 
         ##self.updateMessage(msg) 
-    
+
     def ok_btn_clicked(self):
         """
         Calls MainWindow.toolsDone to exit the current mode. 
@@ -41,7 +40,7 @@ class MoviePropertyManager(Ui_MoviePropertyManager):
         PM_Dialog)
         """
         self.w.toolsDone()
-    
+
     def cancel_btn_clicked(self):
         """
         Calls MainWindow.toolsDone to exit the current mode. 
@@ -49,51 +48,50 @@ class MoviePropertyManager(Ui_MoviePropertyManager):
         PM_Dialog)
         """
         self.w.toolsCancel()
-    
+
     def updateMessage(self, msg = ''):
         """
         Updates the message box with an informative message.
         """
         if not msg:
             msg = "Use movie control buttons in the Property Manager to play \
-            current simulation movie (if it exists). You can also load a previously\
-            saved movie for this model using <b>'Open Movie File...'</b> option."
-            
+                current simulation movie (if it exists). You can also load a \
+                previously saved movie for this model using \
+                <b>'Open Movie File...'</b> option."
+
         self.MessageGroupBox.insertHtmlMessage( msg, 
                                                 minLines      = 6,
                                                 setAsDefault  =  True )
-    
+
     def getOpenMovieFileInfo(self):
-	"""
+        """
 	Updates the message groupbox message in the Movie Property Manager
 	"""
-	msg = ''
-	movie = self.w.assy.current_movie
-	
-	if movie:
-	    if movie.filename:
-		fileName, numOfFrames, numOfAtoms = movie.getMovieInfo()
-		msg1 = "<b>Movie File:</b> [%s] <br>" % (fileName)
-		msg2 = "<b> Number Of Frames: </b>%s <br>" % (str(numOfFrames))
-		msg3 = "<b> Number Of Atoms: </b>%s " % (str(numOfAtoms))
-		msg = msg1 + msg2 + msg3
-	    else:
-		msg1 = redmsg("No movie file opened.<br>")
-		msg2 = " Use <b>'Open movie file'</b> to load any existing"\
-		       " movie for the current part."
-		msg = msg1 + msg2
-	else:
-	    msg = redmsg("No movie file opened.")
-	
-	return msg
-		  
-    
+        msg = ''
+        movie = self.w.assy.current_movie
+
+        if movie:
+            if movie.filename:
+                fileName, numOfFrames, numOfAtoms = movie.getMovieInfo()
+                msg1 = "<b>Movie File:</b> [%s] <br>" % (fileName)
+                msg2 = "<b> Number Of Frames: </b>%s <br>" % (str(numOfFrames))
+                msg3 = "<b> Number Of Atoms: </b>%s " % (str(numOfAtoms))
+                msg = msg1 + msg2 + msg3
+            else:
+                msg1 = redmsg("No movie file opened.<br>")
+                msg2 = " Use <b>'Open movie file'</b> to load any existing"\
+                     " movie for the current part."
+                msg = msg1 + msg2
+        else:
+            msg = redmsg("No movie file opened.")
+
+        return msg
+
     def show(self):
         """
 	Overrides the Ui_MoviePropertyManager.show) method. 
 	Updates the message groupbox with movie file information
         """
-	msg = self.getOpenMovieFileInfo()
+        msg = self.getOpenMovieFileInfo()
         self.updateMessage(msg) 
-	Ui_MoviePropertyManager.show(self)
-        
+        Ui_MoviePropertyManager.show(self)
