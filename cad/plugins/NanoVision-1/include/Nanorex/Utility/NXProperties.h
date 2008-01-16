57340cf1406ac9b1847f037a51d936f887fa3f6b
Brian Helfrich <helfrich9000@gmail.com>
2008-01-16 15:03:01 +0000

    More plumbing.

---
+++ cad/plugins/NanoVision-1/include/Nanorex/Utility/NXProperties.h
@@ -0,0 +1,54 @@
+// Copyright 2008 Nanorex, Inc.  See LICENSE file for details.
+
+#ifndef NX_PROPERTIES_H
+#define NX_PROPERTIES_H
+
+#ifdef WIN32
+#	ifdef _MSC_VER
+#		pragma warning(disable:4786)
+#	endif
+#endif
+
+#include <map>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <strstream>
+
+namespace Nanorex {
+
+
+/* CLASS: NXProperties */
+/**
+ * A set of key/value pairs.
+ * @ingroup NanorexUtility
+ */
+class NXProperties {
+	public:
+		NXProperties();
+		~NXProperties();
+		std::string writeToFile(const std::string& filename);
+		std::string readFromFile(const std::string& filename);
+		void setProperty(const std::string& key, const std::string& value);
+		void addProperties(Properties* newProps, const char* prePendChars);
+		bool keyExists(const char* key);
+		const char* getProperty(const char* key);
+		const char* getProperty(const char* key, const char* defaultValue);
+		const char* getProperty(const std::string& key);
+		const char* getProperty(const std::string& key,
+								const char* defaultValue);
+		std::vector<std::string> getPropertyKeys();
+		void clear();
+		const std::string serialize();
+		bool deSerialize(const std::string& serializedData);
+
+	private:
+		std::map<std::string, std::string> properties;
+
+		void deSerializeLine(const std::string& dataLine);
+};
+
+} // Nanorex::
+
+#endif
---
+++ cad/plugins/NanoVision-1/src/Utility/NXProperties.cpp
@@ -0,0 +1,294 @@
+// Copyright 2008 Nanorex, Inc.  See LICENSE file for details.
+
+#include "Nanorex/Utility/NXProperties.h"
+
+namespace nanohive {
+
+
+/* CONSTRUCTOR */
+/**
+ * Constructs an empty NXProperties object.
+ */
+NXProperties::NXProperties() {
+}
+
+
+/* DESTRUCTOR */
+NXProperties::~NXProperties() {
+}
+
+
+/* FUNCTION: writeToFile */
+/**
+ * Writes these properties to the given file. The format of the file is as
+ * follows:
+ *
+	\code
+	key1=value1
+	key2=value2
+	...
+	\endcode
+ *
+ * @return	If the given file was successfully opened and completely written, an
+ * empty string ("") is returned. Otherwise, an error message is returned.
+ */
+std::string NXProperties::writeToFile(const std::string& filename) {
+	std::string errors = "";
+
+	// Open file for reading.
+	std::ofstream outFile(filename.c_str(), std::ios::out);
+	if (!outFile) {
+		errors = "File could not be opened: " + filename + ".";
+
+	} else {
+		std::string line;
+		std::map<std::string, std::string>::iterator iter;
+		for (iter = properties.begin(); iter != properties.end(); iter++) {
+			line = iter->first;
+			line += "=";
+			line += iter->second;
+			outFile << line << std::endl;
+		}
+		outFile.close();
+	}
+	return errors;
+}
+
+
+/* FUNCTION: readFromFile */
+/**
+ * Opens the given file and reads in the properties it contains. The format
+ * of the file is as follows:
+ *
+	\code
+	# Comment
+	key1=value1
+	key2=value2
+	...
+	\endcode
+ *
+ * Comments and blank lines are ignored as are white space around keys and
+ * values.
+ *
+ * @return	If the given file was successfully opened and completely read, an
+ * empty string ("") is returned. Otherwise, an error message is returned if
+ * any line in the input file is malformed. The whole file is read and parsed
+ * and all good/well-formed lines are incorporated into this set of properties.
+ */
+std::string NXProperties::readFromFile(const std::string& filename) {
+	std::string errors = "";
+
+	// Open file for reading.
+	std::ifstream inFile(filename.c_str(), std::ios::in);
+	if (!inFile) {
+		errors = "File could not be opened: " + filename + ".";
+
+	} else {
+		// Read properties.
+		std::string line;
+		while (std::getline(inFile, line, '\n')) {
+			try {
+				deSerializeLine(line);
+			} catch (...) {
+				errors += " Bad line: " + line;
+			}
+		}
+		inFile.close();
+	}
+	return errors;
+}
+
+
+/* FUNCTION: setProperty */
+/**
+ * Sets the given key's value.
+ */
+void NXProperties::setProperty(const std::string& key,
+							   const std::string& value) {
+	properties[key] = value;
+}
+
+
+/* FUNCTION; addProperties */
+/**
+ * Adds the given Properties to this Properties object, prepending the key of
+ * each new key/value pair with the given string. Any existing properties will
+ * be over-ridden by those found in the new Properties.
+ *
+ * @param	newProps
+ *			The new key/value pairs to add.
+ * @param	prePendChars
+ *			The string to prepend to each new key/value pair's key.
+ */
+void NXProperties::addProperties(Properties* newProps,
+							   const char* prePendChars) {
+	std::string prePendString = std::string(prePendChars);
+
+	std::map<std::string, std::string>::iterator newPropsIter;
+	for (newPropsIter = newProps->properties.begin();
+		 newPropsIter != newProps->properties.end();
+		 newPropsIter++) {
+		properties[prePendString + newPropsIter->first] = newPropsIter->second;
+	}
+}
+
+
+/* FUNCTION: keyExists */
+bool NXProperties::keyExists(const char* key) {
+	return (properties.count(key) != 0);
+}
+
+
+/* FUNCTION: getProperty */
+/**
+ * Returns the value of the given key, or an empty string if the key is not
+ * found.
+ */
+const char* NXProperties::getProperty(const char* key) {
+	const char* value = properties[std::string(key)].c_str();
+	return value;
+}
+/**
+ * Returns the value of the given key, or an empty string if the key is not
+ * found.
+ */
+const char* NXProperties::getProperty(const std::string& key) {
+	const char* value = properties[key].c_str();
+	return value;
+}
+/**
+ * Returns the value of the given key with an optional default value if the key
+ * is not found.
+ */
+const char* NXProperties::getProperty(const char* key,
+									  const char* defaultValue) {
+	if (properties.count(key) == 0)
+		return defaultValue;
+	else
+		return getProperty(key);
+}
+/**
+ * Returns the value of the given key with an optional default value if the key
+ * is not found.
+ */
+const char* NXProperties::getProperty(const std::string& key,
+									const char* defaultValue) {
+	if (properties.count(key) == 0)
+		return defaultValue;
+	else
+		return getProperty(key);
+}
+
+
+/* FUNCTION: getPropertyKeys */
+/**
+ * Returns all the keys in a vector.
+ */
+std::vector<std::string> NXProperties::getPropertyKeys() {
+	std::vector<std::string> keys;
+	std::map<std::string, std::string>::iterator iter = properties.begin();
+	std::map<std::string, std::string>::iterator iterEnd = properties.end();
+	while (iter != iterEnd) {
+		keys.push_back(iter->first);
+		iter++;
+	}
+	return keys;
+}
+
+
+/* FUNCTION: clear */
+/**
+ * Clears all properties.
+ */
+void NXProperties::clear() {
+	properties.clear();
+}
+
+
+/* FUNCTION: serialize */
+/**
+ * Returns this NXProperties object serialized.
+ */
+const std::string NXProperties::serialize() {
+	std::string buffer;
+	std::map<std::string, std::string>::iterator iter;
+
+	for (iter = properties.begin(); iter != properties.end(); iter++) {
+		buffer += iter->first;
+		buffer += "=";
+		buffer += iter->second;
+		buffer += "\n";
+	}
+	return buffer;
+}
+
+
+/* FUNCTION: deSerialize */
+/**
+ * Reads in the key/value pairs found in the given serialized NXProperties
+ * string.
+ *
+ * @return Whether or not the de-serialization was successful.
+ */
+bool NXProperties::deSerialize(const std::string& serializedData) {
+	bool success = true;
+
+	// Open stream
+	std::istrstream inStream(serializedData.c_str());
+	if (!inStream) {
+		// Stream could not be opened.
+		success = false;
+	}
+
+	if (success) {
+		try {
+			// Read properties.
+			std::string line;
+			while (std::getline(inStream, line, '\n')) {
+				deSerializeLine(line);
+			}
+		} catch (...) {
+			// bad input data
+			success = false;
+		}
+	}
+	return success;
+}
+
+
+/* FUNCTION: deSerializeLine */
+void NXProperties::deSerializeLine(const std::string& dataLine) {
+	std::string key, value;
+	std::string line = dataLine;
+
+	// Trim front.
+	while ((line.length() != 0) && (line[0] == ' '))
+		line.erase(0, 1);
+
+	// Ignore commented and blank lines
+	if ((line.length() == 0) || (line[0] == '#'))
+		return;
+
+	// Get key/value pair
+	int index = line.find("=", 1);
+	if (index != std::string::npos) {
+
+		key = line.substr(0, index);
+		// Trim end.
+		while ((key.length() != 0) && (key[key.length() - 1] == ' '))
+			key.erase(key.length() - 1, 1);
+
+		value = line.substr(index + 1, line.length() - 1);
+		//Trim front.
+		while ((value.length() != 0) && (value[0] == ' '))
+			value.erase(0, 1);
+		// Trim end.
+		while ((value.length() != 0) && (value[value.length() - 1] == ' '))
+			value.erase(value.length() - 1, 1);
+
+		// Add to table
+		properties[key] = value;
+	}
+}
+
+} // Nanorex::
