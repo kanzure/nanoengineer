d5434222bf82fa95a8a5279d610924c6019a8bd9
Brian Helfrich <helfrich9000@gmail.com>
2008-01-29 01:14:00 +0000

    Modified for testing the NV1 HDF5_SimResultImportExport plugin.

--- cad/plugins/HDF5_SimResults/src/Testing/Producer.cpp
+++ cad/plugins/HDF5_SimResults/src/Testing/Producer.cpp
@@ -19,16 +19,37 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 	
+	status = simResults->setRunResult(1, "", message);
+
 	status = simResults->addFrameSet("frame-set-1", message);
 	if (status) {
 		printf("Error: %s\n", message.c_str());
 		exit(1);
 	}
 		
-	int frameIndex, atomIndex;
+	int frameIndex = 0;
+	int atomIndex;
 	int nAtoms = 3;
 	float positions[nAtoms*3];
-	while (true) {
+
+	unsigned int atomIds[] = { 0, 1, 2 };
+	status =
+		simResults->setFrameAtomIds("frame-set-1", atomIds, nAtoms, message);
+	if (status) {
+		printf("Error: %s\n", message.c_str());
+		exit(1);
+	}
+
+	unsigned int atomicNumbers[] = { 8, 1, 1 };
+	status =
+		simResults->setFrameAtomicNumbers
+					("frame-set-1", atomicNumbers, nAtoms, message);
+	if (status) {
+		printf("Error: %s\n", message.c_str());
+		exit(1);
+	}
+
+	while (frameIndex < 50/*true*/) {
 		status = simResults->addFrame("frame-set-1", 0.0, frameIndex, message);
 		if (status) {
 			printf("Error: %s\n", message.c_str());
@@ -49,8 +70,9 @@ int main(int argc, char* argv[]) {
 			exit(1);
 		}
 		
-		sleep(2);
+		sleep(1/*2*/);
 	}
-	
+
+	simResults->setRunResult(0, "", message);
 	return 0;
 }
