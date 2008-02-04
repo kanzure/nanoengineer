0647371640b792e4de543a1bb973298334514448
Brian Helfrich <helfrich9000@gmail.com>
2008-02-04 16:26:05 +0000

    Removed extraneous variable.

--- cad/plugins/NanoVision-1/bh.smack.fc6.env.sh
+++ cad/plugins/NanoVision-1/bh.smack.fc6.env.sh
@@ -2,7 +2,6 @@
 
 export OPENBABEL_INCPATH=/usr/local/include/openbabel-2.0
 export OPENBABEL_LIBPATH=/usr/local/lib
-export NANOVISION_BIN=/home/bh/11Nano/SVN-D/cad/plugins/NanoVision-1/bin
 
 kdevelop --profile CandCppIDE src/KDevelop/nv1.kdevelop
 
