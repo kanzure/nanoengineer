2d4d4eba6a33da8ab98f581d634f2e67abda39bd
Brian Helfrich <helfrich9000@gmail.com>
2008-02-07 04:51:42 +0000

    Initial rev.

---
+++ cad/plugins/NanoVision-1/src/Interface/NXRenderingEngine.cpp
@@ -0,0 +1,9 @@
+// Copyright 2008 Nanorex, Inc.  See LICENSE file for details.
+
+#include "Nanorex/Interface/NXRenderingEngine.h"
+
+namespace Nanorex {
+
+	NXRenderingEngine::~NXRenderingEngine() { }
+	
+} // Nanorex
