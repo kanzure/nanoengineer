d6d30d25550b3155945896b355daad71f0af54a6
Manoj Rajagopalan <manoj@nanorex.com>
2008-02-18 22:43:34 +0000

    including icon and changing abs paths to relative

--- cad/plugins/NanoVision-1/src/ResultsWindow.cpp
+++ cad/plugins/NanoVision-1/src/ResultsWindow.cpp
@@ -22,12 +22,12 @@ ResultsWindow::ResultsWindow(NXEntityManager* entityManager, QWidget* parent)
     modelTree->setHeaderLabel(tr("foo.mmp"));
     QList<QTreeWidgetItem*> modelTreeItemList;
     QTreeWidgetItem *homeItem = new QTreeWidgetItem(modelTree);
-    QIcon homeIcon(tr("/home/rmanoj/Nanorex/SVN/trunk/cad/plugins/NanoVision-1/src/Icons/home.png"));
+    QIcon homeIcon(tr(":/Icons/home.png"));
     homeItem->setIcon(0, homeIcon);
     homeItem->setText(0, tr("Home"));
     modelTreeItemList.append(homeItem);
     QTreeWidgetItem *partItem = new QTreeWidgetItem(modelTree);
-    QIcon partIcon(tr("/home/rmanoj/Nanorex/SVN/trunk/cad/plugins/NanoVision-1/src/Icons/Model_Tree.png"));
+    QIcon partIcon(tr(":/Icons/Model_Tree.png"));
     partItem->setIcon(0, partIcon);
     partItem->setText(0, tr("Part"));
     modelTreeItemList.append(partItem);
