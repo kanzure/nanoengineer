691332207b4c8ef5b36f666c0b09899a4a3be3e2
Bruce Smith <bruce@nanorex.com>
2008-02-20 18:11:53 +0000

    split glselect_name_dict out of env.py

--- cad/src/env.py
+++ cad/src/env.py
@@ -1,10 +1,10 @@
-# Copyright 2005-2007 Nanorex, Inc.  See LICENSE file for details. 
+# Copyright 2005-2008 Nanorex, Inc.  See LICENSE file for details. 
 """
 env.py - for global variables and functions treated as "part of the environment".
 
-@author: bruce
+@author: Bruce
 @version: $Id$
-@copyright: 2005-2007 Nanorex, Inc.  See LICENSE file for details. 
+@copyright: 2005-2008 Nanorex, Inc.  See LICENSE file for details. 
 
 This module is for various global or "dynamic" variables,
 which can be considered to be part of the environment of the code
@@ -55,6 +55,8 @@ bruce 050803 new features to help with graphics updates when preferences are cha
 
 bruce 050913 converted most or all remaining uses of win.history to env.history,
 and officially deprecated win.history.
+
+bruce 080219 split glselect_name_dict.py out of env.py so we can make it per-assy.
 """
 
 _mainWindow = None
@@ -277,56 +279,23 @@ def change_counter_for_changed_objects():
     if _change_checkpoint_counter & 1 == 0:
         _change_checkpoint_counter += 1 # make it odd, when recording a change
     return _change_checkpoint_counter
-   
+
 # ==
 
-_last_glselect_name = 0
+# temporary compatibility functions
 
-obj_with_glselect_name = {} # public for lookup
-    ###e this needs to be made weak-valued ASAP! (or, call destroy methods to dealloc) ###@@@
+# [bruce 080219 split out the originals, and will remove these soon,
+#  replacing them with per-assy instances of glselect_name_dict]
 
-def new_glselect_name():
-    """
-    Return a session-unique 32-bit unsigned int for use as a GL_SELECT name.
-    """
-    #e We could recycle these for dead objects (and revise docstring),
-    # but that's a pain, and unneeded (I think), since it's very unlikely
-    # we'll create more than 4 billion objects in one session.
-    global _last_glselect_name
-    _last_glselect_name += 1
-    return _last_glselect_name
+from glselect_name_dict import glselect_name_dict
 
-def alloc_my_glselect_name(obj):
-    """
-    Register obj as the owner of a new GL_SELECT name, and return that name.
+_shared_glselect_name_dict = glselect_name_dict()
 
-    @see: Selobj_API
-    """
-    name = new_glselect_name()
-    obj_with_glselect_name[name] = obj
-    return name
+obj_with_glselect_name = _shared_glselect_name_dict.obj_with_glselect_name
 
-def dealloc_my_glselect_name(obj, name):
-    """
-    #doc
+alloc_my_glselect_name = _shared_glselect_name_dict.alloc_my_glselect_name
 
-    @see: Selobj_API
-    """
-    # objs have to pass the name, since we don't know where they keep it and don't want to have to keep a reverse dict;
-    # but we make sure they own it before zapping it!
-    #e this function could be dispensed with if our dict was weak, but maybe it's useful for other reasons too, who knows
-    if not name:
-        # make repeated destroy methods easier to implement; ok since we never allocate a name of 0
-        return
-    obj1 = obj_with_glselect_name.get(name)
-    if obj1 is obj:
-        del obj_with_glselect_name[name]
-##    elif obj1 is None:
-##        pass # repeated destroy should be legal, and might call this (unlikely, since obj would not know name -- hmm... ok, zap it)
-    else:
-        print "bug: dealloc_my_glselect_name(obj, name) mismatch for name %r: real owner is %r, not" % (name, obj1), obj
-            # print obj separately in case of exceptions in its repr
-    return
+dealloc_my_glselect_name = _shared_glselect_name_dict.dealloc_my_glselect_name
 
 # ==
 
--- cad/src/glselect_name_dict.py
+++ cad/src/glselect_name_dict.py
@@ -11,14 +11,13 @@ History:
 bruce 080219 split this out of env.py so we can make it per-assy.
 """
 
-# UNFINISHED VERSION, just to show the code right after it got split out,
-# before we change it from globals & functions into a class.
-
 _last_glselect_name = 0
 
-def new_glselect_name():
+def _new_glselect_name():
     """
-    Return a session-unique 32-bit unsigned int for use as a GL_SELECT name.
+    Return a session-unique 32-bit unsigned int (not 0) for use as a GL_SELECT name.
+
+    @note: these are unique even between instances of class glselect_name_dict.
     """
     #e We could recycle these for dead objects (and revise docstring),
     # but that's a pain, and unneeded (I think), since it's very unlikely
@@ -27,40 +26,67 @@ def new_glselect_name():
     _last_glselect_name += 1
     return _last_glselect_name
 
-obj_with_glselect_name = {} # public for lookup
-    ###e this needs to be made weak-valued ASAP! (or, call destroy methods to dealloc) ###@@@
-
-
-def alloc_my_glselect_name(obj):
+class glselect_name_dict(object):
     """
-    Register obj as the owner of a new GL_SELECT name, and return that name.
+    Allocate OpenGL GL_SELECT names for use when drawing one model (e.g. an assembly),
+    and record their owners.
 
-    @see: Selobj_API
-    """
-    name = new_glselect_name()
-    obj_with_glselect_name[name] = obj
-    return name
+    @note: The allocated names are session-unique 32-bit unsigned ints (not 0).
+           They are unique even between instances of this class.
 
-def dealloc_my_glselect_name(obj, name):
-    """
-    #doc
+    @note: Callers are free to share one instance of this class over multiple
+           models, or (with suitable changes in current code in GLPane) to use
+           more than one instance for objects drawn together in the same frame.
 
-    @see: Selobj_API
+    @note: The allocated names can be used in OpenGL display lists, provided
+           their owning objects remain alive as long as those display lists
+           remain in use.
     """
-    # objs have to pass the name, since we don't know where they keep it and don't want to have to keep a reverse dict;
-    # but we make sure they own it before zapping it!
-    #e this function could be dispensed with if our dict was weak, but maybe it's useful for other reasons too, who knows
-    if not name:
-        # make repeated destroy methods easier to implement; ok since we never allocate a name of 0
+    def __init__(self):
+        self.obj_with_glselect_name = {} # public for lookup
+            ###e this needs to be made weak-valued ASAP! (or, call destroy methods to dealloc) ###@@@
+    
+    def alloc_my_glselect_name(self, obj):
+        """
+        Allocate and return a new GL_SELECT name, recording obj as its owner.
+        Obj should provide the Selobj_API so it can handle GLPane callbacks during
+        hit-testing.
+        
+        @see: Selobj_API
+        """
+        name = _new_glselect_name()
+        self.obj_with_glselect_name[name] = obj
+        return name
+
+    # Maybe todo: add methods for temporarily removing name from dict (without deallocating it)
+    # and for restoring it, so killed objects needn't have names in the dict.
+    # This would mean we needn't remove the name when destroying a killed object,
+    # thus, needn't find the object at all. But when this class is per-assy, there
+    # won't be a need for that when destroying an entire assy, so it might not be
+    # needed at all.
+    
+    def dealloc_my_glselect_name(self, obj, name):
+        """
+        #doc
+
+        @see: Selobj_API
+        """
+        # objs have to pass the name, since we don't know where they keep it and don't want to have to keep a reverse dict;
+        # but we make sure they own it before zapping it!
+        #e this function could be dispensed with if our dict was weak, but maybe it's useful for other reasons too, who knows
+        if not name:
+            # make repeated destroy methods easier to implement; ok since we never allocate a name of 0
+            return
+        obj1 = self.obj_with_glselect_name.get(name)
+        if obj1 is obj:
+            del self.obj_with_glselect_name[name]
+    ##    elif obj1 is None:
+    ##        pass # repeated destroy should be legal, and might call this (unlikely, since obj would not know name -- hmm... ok, zap it)
+        else:
+            print "bug: dealloc_my_glselect_name(obj, name) mismatch for name %r: real owner is %r, not" % (name, obj1), obj
+                # print obj separately in case of exceptions in its repr
         return
-    obj1 = obj_with_glselect_name.get(name)
-    if obj1 is obj:
-        del obj_with_glselect_name[name]
-##    elif obj1 is None:
-##        pass # repeated destroy should be legal, and might call this (unlikely, since obj would not know name -- hmm... ok, zap it)
-    else:
-        print "bug: dealloc_my_glselect_name(obj, name) mismatch for name %r: real owner is %r, not" % (name, obj1), obj
-            # print obj separately in case of exceptions in its repr
-    return
+
+    pass # end of class
 
 # end
