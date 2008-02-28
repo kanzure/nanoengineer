c9315caa9e8d68e3219e73c87b30e0e2d8b9c9a6
Brian Helfrich <helfrich9000@gmail.com>
2008-02-28 23:24:43 +0000

    Added HDF5 environment variables.

--- cad/plugins/NanoVision-1/bh.smack.fc6.env.sh
+++ cad/plugins/NanoVision-1/bh.smack.fc6.env.sh
@@ -2,6 +2,8 @@
 
 export OPENBABEL_INCPATH=/usr/local/include/openbabel-2.0
 export OPENBABEL_LIBPATH=/usr/local/lib
+export HDF5_SIMRESULTS_LIBPATH=/usr/local/lib
+export HDF5_SIMRESULTS_INCPATH=/usr/local/include
 
 kdevelop --profile CandCppIDE src/KDevelop/nv1.kdevelop
 
