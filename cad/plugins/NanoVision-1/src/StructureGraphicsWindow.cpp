fdb55e81879c84db9560bb7beb00c727da87b40c
Manoj Rajagopalan <manoj@nanorex.com>
2008-02-28 19:57:32 +0000

    Initial commit

---
+++ cad/plugins/NanoVision-1/src/StructureGraphicsWindow.cpp
@@ -0,0 +1,15 @@
+// Copyright 2008 Nanorex, Inc.  See LICENSE file for details.
+
+#include "StructureGraphicsWindow.h"
+
+/* CONSTRUCTOR */
+StructureGraphicsWindow::StructureGraphicsWindow(QWidget *parent)
+: DataWindow(parent), NXOpenGLRenderingEngine(parent)
+{
+}
+
+
+/* DESTRUCTOR */
+StructureGraphicsWindow::~StructureGraphicsWindow()
+{
+}
---
+++ cad/plugins/NanoVision-1/src/StructureGraphicsWindow.h
@@ -0,0 +1,31 @@
+// Copyright 2008 Nanorex, Inc.  See LICENSE file for details.
+
+#ifndef STRUCTUREGRAPHICSWINDOW_H
+#define STRUCTUREGRAPHICSWINDOW_H
+
+// #include <Nanorex/Interface/NXRenderingEngine.h>
+#include "Nanorex/Interface/NXEntityManager.h"
+#include "Nanorex/Interface/NXOpenGLRenderingEngine.h"
+using namespace Nanorex;
+
+#include "DataWindow.h"
+
+/* CLASS: StructureGraphicsWindow */
+class StructureGraphicsWindow
+: public DataWindow,
+  public NXOpenGLRenderingEngine
+{
+    Q_OBJECT;
+    
+public:
+    StructureGraphicsWindow(QWidget *parent=0);
+    ~StructureGraphicsWindow();
+    
+    void setEntityManager(NXEntityManager *theEntityManager)
+    { entityManager = theEntityManager; }
+    
+private:
+    NXEntityManager *entityManager;
+};
+
+#endif // STRUCTUREGRAPHICSWINDOW_H
