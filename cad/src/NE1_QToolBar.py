e9adeaae9982c1d660d4246bf0183548e4843f3f
Mark Sims <mark@nanorex.com>
2008-03-01 06:36:22 +0000

    Separators updated properly when toolbar orientation is changed.

--- cad/src/NE1_QToolBar.py
+++ cad/src/NE1_QToolBar.py
@@ -14,6 +14,8 @@ from PyQt4.Qt import Qt, QToolBar, SIGNAL
 from icon_utilities import getpixmap
 from debug import print_compact_stack
 
+DEBUG = False # Do not commit with DEBUG set to True.
+
 class NE1_QToolBar(QToolBar):
     """
     Main NE1 toolbar class.
@@ -28,23 +30,44 @@ class NE1_QToolBar(QToolBar):
     # _separatorList is a list of all the QLabel widgets in this toolbar.
     # Each QLabel has either a horizontal or vertical separator pixmap.
     _separatorList = []
+    _separatorNumber = 0
     
     def __init__(self, parent):
-        QToolBar.__init__(self, parent)
-        self.connect(self, 
-                     SIGNAL("orientationChanged()"), 
-                     self.updateSeparatorPixmaps)
+        """
+        Constructs an NE1 toolbar for the main window.
+        """
+        QToolBar.__init__(self)
+        self._separatorList = []
+        
+        # ATTENTION - This signal-slot connection is not working and I'm not
+        # sure why. As a workaround, I reimplemented QToolBar.moveEvent()
+        # which does the job nicely. I'm leaving the connect() call here,
+        # but commenting it out. I bet its a Qt bug and it might be fixed 
+        # in Qt 4.3 (or later). Mark 2008-03-01.
+        #self.connect(self, 
+        #             SIGNAL("orientationChanged()"), 
+        #             self.updateSeparatorPixmaps)
 
     def updateSeparatorPixmaps(self):
         """
-        Slot for orientationChanged signal.
+        Updates the pixmap for all separators in this toolbar.
+        
+        @note: This is also supposed to be the slot for the 
+               orientationChanged() signal, but it doesn't work.
         """
-        print "updateSeparatorPixmaps(): HERE!"
         for separator in self._separatorList:
-            setSeparatorWidgetPixmap(separator, self.orientation())
-                # REVIEW: should this line start with self.?
-                # REVIEW: should the arg be self.orientation() like here, or self.orientation like below?
+            if DEBUG:
+                print separator.objectName()
+            self.setSeparatorWidgetPixmap(separator, self.orientation())
             
+    def moveEvent(self, event):
+        """
+        Reimplements moveEvent() of QToolBar. It is used as a workaround for
+        the orientationChanged() signal bug mentioned throughout.
+        """
+        self.updateSeparatorPixmaps()
+        QToolBar.moveEvent(self, event)
+    
     def setSeparatorWidgetPixmap(self, widget, orientation):
         """
         Sets either the horizontal or the vertical pixmap to widget, depending
@@ -56,7 +79,7 @@ class NE1_QToolBar(QToolBar):
         @param orientation: The orientation of this separator.
         @type  orientation: U{B{Qt.Orientation enum}<http://doc.trolltech.com/4.2/qtoolbar.html#orientation-prop>}
         """
-        if orientation() == Qt.Horizontal:
+        if orientation == Qt.Horizontal:
             widget.setPixmap(
                 getpixmap("ui/actions/Toolbars/Standard/horizontal_toolbar_separator"))
         else:
@@ -68,10 +91,15 @@ class NE1_QToolBar(QToolBar):
         Reimplements the addSeparator() method of QToolBar.
         """
         _toolbarSeparator = QtGui.QLabel()
-        self.setSeparatorWidgetPixmap(_toolbarSeparator, self.orientation)
+        if DEBUG:
+            _name = "%s-Separator%d" % (self.objectName(), self._separatorNumber)
+            _toolbarSeparator.setObjectName(_name)
+            self._separatorNumber += 1
+        self.setSeparatorWidgetPixmap(_toolbarSeparator, self.orientation())
         _toolbarSeparator.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         self.addWidget(_toolbarSeparator)
         self._separatorList.append(_toolbarSeparator)
+        
         
     def insertSeparator(self):
         """
@@ -81,7 +109,7 @@ class NE1_QToolBar(QToolBar):
         @note: If you need this method, please code it! Mark 2008-02-29.
         """
         print_compact_stack("insertSeparator() not supported. " \
-            "Use addSeparator instead or implement insertSeparator()")
+            "Use addSeparator() instead or implement insertSeparator()")
         return
     
     
