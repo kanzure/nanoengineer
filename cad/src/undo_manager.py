12dd5437e92b81655a018d9ec56bb42bc9ed4d05
Mark Sims <mark@nanorex.com>
2008-03-01 05:25:18 +0000

    Added comment.

--- cad/src/undo_manager.py
+++ cad/src/undo_manager.py
@@ -701,6 +701,11 @@ def editAutoCheckpointing(win, enabled, update_UI = True, print_to_history = Tru
     if update_UI:
         # Inserting/removing editMakeCheckpointAction from the standardToolBar
         # keeps the toolbar the correct length (i.e. no empty space at the end).
+        # ## WARNING ### - This doesn't check for action already being present
+        # or already being missing, and nothing guarantees this
+        # will not get an exception from removing it when not there
+        # or adding it when already there, though this didn't happen
+        # in initial tests. --Mark 2008-03-01.
         if enabled:
             win.standardToolBar.removeAction(win.editMakeCheckpointAction)
         else:
