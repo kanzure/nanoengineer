1abaeef7d9771e58f221570c8c280dec79ac6af1
Bruce Smith <bruce@nanorex.com>
2008-03-06 17:03:20 +0000

    more

--- cad/src/scratch/content-flags.py
+++ cad/src/scratch/content-flags.py
@@ -19,6 +19,8 @@ if 0:
     # - kinds of atoms (pam, chem, bondpoint, even strand or axis)
     # - various kinds of nodes (eg 3d nodes)
 
+} bug: remove is same as might remove, but add != might add and that's what I did here
+
 class Node:
 
     def get_atom_content(self, flags = -1):
@@ -100,16 +102,45 @@ class NodeWithAtomContents(Node):
         """
         assert 0, "subclass must implement"
 
+    # invalidation
+
+    def invalidate_atom_content(self, flags = -1):
+        """
+        Your kids are changing internally and/or being removed/added
+        more than the caller wants to keep track of, so just make
+        all the given content flags uncertain (and do necessary
+        propogated invalidations, including to the model tree).
+        """
+        # this should be optimized, but the following is correct
+        # and exercises the other methods. (And for repeated invals
+        # with the same flags, those calls will not recurse, and a
+        # simple check here of the need to call them could prevent
+        # them both [###todo].)
+        self.maybe_remove_some_atom_content(flags)
+        self.maybe_add_some_atom_content(flags)
+        return
+    
     # incremental update
     
     def remove_some_atom_content(self, flags): # TODO: call this or an inval method in changed_members of Group; and in delatom etc in Chunk
         """
-        One of your kids is removing some "atom content" of the given type.
+        One of your kids is, or might be (it makes no difference which one
+        in the "remove" case), removing some "atom content" of the given type
+        (due to changes in itself, or being removed as your kid).
         Record and propogate the change, doing mt_update if required.
+
+        @see: add_some_atom_content, maybe_add_some_atom_content
+
+        [unlikely to be overridden in subclasses; if it is,
+         override its alias maybe_remove_some_atom_content too]
         """
+        # possible optimization (only worth the unclarity if this is ever
+        # noticeable on a profile): just iterate up the dad chain, i.e.
+        # inline the recursive calls of this method on self.dad.
+        # much of this would then be doable in Pyrex.
         new = old = self._min_atom_content
         new &= (~flags)
-        print "removed %#x from %#x to get %#x" % (flags, old, new) ###k
+        print "removed %#x from %#x to get %#x" % (flags, old, new) ###k remove when works
         removed = old - new # often 0, so we optimize for that
         if removed:
             self._min_atom_content = new
@@ -121,16 +152,23 @@ class NodeWithAtomContents(Node):
                 dad.remove_some_atom_content(removed)
             ### TODO: mt_update, if we are currently shown in the model tree, in a way this should change
         return
-    
-    def add_some_atom_content(self, flags):
+
+    maybe_remove_some_atom_content = remove_some_atom_content
+
+    def maybe_add_some_atom_content(self, flags):
         """
-        One of your kids is adding some "atom content" of the given type.
+        One of your kids *might be* adding some "atom content" of the given type
+        (due to changes in itself, or being added as your kid).
         Record and propogate the change, doing mt_update if required.
+
+        @see: add_some_atom_content, remove_some_atom_content
+
+        [unlikely to be overridden in subclasses]
         """
-        # note: see implem comment in remove_some_atom_content.
+        # note: see possible optimization comment in remove_some_atom_content
         new = old = self._max_atom_content
         new |= flags
-        print "added %#x to %#x to get %#x" % (flags, old, new) ###k
+        print "added %#x to %#x to get %#x" % (flags, old, new) ###k remove when works
         added = new - old # often 0, so we optimize for that
         if added:
             self._max_atom_content = new
@@ -140,6 +178,40 @@ class NodeWithAtomContents(Node):
                 # directly by self -- it all comes from Atoms
                 # and those are not Nodes.
                 dad.add_some_atom_content(added)
+            # todo: mt_update, if needed
+        return
+
+    def add_some_atom_content(self, flags):
+        """
+        One of your kids *is* adding some "atom content" of the given type
+        (due to changes in itself, or being added as your kid).
+        Record and propogate the change, doing mt_update if required.
+
+        @note: unlike maybe_add_some_atom_content, in this case we can add bits
+               to self._min_atom_content as well as self._max_atom_content.
+
+        @see: maybe_add_some_atom_content, remove_some_atom_content
+
+        [unlikely to be overridden in subclasses]
+        """
+        # note: see possible optimization comment in remove_some_atom_content
+
+        new_max = old_max = self._max_atom_content
+        new_max |= flags
+        print "max: added %#x to %#x to get %#x" % (flags, old_max, new_max) ###k remove when works
+        added_max = new_max - old_max # often 0, so we optimize for that
+        
+        new_min = old_min = self._min_atom_content
+        new_min |= flags
+        print "min: added %#x to %#x to get %#x" % (flags, old_min, new_min) ###k remove when works
+        added_min = new_min - old_min # often 0, so we optimize for that
+
+        if added_max or added_min:
+            self._max_atom_content = new_max
+            self._min_atom_content = new_min
+            dad = self.dad # usually present, optim for that
+            if dad:
+                dad.add_some_atom_content(added_max | added_min)
             # todo: mt_update, if needed
         return
 
