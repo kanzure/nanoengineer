c64623a317bcc91d0f932dbfdcf0395f5e4847e2
Bruce Smith <bruce@nanorex.com>
2008-03-06 06:09:55 +0000

    more

--- cad/src/scratch/content-flags.py
+++ cad/src/scratch/content-flags.py
@@ -5,6 +5,19 @@ temporary scratch file, OWNED BY BRUCE
 $Id$
 """
 
+if 0:
+    pass
+    # define content flags for:
+    # - each display style, and the aspects of them that affect the MT
+    #   (in an array over the style constants) ATOM_CONTENT_FOR_DISPLAY_STYLE
+    #    (not nec. the only source of atom content, but is for now)
+    # - atoms with errors (chunks with errors can be handled separately...
+    #    not sure, they might fit into this scheme; otoh they are not only about "atoms"...
+    #    maybe this whole scheme needs generalization for that reason, at least in classnames)
+    # future:
+    # - kinds of atoms (pam, chem, bondpoint, even strand or axis)
+    # - various kinds of nodes (eg 3d nodes)
+
 class Node:
 
     def get_atom_content(self, flags = -1):
@@ -24,7 +37,7 @@ class Node:
         # default implem, for nodes which can never have atom content
         return 0
 
-    def _updated_atom_content(self):
+    def _f_updated_atom_content(self):
         """
         Recompute, record, and return our atom content,
         optimizing this if it's exactly known on self or on any node-subtrees.
@@ -45,6 +58,8 @@ class NodeWithAtomContents(Node):
     _min_atom_content = 0
     _max_atom_content = -1 # all bits set
 
+    # access
+    
     def get_atom_content(self, flags = -1):
         """
         [overrides Node method]
@@ -52,7 +67,7 @@ class NodeWithAtomContents(Node):
         min_content = self._min_atom_content & flags
         max_content = self._max_atom_content & flags
         if min_content != max_content:
-            min_content = self._updated_atom_content() & flags
+            min_content = self._f_updated_atom_content() & flags
                 # note: we update all of its bits, regardless of flags --
                 # this might be inefficient in some cases by descending
                 # into subnodes we don't care about, but on the whole
@@ -60,15 +75,32 @@ class NodeWithAtomContents(Node):
                 # might be called for several disjoint flags in succession
         return min_content
 
-    def _updated_atom_content(self):
+    # recomputation
+    
+    def _f_updated_atom_content(self):
         """
         Recompute, record, and return our atom content,
         optimizing this if it's exactly known on self or on any node-subtrees.
 
-        [Overrides Node method. All subclasses must override this method.]
+        [Overrides Node method. Subclasses whose kids are not exactly
+         self.members must override or extend this further.]
+        """
+        min_content = self._min_atom_content
+        if min_content == self._max_atom_content:
+            return min_content # assume these attrs are always correct
+        atom_content = self._recompute_atom_content()
+        self._min_atom_content = atom_content
+        self._max_atom_content = atom_content
+        return atom_content
+
+    def _recompute_atom_content(self):
+        """
+        [All subclasses must override this method.]
         """
         assert 0, "subclass must implement"
 
+    # incremental update
+    
     def remove_some_atom_content(self, flags):
         """
         One of your kids is removing some "atom content" of the given type.
@@ -115,37 +147,39 @@ class NodeWithAtomContents(Node):
 
 class Group(NodeWithAtomContents):
     
-    def _updated_atom_content(self):
+    def _recompute_atom_content(self):
         """
-        Recompute, record, and return our atom content,
-        optimizing this if it's exactly known on self or on any node-subtrees.
+        Recompute and return (but do not record) our atom content,
+        optimizing this if it's exactly known on any node-subtrees.
 
-        [Overrides Node method. Subclasses whose kids are not exactly
+        [Overrides superclass method. Subclasses whose kids are not exactly
          self.members must override or extend this further.]
         """
-        min_content = self._min_atom_content
-        if min_content == self._max_atom_content:
-            return min_content # assume these attrs are always correct
         atom_content = 0
         for member in self.members:
-            atom_content |= (member._updated_atom_content())
-        self._min_atom_content = atom_content
-        self._max_atom_content = atom_content
+            atom_content |= (member._f_updated_atom_content())
         return atom_content
 
     pass
 
 class Chunk(NodeWithAtomContents):
 
-    def _updated_atom_content(self):
+    def _recompute_atom_content(self):
         """
-        Recompute, record, and return our atom content,
-        optimizing this if it's exactly known on self or on any node-subtrees.
+        Recompute and return (but do not record) our atom content,
+        optimizing this if it's exactly known on any node-subtrees.
 
-        [Overrides Node method. Subclasses whose atoms are stored differently
+        [Overrides superclass method. Subclasses whose atoms are stored differently
          may need to override this further.]
         """
-        } iterate over atoms
+        atom_content = 0
+        for atom in self.atoms.itervalues():
+            ## atom_content |= (atom._f_updated_atom_content())
+                # IMPLEM that method on class Atom (look up from self.display)?
+                # no, probably best to inline it here instead:
+            atom_content |= ATOM_CONTENT_FOR_DISPLAY_STYLE[atom.display] # or, 2**(atom.display) and then postprocess
+                # but we might optim by skipping bondpoints and/or skipping all diDEFAULT atoms
+        return atom_content
 
     pass
 
