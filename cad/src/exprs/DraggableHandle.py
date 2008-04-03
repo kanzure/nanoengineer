58076b53875b096e63bc4db48f7ff66c70704d9b
Ninad Sathaye <ninad@nanorex.com>
2008-04-03 03:39:23 +0000

    new If_Exprs check in the delegate to decide whether to draw the handle object based on State attr should_draw

--- cad/src/exprs/DraggableHandle.py
+++ cad/src/exprs/DraggableHandle.py
@@ -17,6 +17,7 @@ from exprs.attr_decl_macros import Instance, Option
 from exprs.transforms import Translate
 from exprs.Center import Center
 from exprs.ExprsConstants import StateRef, Vector
+from exprs.attr_decl_macros import State
 from exprs.ExprsConstants import Point, Drawable
 from exprs.ExprsConstants import ORIGIN, DX ##,DY
 from exprs.Highlightable import Highlightable
@@ -27,6 +28,8 @@ from exprs.instance_helpers import DelegatingInstanceOrExpr
 from exprs.DragBehavior_AlongLine import DragBehavior_AlongLine # old todo: clean up
 from exprs.geometry_exprs import Ray
 from exprs.Set import Action
+from exprs.Exprs import call_Expr
+from exprs.If_expr import If_expr
 
 class DraggableHandle_AlongLine(DelegatingInstanceOrExpr): ### TODO: all options might need renaming! replace "height" everywhere.
     """
@@ -64,11 +67,16 @@ class DraggableHandle_AlongLine(DelegatingInstanceOrExpr): ### TODO: all options
     on_release_in = Option(Action, on_release)
     on_release_out = Option(Action, on_release)
     on_doubleclick = Option(Action)
-    
+        
     # line along which to drag it, and how to interpret the state as a position along that line (origin and scale)
     origin = Option( Point, ORIGIN)
     direction = Option( Vector, DX, doc = "vector giving direction and scale")
         # providing a default value is mainly just for testing
+        
+    #If this is false, the 'highlightable' object i.e. this handle 
+    #won't be drawn. The delegate(that defines a Highlightable) 
+    #We define a If_Exprs to check  whether to draw the highlightable object. 
+    should_draw = State(bool, True) 
 
     # == internal instances and formulae (modified from test_statearray_3.py)
     
@@ -80,26 +88,40 @@ class DraggableHandle_AlongLine(DelegatingInstanceOrExpr): ### TODO: all options
         range = range
      ))
     # note: _drag_handler is also being used to compute the translation from the height, even between drags.
+    #@see: DnaStrand_ResizeHandle.hasValidParamsForDrawing
+    #@see: definition of stat attr should_draw
     delegate = \
-        Highlightable(
-            Translate(
-                appearance,
-                _drag_handler._translation #k ok?? only if that thing hangs around even in between drags, i guess!
-                    #e #k not sure if this code-commoning is good, but it's tempting. hmm.
-             ),
-            highlighted = Translate(
-                appearance_highlighted,
-                _drag_handler._translation
-             ),
-            sbar_text = sbar_text,
-            behavior = _drag_handler,
-            on_press = _self.on_press,
-            on_drag = _self.on_drag,
-            # (note: no need to pass on_release)
-            on_release_in = _self.on_release_in,
-            on_release_out = _self.on_release_out,
-            on_doubleclick = _self.on_doubleclick,
-         )
+             If_expr(
+                     _self.should_draw,
+                     Highlightable(
+                         Translate(
+                             appearance,
+                             _drag_handler._translation #k ok?? only if that thing hangs around even in between drags, i guess!
+                                 #e #k not sure if this code-commoning is good, but it's tempting. hmm.
+                          ),
+                         highlighted = Translate(
+                             appearance_highlighted,
+                             _drag_handler._translation
+                          ),
+                         sbar_text = sbar_text,
+                         behavior = _drag_handler,
+                         on_press = _self.on_press,
+                         on_drag = _self.on_drag,
+                         # (note: no need to pass on_release)
+                         on_release_in = _self.on_release_in,
+                         on_release_out = _self.on_release_out,
+                         on_doubleclick = _self.on_doubleclick            
+                      )#end of Highlightable
+         )#end of If_expr
     
+    def hasValidParamsForDrawing(self):
+        """
+        Overridden in subclasses. Default implementation returns True
+        i.e. this object (the highlightable) can be drawn without any known
+        issues
+        @see: DnaStrand_ResizeHandle.hasValidParamsForDrawing for more notes.
+        """
+        self.should_draw = True
+        return self.should_draw
  
     pass # end of class
