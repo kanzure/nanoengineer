fc35773542b6ae51b7a77d67be1660234504019f
Brian Helfrich <helfrich9000@gmail.com>
2008-05-06 15:12:08 +0000

    Version tweaks.

--- packaging/Pref_Mod/buildMac.sh
+++ packaging/Pref_Mod/buildMac.sh
@@ -4,6 +4,6 @@ rm -rf build dist
 python setup_mac.py py2app --frameworks=/usr/local/BerkeleyDB.4.5/lib/libdb-4.5.dylib --packages=bsddb3
 cp py2app-Info.plist dist/pref_modifier.app/Contents/Info.plist 
 cd dist
-tar cf pref_modifier.app.tar pref_modifier.app
-gzip -9 pref_modifier.app.tar
+tar cf pref_modifier.app_0.0.1.tar pref_modifier.app
+gzip -9 pref_modifier.app_0.0.1.tar
 
