63ac520d93e6bba50ef1ccbf5a9b235dd3aea4c4
Ninad Sathaye <ninad@nanorex.com>
2008-05-14 20:18:20 +0000

    bug fixes, new access methods in PM_SelectionListWidget.py

--- cad/src/PM/PM_SelectionListWidget.py
+++ cad/src/PM/PM_SelectionListWidget.py
@@ -53,7 +53,9 @@ from PyQt4.Qt  import QPalette
 from PyQt4.Qt  import QAbstractItemView
 from PyQt4.Qt import Qt
 from PM.PM_Colors import getPalette
-from utilities.constants import yellow
+from widgets.widget_helpers import RGBf_to_QColor
+
+from utilities.constants import yellow, white
 from utilities.icon_utilities import geticon
 
 TAG_INSTRUCTIONS = ['TAG_ITEM_IN_GLPANE', 
@@ -67,20 +69,20 @@ class PM_SelectionListWidget(PM_ListWidget):
     means if you select the items in this list widget, the corresponding
     item in the GLPane will be tagged or picked or both depending on the 
     'tag instructions' 
-    
+
     @param _tagInstruction: Sets the tag instruction. Based on its value, the 
                             items selected in the List widget will be either 
                             tagged or picked or both in the glpane. 
     @type  _tagInstruction: string
-    
+
     @param _itemDictionary: This QListWidgetItem object defines a 'key' of a
                            dictionary and the 'value' of this key is the object
                            specified by the 'item' itself. Example: 
                            self._itemDictionary['C6'] = instance of class Atom.
     @type  _itemDictionary: dictionary                          
     """
-           
-    
+
+
     def __init__(self, 
                  parentWidget, 
                  win,
@@ -94,42 +96,42 @@ class PM_SelectionListWidget(PM_ListWidget):
         means if you select the items in this list widget, the corresponding
         item in the GLPane will be tagged or picked or both depending on the 
         'tag instructions' 
-        
+
         @param parentWidget: The parent group box containing this widget.
         @type  parentWidget: PM_GroupBox
-        
+
         @param win: Mainwindow object
         @type win: MWSemantics
-        
+
         @param label: The label that appears to the left or right of the 
                       checkbox. 
-                      
+
                       If spanWidth is True, the label will be displayed on
                       its own row directly above the list widget.
-                      
+
                       To suppress the label, set I{label} to an 
                       empty string.
         @type  label: str
-        
+
         @param color: color of the ListWidget
         @type : Array
-        
+
         @param heightByRows: The height of the list widget.
         @type  heightByRows: int
-        
+
         @param spanWidth: If True, the widget and its label will span the width
                           of the group box. Its label will appear directly above
                           the widget (unless the label is empty) and is left 
                           justified.
         @type  spanWidth: bool
-        
+
         @see: U{B{QListWidget}<http://doc.trolltech.com/4/qlistwidget.html>}
-        
+
         """ 
-             
+
         self.win = win
         self.glpane = self.win.glpane
-        
+
         #Note: self._tagInstruction and  self._itemDictionary  are instance 
         #variables and not class constants as we 
         #have many PM_SelectionListWidget objects (e.g. in Build Dna mode, we 
@@ -137,11 +139,11 @@ class PM_SelectionListWidget(PM_ListWidget):
         #as a class constant will make class objects share it and create bugs.
         self._tagInstruction = 'TAG_ITEM_IN_GLPANE'
         self._itemDictionary = {}
-        
+
         #The following flag supresses the itemSelectionChanged signal
         #see self.updateSelection for more comments. 
         self._supress_itemSelectionChanged_signal = False
-        
+
         #The following flag supresses the itemChanged signal
         #ItemChanged signal is emitted too frequently. We use this to know that
         #the data of an item has changed...example : to know that the renaming
@@ -156,15 +158,15 @@ class PM_SelectionListWidget(PM_ListWidget):
         #@see: self.renameItemValue(), 
         #@self.editItem() (This is a QListWidget method)
         self._supress_itemChanged_signal = False
-        
+
         PM_ListWidget.__init__(self, 
                                parentWidget, 
                                label = '',
                                heightByRows = heightByRows,
                                spanWidth = spanWidth)
-        
+
         self.setSelectionMode(QAbstractItemView.ExtendedSelection)
-                
+
         #Assigning color to the widget -- to be revised. (The color should 
         #change only when the focus is inside this widget -- the color change
         #will also suggest object(s) selected in glpane will be added as 
@@ -173,9 +175,28 @@ class PM_SelectionListWidget(PM_ListWidget):
         if color:
             self.setAutoFillBackground(True)
             self.setPalette(getPalette( None, 
-                                       QPalette.Base,
-                                       color))
-    
+                                        QPalette.Base,
+                                        color))
+
+
+    def deleteSelection(self):
+        """
+        Remove the selected items from the list widget (and 
+        self._itemDictionary)
+        """
+        for key in self.selectedItems():   
+            assert self._itemDictionary.has_key(key)
+            del self._itemDictionary[key]
+
+    def getItemDictonary(self):
+        """
+        Returns the dictonary of self's items. 
+        @see: MultipleSegments_PropertyManager.listWidget_keyPressEvent_delegate
+        for details.
+        """
+        return self._itemDictionary
+
+
     def connect_or_disconnect_signals(self, isConnect):
         """
         Connect or disconnect widget signals sent to their slot methods.
@@ -188,7 +209,7 @@ class PM_SelectionListWidget(PM_ListWidget):
             change_connect = self.win.connect
         else:
             change_connect = self.win.disconnect                
-        
+
         change_connect(self,
                        SIGNAL('itemSelectionChanged()'), 
                        self.tagItems)
@@ -198,13 +219,13 @@ class PM_SelectionListWidget(PM_ListWidget):
         change_connect(self, 
                        SIGNAL('itemChanged ( QListWidgetItem *)'),
                        self.renameItemValue)
-        
+
         #Not USED -- editing widgets items is not supported
         #change_connect(self, 
-                       #SIGNAL('itemDoubleClicked(QListWidgetItem *)'), 
-                       #self.editItem)   
-        
-    
+                        #SIGNAL('itemDoubleClicked(QListWidgetItem *)'), 
+                        #self.editItem)   
+
+
     def editItem(self, item):
         """
         Edit the widget item.
@@ -215,31 +236,32 @@ class PM_SelectionListWidget(PM_ListWidget):
         #explicitely set the flag to False for safety. 
         self._supress_itemChanged_signal = False
         PM_ListWidget.editItem(self, item)
-            
-    
+
+
     def renameItemValue(self, item):
         """
         slot method that gets called when itemChanged signal is emitted. 
-        
+
         Example: 1. User double clicks an item in the strand list widget of the 
         BuildDna mode 2. Edits the name. 3.Hits Enter key or clicks outside the 
         selection to end rename operation. 
         During step1, itemDoubleClicked signal is emitted which calls self.editItem
         and at the end of step3, it emits itemChanged signal which calls this 
         method
-        
+
         @self.editItem() (This is a QListWidget method)        
-        
+
         """
+
         #See a detailed note in self.__init__  where the following flag is 
         #declared. The flag is set to True when 
         if self._supress_itemChanged_signal:
             return
-        
-      
+
+
         if self._itemDictionary.has_key(item):
             val = self._itemDictionary[item]
-            
+
             #Check if the 'val' (obj which this widgetitem represents) has an 
             #attr name. 
             if not hasattr(val, 'name'):
@@ -247,7 +269,7 @@ class PM_SelectionListWidget(PM_ListWidget):
                     #don't permit empty names -- doesn't make sense. 
                     item.setText('name')
                 return
-            
+
             #Do the actual renaming of the 'val' 
             if item.text():
                 val.name = item.text()
@@ -255,72 +277,93 @@ class PM_SelectionListWidget(PM_ListWidget):
             else:
                 #Don't allow assignment of a blank name
                 item.setText(val.name)
-            
-  
+
     def insertItems(self, row, items, setAsDefault = True):
         """
         Insert the <items> specified items in this list widget. 
         The list widget shows item name string , as a QListwidgetItem. 
-        
+
         This QListWidgetItem object defines a 'key' of a dictionary 
         (self._itemDictionary) and the 'value' of this key is the object 
         specified by the 'item' itself.         
         Example: self._itemDictionary['C6'] = instance of class Atom. 
-        
+
         @param row: The row number for the item. 
         @type row: int
-        
+
         @param items: A list of objects. These objects are treated as values in 
                       the self._itemDictionary
         @type items: list 
-        
+
         @param setAsDefault: Not used here. See PM_ListWidget.insertItems where 
                              it is used.
-                             
+
         @see: self.renameItemValue() for a comment about 
               self._supress_itemChanged_signal
-        
+
         """       
-        
+
         #delete unused argument. Should this be provided as an argument in this
         #class method ?  
         del setAsDefault
-        
+
         #self.__init__ for a comment about this flag
         self._supress_itemChanged_signal = True
-                
+
         #Clear the previous contents of the self._itemDictionary 
         self._itemDictionary.clear()
-        
+
         #Clear the contents of this list widget, using QListWidget.clear()
         #See U{<http://doc.trolltech.com/4.2/qlistwidget.html>} for details
         self.clear()
-        
+
         for item in items:
             if hasattr(item.__class__, 'name'):
                 itemName = item.name
             else:
                 itemName = str(item)
             listWidgetItem = QListWidgetItem(itemName, self)
-            
+
             #When we support editing list widget items , uncomment out the 
             #following line . See also self.editItems -- Ninad 2008-01-16
             listWidgetItem.setFlags( listWidgetItem.flags()| Qt.ItemIsEditable)
-            
+
             if hasattr(item.__class__, 'iconPath'): 
                 listWidgetItem.setIcon(geticon(item.iconPath))
-                
+
             self._itemDictionary[listWidgetItem] = item  
-            
+
         #Reset the flag that temporarily supresses itemChange signal.   
         self._supress_itemChanged_signal = False
-                
+
+    def setColor(self, color):    
+        """
+        Set the color of the widget to the one given by param color
+        @param color: new palette color of self.
+        """
+        self.setAutoFillBackground(True)
+        color = RGBf_to_QColor(color)
+        self.setPalette(getPalette( None, 
+                                    QPalette.Base,
+                                    color))
+
+
+    def resetColor(self):
+        """
+        Reset the paletter color of the widget (and set it to white)
+        """
+        self.setAutoFillBackground(True)
+        color = RGBf_to_QColor(white)
+        self.setPalette(getPalette( None, 
+                                    QPalette.Base,
+                                    color))
+
     def clearTags(self):
         """
         Clear the previously drawn tags if any.
         """
         self.glpane.graphicsMode.drawTags(tagPositions = ())
-    
+
     def setTagInstruction(self, tagInstruction = 'TAG_ITEM_IN_GLPANE'):
         """
         Sets the client specified tag instruction. 
@@ -330,10 +373,10 @@ class PM_SelectionListWidget(PM_ListWidget):
         is decided using the tag instruction.
         """
         assert tagInstruction in TAG_INSTRUCTIONS
-            
+
         self._tagInstruction = tagInstruction
-        
-        
+
+
     def tagItems(self):
         """
         For the selected items in the list widget, tag and/or select the 
@@ -344,10 +387,10 @@ class PM_SelectionListWidget(PM_ListWidget):
             return
 
         graphicsMode = self.glpane.graphicsMode
-        
+
         #Clear the previous tags if any
         self.clearTags()        
-        
+
         if self._tagInstruction != 'TAG_ITEM_IN_GLPANE':
             #Unpick all list widget items in the 3D workspace
             #if no modifier key is pressed. Earlier implementation 
@@ -358,17 +401,17 @@ class PM_SelectionListWidget(PM_ListWidget):
             #self.glpan.modkeys not changed when focus is inside the 
             #Property manager? Example: User holds down Shift key and starts 
             #selecting things inside  -- Ninad 2008-03-18
-            
+
             if self.glpane.modkeys is None:
                 self.win.assy.unpickall_in_GLPane()
-                
+
             #Deprecated call of _unpick_all_listWidgetItems_in_glpane 
             #(deprecated on 2008-03-18
             ##self._unpick_all_listWidgetItems_in_glpane()  
-            
+
             #Now pick the items selected in this list widget 
             self._pick_selected_listWidgetItems_in_glpane()
-            
+
         #Note: 2008-03-18 Following tag instruction is deprecated sometime ago 
         #LKeeping this code intact for now. 
         if self._tagInstruction != 'PICK_ITEM_IN_GLPANE':
@@ -377,37 +420,33 @@ class PM_SelectionListWidget(PM_ListWidget):
             #see: U{<http://doc.trolltech.com/4.2/qlistwidget.html>} for details
             for key in self.selectedItems():   
                 assert self._itemDictionary.has_key(key)
-                item = self._itemDictionary[key]                            
-                if hasattr(item.__class__, 'posn'):
+                item = self._itemDictionary[key]     
+                if isinstance(item, self.win.assy.DnaSegment):
+                    end1, end2 = item.getAxisEndPoints()
+                    for endPoint in (end1, end2):
+                        if endPoint is not None:
+                            tagPositions.append(endPoint)
+                elif hasattr(item.__class__, 'posn'):
                     tagPositions.append(item.posn())
-                elif hasattr(item, 'center'):
-                    chunkAtomList = item.atoms.values() 
-                    #@TODO: The following is only a kludge, just to get things
-                    # working while editing a the DnaDuplex. 
-                    # Once Dna object model is implemented, this will be 
-                    # completely revised. -- Ninad 2007-11-12
-                    for atm in chunkAtomList:
-                        if atm.element.name in ['PAM3-Axis-End', 'Singlet']:
-                            tagPositions.append(atm.posn())
-                            break                        
+
             if tagPositions: 
                 graphicsMode.drawTags(tagPositions = tagPositions, 
                                       tagColor = yellow)            
-            
+
         self.glpane.gl_update()
-            
+
     def _unpick_all_listWidgetItems_in_glpane(self):
         """
         Deselect (unpick) all the items (object) in the GLPane that 
         correspond to the items in this list widget.
-        
+
         Deprecated as of 2008-03-18. See a comment in self.tagItems
         """        
-        
+
         for item in self._itemDictionary.values():
             if item.picked:
                 item.unpick()
-                
+
     def _pick_selected_listWidgetItems_in_glpane(self):
         """
         If some items in the list widgets are selected (in the widget) 
@@ -418,16 +457,16 @@ class PM_SelectionListWidget(PM_ListWidget):
             item = self._itemDictionary[key]
             if not item.picked:
                 item.pick() 
-    
+
     def updateSelection(self, selectedItemList):
         """
         Update the selected items in this selection list widget. The items 
         given by the parameter selectedItemList will get selected. 
-        
+
         This suppresses the 'itemSelectionChanged signal because the items 
         are already selected in the 3D workspace and we just want to select
         the corresponding items (QWidgetListItems) in this list widget.
-        
+
         @param selectedItemList:  List of items provided by the client 
                                  that need to be selected in this list widget
         @type  selectedItemList: list
@@ -439,43 +478,43 @@ class PM_SelectionListWidget(PM_ListWidget):
         #selection state of the corresponding items in the list widget must be
         #updated. 
         self._supress_itemSelectionChanged_signal = True
-        
+
         for key, value in self._itemDictionary.iteritems():
-            
+
             if value in selectedItemList:
                 if not key.isSelected():
                     key.setSelected(True)     
             else:
                 if key.isSelected():
                     key.setSelected(False) 
-                    
+
         self._supress_itemSelectionChanged_signal = False
-                
-    
+
+
     def clear(self):
         """
         Clear everything inside this list widget including the 
         contents of self._itemDictionary and tags if any. 
-        
+
         Overrides QListWidget.clear()
         """
         self.clearTags()
-        
+
         #Clear the previous contents of the self._itemDictionary 
         self._itemDictionary.clear()
-        
+
         #Clear the contents of this list widget, using QListWidget.clear()
         #See U{<http://doc.trolltech.com/4.2/qlistwidget.html>} for details
         PM_ListWidget.clear(self)
-        
-            
+
+
     def getPickedItem(self):
         """
         Return the 'real' item picked (selected) inside this selection list 
         widget. The 'real' item is the object whose name appears inside the 
         selection list widget. (it does not return the 'QWidgetItem' but the 
         key.value() that actually stores the NE1 object)
-        
+
         NOTE: If there are more than one items selected, it returns only the 
               FIRST ITEM in the list. This class is designed to select only 
               a single item at a time , but in case this implementation changes,
@@ -485,18 +524,18 @@ class PM_SelectionListWidget(PM_ListWidget):
         #Using self.selectedItems() doesn't work for some reason! (when you 
         # select item , go to the sequence editor and hit assign button, 
         # it spits an error 
-        
+
         pickedItem = None
         selectedItemList = self.selectedItems()
         key = selectedItemList[0]
         pickedItem = self._itemDictionary[key]
-        
+
         return pickedItem
-        
-        
+
+
         #for item in self._itemDictionary.values():
             #if item.picked:
                 #return item 
-            
+
         #return None
-    
+
