0e8c21c1d446ef6ae61fec678162f6c53406dccd
Kyle Alsup <kyle@nanorex.com>
2008-06-04 20:00:51 +0000

    Finished any formatting changes to What't This text for preferences. Created What's This explanation for color Category and some in Model View.

--- cad/src/ne1_ui/prefs/WhatsThisText_for_PreferencesDialog.py
+++ cad/src/ne1_ui/prefs/WhatsThisText_for_PreferencesDialog.py
@@ -26,7 +26,7 @@ def whatsThis_PreferencesDialog(preferencesDialog):
         "<b>Bond line thickness</b>"\
         "<p>"\
         "Sets the line thickness to <i>n</i> pixels wheneven bonds "\
-        "are rendered as lines (i.e. when the Global Display Style is set to "\
+        "are rendered as lines (i.e. when the global display style is set to "\
         "<b>Lines</b> display style)."\
         "</p>"
 
@@ -35,25 +35,29 @@ def whatsThis_PreferencesDialog(preferencesDialog):
     # and What's This text for all the others widgets (yuk!)
     
     _pd.display_origin_axis_checkbox.setWhatsThis(
-        """<p><b>Display Origin Axis</b></p>
+        """<p><b>Display origin axis</b></p>
         <p>
-        Shows/Hides the Origin Axis""")
+        Shows/Hides the origin axis""")
     _pd.display_pov_axis_checkbox.setWhatsThis(
-        """<p><b>Display Point of View Axis</b></p>
+        """<p><b>Display point of view axis</b></p>
         <p>
-        Shows/Hides the Point of View Axis""")
+        Shows/Hides the point of view axis""")
     _text = \
-        """<p><b>Display Compass</b></p>
+        """<p><b>Display compass</b></p>
         <p>
-        Shows/Hides the Display Compass"""
+        Shows/Hides the display compass"""
     _pd.compassGroupBox.setWhatsThis(_text)
-    _pd.display_compass_labels_checkbox.setWhatsThis(_text)
+    _pd.display_compass_labels_checkbox.setWhatsThis(
+        """Shows/Hides the display compass axis labels.""")
     _pd.update_btngrp.setWhatsThis(
         """<p><b>Watch motion in real time</b></p>
         <p>
         Enables/disables realtime graphical updates during adjust operations
         when using <b>Adjust All</b> or <b>Adjust Selection</b>""")
-
+    _text = \
+        """Changes the location of the display compass."""
+    _pd.textLabel1_4.setWhatsThis(_text)
+    _pd.compass_position_combox.setWhatsThis(_text)
     _text = \
         """<b>Update every <i>n units.</u></b>
         <p>
@@ -106,7 +110,7 @@ def whatsThis_PreferencesDialog(preferencesDialog):
     _pd.cutoverrms_lbl.setWhatsThis(_text)
     # Sponsor Logos Download Permission
     _pd.sponsorLogosGroupBox.setWhatsThis(
-        """<b>Sponsor Logos Download Permission</b>
+        """<b>Sponsor logos download permission</b>
         <p>
         This group of buttons sets the permission for downloading sponsor logos.
         </p>""")
@@ -128,7 +132,7 @@ def whatsThis_PreferencesDialog(preferencesDialog):
         </p>""")
 
     _pd.animate_views_checkbox.setWhatsThis(
-        """<p><b>Animate Between Views</b></p>
+        """<p><b>Animate between views</b></p>
         <p>
         Enables/disables animation when switching between the current view
         and a new view.
@@ -137,7 +141,7 @@ def whatsThis_PreferencesDialog(preferencesDialog):
         """<p><b>View animation speed</b></p>
         <p>
         Sets the animation speed when animating between views 
-        (i.e. Front View to Right View).  It is recommended that this be set 
+        (i.e. Front view to Right view).  It is recommended that this be set 
         to Fast when working on large models.
         </p>"""
     _pd.textLabel1_5.setWhatsThis(_text)
@@ -168,36 +172,40 @@ def whatsThis_PreferencesDialog(preferencesDialog):
     _pd.level_of_detail_combox.setWhatsThis(_text)
     
     _pd.textLabel1_3_2.setWhatsThis(
-        """<p><b>Ball and Stick Atom Scale</b></p>
+        """<p><b>Ball and stick atom scale</b></p>
         <p>
-        Sets the Ball and Stick Atom Scale factor. It is best to
+        Sets the ball and stick atom scale factor. It is best to
         change the scale factor while the current model is displayed
-        in Ball and Stick mode.""")
+        in ball and stick mode.""")
     _pd.ballStickAtomScaleFactorSpinBox.setWhatsThis(
-        """<p><b>Ball and Stick Atom Scale</b></p>
+        """<p><b>Ball and stick atom scale</b></p>
         <p>
-        Sets the Atom Scale factor for Ball and Stick display style. It is best
-        to change the scale factor while the Global Display Style is
-        set to Ball and Stick.""")
+        Sets the atom scale factor for ball and stick display style. It is best
+        to change the scale factor while the global display style is
+        set to ball and stick.""")
     _pd.textLabel1_3_2_2.setWhatsThis(
-        """<p><b>CPK Atom Scale</b></p>
+        """<p><b>CPK atom scale</b></p>
         <p>
-        Changes the CPK Atom Scale factor. It is best to change the scale
+        Changes the CPK atom scale factor. It is best to change the scale
         factor while in CPK display mode so you can see the graphical effect of
         changing the scale.""")
     _pd.cpkAtomScaleFactorDoubleSpinBox.setWhatsThis(
-        """<p><b>CPK Atom Scale</b></p>
+        """<p><b>CPK atom scale</b></p>
         <p>
         Set the atom scale factor for CPK display style. It is best to change
-        the scale factor while the Global Display style is set to CPK so you
+        the scale factor while the global display style is set to CPK so you
         can see the graphical effect of changing the scale.""")
     _pd.reset_cpk_scale_factor_btn.setWhatsThis(
-        """Restore the default value of the CPK Scale Factor""")
+        """Restore the default value of the CPK scale factor""")
+    _pd.reset_ballstick_scale_factor_btn.setWhatsThis(
+        """Restore the default value of the ball and stick scale factor.""")
+    _pd.haloWidthResetButton.setWhatsThis(
+        """Restore the default value of halo width.""")
     _pd.multCyl_radioButton.setWhatsThis(
-        """<p><b>Multiple Cylinders</b></p>
+        """<p><b>Multiple cylinders</b></p>
         <p>
-        <p><b>High Order Bonds</b>
-        are displayed using <b>Multiple Cylinders.</b></p>
+        <p><b>High order bonds</b>
+        are displayed using <b>multiple cylinders.</b></p>
         <p>
         <b>Double bonds</b> are drawn with two cylinders.<br>
         <b>Triple bonds</b> are drawn with three cylinders.<br>
@@ -206,7 +214,7 @@ def whatsThis_PreferencesDialog(preferencesDialog):
     _pd.vanes_radioButton.setWhatsThis(
         """<p><b>Vanes</b></p>
         <p>
-        <p><i>High Order Bonds</i> are displayed using <b>Vanes.</b></p>
+        <p><i>High order bonds</i> are displayed using <b>Vanes.</b></p>
         <p>
         <p>Vanes represent <i>pi systems</i> in high order bonds and are
         rendered as rectangular polygons. The orientation of the vanes
@@ -217,7 +225,7 @@ def whatsThis_PreferencesDialog(preferencesDialog):
     _pd.ribbons_radioButton.setWhatsThis(
         """<p><b>Ribbons</b></p>
         <p>
-        <p><i>High Order Bonds</i> are displayed using <b>Ribbons.</b></p>
+        <p><i>High order bonds</i> are displayed using <b>Ribbons.</b></p>
         <p>
         <p>Ribbons represent <i>pi systems</i> in high order bonds and are
         rendered as ribbons. The orientation of the ribbons approximates the
@@ -226,10 +234,10 @@ def whatsThis_PreferencesDialog(preferencesDialog):
         to see how ribbons are rendered.
         </p>""")
     _pd.show_bond_labels_checkbox.setWhatsThis(
-        """<p><b>Show Bond Type Letters</b></p>
+        """<p><b>Show bond type letters</b></p>
         <p>
-        <p>Shows/Hides Bond Type letters (labels) on top of bonds.</p>
-        <u>Bond Type Letters:</u><br>
+        <p>Shows/Hides bond type letters (labels) on top of bonds.</p>
+        <u>Bond type letters:</u><br>
         <b>2</b> = Double bond<br>
         <b>3</b> = Triple bond<br>
         <b>A</b> = Aromatic bond<br>
@@ -237,7 +245,7 @@ def whatsThis_PreferencesDialog(preferencesDialog):
     _pd.show_valence_errors_checkbox.setWhatsThis(
         """<p><b>Show valence errors</b></p>
         <p>
-        Enables/Disables Valence Error Checker.</p>
+        Enables/Disables valence error checker.</p>
         When enabled, atoms with valence errors are displayed with a pink
         wireframe sphere. This indicates that one or more of the atom's bonds
         are not of the correct order (type), or that the atom has the wrong
@@ -245,26 +253,26 @@ def whatsThis_PreferencesDialog(preferencesDialog):
         in bond directions or in which PAM elements are bonded. The error
         details can be seen in the tooltip for the atom.""")
     _text = \
-        """<p><b>Ball and Stick Bond Scale</b></p>
+        """<p><b>Ball and stick bond scale</b></p>
         <p>
         Set scale (size) factor for the cylinder representing bonds
-        in Ball and Stick display mode"""
+        in ball and stick display mode"""
     _pd.textLabel1_3.setWhatsThis(_text)
     _pd.cpk_cylinder_rad_spinbox.setWhatsThis(_text)
     
     _pd.autobond_checkbox.setWhatsThis(
-        """Build mode's default setting for Autobonding
+        """Build mode's default setting for autobonding
         at startup (enabled/disabled)""")
     _pd.water_checkbox.setWhatsThis(
-        """Build mode's default setting for Water at
+        """Build mode's default setting for water at
         startup (enabled/disabled)""")
     _pd.buildmode_select_atoms_checkbox.setWhatsThis(
-        """<p><b>Select Atoms of Deposited Object</b></p>
+        """<p><b>Auto select atoms of deposited object</b></p>
         <p>
         When depositing atoms, clipboard chunks or library parts,
         their atoms will automatically be selected.""")
     _pd.buildmode_highlighting_checkbox.setWhatsThis(
-        """Build mode's default setting for Highlighting at
+        """Build mode's default setting for highlighting at
         startup (enabled/disabled)""")
     _pd.gromacs_label.setWhatsThis(
         """Enable GROMACS and choose the mdrun executable path to use.""")
@@ -365,23 +373,23 @@ def whatsThis_PreferencesDialog(preferencesDialog):
         """Select a user-customized directory for POV-Ray and MegaPOV include
         files, such as transforms.inc.""")
     _pd.undo_automatic_checkpoints_checkbox.setWhatsThis(
-        """<p><b>Automatic Checkpoints</b></p>
+        """<p><b>Automatic checkpoints</b></p>
         <p>
-        Specifies whether <b>Automatic Checkpointing</b> is enabled/disabled
+        Specifies whether <b>automatic checkpointing</b> is enabled/disabled
         during program startup only. It does not enable/disable
-        <b>Automatic Checkpointing</b> when the program is running.
-        <p><b>Automatic Checkpointing</b> can be enabled/disabled by the user
-        at any time from <b>Edit > Automatic Checkpointing</b>. When enabled,
-        the program maintains the Undo stack automatically.  When disabled,
-        the user is required to manually set Undo checkpoints using the
-        <b>Set Checkpoint</b> button in the Edit Toolbar/Menu.</p>
-        <p><b>Automatic Checkpointing</b> can impact program performance. By
-        disabling Automatic Checkpointing, the program will run faster.</p>
-        <p><b><i>Remember to you must set your own Undo checkpoints manually
-        when Automatic Checkpointing is disabled.</i></b></p>
+        <b>automatic checkpointing</b> when the program is running.
+        <p><b>Automatic checkpointing</b> can be enabled/disabled by the user
+        at any time from <b>Edit > Automatic checkpointing</b>. When enabled,
+        the program maintains the undo stack automatically.  When disabled,
+        the user is required to manually set undo checkpoints using the
+        <b>set checkpoint</b> button in the Edit Toolbar/Menu.</p>
+        <p><b>Automatic checkpointing</b> can impact program performance. By
+        disabling automatic checkpointing, the program will run faster.</p>
+        <p><b><i>Remember to you must set your own undo checkpoints manually
+        when automatic checkpointing is disabled.</i></b></p>
         <p>""")
     _pd.undo_restore_view_checkbox.setWhatsThis(
-        """<p><b>Restore View when Undoing Structural Changes</b></p>
+        """<p><b>Restore view when undoing structural changes</b></p>
         <p>
         <p>When checked, the current view is stored along with each
         <b><i>structural change</i></b> on the undo stack.  The view is then
@@ -394,14 +402,42 @@ def whatsThis_PreferencesDialog(preferencesDialog):
         (i.e. are not structural changes).
         </p>""")
     _pd.groupBox3.setWhatsThis(
-        """Format Prefix and Suffix text the delimits the part name in the
+        """Format prefix and suffix text the delimits the part name in the
         caption in window border.""")
     _text = \
         """Saves the main window's current position and size for the next
         time the program starts."""
-    _pd.save_current_btn.setWhatsThis(_text)
+    _pd.save_current_btn.setWhatsThis(_text)     
+    _pd.restore_saved_size_btn.setWhatsThis(
+        """Restores the main window's current position from the last time
+        the program was closed""")
     _text = \
-        """Saves the main window's current position and size for the next
-        time the program starts."""      
-    _pd.restore_saved_size_btn.setWhatsThis(_text)
+        """Sets background color from preset list or choose to
+        customize your own background color"""
+    _pd.label_8.setWhatsThis(_text)
+    _pd.backgroundColorComboBox.setWhatsThis(_text)
+    _pd.hoverHighlightingStyleGroupBox.setWhatsThis(
+        """<p><b>Hover highlighting style</b></p>
+        <p>
+        <p>Creates a highlight surrounding an object that the cursor is
+        placed over. The highlight shows what would be selected if the mouse
+        was clicked. Has options to view the highlighting in many ways as
+        well as choose the color of the highlight.
+        </p>""")
+    _pd.selectionColorStyleGroupBox.setWhatsThis(
+        """<p><b>Selection style</b></p>
+        <p>
+        <p>When an object or objects have been selected they become
+        highlighted. The highlighting has many textures and colors that
+        may be changed.
+        </p>""")
+    _text = \
+        """Sets the width of the colored halo if colored halo is chosen
+        as a highlighting style or selection style."""
+    _pd.label_25.setWhatsThis(_text)
+    _pd.haloWidthSpinBox.setWhatsThis(_text)
+    _text = \
+        """Changes the appearance of any objects in the work place."""
+    _pd.label_9.setWhatsThis(_text)
+    _pd.globalDisplayStyleStartupComboBox.setWhatsThis(_text)
     return
