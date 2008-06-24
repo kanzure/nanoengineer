5795a843146e24c5de6a016aab0289a989d81c75
Ninad Sathaye <ninad@nanorex.com>
2008-06-24 18:40:28 +0000

    forgot to commit this file in rev 13229

--- cad/src/PM/PM_RadioButtonList.py
+++ cad/src/PM/PM_RadioButtonList.py
@@ -15,6 +15,7 @@ from PyQt4.Qt import QButtonGroup
 from PyQt4.Qt import QRadioButton
 
 from PM.PM_GroupBox import PM_GroupBox
+from PyQt4.Qt import QLabel
 
 class PM_RadioButtonList( PM_GroupBox ):
     """
@@ -30,9 +31,13 @@ class PM_RadioButtonList( PM_GroupBox ):
     def __init__(self, 
                  parentWidget, 
                  title        = '', 
+                 label = '',
+                 labelColumn = 0,
                  buttonList   = [],
-                 checkedId    = -1, 
-                 setAsDefault = False 
+                 checkedId    = -1,
+                 setAsDefault = False, 
+                 spanWidth   = True, 
+                 borders = True
                  ):
         """
         Appends a PM_RadioButtonList widget to the bottom of I{parentWidget}, 
@@ -44,6 +49,15 @@ class PM_RadioButtonList( PM_GroupBox ):
         @param title: The group box title.
         @type  title: str
         
+        @param label:      The label for the coordinate spinbox.
+        @type  label:      str
+        
+        @param labelColumn: The column in the parentWidget's grid layout to 
+                            which this widget's label will be added. 
+                            The labelColum can only be E{0} or E{1}
+        @type  labelColumn: int
+                
+        
         @param buttonList: A list of I{button info lists}. There is one button
                            info list for each radio button in the list. The 
                            button info list contains the following three items:
@@ -51,7 +65,39 @@ class PM_RadioButtonList( PM_GroupBox ):
                            2). Button text (str),
                            3). Button tool tip (str).
         @type  buttonList: list
+        
+        @param spanWidth: If True, the widget and its label will span the width
+                         of the group box. Its label will appear directly above
+                         the widget (unless the label is empty) and is left 
+                         justified.
+        @type  spanWidth: bool (default False)
+        
+        
+        @param borders: If true (default), this widget will have borders displayed. 
+                        otherwise the won't be any outside borders around the 
+                        set of radio buttons this class provides
+        @type borders: boolean
         """
+        
+        # Intializing label, labelColumn etc is needed before doing 
+        # PM_GroupBox.__init__. This is done so that 
+        # self.parentWidget.addPmWidget(self) done at the end of __init__
+        # works properly. 
+        # 'self.parentWidget.addPmWidget(self)' is done to avoid a bug where a 
+        # groupbox is always appended as the 'last widget' when its 
+        # parentWidget is also a groupbox. This is due to other PM widgets 
+        #(e.g. PM_PushButton)add themselves to their parent widget in their 
+        #__init__ using self.parentWidget.addPmWidget(self). So doing the
+        #same thing here. More general fix is needed in PM_GroupBox code
+        # --Ninad 2007-11-14 (comment copied from PM_coordinateSpinBoxes)
+        self.label = label
+        self.labelColumn = labelColumn
+        self.spanWidth = spanWidth
+        
+        if label: # Create this widget's QLabel.
+            self.labelWidget = QLabel()
+            self.labelWidget.setText(label)
+            
         
         PM_GroupBox.__init__(self, parentWidget, title)
         
@@ -91,9 +137,18 @@ class PM_RadioButtonList( PM_GroupBox ):
             self.buttonsById[buttonId]    = button
             self.buttonsByText[buttonText] = button
             
-        self._widgetList.append(self)
+        if isinstance(self.parentWidget, PM_GroupBox):
+            self.parentWidget.addPmWidget(self)
+        else:   
+            #@@ Should self be added to self.parentWidget's widgetList?
+            #don't know. Retaining old code -- Ninad 2008-06-23
+            self._widgetList.append(self)            
+            self._rowCount += 1
         
-        self._rowCount += 1
+        if not borders:
+            #reset the style sheet so that there are no borders around the 
+            #radio button set this class provides. 
+            self.setStyleSheet(self._getAlternateStyleSheet())
         
     def restoreDefault(self):
         """
@@ -148,5 +203,25 @@ class PM_RadioButtonList( PM_GroupBox ):
             return self.buttonsById[buttonId]
         else:
             return None
+              
         
+    def _getAlternateStyleSheet(self):
+        """
+        Return the style sheet for the groupbox. This sets the following 
+        properties only:
+         - border style
+         - border width
+         - border color
+         - border radius (on corners)
+        @see: L{PM_GroupBox._getStyleSheet} (overrided here)
+        """
+        
+        styleSheet = "QGroupBox {border-style:hidden;\
+        border-width: 0px;\
+        border-color: "";\
+        border-radius: 0px;\
+        min-width: 10em; }" 
+   
+        return styleSheet
+            
 # End of PM_RadioButtonList ############################
