cd9a48e12340ee51a29fd86082b86aea98647fe2
Derrick Hendricks <derrick@nanorex.com>
2008-07-03 16:13:50 +0000

    corrected a spelling error and modified the default for the use of bsddb3 to false so the default matches the way the code used to work.

--- cad/src/NE1_Build_Constants.py
+++ cad/src/NE1_Build_Constants.py
@@ -1,10 +1,10 @@
 # Copyright 2008 Nanorex, Inc.  See LICENSE file for details. 
 """
 NE1_Version.py is intended to be a very small file which consists of only the
-data that is likely to change for any given release None other than system 
+data that is likely to change for any given release.  None other than system 
 imports should be used without good reason.
 """
 
 NE1_RELEASE_VERSION = "1.1.0"
 NE1_RELEASE_DATE = "July 3, 2008"
-NE1_USE_bsddb3 = True
+NE1_USE_bsddb3 = False
