a6800a2d8edff6ce7127d255a03f2e6e4215e1d9
Derrick Hendricks <derrick@nanorex.com>
2008-07-30 19:58:13 +0000

    Build script for building QuteMolX and supporting files

--- cad/plugins/QuteMol/packaging/buildMac.sh
+++ cad/plugins/QuteMol/packaging/buildMac.sh
@@ -2,19 +2,36 @@
 # This build depends on pre-requisite files that are stored in ~/QMX_support.
 # These files are also backed up on the nanoengineer-1.com server
 
-QUTEMOLX_VERSION="0.5.1"
+# Set version information
+if [ "$1" = "" ]
+then
+  QUTEMOLX_VERSION="0.5.1"
+else
+  QUTEMOLX_VERSION="$1"
+fi
+DIST_NAME="QuteMolX_$QUTEMOLX_VERSION"
 
-cd ..
+# Figure out where the root of the tree is
+cd .. || exit 1
 TOP_LEVEL=`pwd`
+
 if [ ! -e "qutemol/src/osx-build" ]
 then
   echo "Improper build environment"
   exit 1
 fi
+
+# set up a directory to store pre-built stuff
+if [ ! -e "~/MacOSX_Installers" ]
+then
+  mkdir ~/MacOSX_Installers
+fi
+
+# if it looks like there was already a build in this tree, clean it out
 if [ -e "qutemol/src/osx-build/local" ]
 then
   cd qutemol/src/osx-build
-  make clean
+  sudo make clean
   cd $TOP_LEVEL
   cd qutemol
   rm -rf glew
@@ -22,33 +39,63 @@ then
   rm -rf wrap
   rm -rf vcg
 fi
-cd $TOP_LEVEL/packaging/MacOSX
-rm -rf install
-mkdir $TOP_LEVEL/packaging/MacOSX/install
-cd $TOP_LEVEL
-cp -R qutemol $TOP_LEVEL/packaging/MacOSX/install
-cd  $TOP_LEVEL/packaging/MacOSX/install/qutemol
+DATECODE=`date "+%B %d, %Y"`
+cd $TOP_LEVEL/packaging/MacOSX || exit 1
+# remove any oyher previously built versions
+sudo rm -rf install build
+
+# create directories to store the new build
+mkdir $TOP_LEVEL/packaging/MacOSX/install || exit 1
+mkdir $TOP_LEVEL/packaging/MacOSX/install/$DIST_NAME || exit 1
+cd $TOP_LEVEL || exit 1
+
+# Modify the Welcome screen information
+cat packaging/MacOSX/Welcome_template.rtf | sed -e "s:VERSION_GOES_HERE:$QUTEMOLX_VERSION:g" | sed -e "s:DATE_GOES_HERE:$DATECODE:g" > packaging/MacOSX/Welcome.rtf
+
+# create the tarball of the source tree
+cp -R qutemol $TOP_LEVEL/packaging/MacOSX/install || exit 1
+cd  $TOP_LEVEL/packaging/MacOSX/install/qutemol || exit 1
 find . -depth -type d -name ".svn" -print -exec rm -rf {} \;
-cd ..
-tar -czf QuteMolX.tar.gz qutemol
+cd .. || exit 1
+tar -czf $DIST_NAME/QuteMolX.tar.gz qutemol || exit 1
 rm -rf qutemol
-cd $TOP_LEVEL/qutemol
+cd $TOP_LEVEL/qutemol || exit 1
 
-tar -xzf ~/QMX_support/glew.tar.gz
-cd src
-tar -xzf ~/QMX_support/wrap.tar.gz
-tar -xzf ~/QMX_support/vcg.tar.gz
+# extract the libraries needed to do the build
+tar -xzf ~/QMX_support/glew.tar.gz || exit 1
+cd src || exit 1
+tar -xzf ~/QMX_support/wrap.tar.gz || exit 1
+tar -xzf ~/QMX_support/vcg.tar.gz || exit 1
+
+# change the version information compiled into QuteMolX
 cat MyTab.cpp | sed -e "s:Version [0-9]*\.[0-9]*\.[0-9]*:Version $QUTEMOLX_VERSION:" > MyTab.cpp.btmp
-mv MyTab.cpp.btmp MyTab.cpp
-cd osx-build
-mkdir local
-cp $TOP_LEVEL/packaging/MacOSX/Makefile .
-make clean
-make
+mv -f MyTab.cpp.btmp MyTab.cpp || exit 1
 
-cd $TOP_LEVEL/qutemol/src/osx-build
-cp -R QuteMolX.app $TOP_LEVEL/packaging/MacOSX/install
-cd $TOP_LEVEL
-cp packaging/MacOSX/License.txt packaging/MacOSX/install
+# Do the actual build
+cd osx-build || exit 1
+mkdir local
+cp $TOP_LEVEL/packaging/MacOSX/Makefile . || exit 1
+sudo make clean
+sudo rm -rf QuteMolX.app
+make || exit 1
+
+# create the directory structure needed for the build
+cd $TOP_LEVEL/qutemol/src/osx-build || exit 1
+cp -R QuteMolX.app $TOP_LEVEL/packaging/MacOSX/install/$DIST_NAME || exit 1
+cd $TOP_LEVEL/packaging/MacOSX || exit 1
+cp License.txt install/$DIST_NAME || exit 1
+sudo find install -exec chown root:admin {} \;
 
 # Next step is to build the package
+mkdir -p build/QuteMolX || exit 1
+sudo /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -o $TOP_LEVEL/packaging/MacOSX/build/QuteMolX/QuteMolX_$QUTEMOLX_VERSION.pkg -r ./install -v -e $TOP_LEVEL/packaging/MacOSX -f QMX-info.plist || exit 1
+
+# Build the dmg file
+sudo sync
+sudo hdiutil create -srcfolder $TOP_LEVEL/packaging/MacOSX/build/QuteMolX -fs HFS+ -format UDZO $TOP_LEVEL/packaging/MacOSX/build/${DIST_NAME}.dmg || exit 1
+
+if [ ! -e ~/MacOSX_Installers/$DIST_NAME.pkg ]
+then
+  sudo cp -R $TOP_LEVEL/packaging/MacOSX/build/QuteMolX/$DIST_NAME.pkg ~/MacOSX_Installers
+  cp $TOP_LEVEL/packaging/MacOSX/build/$DIST_NAME.dmg ~/MacOSX_Installers
+fi
