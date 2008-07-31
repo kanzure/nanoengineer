b87aeb97e2da740f30bd8c37f70a4f47e65ce3ca
Derrick Hendricks <derrick@nanorex.com>
2008-07-31 19:03:26 +0000

    buildTars.sh: changed to not destroy the tree

--- packaging/buildTars.sh
+++ packaging/buildTars.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-# WARNING: once you run this command, this trunk is completely useless for
-# doing anything with subversion tree or any other builds.  Also, it doesn't 
-# need root privilages, so do not run as root.
-# You have been warned!!!
+# This should be run from the packaging directory
 
 # Set up some necessary variables
 GMX_VERSION="3.3.3"
@@ -10,29 +7,14 @@ QMX_VERSION="0.5.1"
 NE1_VERSION="1.1.1"
 TAR_DIR="/tmp/dist_tars"
 FORCE_REMOVE=0
-FORCE_CONTINUE=0
 cd .. || exit 1
 TOP_LEVEL=`pwd`
 
 # See if we are being run from somwhere that makes sense
-if [ ! -e cad/src ]
+if [ ! -e cad/src -o ! -e packaging ]
 then
   echo "Error in directory structure"
   exit 1
-fi
-
-# This is a one last chance to not delete stuff.  So, don't remove this.
-# The it can be bypassed, but that should only be done by a tested automated
-# script used for building.
-if [ $FORCE_CONTINUE -ne 1 ]
-then
-  ans="n"
-  echo "This will completely remove all .svn and packaging directories from this source tree.  Are you absolutely positive you want to do that? (y/N)"
-  read ans
-  if [ "$ans" != "y" -a "$ans" != "Y" ]
-  then
-    exit 1
-  fi
 fi
 
 # Check to figure out if the tars directory exists and what to do about it.
@@ -56,24 +38,13 @@ fi
 # Create the output directory for the tar files
 mkdir $TAR_DIR
 
-# about do delete the packaging and svn directories, copy what might be needed
-cp packaging/buildTars.sh . || exit 1
-cp packaging/buildTars.sh ~ || exit 1
-cp packaging/Win32/exclude_files.txt . || exit 1
-
-# Remove the .svn and packaging directories
-echo "Running: find . -name ".svn" -depth -type d -exec rm -rf {} \;"
-find . -name ".svn" -depth -type d -exec rm -rf {} \;
-echo "Running: find . -name "packaging" -depth -type d -exec rm -rf {} \;"
-find . -name "packaging" -depth -type d -exec rm -rf {} \;
-
 # Create the Suite tar file
 echo "Creating $TAR_DIR/NanoEngineer-1_Suite_v$NE1_VERSION.tar.gz"
-tar -czf $TAR_DIR/NanoEngineer-1_Suite_v$NE1_VERSION.tar.gz * || exit 1
+tar -cz --exclude "*packaging/*" --exclude "*packaging" --exclude "*/\.svn/*" --exclude "*\.svn" -f /tmp/dist_tars/NanoEngineer-1_Suite_v1.1.1.tar.gz * || exit 1
 
 # Create the NE1 tar file
 echo "Creating $TAR_DIR/NanoEngineer-1_v$NE1_VERSION.tar.gz"
-tar -cz -X exclude_files.txt -f $TAR_DIR/NanoEngineer-1_v$NE1_VERSION.tar.gz * || exit 1
+tar -cz --exclude "*packaging/*" --exclude "*packaging" --exclude "*/\.svn/*" --exclude "*\.svn" -X packaging/Win32/exclude_files.txt -f /tmp/dist_tars/NanoEngineer-1_v1.1.1.tar.gz * || exit 1
 
 # Create the GROMACS tar file
 echo "Creating GROMACS_$GMX_VERSION.tar.gz"
@@ -82,9 +53,9 @@ echo "GROMACS is a stock $GMX_VERSION - skipping" || exit 1
 # Create the QuteMolX tar file
 echo "Creating $TAR_DIR/QuteMolX_$QMX_VERSION.tar.gz"
 cd $TOP_LEVEL/cad/plugins/QuteMol || exit 1
-tar -czf $TAR_DIR/QuteMolX_$QMX_VERSION.tar.gz qutemol || exit 1
+tar -cz --exclude "*packaging/*" --exclude "*packaging" --exclude "*/\.svn/*" --exclude "*\.svn" -f $TAR_DIR/QuteMolX_$QMX_VERSION.tar.gz qutemol || exit 1
 
 # Create the HDF5_SimResults tar file
-echo "Creating _DIR/HDF5_SimResults_0.1.0.tar.gz"
+echo "Creating $TAR_DIR/HDF5_SimResults_0.1.0.tar.gz"
 cd $TOP_LEVEL/cad/plugins || exit 1
-tar -czf $TAR_DIR/HDF5_SimResults_0.1.0.tar.gz HDF5_SimResults || exit 1
+tar -cz --exclude "*packaging/*" --exclude "*packaging" --exclude "*/\.svn/*" --exclude "*\.svn" -f $TAR_DIR/HDF5_SimResults_0.1.0.tar.gz HDF5_SimResults || exit 1
