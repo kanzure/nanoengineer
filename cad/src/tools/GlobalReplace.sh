6deafd562492bc0d8b7cd885850581c9ac2eec81
Bruce Smith <bruce@nanorex.com>
2008-07-31 04:51:05 +0000

    comment about bug

--- cad/src/tools/GlobalReplace.sh
+++ cad/src/tools/GlobalReplace.sh
@@ -5,6 +5,9 @@
 # NOTE: it's a good idea to run this without any files to process to
 # check that the quoting in the sed script is ok.
 
+# WARNING: at least on the Mac, and judging by the diff output using -n,
+# this also adds a newline at the end of every source file which lacks one.
+
 # Example:
 #
 #   tools/GlobalReplace.sh "old\.symbol" "new.symbol" `tools/AllPyFiles.sh`
