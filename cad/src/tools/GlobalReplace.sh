a97a338ed235f4e2a44105bd904c4dd5b61039ee
Bruce Smith <bruce@nanorex.com>
2008-08-06 22:32:43 +0000

    comment

--- cad/src/tools/GlobalReplace.sh
+++ cad/src/tools/GlobalReplace.sh
@@ -2,11 +2,23 @@
 
 # usage: GlobalReplace.sh [-n] searchRegex replaceString filelist...
 
+# $Id$
+
 # NOTE: it's a good idea to run this without any files to process to
 # check that the quoting in the sed script is ok.
 
 # WARNING: at least on the Mac, and judging by the diff output using -n,
 # this also adds a newline at the end of every source file which lacks one.
+# This is ok on the files that it needs to modify anyway,
+# but not on the files it otherwise doesn't need to.
+# To work around this problem, only run it on the necessary files,
+# which can be found using a command like
+#
+# % fgrep -l <string> `tools/AllPyFiles.sh`  # to find only source files
+#
+# or
+#
+# % find . -name '*.py' -print | xargs fgrep -l <string>  # to find outtakes files, etc
 
 # Example:
 #
