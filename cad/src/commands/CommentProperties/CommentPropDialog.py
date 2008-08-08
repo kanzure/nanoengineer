b22cb446a987117896b6ae1ef26d4ece2e497e1a
Mark Sims <mark@nanorex.com>
2008-08-08 04:53:36 +0000

    Added spacer to the bottom right corner of the Comment dialog.

--- cad/src/commands/CommentProperties/CommentPropDialog.py
+++ cad/src/commands/CommentProperties/CommentPropDialog.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2006-2007 Nanorex, Inc.  See LICENSE file for details. 
-# Form implementation generated from reading ui file 'CommentPropDialog.ui'
+# Form implementation generated from reading ui file 'COmmentPropDialog.ui'
 #
-# Created: Wed Sep 20 06:42:18 2006
-#      by: PyQt4 UI code generator 4.0.1
+# Created: Thu Aug 07 18:04:11 2008
+#      by: PyQt4 UI code generator 4.3.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -16,8 +15,8 @@ class Ui_CommentPropDialog(object):
         CommentPropDialog.resize(QtCore.QSize(QtCore.QRect(0,0,390,275).size()).expandedTo(CommentPropDialog.minimumSizeHint()))
 
         self.gridlayout = QtGui.QGridLayout(CommentPropDialog)
-        self.gridlayout.setMargin(0)
-        self.gridlayout.setSpacing(0)
+        self.gridlayout.setContentsMargins(0,0,0,2)
+        self.gridlayout.setSpacing(2)
         self.gridlayout.setObjectName("gridlayout")
 
         self.comment_textedit = QtGui.QTextEdit(CommentPropDialog)
@@ -25,15 +24,14 @@ class Ui_CommentPropDialog(object):
         self.gridlayout.addWidget(self.comment_textedit,0,0,1,1)
 
         self.hboxlayout = QtGui.QHBoxLayout()
-        self.hboxlayout.setMargin(3)
-        self.hboxlayout.setSpacing(6)
+        self.hboxlayout.setSpacing(4)
         self.hboxlayout.setObjectName("hboxlayout")
 
         self.date_time_btn = QtGui.QPushButton(CommentPropDialog)
         self.date_time_btn.setObjectName("date_time_btn")
         self.hboxlayout.addWidget(self.date_time_btn)
 
-        spacerItem = QtGui.QSpacerItem(40,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        spacerItem = QtGui.QSpacerItem(81,25,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
         self.hboxlayout.addItem(spacerItem)
 
         self.cancel_btn = QtGui.QPushButton(CommentPropDialog)
@@ -43,17 +41,19 @@ class Ui_CommentPropDialog(object):
         self.ok__btn = QtGui.QPushButton(CommentPropDialog)
         self.ok__btn.setObjectName("ok__btn")
         self.hboxlayout.addWidget(self.ok__btn)
+
+        spacerItem1 = QtGui.QSpacerItem(20,20,QtGui.QSizePolicy.Fixed,QtGui.QSizePolicy.Minimum)
+        self.hboxlayout.addItem(spacerItem1)
         self.gridlayout.addLayout(self.hboxlayout,1,0,1,1)
 
         self.retranslateUi(CommentPropDialog)
         QtCore.QObject.connect(self.cancel_btn,QtCore.SIGNAL("clicked()"),CommentPropDialog.reject)
         QtCore.QObject.connect(self.ok__btn,QtCore.SIGNAL("clicked()"),CommentPropDialog.accept)
-        QtCore.QObject.connect(self.date_time_btn,QtCore.SIGNAL("clicked()"),CommentPropDialog.insert_date_time_stamp)
         QtCore.QMetaObject.connectSlotsByName(CommentPropDialog)
 
     def retranslateUi(self, CommentPropDialog):
         CommentPropDialog.setWindowTitle(QtGui.QApplication.translate("CommentPropDialog", "Comment", None, QtGui.QApplication.UnicodeUTF8))
-        CommentPropDialog.setWindowIcon(QtGui.QIcon("ui/border/Comment"))
         self.date_time_btn.setText(QtGui.QApplication.translate("CommentPropDialog", "Date/Time Stamp", None, QtGui.QApplication.UnicodeUTF8))
         self.cancel_btn.setText(QtGui.QApplication.translate("CommentPropDialog", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
         self.ok__btn.setText(QtGui.QApplication.translate("CommentPropDialog", "OK", None, QtGui.QApplication.UnicodeUTF8))
+
