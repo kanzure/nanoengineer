44a438d766e36aee6ae0eb16cc929f6a006fb89e
Derrick Hendricks <derrick@nanorex.com>
2008-08-13 18:49:12 +0000

    very simple test program for UTF_8 support

---
+++ cad/src/experimental/language_test/language1.py
@@ -0,0 +1,9 @@
+# This file was created using a text editor that displayed unicode 
+# characters using their native format.  The characters in the
+# shelfname string below are taking from a Korean newspaper.
+# I have no idea what they mean.
+
+shelfname = u"C:\\Documents and Settings\\배드민턴\\Nanorex"
+print "ShelfName = %r" % shelfname.encode("utf_8")
+print "ShelfName = " + shelfname.encode("utf_8")
+print shelfname.encode("utf_8")
