b17f27b93ad7ed31fa71079550b720a906018937
Derrick Hendricks <derrick@nanorex.com>
2008-08-13 20:34:30 +0000

    slight modifications to the test script

--- cad/src/experimental/language_test/language1.py
+++ cad/src/experimental/language_test/language1.py
@@ -1,3 +1,4 @@
+# -*- coding: utf_8 -*-
 # This file was created using a text editor that displayed unicode 
 # characters using their native format.  The characters in the
 # shelfname string below are taking from a Korean newspaper.
@@ -7,3 +8,9 @@ shelfname = u"C:\\Documents and Settings\\배드민턴\\Nanorex"
 print "ShelfName = %r" % shelfname.encode("utf_8")
 print "ShelfName = " + shelfname.encode("utf_8")
 print shelfname.encode("utf_8")
+print "ShelfName = %s".encode("utf_8") % shelfname.encode("utf_8")
+try:
+    shelfname = "C:\\Documents and Settings\\배드민턴\\Nanorex"
+    print "ShelfName = %s" % shelfname.encode("utf_8")
+except:
+    pass
