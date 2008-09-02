72bf33841662c7c78438416c6ac4a3f32bca33e2
Ninad Sathaye <ninad@nanorex.com>
2008-09-02 21:08:32 +0000

    documentation for command_exit_flyout

--- cad/src/command_support/baseCommand.py
+++ cad/src/command_support/baseCommand.py
@@ -254,8 +254,32 @@ class baseCommand(object):
         """
         Undo whatever was done (to the parent command's flyout toolbar)
         by self.command_enter_flyout() when this command was entered.
-
+        
+        NOTE: This method is not responsible for the 'next flyout toolbar' 
+        the program is going to display. All it needs to do is to a) either 
+        deactiavate the flyout toolbar or b) if the command is reusing the 
+        flyout toolbar of the parent command, just uncheck the action 
+        that corresponds to the command. 
+        
+        Example: 
+        - While Default mode, the user invoked Play Movie command 
+          by first clicking on the 'Simulation' button in the Control Area' of 
+          the 'Command Toolbar' and then by clicking on the 'Movie' button in the 
+          flyout toolbar that appears. 
+        - User thus enters the Movie command and the custom flyout toolbar is 
+          displayed. 
+        - When user exits the Movie command, the 'command_exit_flyout' method 
+          of the movie command only deactivates the flyout toolbar it was 
+          displaying while in that command. 
+        - But it is not responsible for the displaying the 'next flyout' after
+          exiting self. That part is handled by the next command 
+          (SelectChunks_Command or default command in this case) . The default 
+          commandthen resets the flyout toolbar such that it displays the 
+          'Build' flyout (actions such as Build Dna, Build Crystal' etc) 
+        
+        
         [subclasses should override this as needed]
+        @see: Ui_AbstractToolBar.deActivateFlyoutToolbar()
         """
         return
     
@@ -457,6 +481,9 @@ class baseCommand(object):
         @note: Called by base class implementation of command_entered.
 
         [subclasses should override as needed]
+        
+        @see: Ui_AbstractToolBar.activateFlyoutToolbar()
+        @see: self.command_exit_flyout() 
         """
         return
 
