51c99aaeff9de9685270cc07f432fc32b7ba2106
Ninad Sathaye <ninad@nanorex.com>
2008-09-11 18:38:49 +0000

    ported BuildProtein_EditCommand to USE_COMMAND_STACK

--- cad/src/protein/commands/BuildProtein/BuildProtein_EditCommand.py
+++ cad/src/protein/commands/BuildProtein/BuildProtein_EditCommand.py
@@ -9,16 +9,19 @@ BuildProtein_EditCommand.py
 """
 from command_support.EditCommand import EditCommand
 from utilities.Log  import greenmsg
-from utilities.constants import gensym
 from ne1_ui.toolbars.Ui_ProteinFlyout import ProteinFlyout
 
 from protein.commands.BuildProtein.BuildProtein_PropertyManager import BuildProtein_PropertyManager
+from utilities.GlobalPreferences import USE_COMMAND_STACK
 
+_superclass = EditCommand
 class BuildProtein_EditCommand(EditCommand):
     """
     BuildProtein_EditCommand provides a convenient way to edit or create
     a Protein object
     """
+    #Temporary attr 'command_porting_status. See baseCommand for details.
+    command_porting_status = "PARTIAL: 2008-09-11: resume_gui not ported.. implement command_update_* ?"
     
     #Property Manager
     PM_class = BuildProtein_PropertyManager
@@ -37,32 +40,8 @@ class BuildProtein_EditCommand(EditCommand):
     command_can_be_suspended = True
     create_name_from_prefix  =  True
     call_makeMenus_for_each_event = True
-    
-    
-
-    def __init__(self, commandSequencer, struct = None):
-        """
-        Constructor for BuildDna_EditCommand
-        """
-        EditCommand.__init__(self, commandSequencer)
-        self.struct = struct
-
-
-    def init_gui(self):
-        """
-        Do changes to the GUI while entering this command. This includes opening
-        the property manager, updating the command toolbar , connecting widget
-        slots (if any) etc. Note: The slot connection in property manager and
-        command toolbar is handled in those classes.
-
-        Called once each time the command is entered; should be called only
-        by code in modes.py
-
-        @see: L{self.restore_gui}
-        """
-        EditCommand.init_gui(self)
-        self.command_enter_flyout()
-        
+      
+          
     def command_enter_flyout(self):
         if self.flyoutToolbar is None:
             self.flyoutToolbar = self._createFlyoutToolBarObject()
@@ -78,39 +57,55 @@ class BuildProtein_EditCommand(EditCommand):
         #from ne1_ui.toolbars.Ui_ProteinFlyout_v2 import ProteinFlyout_v2
         flyoutToolbar = ProteinFlyout(self.win, self.propMgr) 
         return flyoutToolbar    
-        
-    def resume_gui(self):
-        """
-        Called when this command, that was suspended earlier, is being resumed.
-        The temporary command (which was entered by suspending this command)
-        might have made some changes to the model which need to be reflected
-        while resuming command.
-
-        Example: A user enters BreakStrands_Command by suspending
-        BuildDna_EditCommand, then breaks a few strands, thereby creating new
-        strand chunks. Now when the user returns to the BuildDna_EditCommand,
-        the command's property manager needs to update the list of strands
-        because of the changes done while in BreakStrands_Command.
-        @see: Command.resume_gui
-        @see: Command._enterMode where this method is called.
-        """
-        
-        if self.flyoutToolbar:
-            self.flyoutToolbar.resetStateOfActions()
-
-
-    def restore_gui(self):
-        """
-        Do changes to the GUI while exiting this command. This includes closing
-        this mode's property manager, updating the command toolbar ,
-        Note: The slot connection/disconnection in property manager and
-        command toolbar is handled in those classes.
-        @see: L{self.init_gui}
-        """
-        EditCommand.restore_gui(self)
-        
-        if self.flyoutToolbar:
-            self.flyoutToolbar.deActivateFlyoutToolbar()
+    
+    if not USE_COMMAND_STACK:    
+        def init_gui(self):
+            """
+            Do changes to the GUI while entering this command. This includes opening
+            the property manager, updating the command toolbar , connecting widget
+            slots (if any) etc. Note: The slot connection in property manager and
+            command toolbar is handled in those classes.
+    
+            Called once each time the command is entered; should be called only
+            by code in modes.py
+    
+            @see: L{self.restore_gui}
+            """
+            EditCommand.init_gui(self)
+            self.command_enter_flyout()
+            
+        def resume_gui(self):
+            """
+            Called when this command, that was suspended earlier, is being resumed.
+            The temporary command (which was entered by suspending this command)
+            might have made some changes to the model which need to be reflected
+            while resuming command.
+    
+            Example: A user enters BreakStrands_Command by suspending
+            BuildDna_EditCommand, then breaks a few strands, thereby creating new
+            strand chunks. Now when the user returns to the BuildDna_EditCommand,
+            the command's property manager needs to update the list of strands
+            because of the changes done while in BreakStrands_Command.
+            @see: Command.resume_gui
+            @see: Command._enterMode where this method is called.
+            """
+            
+            if self.flyoutToolbar:
+                self.flyoutToolbar.resetStateOfActions()
+    
+    
+        def restore_gui(self):
+            """
+            Do changes to the GUI while exiting this command. This includes closing
+            this mode's property manager, updating the command toolbar ,
+            Note: The slot connection/disconnection in property manager and
+            command toolbar is handled in those classes.
+            @see: L{self.init_gui}
+            """
+            EditCommand.restore_gui(self)
+            
+            if self.flyoutToolbar:
+                self.flyoutToolbar.deActivateFlyoutToolbar()
 
     def runCommand(self):
         """
@@ -139,22 +134,3 @@ class BuildProtein_EditCommand(EditCommand):
                 bool_keep = True
 
         return bool_keep
-
-    def create_and_or_show_PM_if_wanted(self, showPropMgr = True):
-        """
-        Create the property manager object if one doesn't already exist
-        and then show the propMgr if wanted by the user.
-        @param showPropMgr: If True, show the property manager
-        @type showPropMgr: boolean
-        """
-        EditCommand.create_and_or_show_PM_if_wanted(
-            self,
-            showPropMgr = showPropMgr)
-        
-        self.propMgr.updateMessage("Use appropriate command in the command "\
-                                   "toolbar to modify a Protein."\
-                                   "<br>"\
-                                   "If you change the sequence, make sure to "\
-                                   "hit Enter key at the end so that the changes "
-                                   "can be saved."
-                                   "<br>"  )
