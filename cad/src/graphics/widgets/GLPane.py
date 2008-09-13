1b973efe8afcc8e49259919252072f12bfbb6f9e
Bruce Smith <bruce@nanorex.com>
2008-09-13 04:04:09 +0000

    split frustum culling methods into new file

--- cad/src/graphics/widgets/GLPane.py
+++ cad/src/graphics/widgets/GLPane.py
@@ -52,7 +52,6 @@ if TEST_DRAWING:
     from prototype.test_drawing import test_drawing
     pass
 
-import math
 import sys
 import time
 
@@ -106,13 +105,10 @@ from OpenGL.GL import glRasterPos3f
 from OpenGL.GL import GL_CURRENT_RASTER_POSITION_VALID
 from OpenGL.GL import glGetBooleanv
 from OpenGL.GL import glDrawPixels
-from OpenGL.GL import glGetFloatv
-from OpenGL.GL import GL_PROJECTION_MATRIX
-from OpenGL.GL import GL_MODELVIEW_MATRIX
 
 from OpenGL.GLU import gluUnProject, gluPickMatrix
 
-from geometry.VQT import V, Q, A, norm, vlen, angleBetween
+from geometry.VQT import V, Q, A, norm, angleBetween
 from geometry.VQT import planeXline, ptonline
 
 from Numeric import dot
@@ -189,6 +185,7 @@ from graphics.widgets.GLPane_mixin_for_DisplayListChunk import GLPane_mixin_for_
 from graphics.widgets.GLPane_lighting_methods import GLPane_lighting_methods
 from graphics.widgets.GLPane_text_and_color_methods import GLPane_text_and_color_methods
 from graphics.widgets.GLPane_stereo_methods import GLPane_stereo_methods
+from graphics.widgets.GLPane_frustum_methods import GLPane_frustum_methods
 from graphics.widgets.GLPane_view_change_methods import GLPane_view_change_methods
 
 from graphics.drawing.drawcompass import drawcompass
@@ -212,20 +209,26 @@ else:
     class _CommandSequencer_for_glpane(object):
         pass
 
-class GLPane(GLPane_lighting_methods, # this needs to come before GLPane_minimal
-                 # since it needs to override _setup_lighting
-             
-             GLPane_minimal, # the "main superclass"; inherits QGLWidget
-             
-             _CommandSequencer_for_glpane,
-             DebugMenuMixin,
-             SubUsageTrackingMixin,
-             GLPane_mixin_for_DisplayListChunk,
-             
-             GLPane_text_and_color_methods,
-             GLPane_stereo_methods,
-             GLPane_view_change_methods
-            ):
+class GLPane(
+    # these superclasses need to come first, since they need to
+    # override methods in GLPane_minimal:
+    GLPane_lighting_methods, # needs to override _setup_lighting
+    GLPane_frustum_methods, # needs to override is_sphere_visible etc
+    
+    # these don't yet need to come first, but we'll put them first
+    # anyway in case someone adds a default def of some method
+    # into GLPane_minimal in the future:
+    GLPane_text_and_color_methods,
+    GLPane_stereo_methods,
+    GLPane_view_change_methods,
+
+    GLPane_minimal, # the "main superclass"; inherits QGLWidget
+
+    _CommandSequencer_for_glpane,
+    DebugMenuMixin,
+    SubUsageTrackingMixin,
+    GLPane_mixin_for_DisplayListChunk
+    ):
     """
     Widget for OpenGL graphics and associated mouse/key input,
     with lots of associated/standard behavior and helper methods.
@@ -2377,212 +2380,6 @@ class GLPane(GLPane_lighting_methods, # this needs to come before GLPane_minimal
         # - 'selobj/preDraw_glselect_dict' -- like selobj, but color buffer drawing is off ###e which coord system, incl projection??
         # [end]
 
-    def _compute_frustum_planes(self): # Piotr 080331
-        """
-        Compute six planes to be used for frustum culling
-        (if the use of that feature is enabled).
-
-        @note: this must only be called when the matrices are set up
-               to do drawing in absolute model space coordinates.
-               Callers which later change those matrices should review
-               whether they need to set self._frustum_planes_available = False
-               when they change them, to avoid erroneous culling.
-        """
-        # The following comment comes from emails exchanged between Russ and Piotr
-        # on 080403.
-        #        
-        # This code is based on the information from OpenGL FAQ on clipping and culling:
-        # http://www.opengl.org/resources/faq/technical/clipping.htm
-        #
-        # I tried to dust off the math behind it and I found the following paper
-        # dealing with the homogeneous coordinates in great detail:
-        # http://www.unchainedgeometry.com/jbloom/pdf/homog-coords.pdf
-        #
-        # The computations are in homogeneous coordinates, so every vertex is 
-        # represented by four coordinates (x,y,z,w). The composite matrix C (cmat 
-        # in the code) transforms a point from the Euclidean coordinates to the 
-        # homogeneous coordinates.
-        #
-        # Let's transform a point p(xp,yp,zp,wp) using the computed composite 
-        # matrix:
-        #
-        # C[4][4] (in 3D Euclidean space, wp = 1):
-        #
-        # r = p^T * C
-        #
-        # so we get a transformed point r(p*C0,p*C1,p*C2,p*C3), where Cx represents
-        # a corresponding row of the matrix C. In homogeneous coordinates, if 
-        # the transformed point is inside the viewing frustum, the following 
-        # inequalities have to be true:
-        #
-        # -wr < xr < wr
-        # -wr < yr < wr
-        # -wr < zr < wr
-        #
-        # So if -wr < xr, the point p is on + side of the left frustum plane, 
-        # if xr < wr it is on + side of the right frustum plane, if -wr < yr, 
-        # the point is on + side of the bottom plane, and so on. Let's take the 
-        # left frustum plane:
-        # 
-        # -wr < xr
-        #
-        # so:
-        # 
-        # -p*C3 < p*C0
-        # 
-        # so:
-        #
-        # 0 < p*C0+p*C3
-        # 
-        # that is equal to:
-        # 
-        # 0 < p*(C0+C3)
-        # 
-        # which gives us the plane equation (ax+bx+cx+d=0):
-        # 
-        # x*(C0[0]+C3[0]) + y*(C0[1]+C3[1]) + z*(C0[2]+C3[2]) + w(C0[3]+C3[3]) = 0
-        # 
-        # Because w = 1:
-        # 
-        # x*(C0[0]+C3[0]) + y*(C0[1]+C3[1]) + z*(C0[2]+C3[2]) + (C0[3]+C3[3]) = 0
-        # 
-        # so the plane coefficients are as follows:
-        # 
-        # a = C0[0] + C3[0]
-        # b = C0[1] + C3[1]
-        # c = C0[2] + C3[2]
-        # d = C0[3] + C3[3]
-        # 
-        # Similarly, for the right plane (xr < wr) we get:
-        #
-        # a = -C0[0] + C3[0]
-        # b = -C0[1] + C3[1]
-        # c = -C0[2] + C3[2]
-        # d = -C0[3] + C3[3]
-        # 
-        # and so on for every plane.
-        #
-        
-        if not self._use_frustum_culling:
-            # [bruce 080331]
-            assert not self._frustum_planes_available
-            return
-
-        # Get current projection and modelview matrices 
-        pmat = glGetFloatv(GL_PROJECTION_MATRIX)
-        mmat = glGetFloatv(GL_MODELVIEW_MATRIX)
-
-        # Allocate a composite matrix float[4, 4]
-        cmat = [None] * 4
-        for i in range(0, 4):
-            cmat[i] = [0.0] * 4
-
-        # Compute a composite transformation matrix. 
-        # cmat = mmat * pmat         
-        for i in range(0, 4):            
-            for j in range (0, 4):
-                cmat[i][j] = (mmat[i][0] * pmat[0][j] + 
-                              mmat[i][1] * pmat[1][j] + 
-                              mmat[i][2] * pmat[2][j] + 
-                              mmat[i][3] * pmat[3][j])
-
-        # Allocate six frustum planes
-        self.fplanes = [None] * 6
-        for p in range(0, 6):
-            self.fplanes[p] = [0.0] * 4
-
-        # subtract and add the composite matrix rows to get the plane equations
-        
-        for p in range(0, 3):
-            for i in range(0, 4):
-                self.fplanes[2*p][i] = cmat[i][3] - cmat[i][p]
-                self.fplanes[2*p+1][i] = cmat[i][3] + cmat[i][p]
-
-        # normalize the plane normals
-        for p in range(0, 6):
-            n = math.sqrt(float(self.fplanes[p][0] * self.fplanes[p][0] +
-                                self.fplanes[p][1] * self.fplanes[p][1] +
-                                self.fplanes[p][2] * self.fplanes[p][2]))
-            if n > 1e-8:
-                self.fplanes[p][0] /= n
-                self.fplanes[p][1] /= n
-                self.fplanes[p][2] /= n
-                self.fplanes[p][3] /= n
-
-        # cause self.is_sphere_visible() to use these planes
-        self._frustum_planes_available = True # [bruce 080331]
-
-        return
-
-    def is_sphere_visible(self, center, radius): # Piotr 080331
-        """
-        Perform a simple frustum culling test against a spherical object
-        in absolute model space coordinates. Assume that the frustum planes 
-        are allocated, i.e. glpane._compute_frustum_planes was already called.
-        (If it wasn't, the test will always succeed.)
-
-        @warning: this will give incorrect results unless the current
-                  GL matrices are in the same state as when _compute_frustum_planes
-                  was last called (i.e. in absolute model space coordinates).
-        """
-        ### uncomment the following line for the bounding sphere debug
-        ### drawwiresphere(white, center, radius, 2)
-
-        if self._frustum_planes_available:
-            c0 = center[0]
-            c1 = center[1]
-            c2 = center[2]
-            for p in range(0, 6): # go through all frustum planes
-                # calculate a distance to the frustum plane 'p'
-                # the sign corresponds to the plane normal direction
-                # piotr 080801: getting the frustum plane equation
-                # before performing the test gives about 30% performance 
-                # improvement
-                fp = self.fplanes[p]
-                dist =  (fp[0] * c0 + 
-                         fp[1] * c1 + 
-                         fp[2] * c2 + 
-                         fp[3])
-                # sphere outside of the plane - exit
-                if dist < -radius:
-                    return False
-                continue
-            # At this point, the sphere might still be outside
-            # of the box (e.g. if it is bigger than the box and a
-            # box corner is close to the sphere and points towards it),
-            # but this is an acceptable approximation for now
-            # (since false positives are safe, and this will not affect
-            #  most chunks in typical uses where this is a speedup.)
-            # [bruce 080331 comment]
-            pass
-
-        return True
-
-    def is_lozenge_visible(self, pos1, pos2, radius): # piotr 080402
-        """
-        Perform a simple frustum culling test against a "lozenge" object
-        in absolute model space coordinates. The lozenge is a cylinder
-        with two hemispherical caps. Assume that the frustum planes 
-        are allocated, i.e. glpane._compute_frustum_planes was already called.
-        (If it wasn't, the test will always succeed.)
-
-        Currently, this is a loose (but correct) approximation which calls
-        glpane.is_sphere_visible on the lozenge's bounding sphere.
-
-        @warning: this will give incorrect results unless the current
-                  GL matrices are in the same state as when _compute_frustum_planes
-                  was last called (i.e. in absolute model space coordinates).
-        """
-
-        if self._frustum_planes_available:
-            center = 0.5 * (pos1 + pos2)
-            sphere_radius = 0.5 * vlen(pos2 - pos1) + radius
-            res = self.is_sphere_visible(center, sphere_radius)
-            # Read Bruce's comment in glpane.is_sphere_visible
-            # It applies here, as well.
-            return res 
-
-        return True
 
     def standard_repaint_0(self):
 
@@ -2641,7 +2438,8 @@ class GLPane(GLPane_lighting_methods, # this needs to come before GLPane_minimal
         # Note that this method is also called by "do_glselect_if_wanted".
         # The second call will re-compute the frustum planes according to 
         # the current projection matrix.
-        self._compute_frustum_planes()
+        if self._use_frustum_culling:
+            self._compute_frustum_planes()
 
         # In the glselect_wanted case, we now know (in glselect_dict) which objects draw any pixels at the mouse position,
         # but not which one is in front (the near/far info from GL_SELECT has too wide a range to tell us that).
@@ -2861,7 +2659,8 @@ class GLPane(GLPane_lighting_methods, # this needs to come before GLPane_minimal
             self.current_glselect = (wX, wY, 3, 3) #bruce 050615 for use by nodes which want to set up their own projection matrix
             self._setup_projection( glselect = self.current_glselect ) # option makes it use gluPickMatrix
                 # replace 3, 3 with 1, 1? 5, 5? not sure whether this will matter... in principle should have no effect except speed
-            self._compute_frustum_planes() 
+            if self._use_frustum_culling:
+                self._compute_frustum_planes() 
                 # piotr 080331 - the frustum planes have to be setup after the 
                 # projection matrix is setup. I'm not sure if there may
                 # be any side effects - see the comment below about
---
+++ cad/src/graphics/widgets/GLPane_frustum_methods.py
@@ -0,0 +1,248 @@
+# Copyright 2008 Nanorex, Inc.  See LICENSE file for details.
+"""
+GLPane_frustum_methods.py
+
+@author: Piotr
+@version: $Id$
+@copyright: 2008 Nanorex, Inc.  See LICENSE file for details.
+
+History:
+
+piotr circa 080331 wrote these in GLPane.py
+
+bruce 080912 split this code into its own file
+
+Explanation of code:
+
+# The following comment comes from emails exchanged between Russ and Piotr
+# on 080403.
+#        
+# This code is based on the information from OpenGL FAQ on clipping and culling:
+# http://www.opengl.org/resources/faq/technical/clipping.htm
+#
+# I tried to dust off the math behind it and I found the following paper
+# dealing with the homogeneous coordinates in great detail:
+# http://www.unchainedgeometry.com/jbloom/pdf/homog-coords.pdf
+#
+# The computations are in homogeneous coordinates, so every vertex is 
+# represented by four coordinates (x,y,z,w). The composite matrix C (cmat 
+# in the code) transforms a point from the Euclidean coordinates to the 
+# homogeneous coordinates.
+#
+# Let's transform a point p(xp,yp,zp,wp) using the computed composite 
+# matrix:
+#
+# C[4][4] (in 3D Euclidean space, wp = 1):
+#
+# r = p^T * C
+#
+# so we get a transformed point r(p*C0,p*C1,p*C2,p*C3), where Cx represents
+# a corresponding row of the matrix C. In homogeneous coordinates, if 
+# the transformed point is inside the viewing frustum, the following 
+# inequalities have to be true:
+#
+# -wr < xr < wr
+# -wr < yr < wr
+# -wr < zr < wr
+#
+# So if -wr < xr, the point p is on + side of the left frustum plane, 
+# if xr < wr it is on + side of the right frustum plane, if -wr < yr, 
+# the point is on + side of the bottom plane, and so on. Let's take the 
+# left frustum plane:
+# 
+# -wr < xr
+#
+# so:
+# 
+# -p*C3 < p*C0
+# 
+# so:
+#
+# 0 < p*C0+p*C3
+# 
+# that is equal to:
+# 
+# 0 < p*(C0+C3)
+# 
+# which gives us the plane equation (ax+bx+cx+d=0):
+# 
+# x*(C0[0]+C3[0]) + y*(C0[1]+C3[1]) + z*(C0[2]+C3[2]) + w(C0[3]+C3[3]) = 0
+# 
+# Because w = 1:
+# 
+# x*(C0[0]+C3[0]) + y*(C0[1]+C3[1]) + z*(C0[2]+C3[2]) + (C0[3]+C3[3]) = 0
+# 
+# so the plane coefficients are as follows:
+# 
+# a = C0[0] + C3[0]
+# b = C0[1] + C3[1]
+# c = C0[2] + C3[2]
+# d = C0[3] + C3[3]
+# 
+# Similarly, for the right plane (xr < wr) we get:
+#
+# a = -C0[0] + C3[0]
+# b = -C0[1] + C3[1]
+# c = -C0[2] + C3[2]
+# d = -C0[3] + C3[3]
+# 
+# and so on for every plane.
+"""
+
+import math
+
+from geometry.VQT import vlen
+
+import foundation.env as env
+
+from OpenGL.GL import GL_MODELVIEW_MATRIX
+from OpenGL.GL import GL_PROJECTION_MATRIX
+from OpenGL.GL import glGetFloatv
+
+class GLPane_frustum_methods(object):
+    """
+    Private mixin superclass to provide frustum culling support to class GLPane.
+    The main class needs to call these methods at appropriate times
+    and use some attributes we maintain in appropriate ways.
+    For documentation see the method docstrings and code comments.
+    
+    All our attribute names and method names contain the string 'frustum',
+    making them easy to find by text-searching the main class source code.
+    """
+
+    def _compute_frustum_planes(self): # Piotr 080331
+        """
+        Compute six planes to be used for frustum culling
+        (assuming the use of that feature is enabled,
+         which is up to the client code in the main class).
+
+        Whenever the main class client code changes the projection matrix,
+        it must either call this method, or set
+        self._frustum_planes_available = False to turn off frustum culling.
+
+        @note: this must only be called when the matrices are set up
+               to do drawing in absolute model space coordinates.
+               Callers which later change those matrices should review
+               whether they need to set self._frustum_planes_available = False
+               when they change them, to avoid erroneous culling.
+        """
+        # Get current projection and modelview matrices 
+        pmat = glGetFloatv(GL_PROJECTION_MATRIX)
+        mmat = glGetFloatv(GL_MODELVIEW_MATRIX)
+
+        # Allocate a composite matrix float[4, 4]
+        cmat = [None] * 4
+        for i in range(0, 4):
+            cmat[i] = [0.0] * 4
+
+        # Compute a composite transformation matrix. 
+        # cmat = mmat * pmat         
+        for i in range(0, 4):            
+            for j in range (0, 4):
+                cmat[i][j] = (mmat[i][0] * pmat[0][j] + 
+                              mmat[i][1] * pmat[1][j] + 
+                              mmat[i][2] * pmat[2][j] + 
+                              mmat[i][3] * pmat[3][j])
+
+        # Allocate six frustum planes
+        self.fplanes = [None] * 6
+        for p in range(0, 6):
+            self.fplanes[p] = [0.0] * 4
+
+        # subtract and add the composite matrix rows to get the plane equations
+        
+        for p in range(0, 3):
+            for i in range(0, 4):
+                self.fplanes[2*p][i] = cmat[i][3] - cmat[i][p]
+                self.fplanes[2*p+1][i] = cmat[i][3] + cmat[i][p]
+
+        # normalize the plane normals
+        for p in range(0, 6):
+            n = math.sqrt(float(self.fplanes[p][0] * self.fplanes[p][0] +
+                                self.fplanes[p][1] * self.fplanes[p][1] +
+                                self.fplanes[p][2] * self.fplanes[p][2]))
+            if n > 1e-8:
+                self.fplanes[p][0] /= n
+                self.fplanes[p][1] /= n
+                self.fplanes[p][2] /= n
+                self.fplanes[p][3] /= n
+
+        # cause self.is_sphere_visible() to use these planes
+        self._frustum_planes_available = True # [bruce 080331]
+
+        return
+
+    def is_sphere_visible(self, center, radius): # Piotr 080331
+        """
+        Perform a simple frustum culling test against a spherical object
+        in absolute model space coordinates. Assume that the frustum planes 
+        are allocated, i.e. glpane._compute_frustum_planes was already called.
+        (If it wasn't, the test will always succeed.)
+
+        @warning: this will give incorrect results unless the current
+                  GL matrices are in the same state as when _compute_frustum_planes
+                  was last called (i.e. in absolute model space coordinates).
+        """
+        ### uncomment the following line for the bounding sphere debug
+        ### drawwiresphere(white, center, radius, 2)
+
+        if self._frustum_planes_available:
+            c0 = center[0]
+            c1 = center[1]
+            c2 = center[2]
+            for p in range(0, 6): # go through all frustum planes
+                # calculate a distance to the frustum plane 'p'
+                # the sign corresponds to the plane normal direction
+                # piotr 080801: getting the frustum plane equation
+                # before performing the test gives about 30% performance 
+                # improvement
+                fp = self.fplanes[p]
+                dist =  (fp[0] * c0 + 
+                         fp[1] * c1 + 
+                         fp[2] * c2 + 
+                         fp[3])
+                # sphere outside of the plane - exit
+                if dist < -radius:
+                    return False
+                continue
+            # At this point, the sphere might still be outside
+            # of the box (e.g. if it is bigger than the box and a
+            # box corner is close to the sphere and points towards it),
+            # but this is an acceptable approximation for now
+            # (since false positives are safe, and this will not affect
+            #  most chunks in typical uses where this is a speedup.)
+            # [bruce 080331 comment]
+            pass
+
+        return True
+
+    def is_lozenge_visible(self, pos1, pos2, radius): # piotr 080402
+        """
+        Perform a simple frustum culling test against a "lozenge" object
+        in absolute model space coordinates. The lozenge is a cylinder
+        with two hemispherical caps. Assume that the frustum planes 
+        are allocated, i.e. glpane._compute_frustum_planes was already called.
+        (If it wasn't, the test will always succeed.)
+
+        Currently, this is a loose (but correct) approximation which calls
+        glpane.is_sphere_visible on the lozenge's bounding sphere.
+
+        @warning: this will give incorrect results unless the current
+                  GL matrices are in the same state as when _compute_frustum_planes
+                  was last called (i.e. in absolute model space coordinates).
+        """
+
+        if self._frustum_planes_available:
+            center = 0.5 * (pos1 + pos2)
+            sphere_radius = 0.5 * vlen(pos2 - pos1) + radius
+            res = self.is_sphere_visible(center, sphere_radius)
+            # Read Bruce's comment in glpane.is_sphere_visible
+            # It applies here, as well.
+            return res 
+
+        return True
+
+    pass
+
+# end
+
