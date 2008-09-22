e4ea04e360dffeb24e2ab5e7b1d0087176b9c085
Ninad Sathaye <ninad@nanorex.com>
2008-09-22 21:01:48 +0000

    updated porting status, PM_class

--- cad/src/dna/commands/OrderDna/OrderDna_Command.py
+++ cad/src/dna/commands/OrderDna/OrderDna_Command.py
@@ -48,6 +48,8 @@ class OrderDna_Command(EditCommand):
     command_level = CL_EXTERNAL_ACTION
          
     GraphicsMode_class = OrderDna_GraphicsMode
+    
+    PM_class = OrderDna_PropertyManager
    
     command_can_be_suspended = False
     command_should_resume_prevMode = True 
@@ -87,50 +89,47 @@ class OrderDna_Command(EditCommand):
             """
             if self.propMgr is not None:
                 self.propMgr.close()
+        
+        def command_enter_PM(self):
+            """
+            Overrides superclass method. 
             
-        
-    #START new command API methods =============================================
-        
-    def command_enter_PM(self):
-        """
-        Overrides superclass method. 
-        
-        @see: baseCommand.command_enter_PM()  for documentation
-        """
-        #important to check for old propMgr object. Reusing propMgr object 
-        #significantly improves the performance.
-        if not self.propMgr:
-            self.propMgr = self._createPropMgrObject()
-            #@bug BUG: following is a workaround for bug 2494.
-            #This bug is mitigated as propMgr object no longer gets recreated
-            #for modes -- ninad 2007-08-29
-            changes.keep_forever(self.propMgr)  
-            
-        
-        if not USE_COMMAND_STACK:
-            self.propMgr.show()    
-            
-            
-    def command_enter_flyout(self):
-        """
-        Overrides superclass method. 
-        @see: EditCommand.command_enter_flyout()
-        """
-        self._init_gui_flyout_action( 'orderDnaAction' , 'BUILD_DNA') 
-        
+            @see: baseCommand.command_enter_PM()  for documentation
+            """
+            #important to check for old propMgr object. Reusing propMgr object 
+            #significantly improves the performance.
+            if not self.propMgr:
+                self.propMgr = self._createPropMgrObject()
+                #@bug BUG: following is a workaround for bug 2494.
+                #This bug is mitigated as propMgr object no longer gets recreated
+                #for modes -- ninad 2007-08-29
+                changes.keep_forever(self.propMgr)  
                 
-    def command_exit_flyout(self):
-        """
-        Overrides superclass method. 
-        @see: EditCommand.command_exit_flyout()
-        """
-        if self.flyoutToolbar:
-            self.flyoutToolbar.orderDnaAction.setChecked(False)
+            
+            if not USE_COMMAND_STACK:
+                self.propMgr.show()    
+                
+                
+        def command_enter_flyout(self):
+            """
+            Overrides superclass method. 
+            @see: EditCommand.command_enter_flyout()
+            """
+            self._init_gui_flyout_action( 'orderDnaAction' , 'BUILD_DNA') 
+            
+                    
+        def command_exit_flyout(self):
+            """
+            Overrides superclass method. 
+            @see: EditCommand.command_exit_flyout()
+            """
+            if self.flyoutToolbar:
+                self.flyoutToolbar.orderDnaAction.setChecked(False)
             
             
-    def _createPropMgrObject(self):
-        propMgr = OrderDna_PropertyManager(self)
-        return propMgr
+        def _createPropMgrObject(self):
+            propMgr = OrderDna_PropertyManager(self)
+            return propMgr
   
     
     def keep_empty_group(self, group):
