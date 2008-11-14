f7c39619a400eb99c0fb15c32bd7ff05f3edf344
nobody <nobody@localhost>
2008-11-14 21:01:59 +0000

    implemented NFR: Temporarily change the CPK atom scale factor to .5 when using "Break Strands" command

--- cad/src/dna/commands/BreakStrands/BreakStrands_Command.py
+++ cad/src/dna/commands/BreakStrands/BreakStrands_Command.py
@@ -12,6 +12,8 @@ TODOs: [ as of 2008-01-04]
 from dna.command_support.BreakOrJoinStrands_Command import BreakOrJoinStrands_Command
 from dna.commands.BreakStrands.BreakStrands_PropertyManager import BreakStrands_PropertyManager
 from dna.commands.BreakStrands.BreakStrands_GraphicsMode import BreakStrands_GraphicsMode
+from utilities.prefs_constants import cpkScaleFactor_prefs_key
+import foundation.env as env
 
 #debug flag for experimental code Ninad is
 #working on (various break strands options)
@@ -33,6 +35,46 @@ class BreakStrands_Command(BreakOrJoinStrands_Command):
    
     def _get_init_gui_flyout_action_string(self):
         return 'breakStrandAction'
+    
+    
+    def command_entered(self):
+        """
+        Extends superclass method.
+        """
+        _superclass.command_entered(self)
+        self._modifyCPKScaleFactor()
+        
+        
+    def command_will_exit(self):   
+        """
+        Extends superclass method.
+        """
+        self._restoreCPKScaleFactor()        
+        _superclass.command_will_exit(self)
+        
+    def _modifyCPKScaleFactor(self):
+        """
+        Modify the global CPK scale factor prefs_key temporarily , while in 
+        BreakStrands command. This prefs_key will be restored while exiting this
+        command
+        @see: self._restoreCPKScaleFactor()
+        @see: self.command_entered()
+        """
+        #Note 2008-11-14: This is implemented based on Mark's NFR. 
+        self._original_cpkScaleFactor_prefs_key = env.prefs[cpkScaleFactor_prefs_key]
+        env.prefs[cpkScaleFactor_prefs_key] = 0.5
+            
+    def _restoreCPKScaleFactor(self):
+        """        
+        @see: self._modifyCPKScaleFactor()
+        @see: self.command_entered()
+        @see: self.command_will_exit()
+        """
+        env.prefs[cpkScaleFactor_prefs_key] = self._original_cpkScaleFactor_prefs_key
+        
+    
+    
+    
     
     #===========================================================================
     #Methods used in experimental feature that provide various break options
