3b8d8069b1a5bc61032820ddfabe33a78eed720d
Bruce Smith <bruce@nanorex.com>
2008-12-02 18:46:17 +0000

    tab2space -t8 samevalshelp.c (for doc see tab2space -h on Mac)

--- cad/src/samevalshelp.c
+++ cad/src/samevalshelp.c
@@ -56,33 +56,33 @@ _same_vals_helper(PyObject *v1, PyObject *v2)
         }
         return 0;
     } else if (typ1 == &PyList_Type) {
-	int i, n;
+        int i, n;
 
-	n = PyList_Size(v1);
-	if (n != PyList_Size(v2)) {
+        n = PyList_Size(v1);
+        if (n != PyList_Size(v2)) {
             return 1;
         }
-	for (i = 0; i < n; i++)
-	    if (_same_vals_helper(PyList_GetItem(v1, i),
-				 PyList_GetItem(v2, i)))
-		return 1;
-	return 0;
+        for (i = 0; i < n; i++)
+            if (_same_vals_helper(PyList_GetItem(v1, i),
+                                 PyList_GetItem(v2, i)))
+                return 1;
+        return 0;
     } else if (typ1 == &PyTuple_Type) {
-	int i, n;
+        int i, n;
 
-	n = PyTuple_Size(v1);
-	if (n != PyTuple_Size(v2)) {
+        n = PyTuple_Size(v1);
+        if (n != PyTuple_Size(v2)) {
             return 1;
         }
-	for (i = 0; i < n; i++)
-	    if (_same_vals_helper(PyTuple_GetItem(v1, i),
-				 PyTuple_GetItem(v2, i)))
-		return 1;
-	return 0;
+        for (i = 0; i < n; i++)
+            if (_same_vals_helper(PyTuple_GetItem(v1, i),
+                                 PyTuple_GetItem(v2, i)))
+                return 1;
+        return 0;
     } else if (arrayType != NULL && typ1 == arrayType) {
-	PyArrayObject *x = (PyArrayObject *) v1;
-	PyArrayObject *y = (PyArrayObject *) v2;
-	int i;
+        PyArrayObject *x = (PyArrayObject *) v1;
+        PyArrayObject *y = (PyArrayObject *) v2;
+        int i;
         int elementSize;
         int *indices;
         int topDimension;
@@ -91,30 +91,30 @@ _same_vals_helper(PyObject *v1, PyObject *v2)
         int objectCompare = 0;
 
         // do all quick rejects first (no loops)
-	if (x->nd != y->nd) {
+        if (x->nd != y->nd) {
             // number of dimensions doesn't match
             return 1;
             // note that a (1 x X) array can never equal a single
             // dimensional array of length X.
         }
-	if (x->descr->type_num != y->descr->type_num) {
+        if (x->descr->type_num != y->descr->type_num) {
             // type of elements doesn't match
             return 1;
         }
         if (x->descr->type_num == PyArray_OBJECT) {
             objectCompare = 1;
         }
-	elementSize = x->descr->elsize;
-	if (elementSize != y->descr->elsize) {
+        elementSize = x->descr->elsize;
+        if (elementSize != y->descr->elsize) {
             // size of elements doesn't match (shouldn't happen if
             // types match!)
             return 1;
         }
         for (i = x->nd - 1; i >= 0; i--) {
-	    if (x->dimensions[i] != y->dimensions[i])
+            if (x->dimensions[i] != y->dimensions[i])
                 // shapes don't match
-		return 1;
-	}
+                return 1;
+        }
         // we do a lot of these, so handle them early
         if (x->nd == 1 && !objectCompare && x->strides[0]==elementSize && y->strides[0]==elementSize) {
             // contiguous one dimensional array of non-objects
@@ -161,19 +161,19 @@ _same_vals_helper(PyObject *v1, PyObject *v2)
             }
         }
         // all elements match
-	return 0;
+        return 0;
     }
 #if 0
     XX({
-	if (typ1 == &PyInstance_Type) {
-	    PyInstanceObject *x = (PyInstanceObject *) v1;
-	    PyObject_Print(x->in_class->cl_name, stdout, 0);
-	} else {
-	    PyObject_Print((PyObject*)typ1, stdout, 0);
-	}
-	if (typ1->tp_compare == NULL)
-	    printf(" (rich comparison)");
-	printf("\n");
+        if (typ1 == &PyInstance_Type) {
+            PyInstanceObject *x = (PyInstanceObject *) v1;
+            PyObject_Print(x->in_class->cl_name, stdout, 0);
+        } else {
+            PyObject_Print((PyObject*)typ1, stdout, 0);
+        }
+        if (typ1->tp_compare == NULL)
+            printf(" (rich comparison)");
+        printf("\n");
     });
     if (typ1->tp_compare != NULL) {
       int i;
@@ -186,7 +186,7 @@ _same_vals_helper(PyObject *v1, PyObject *v2)
     }
 #endif
     if (PyObject_RichCompareBool(v1, v2, Py_EQ) == 1)
-	return 0;
+        return 0;
     return 1;
 }
 
@@ -194,11 +194,11 @@ static PyObject *
 c_same_vals(PyObject *o1, PyObject *o2)
 {
     if (arrayType == NULL) {
-	PyErr_SetString(PyExc_RuntimeError, "please set arrayType first");
-	return NULL;
+        PyErr_SetString(PyExc_RuntimeError, "please set arrayType first");
+        return NULL;
     }
     if (_same_vals_helper(o1, o2) != 0) {
-	return PyInt_FromLong(0);  // false
+        return PyInt_FromLong(0);  // false
     }
     return PyInt_FromLong(1);  // true
 }
@@ -211,13 +211,13 @@ internal_copy_val(PyObject *v)
     
     typ = v->ob_type;
     if (typ == &PyInt_Type ||
-	typ == &PyLong_Type ||
-	typ == &PyFloat_Type ||
-	typ == &PyComplex_Type ||
-	typ == &PyString_Type ||
-	typ == &PyUnicode_Type) {
-	Py_INCREF(v);
-	return v;
+        typ == &PyLong_Type ||
+        typ == &PyFloat_Type ||
+        typ == &PyComplex_Type ||
+        typ == &PyString_Type ||
+        typ == &PyUnicode_Type) {
+        Py_INCREF(v);
+        return v;
     } else if (v == Py_False) {
         Py_RETURN_FALSE;
     } else if (v == Py_True) {
@@ -225,29 +225,29 @@ internal_copy_val(PyObject *v)
     } else if (v == Py_None) {
         Py_RETURN_NONE;
     } else if (typ == &PyDict_Type) {
-	int i, n = PyDict_Size(v);
-	PyObject *newdict = PyDict_New();
-	PyObject *keys = PyDict_Keys(v);
-	PyObject *values = PyDict_Values(v);
-	for (i = 0; i < n; i++)
-	    PyDict_SetItem(newdict,
-			   PyList_GetItem(keys, i),
-			   internal_copy_val(PyList_GetItem(values, i)));
-	return newdict;
+        int i, n = PyDict_Size(v);
+        PyObject *newdict = PyDict_New();
+        PyObject *keys = PyDict_Keys(v);
+        PyObject *values = PyDict_Values(v);
+        for (i = 0; i < n; i++)
+            PyDict_SetItem(newdict,
+                           PyList_GetItem(keys, i),
+                           internal_copy_val(PyList_GetItem(values, i)));
+        return newdict;
     } else if (typ == &PyList_Type) {
-	int i, n = PyList_Size(v);
-	PyObject *newlist = PyList_New(n);
-	for (i = 0; i < n; i++)
-	    PyList_SetItem(newlist, i,
-			   internal_copy_val(PyList_GetItem(v, i)));
-	return newlist;
+        int i, n = PyList_Size(v);
+        PyObject *newlist = PyList_New(n);
+        for (i = 0; i < n; i++)
+            PyList_SetItem(newlist, i,
+                           internal_copy_val(PyList_GetItem(v, i)));
+        return newlist;
     } else if (typ == &PyTuple_Type) {
-	int i, n = PyTuple_Size(v);
-	PyObject *newtuple = PyTuple_New(n);
-	for (i = 0; i < n; i++)
-	    PyTuple_SetItem(newtuple, i,
-			   internal_copy_val(PyTuple_GetItem(v, i)));
-	return newtuple;
+        int i, n = PyTuple_Size(v);
+        PyObject *newtuple = PyTuple_New(n);
+        for (i = 0; i < n; i++)
+            PyTuple_SetItem(newtuple, i,
+                           internal_copy_val(PyTuple_GetItem(v, i)));
+        return newtuple;
     } else if (typ == &PyInstance_Type) {
         PyObject *args = PyTuple_New(1);
         Py_INCREF(v);
@@ -259,7 +259,7 @@ internal_copy_val(PyObject *v)
         PyObject *args = PyTuple_New(1);
         PyTuple_SetItem(args, 0, v);
         Py_INCREF(v);
-	copy = PyObject_CallObject(arrayCopier, args);
+        copy = PyObject_CallObject(arrayCopier, args);
         Py_DECREF(args);
         return copy;
     } else {
@@ -279,10 +279,10 @@ internal_copy_val(PyObject *v)
           }
         }
       
-	// no good ideas here...
+        // no good ideas here...
         fprintf(stderr, "copy_val(0x%x): not copying, type == %s\n", (int)v, typ->tp_name);
-	Py_INCREF(v);
-	return v;
+        Py_INCREF(v);
+        return v;
     }
 }
 
@@ -290,16 +290,16 @@ static PyObject *
 c_copy_val(PyObject *obj)
 {
     if (arrayType == NULL) {
-	PyErr_SetString(PyExc_RuntimeError, "please set arrayType first");
-	return NULL;
+        PyErr_SetString(PyExc_RuntimeError, "please set arrayType first");
+        return NULL;
     }
     if (instanceCopier == NULL) {
-	PyErr_SetString(PyExc_RuntimeError, "please set instanceCopier first");
-	return NULL;
+        PyErr_SetString(PyExc_RuntimeError, "please set instanceCopier first");
+        return NULL;
     }
     if (arrayCopier == NULL) {
-	PyErr_SetString(PyExc_RuntimeError, "please set arrayCopier first");
-	return NULL;
+        PyErr_SetString(PyExc_RuntimeError, "please set arrayCopier first");
+        return NULL;
     }
     return internal_copy_val(obj);
 }
