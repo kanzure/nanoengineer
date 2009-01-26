8ba324a3a28d3271881b94987fff8e4060ad368a
Bruce Smith <bruce@nanorex.com>
2009-01-26 22:37:51 +0000

    also report line number of longest line

--- cad/src/tools/linelen.py
+++ cad/src/tools/linelen.py
@@ -1,49 +1,55 @@
 #!/usr/bin/env python
-
-# Copyright 2007 Nanorex, Inc.  See LICENSE file for details. 
 """
 linelen.py - print max line length of each input file, assuming no tabs.
+Also print line number of at least one line which is that long.
 
 @author: Bruce
 @version: $Id$
-@copyright: 2007 Nanorex, Inc.  See LICENSE file for details.
+@copyright: 2007-2009 Nanorex, Inc.  See LICENSE file for details.
 """
 
 import sys, os, time
 
-# bruce 070304
+# bruce 070304, added lineno 090126
 
-def linelen(filename):
-    file = open(filename, 'rU')
+def linelen(fn):
+    """
+    Given filename fn,
+    return max line length (assuming no tabs and all lines \n-terminated),
+    and one of the line numbers it occurs on (the first one, in current implem).
+    """
+    file = open(fn, 'rU')
     lines = file.readlines()
     file.close()
-    return max(map(len, lines)) - 1 # -1 is for the terminating newline
+    if not lines:
+        return 0, 0
+    rawlengths = map(len, lines)
+    maxlen = max(rawlengths) - 1 # -1 is for the terminating newline
         ###BUG if file ends non-\n
         ###BUG if file contains tabs
+    lineno = 1 + rawlengths.index(maxlen + 1)
+    return maxlen, lineno
 
 # ==
 
-filenames = sys.argv[1:]
+if __name__ == '__main__':
+    filenames = sys.argv[1:]
 
-program = os.path.basename(sys.argv[0])
+    program = os.path.basename(sys.argv[0])
 
-if not filenames:
-    msg = "usage: %s <files> [no stdin reading supported for now]" % (program,)
-    print >> sys.stderr, msg
-    sys.exit(1)
-    
-# not desirable (shell * sorts them, but user might want
-# a different specific order): 
-## filenames.sort()
+    if not filenames:
+        msg = "usage: %s <files> [no stdin reading supported for now]" % (program,)
+        print >> sys.stderr, msg
+        sys.exit(1)
 
-for filename in filenames:
-    if os.path.isfile(filename):
-        print "linelen(%r) = %d" % (filename, linelen(filename))
-    else:
-        print "not found or not a plain file: %r" % filename
-    continue
+    for fn in filenames:
+        if os.path.isfile(fn):
+            maxlen, lineno = linelen(fn)
+            print "linelen(%r) = %d (line %d)" % (fn, maxlen, lineno)
+        else:
+            print "not found or not a plain file: %r" % fn
+        continue
 
-print "done"
-sys.exit(0)
+    print "done"
 
 # end
