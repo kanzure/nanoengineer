f21165eba41c2442b93e21f28154c581ea1584eb
Bruce Smith <bruce@nanorex.com>
2009-02-11 19:20:44 +0000

    scratch

--- cad/src/model/TransformNode.py
+++ cad/src/model/TransformNode.py
@@ -13,57 +13,63 @@ NOT YET USED as of 090204
 
 from foundation.state_constants import S_CHILD
 
+from graphics.drawing.TransformControl import TransformControl
+
 Node3D
+
+_DEBUG = True # for now
 
 class TransformNode(Node3D): # review superclass and its name
     """
-    A Node3D whose position and orientation is efficiently modifiable
+    A Node3D which can be efficiently moved (in position and orientation)
     by modifying a contained transform. Children and control points may store
     relative and/or absolute coordinates, but the relative ones are considered
     fundamental by most operations which modify the transform (so the absolute
     ones must be invalidated or recomputed, and modifying the transform moves
-    the node).
+    the node). (But certain operations can explicitly cause data to flow the
+    other way, i.e. modify the transform and relative coordinates in such a way
+    that the absolute position and coordinates remain unchanged.)
 
-    The transform is actually the composition of an optional outer "dynamic
-    transform" (used for efficient dragging of sets of TransformNodes)
-    and an inner "static transform" (often non-identity for long periods,
-    so we needn't remake or transform all affected CSDLs after every drag
-    operation).
+    The transform is actually the composition of two "transform components":
+    an optional outer "dynamic transform" (used for efficient dragging of sets
+    of TransformNodes) and an inner "static transform" (often non-identity for
+    long periods, so we needn't remake or transform all affected CSDLs after
+    every drag operation).
     
     Each transform component can be shared with other nodes. That is, several
     nodes can refer to the same mutable transform, so that changes to it affect
     all of them.
 
     The way self's overall transform is represented by these component
-    transforms can change (by replacing them with others and/or merging the
-    dynamic one into the static one), without changing the overall transform
-    value. We optimize for the lack of overall change in such cases.
+    transforms can change (by replacing one of them with a copy, and/or merging
+    the dynamic transform into the static transform), without changing the
+    overall transform value. We optimize for the lack of absolute position
+    change in such cases.
     """
 
     # pointer to our current (usually shared) dynamic transform, or None.
-    # (note: rendered by putting self in a DrawingSet that will be drawn within
-    #  this transform)
+    # (Note: this affects rendering by making sure self is in a DrawingSet
+    #  which will be drawn within GL state which includes this transform.)
     dynamic_transform = None
     _s_attr_dynamic_transform = S_CHILD
+        # (_s_attr: perhaps unnecessary, since no Undo checkpoints during drag)
 
-    # pointer to our current (usually shared) static transform, or None [### DECIDE -- is none ok? some code below says no].
-    # (note: rendered by being applied to the coordinates within all CSDLs
-    #  associated with self)
+    # pointer to our current (usually shared) static transform, or None
+    # (note: affects rendering by means of an associated TransformControl,
+    #  which transforms the relative coordinates used to make self's CSDLs
+    #  into absolute coordinates (not counting dynamic_transform) used to draw
+    #  self)
     static_transform = None
     _s_attr_static_transform = S_CHILD
 
-    # REVIEW: does the static_transform contain its TransformControl or is that
-    # held externally somewhere? Ideally, it's held externally (if we still need it at all).
-
-    # IMPLEM NOTE: the sharing of static transforms requires
-    # one coordinated operation of folding dynamic trans into static, for all nodes at once,
-    # since you have to split any shared static touched only in some nodes into two statics,
-    # and merge some statics into the identity if you run out of TransformControls.
+    # REVIEW: does the static_transform contain its TransformControl?
+    # I now think it's contained, and that we won't allocate an ST unless a
+    # TC is available, and that we'll use None for the "identity ST". ####
 
     ### REVIEW: should we limit the set of static transforms for all purposes (incl Undo)
     # due to a limitation on the number of TransformControls which arises from the rendering implementation?
     # If we do that, should we also remake CSDLs, or just transform them (presumably no difference in end result)?
-    # Note: transforming them is what requires them to have mutable TC refs.
+    # Note: transforming them is what requires them to have mutable TC refs -- a change to their current API. ###
 
     def set_dynamic_transform(self, t): # review: use a setter? the initial assert might not be appropriate then...
         #### FIX: I don't like having a setter but also allowing direct sets, like we use in other code.
@@ -89,6 +95,34 @@ class TransformNode(Node3D): # review superclass and its name
         self.static_transform = st ##### TODO: MAKE SURE this invalidates subscribers to self.static_transform
         st.add_node(self)
         return
+
+    def bridging_objects(self):
+        """
+        Return the list of objects which "bridge" more than one TransformNode
+        including this one, and whose TransformNodes presently have different
+        dynamic_transforms (not all the same one) (regardless of the current
+        transform values of those dynamic transforms).
+        """
+        res = []
+        for obj in self.potential_bridging_objects(): # implem is per-subclass, I guess? ### DECIDE
+            if obj.is_currently_bridging_dynamic_transforms(): ### IMPLEM in ebset
+                res.append(obj)
+        return res
+
+    def potential_bridging_objects(self): ### TODO: override in class Chunk
+        """
+        Return a list of objects whose coordinates depend on the value of
+        self's dynamic_transform and also on the value of one or more
+        other nodes' dynamic_transforms (though we don't remove the ones
+        for which the other nodes depend on the *same* dynamic transform).
+
+        (These are the objects which might get distorted due to a change
+        in self's dynamic transform value, if the other nodes have different
+        dynamic transforms at the time.)
+        
+        [subclasses should override as needed]
+        """
+        return ()
     
     pass
 
@@ -100,9 +134,15 @@ def merge_dynamic_into_static_transform(nodes):
     Given a set of TransformNodes with the same dynamic transform,
     but perhaps different static transforms, which may or may not be shared
     with nodes not in the set, remove the dynamic transform from all the nodes
-    without changing their overall transforms, splitting their static transforms
-    as needed, and removing or merging their static transforms as needed to
-    observe a global limit on number of static transforms.
+    without changing their overall transforms, by merging it into their static
+    transforms.
+
+    This requires splitting any static transforms that bridge
+    touched and untouched nodes (so we can merge the dynamic transform into
+    only the one on the touched nodes), and if we run out of TransformControls
+    needed to make more static transforms, merging some static transforms
+    into the identity (and either remaking or transforming all our CSDLs,
+    or invalidating them so that happens lazily).
     """
     assert nodes
     dt = nodes[0].dynamic_transform
@@ -121,10 +161,9 @@ def merge_dynamic_into_static_transform(nodes):
     # splitting the ones that also cover unaffected nodes.
     
     # get the affected static transforms and their counts
-    statics = {}
+    statics = {} # maps static transform to number of times we see it in nodes
     for node in nodes:
-        st = node.static_transform
-        assert st # assume all nodes have one even if not needed ### REVIEW -- does this make splitting easier/simpler?
+        st = node.static_transform # might be None
         statics.setdefault(st, 0)
         statics[st] += 1
 
@@ -134,15 +173,19 @@ def merge_dynamic_into_static_transform(nodes):
     for st, count in statics.iteritems():
         # count is the number of nodes which have st;
         # does that cover all of st's nodes?
-        assert count <= st.nodecount()
-        if count < st.nodecount():
-            straddlers[st] = st # or store a number?
+        if st is not None:
+            assert count <= st.nodecount()
+            if count < st.nodecount():
+                straddlers[st] = st # or store a number?
+        else:
+            straddlers[st] = st # None is always a straddler
         continue
     
     # allocate more TCs (up to the externally known global limit)
+    # for making more static transforms when we split the straddlers
     want_TCs = len(straddlers)
     if want_TCs:
-        new_TCs = allocate_TransformControls(want_TCs) # IMPLEM
+        new_TCs = allocate_TransformControls(want_TCs)
 
         if len(new_TCs) < want_TCs:
             # There are not enough new TCs to split all the straddlers,
@@ -152,27 +195,47 @@ def merge_dynamic_into_static_transform(nodes):
             print "THIS IS NIM, BUGS WILL ENSUE"
             assert 0
             # (We're hoping that we'll find a way to allocate as many TCs as needed,
-            #  so we never need to write this code.)
+            #  so we never need to write this code. OTOH, initially we might need to
+            #  make this work in the absence of any TCs, i.e. implem and always use
+            #  this code, never allocating STs at all. ###)
 
         # now split each straddler in two, leaving the original on the "other nodes"
         # (this matters if one of them is a "permanent identity StaticTransform")
+
+        if _DEBUG:
+            print "splitting %d straddlers" % want_TCs
         
         replacements = {} # map old to new, for use on nodes
         for straddler, tc in zip(straddlers, new_TCs):
-            replacements[straddler] = StaticTransform(straddler, tc)
+            # note: straddler might be None
+            replacements[straddler] = StaticTransform(straddler, tc) 
 
         for node in nodes:
             st = node.static_transform
             if st in replacements:
-                node.set_static_transform(replacements[st])
+                node.set_static_transform(replacements[st]) ### IMPLEM: inval that field but not the overall transform...
 
-    # now push the data from dt into each (owned or new) st
+    # now push the data from dt into each (owned or new) static transform
     for st in statics.keys():
         if st in replacements:
             st = replacements[st]
         st.applyDataFrom(dt)
     
-    ### more... 
+    ### more... only if we need to wrap some some specialized code to inval just the right aspects of the nodes ###
+    
     return
+
+def allocate_TransformControls(number):
+    res = []
+    for i in range(number):
+        tc = allocate_TransformControl()
+        if tc:
+            res.append(tc)
+        else:
+            break
+    return res
+
+def allocate_TransformControl():
+    return TransformControl() ### STUB; todo: check tc.transform_id against limit? recycle?
 
 # end
--- cad/src/model/TransformState.py
+++ cad/src/model/TransformState.py
@@ -31,7 +31,7 @@ class TransformState(StateMixin, object):
     A mutable orthonormal 3d transform, represented as a rotation followed by
     a translation (with the rotation and translation being undoable state).
 
-    Changes to the transform value can be subscribed to.
+    Changes to the transform value can be subscribed to. ###doc how
     """
     # REVIEW:
     # - can you subscribe separately to changes to its translation and rotation?
@@ -45,23 +45,40 @@ class TransformState(StateMixin, object):
     # - finish nim ops to change the data (translate, rotate, etc),
     # - make sure the data (or overall value only?) can be subscribed to
 
-    rotation = State(Quaternion)
+    # TODO: make the State version work... initially we just use hand-invals, below.
+    # but we'll need to make State work here as soon as we need to usage-track these
+    # for purposes of properly implementing the DrawingSet graphics API.
+    #
+    ##rotation = State(Quaternion)
+    ##
+    ##translation = State(Vector3D)
+    ##
+    ##    ### IMPLEM:
+    ##    # - how do we make State work here? ###REVIEW how we do it in Test_ConnectWithState, funmode
+    ##    #   - and how do we let it be undoable state? compatible with _s_attr decls?? ###
+    ##    # - get default value from type;
+    ##    # - define these type names
+    ##    ### REVIEW:
+    ##    # - review type name: Vector vs Vector3 vs Vector3D ? Or just use V and Q?
 
-    translation = State(Vector3D)
+    rotation = _IDENTITY_ROTATION
+    translation = _IDENTITY_TRANSLATION
     
-        ### IMPLEM:
-        # - how do we make State work here? ###REVIEW how we do it in Test_ConnectWithState, funmode
-        #   - and how do we let it be undoable state? compatible with _s_attr decls?? ###
-        # - get default value from type;
-        # - define these type names
-        ### REVIEW:
-        # - review type name: Vector vs Vector3 vs Vector3D ? Or just use V and Q?
-
-    value = TransformData( rotation, translation ) # REVIEW expr name, whether this formula def is appropriate
+    # not yet needed:
+    ## value = TransformData( rotation, translation ) # REVIEW expr name, whether this formula def is appropriate
 
     
     # TODO: add formula for matrix? (to help merge with TransformControl?)
 
+
+    def _kluge_manual_inval(self):
+        """
+        Kluge: manually invalidate our overall transform value.
+        """
+        # calls only needed until we make State work above;
+        # implem only needed in some subclasses
+        return
+    
     def applyDataFrom(self, other):
         """
         """
@@ -70,6 +87,7 @@ class TransformState(StateMixin, object):
         
     def translate(self, vector): # see also 'def move' in other classes
         self.translation = self.translation + vector
+        self._kluge_manual_inval()
 
     def rotate(self, quat, center = None):
         """
@@ -80,6 +98,7 @@ class TransformState(StateMixin, object):
         if center is not None:
             offset = self.translation - center
             self.translate( quat.rot(offset) - offset ) # REVIEW - compare with chunk.py
+        self._kluge_manual_inval()
         return
 
     def is_identity(self): # review: optimize? special fast case if data never modified?
@@ -101,6 +120,7 @@ class StaticTransform( TransformState):
     transformControl = None
     
     def __init__(self, value = None, tc = None):
+        self._nodes = {}
         if value is not None:
             # copy our value from the given value or TransformState
             if isinstance(value, TransformState):
@@ -133,6 +153,54 @@ class StaticTransform( TransformState):
         """
         return self._nodes.itervalues()
     
+    pass
+
+# ==
+
+class DynamicTransform( TransformState):
+    """
+    A subclass of TransformState that keeps track of which objects bridge
+    between nodes which have it and nodes which don't have it, and passes
+    its value invalidations on to those objects.
+    """
+    def __init__(self, nodes):
+        """
+        Initialize self, and add self to the given nodes.
+        """
+        TransformState.__init__(self)
+        self._nodes = nodes # list, not dict -- no need to modify or index
+            # only needed for self.destroy();
+            # not needed for determining whether node-bridging objects added
+            # later are self-bridging, since our nodes point to us too
+            # (and anyway, AFAIK we never add bridging objects later)
+        for node in nodes:
+            node.dynamic_transform = self ### REVIEW; needs to inval subscribers
+        self._bridging_objects = self._get_bridging_objects()
+        # note: if our initial value wasn't identity, we'd also need to call
+        # self._invalidate_transform_value() here.
+        return
+
+    def _get_bridging_objects(self):
+        # Anything on one of our nodes which bridges *anything*,
+        # is also known to be on self, therefore one of *our* bridging objects.
+        # More precisely: it's on self; it's not only on self (or not bridging);
+        # thus it's a self-bridging object (a node on self, a node not on self).
+        res = []
+        for node in self.nodes:
+            res.extend( node.bridging_objects() )
+            ### REVIEW: can one be included more than once? if so, use a dict...
+            # guess: yes, once we have multi-node ones. ### 
+        return res
+
+    def _kluge_manual_inval(self):
+        TransformState._kluge_manual_inval(self)
+        self._invalidate_transform_value()
+        
+    def _invalidate_transform_value(self):
+        for bo in self._bridging_objects:
+            bo.inval_bridging_dynamic_transform_value() ### IMPLEM in EBSet
+        return
+
     pass
 
 # end
